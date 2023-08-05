6c28471058cad44cd8d1c30aed03b808e8e134c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:24 +0000

    Add package wfdload/0.0.5.0/wfdload-0.0.5.0.tar.gz

---
+++ packages/wfdload/wfdload-0.0.5.0.tar.gz/wfdload-0.0.5.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wfdload",
+    version="0.0.5.0",
+    description="A library to handle wfd in python.",
+    install_requires=["numpy", "pandas"],
+    author="anime-song",
+    url="https://github.com/anime-song/wfdload",
+    license="MIT",
+    packages=["wfdload"],
+)
