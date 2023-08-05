f1ef40ee633234aeba2c4f0ce4e5e743da255990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:54 +0000

    Add package pydialogflow-fulfillment/0.0.6/pydialogflow_fulfillment-0.0.6.tar.gz

---
+++ packages/pydialogflow-fulfillment/pydialogflow_fulfillment-0.0.6.tar.gz/pydialogflow_fulfillment-0.0.6/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pydialogflow_fulfillment',  
+    version='0.0.6',
+    author="Tairu Oluwafemi Emmanuel",
+    author_email="developer.emmarex@gmail.com",
+    description="This Library makes creating fulfillment for Dialogflow v2 agents with Django or Flask easy and simple",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Emmarex/dialogflow-fulfillment-python",
+    packages=setuptools.find_packages(),
+    classifiers=[ 
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 6 - Mature",
+        "Intended Audience :: Developers",
+        "Framework :: Django",
+        "Framework :: Flask"
+    ],
+ )
