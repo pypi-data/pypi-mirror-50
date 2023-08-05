0ac934a505e16b0266992609d8297b9fc884d86a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:22 +0000

    Add package celery-slack-webhooks/0.4.5rc3/celery-slack-webhooks-0.4.5rc3.tar.gz

---
+++ packages/celery-slack-webhooks/celery-slack-webhooks-0.4.5rc3.tar.gz/celery-slack-webhooks-0.4.5rc3/celery_slack/__version__.py
@@ -0,0 +1,10 @@
+"""Version information."""
+__title__ = 'celery-slack-webhooks'
+__description__ = 'A Slack extension for Celery.'
+__url__ = 'https://github.com/leigh-johnson/celery-slack'
+__version__ = '0.4.5-rc3'
+__author__ = 'Leigh Johnson'
+__author_email__ = 'ljohnson@slack-corp.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017-2018 Christopher Flynn, 2019 Leigh Johnson'
+__docs_copyright__ = '2017-2018 Christopher Flynn, 2019 Leigh Johnson'
