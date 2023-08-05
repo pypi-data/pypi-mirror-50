6548f1d4ac196179e0f26ab349a9857c52857c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:00 +0000

    Add package mkdocs-image-localization/0.1.3/mkdocs_image_localization-0.1.3-py2-none-any.whl

---
+++ packages/mkdocs-image-localization/mkdocs_image_localization-0.1.3-py2-none-any.whl/mkdocs_image_localization/plugin.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import re
+
+from timeit import default_timer as timer
+from datetime import datetime, timedelta
+
+from mkdocs import utils as mkdocs_utils
+from mkdocs.config import config_options, Config
+from mkdocs.plugins import BasePlugin
+
+class ImageLocalization(BasePlugin):
+
+    config_scheme = (
+        ('param', config_options.Type(mkdocs_utils.string_types, default='')),
+    )
+
+    def __init__(self):
+        self.enabled = True
+        self.total_time = 0
+
+    def on_page_markdown(self, markdown, page, config, site_navigation=None, **kwargs):
+
+        result = re.search('/(_assets\/img.[^\"\)]*)', markdown)
+
+        while result:
+          target = result.group(1)
+          result = result.group(0)
+
+          if (os.path.exists(config['docs_dir']+'/'+config['locale']+result)):
+            markdown = markdown.replace(result, '/'+config['locale']+'/DeleteMarker'+target)
+          else:
+            markdown = markdown.replace(result, '/DeleteMarker'+target)
+
+          result = re.search('/(_assets\/img.[^\"\)]*)', markdown)
+
+        markdown = markdown.replace('DeleteMarker', '')
+
+        return markdown
