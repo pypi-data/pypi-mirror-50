5f936fa2327e8e92760875bd861a5a05c8334910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:42 +0000

    Add package resolwe/19.0.0a1/resolwe-19.0.0a1-py3-none-any.whl

---
+++ packages/resolwe/resolwe-19.0.0a1-py3-none-any.whl/resolwe/flow/serializers/data.py
@@ -0,0 +1,84 @@
+"""Resolwe data serializer."""
+from rest_framework import serializers
+
+from resolwe.flow.models import Collection, Data, DescriptorSchema, Entity, Process
+from resolwe.rest.fields import ProjectableJSONField
+
+from .base import ResolweBaseSerializer
+from .collection import CollectionSerializer
+from .descriptor import DescriptorSchemaSerializer
+from .entity import EntitySerializer
+from .fields import DictRelatedField
+from .process import ProcessSerializer
+
+
+class DataSerializer(ResolweBaseSerializer):
+    """Serializer for Data objects."""
+
+    input = ProjectableJSONField(required=False)
+    output = ProjectableJSONField(required=False)
+    descriptor = ProjectableJSONField(required=False)
+    process = DictRelatedField(queryset=Process.objects.all(), serializer=ProcessSerializer)
+    descriptor_schema = DictRelatedField(
+        queryset=DescriptorSchema.objects.all(),
+        serializer=DescriptorSchemaSerializer,
+        required=False
+    )
+    collection = DictRelatedField(
+        queryset=Collection.objects.all(),
+        serializer=CollectionSerializer,
+        required=False,
+        write_permission='add',
+    )
+    entity = DictRelatedField(
+        queryset=Entity.objects.all(),
+        serializer=EntitySerializer,
+        required=False,
+        write_permission='add',
+    )
+
+    class Meta:
+        """DataSerializer Meta options."""
+
+        model = Data
+        read_only_fields = (
+            'checksum',
+            'created',
+            'descriptor_dirty',
+            'duplicated',
+            'finished',
+            'id',
+            'modified',
+            'process_cores',
+            'process_error',
+            'process_info',
+            'process_memory',
+            'process_progress',
+            'process_rc',
+            'process_warning',
+            'output',
+            'scheduled',
+            'size',
+            'started',
+            'status',
+        )
+        update_protected_fields = (
+            'contributor',
+            'input',
+            'process',
+        )
+        fields = read_only_fields + update_protected_fields + (
+            'collection',
+            'descriptor',
+            'descriptor_schema',
+            'entity',
+            'name',
+            'slug',
+            'tags',
+        )
+
+    def validate_process(self, process):
+        """Check that process is active."""
+        if not process.is_active:
+            raise serializers.ValidationError("Process {} is not active.".format(process))
+        return process
