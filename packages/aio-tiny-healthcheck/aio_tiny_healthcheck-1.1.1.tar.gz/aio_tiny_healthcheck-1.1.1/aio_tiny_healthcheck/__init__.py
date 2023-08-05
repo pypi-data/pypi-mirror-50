f64ac3c792f4b026b1bdd031d3b8e33fa742d915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:03 +0000

    Add package aio-tiny-healthcheck/1.1.1/aio_tiny_healthcheck-1.1.1.tar.gz

---
+++ packages/aio-tiny-healthcheck/aio_tiny_healthcheck-1.1.1.tar.gz/aio_tiny_healthcheck-1.1.1/aio_tiny_healthcheck/__init__.py
@@ -0,0 +1,14 @@
+"""Package provides async healthcheck
+interface and http-server able to run concurrently"""
+
+from .checker import Checker, HealthcheckResponse
+from .http_server import HttpServer
+
+__all__ = [
+    'Checker',
+    'HealthcheckResponse',
+    'HttpServer'
+]
+
+name = 'aio_tiny_healthcheck'
+__version__ = '1.1.1'
