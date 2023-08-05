b8896969a4575b69fcac78b35b974c297c87543c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:14 +0000

    Add package clearshare/0.0.7/clearshare-0.0.7.tar.gz

---
+++ packages/clearshare/clearshare-0.0.7.tar.gz/clearshare-0.0.7/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+    args = {}
+except ImportError:
+    from distutils.core import setup
+    print("""\
+*** WARNING: setuptools is not found.  Using distutils...
+""")
+
+from setuptools import setup
+try:
+    from pypandoc import convert_file
+    read_md = lambda f: convert_file(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+setup(name='clearshare',
+      version='0.0.7',
+      description='Data management REST API for ClearSHARE.',
+      author='ClearFoundation',
+      author_email='developer@clearfoundation.com',
+      url='https://gitlab.com/clearos/clearfoundation/clearshare',
+      license='GPL-3.0',
+      install_requires=[
+          "datacustodian"
+      ],
+      packages=['clearshare'],
+      scripts=['clearshare/clearshare_app.py'],
+      package_data={'clearshare': ['../docker/*',
+                                   '../docker/specs/*',
+                                   '../docker/specs/global/*',
+                                   '../docker/specs/ipfs/*',
+                                   '../docker/deploy/*']},
+      include_package_data=True,
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Natural Language :: English',
+          'Operating System :: MacOS',
+          'Operating System :: Unix',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.7'
+      ],
+     )
