c2477823d6c0e137b0a44037b60e94c8dedbb573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:56 +0000

    Add package test0801-1/1.0.0/test0801_1-1.0.0.tar.gz

---
+++ packages/test0801-1/test0801_1-1.0.0.tar.gz/test0801_1-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+#从python发布工具导入setup函数
+setup(
+		name          = 'test0801_1',
+		version       ='1.0.0',
+		py_modules    =['test0801_1'],                  #模块元数据与setup函数参数关联
+		author        ='hfpython',                    #该行以下，元数据可不同
+		author_email  ='hfpython@headfirstlabs.com',
+		url           ='http://www.headfirstlabs.com',
+		description   ='A simple printer of nested lists',
+	   #函数参数名
+	)
+	
