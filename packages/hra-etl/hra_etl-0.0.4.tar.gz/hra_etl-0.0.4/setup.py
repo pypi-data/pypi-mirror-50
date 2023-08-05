bb3bcbf3f7bbd7f1b756289bfbf4b15be5fc3c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:56 +0000

    Add package hra-etl/0.0.4/hra_etl-0.0.4.tar.gz

---
+++ packages/hra-etl/hra_etl-0.0.4.tar.gz/hra_etl-0.0.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hra_etl",
+    version="0.0.4",
+    author="Gyanendra Kumar Patro",
+    author_email="gyanendrapatro02@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.dell.com/corporate/hr-data-lake/ETL/etl-architecture",
+    packages=setuptools.find_packages(),
+    keywords = ['ETL', 'PYTHONETL','PYTHON'],   # Keywords that define your package best
+    install_requires=['pyodbc','pandas','gnupg','chardet','datetime','configparser'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
