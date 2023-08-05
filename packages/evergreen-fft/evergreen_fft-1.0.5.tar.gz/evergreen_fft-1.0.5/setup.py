8682f973a9218c7eb634e58f5537896c2f80cfe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:07 +0000

    Add package evergreen-fft/1.0.5/evergreen_fft-1.0.5.tar.gz

---
+++ packages/evergreen-fft/evergreen_fft-1.0.5.tar.gz/evergreen_fft-1.0.5/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='evergreen_fft',  
+     version='1.0.5',
+     author="Rice",
+     author_email="mrseanrice@gmail.com",
+     description="A python fft package based on evergreenforest",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/SeanSRice/evergreen_fft.git",
+     packages =setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 2.7",
+         "License :: OSI Approved :: MIT License",
+     ],
+ )
