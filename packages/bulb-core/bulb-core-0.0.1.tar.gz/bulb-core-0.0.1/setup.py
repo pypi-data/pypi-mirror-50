de450c1c5aa0d7cc6f7e924abc0aea3a02565edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:56 +0000

    Add package bulb-core/0.0.1/bulb-core-0.0.1.tar.gz

---
+++ packages/bulb-core/bulb-core-0.0.1.tar.gz/bulb-core-0.0.1/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bulb-core",
+    version="0.0.1",
+    author="Lilian Cruanes",
+    author_email="cruaneslilian@gmail.com",
+    description="Neo4j integration for Django, and much more tools for deployment of consequent websites...",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LilianCruanes/bulb",
+    packages=setuptools.find_packages(),
+    install_requires=['neo4j', "Pillow-SIMD", "requests", "paramiko", "pysftp"],
+    # bcrypt: for password hashing,
+    # requests: for CDNs purge requests,
+    # pysftp: for sftp content saving,
+    # django-compressor & django-libsass: for sass/scss support during bundle operations,
+    # Pillow-SIMD: for images compression and optimization.
+    # paramiko: for SSH handling
+    extras_require={"bcrypt": ["bcrypt"],
+                    "django-compressor": ["django-compressor"],
+                    "django-libsass": ["django-libsass"]},
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Natural Language :: English",
+        "Topic :: Database",
+        "Topic :: Internet :: File Transfer Protocol (FTP)",
+        "Topic :: Internet :: WWW/HTTP :: Session",
+        "Topic :: Internet :: WWW/HTTP :: WSGI"
+    ],
+)
