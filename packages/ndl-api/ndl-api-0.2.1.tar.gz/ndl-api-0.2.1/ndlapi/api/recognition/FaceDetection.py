f75ad0013a3271328b117de4d0e9d268c3462c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:51 +0000

    Add package ndl-api/0.2.1/ndl-api-0.2.1.tar.gz

---
+++ packages/ndl-api/ndl-api-0.2.1.tar.gz/ndl-api-0.2.1/ndlapi/api/recognition/FaceDetection.py
@@ -0,0 +1,17 @@
+"""
+NeurodataLab LLC 12.04.2019
+Created by Andrey Belyaev
+"""
+import ndlapi.api.recognition.recognition as RC
+import ndlapi._pyproto.FaceDetectionService_pb2_grpc as fd_pb2_grpc
+
+
+class FaceDetector(RC.IRecognition):
+
+    def __init__(self, auth):
+        super().__init__(auth)
+        self.stub = fd_pb2_grpc.FaceDetectionStub(self.channel)
+
+    @staticmethod
+    def postprocess_result(result):
+        return result['FaceDetector']['cutted_result']
