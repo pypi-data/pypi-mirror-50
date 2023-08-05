25de43ed12b6aacb4a11fc56230faa2e924a2fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:38 +0000

    Add package cbar-currency/1.0.5/cbar_currency-1.0.5.tar.gz

---
+++ packages/cbar-currency/cbar_currency-1.0.5.tar.gz/cbar_currency-1.0.5/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+  name = 'cbar_currency',    
+  packages = ['cbar_currency'],
+  version = '1.0.5',
+  license = 'MIT',
+  description = 'Getting currencies and differencies from CBAR',
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
+  author = 'KENAN SULTAN',
+  author_email = 'kenansultan@yahoo.com',
+  url = 'https://github.com/KenanSultan/Django-cbar-currency', 
+  download_url = 'https://github.com/KenanSultan/Django-cbar-currency/archive/v_1.0.5.tar.gz',
+  keywords = ['CBAR', 'CURRENCY'],
+  install_requires=['requests'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
