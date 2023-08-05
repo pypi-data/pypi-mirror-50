fc2649fe67394686ac06e3007607938b6f72e972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:05 +0000

    Add package parse-it/2.0.0/parse_it-2.0.0-py3-none-any.whl

---
+++ packages/parse-it/parse_it-2.0.0-py3-none-any.whl/parse_it/envvars/envvars.py
@@ -0,0 +1,39 @@
+import os
+from typing import Optional
+
+
+def read_envvar(envvar: str, force_uppercase: bool = True) -> Optional[str]:
+    """Read an environment variable .
+
+            Arguments:
+                envvar -- name of the envvar to get the value of
+                force_uppercase -- if the envvar key will be forced to be all in UPPERCASE, defaults to True
+            Returns:
+                the value of the envvar, None if doesn't exist
+    """
+    if force_uppercase is True:
+        envvar = envvar.upper()
+    envvar_value = os.getenv(envvar)
+
+    if isinstance(envvar_value, str) is True:
+        # this weird encode and decode is to avoid some cases where envvar get special characters escaped
+        envvar_value = envvar_value.encode('latin1').decode('unicode_escape')
+    return envvar_value
+
+
+def envvar_defined(envvar: str, force_uppercase: bool = True) -> bool:
+    """Read an environment variable .
+
+            Arguments:
+                envvar -- name of the envvar to get the value of
+                force_uppercase -- if the envvar key will be forced to be all in UPPERCASE, defaults to True
+            Returns:
+                True if envvar is declared, False otherwise
+    """
+    if force_uppercase is True:
+        envvar = envvar.upper()
+
+    if envvar in os.environ:
+        return True
+    else:
+        return False
