57fc7aab845f300129194caea6a29a478231e564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:21 +0000

    Add package PyU4V/3.0.0.16/PyU4V-3.0.0.16-py3-none-any.whl

---
+++ packages/PyU4V/PyU4V-3.0.0.16-py3-none-any.whl/PyU4V/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+#   __
+#  /__)      /  /   / | \   /
+# /    ( /  / _/  / _ |  \ /
+#       /             |
+"""__init__.py."""
+
+__title__ = 'pyu4v'
+__version__ = '3.0.0.16'
+__author__ = 'Dell EMC or its subsidieries'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 Dell EMC Inc'
