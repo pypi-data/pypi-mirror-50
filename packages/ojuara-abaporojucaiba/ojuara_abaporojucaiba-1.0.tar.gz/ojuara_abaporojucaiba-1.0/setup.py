51022958a7db2aa485430e3486780a7d81231e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:25 +0000

    Add package ojuara-abaporojucaiba/1.0/ojuara_abaporojucaiba-1.0.tar.gz

---
+++ packages/ojuara-abaporojucaiba/ojuara_abaporojucaiba-1.0.tar.gz/ojuara_abaporojucaiba-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+        name='ojuara_abaporojucaiba',
+        version='1.0',
+        author='Ojuara Abaporojucaíba',
+        author_email='ojuara@araujo.com',
+        description='Brincando mais uma vez',
+        py_modules = ['hello']
+)
