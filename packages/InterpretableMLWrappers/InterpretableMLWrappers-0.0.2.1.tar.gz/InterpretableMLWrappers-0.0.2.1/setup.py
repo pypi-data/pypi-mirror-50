4647ee68cc0fcb70d527ac3779c370976413a8ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:04 +0000

    Add package InterpretableMLWrappers/0.0.2.1/InterpretableMLWrappers-0.0.2.1.tar.gz

---
+++ packages/InterpretableMLWrappers/InterpretableMLWrappers-0.0.2.1.tar.gz/InterpretableMLWrappers-0.0.2.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "InterpretableMLWrappers",
+    version = "0.0.2.1",
+    author = "Emile Givental",
+    author_email = "emilegivental@gmail.com",
+    description="A selection of interpretable methods with logging and printouts",
+    long_description=long_description,
+    url="https://github.com/egivental/resetInterpretability",
+    packages = [],
+    install_requires = ['cplex>=12.9.0.0', 'Prettytable>=0.7.2', 'pandas>=0.24.2', 'numpy>=1.16.4', 'sklearn>=0.0'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent"
+    ],
+
+    )
+
