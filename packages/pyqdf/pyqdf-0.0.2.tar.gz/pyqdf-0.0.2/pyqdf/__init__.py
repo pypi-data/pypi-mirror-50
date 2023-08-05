c23ccbf0d9d3e42b56baed52f4d0952434d3af98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:35 +0000

    Add package pyqdf/0.0.2/pyqdf-0.0.2.tar.gz

---
+++ packages/pyqdf/pyqdf-0.0.2.tar.gz/pyqdf-0.0.2/pyqdf/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Qlik Deployment Framework."""
+
+__author__ = """Paralelo Consultoria e Serviços Ltda"""
+__email__ = 'contato@paralelocs.com.br'
+__version__ = '0.0.2'
+__all__ = ['container', 'qvf', 'qvs', 'repository']
