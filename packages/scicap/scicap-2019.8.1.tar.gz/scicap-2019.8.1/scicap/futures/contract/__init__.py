01137a02008eb3a220920165201d5eb910db2f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:17 +0000

    Add package scicap/2019.8.1/scicap-2019.8.1.tar.gz

---
+++ packages/scicap/scicap-2019.8.1.tar.gz/scicap-2019.8.1/scicap/futures/contract/__init__.py
@@ -0,0 +1,5 @@
+from .product import get_product_info
+from .instrument import get_instrument_info
+from .utils import parse_last_trading_day
+from .utils import parse_time_line
+from .utils import parse_std_instrument_id
---
+++ packages/scicap/scicap-2019.8.1.tar.gz/scicap-2019.8.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='scicap',
+    version='2019.8.1',
+    author='scicapital',
+    author_email='hutianye@me.com',
+    description='futures tool',
+    license='MIT Licence',
+    url='http://scicapital.com',
+    packages=setuptools.find_packages(),
+    platforms='any',
+    install_requires=[],
+)
