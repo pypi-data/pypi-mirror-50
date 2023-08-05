95be52f028862926398b1bab4a058aa6d307e6fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:55 +0000

    Add package Kaggler/0.8.1/Kaggler-0.8.1-cp36-cp36m-macosx_10_7_x86_64.whl

---
+++ packages/Kaggler/Kaggler-0.8.1-cp36-cp36m-macosx_10_7_x86_64.whl/kaggler/preprocessing/__init__.py
@@ -0,0 +1,7 @@
+from .data import OneHotEncoder
+from .data import LabelEncoder
+from .data import TargetEncoder
+from .data import Normalizer
+
+__all__ = ['OneHotEncoder', 'LabelEncoder', 'TargetEncoder',
+           'Normalizer']
