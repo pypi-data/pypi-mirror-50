b91ea3150dd897155f2a12bb806572a137338620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:26 +0000

    Add package toomanyobjs/0.0.0/toomanyobjs-0.0.0.tar.gz

---
+++ packages/toomanyobjs/toomanyobjs-0.0.0.tar.gz/toomanyobjs-0.0.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='toomanyobjs',
+      version='0.0.0',
+      description='Share and store Python objects',
+      author='Kevin Froman',
+      author_email='beardog@mailbox.org',
+      url='https://github.com/beardog108/toomanyobjs',
+      packages=['toomanyobjs'],
+      install_requires=[],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+      ],
+     )
