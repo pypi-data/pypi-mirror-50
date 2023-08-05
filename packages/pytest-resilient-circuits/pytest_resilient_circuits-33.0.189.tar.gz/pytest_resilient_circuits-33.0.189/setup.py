9026b71d29935d37a3fde68890190068a5b2054c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:19 +0000

    Add package pytest-resilient-circuits/33.0.189/pytest_resilient_circuits-33.0.189.tar.gz

---
+++ packages/pytest-resilient-circuits/pytest_resilient_circuits-33.0.189.tar.gz/pytest_resilient_circuits-33.0.189/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# (c) Copyright IBM Corp. 2010, 2017. All Rights Reserved.
+
+""" Setup for pytest_resilient_circuits module """
+
+import os
+from setuptools import setup
+
+requires_resilient_version = "29.0"
+major, minor = requires_resilient_version.split('.', 2)[:2]
+
+requirements = [
+    'pytest>=3.0.0, <4.1.0',
+    'resilient>={}.{}'.format(major, minor),
+    'resilient-circuits>={}.{}'.format(major, minor),
+    'ConfigParser'
+]
+
+setup(
+    name='pytest_resilient_circuits',
+    use_scm_version={"root": "../", "relative_to": __file__},
+    setup_requires=['setuptools_scm'],
+    url='https://developer.ibm.com/resilient',
+    license='MIT',
+    author='IBM Resilient',
+    author_email='support@resilientsystems.com',
+    description='Resilient Circuits fixtures for PyTest.',
+    packages=['pytest_resilient_circuits'],
+    package_data={'pytest_resilient_circuits': ['version.txt']},
+    install_requires=requirements,
+    include_package_data=True,
+    entry_points={
+        'pytest11': [
+            'pytest_resilient_circuits = pytest_resilient_circuits.plugin'
+        ]},
+)
