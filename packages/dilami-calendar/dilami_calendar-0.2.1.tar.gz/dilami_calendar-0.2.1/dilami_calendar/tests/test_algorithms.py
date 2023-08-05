ce0b18f307f265ddda118dd7db16ee49930504cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:13 +0000

    Add package dilami-calendar/0.2.1/dilami_calendar-0.2.1.tar.gz

---
+++ packages/dilami-calendar/dilami_calendar-0.2.1.tar.gz/dilami_calendar-0.2.1/dilami_calendar/tests/test_algorithms.py
@@ -0,0 +1,45 @@
+from dilami_calendar.pure_algorithms import (
+    dilami_to_jalali,
+    jalali_to_dilami
+)
+
+
+def test_jalali_to_dilami():
+    dy, dm, dd = jalali_to_dilami(1398, 5, 1)
+    assert dy == 1592
+    assert dm == 12
+    assert dd == 15
+
+    for jy in range(1390, 1395):
+        for jm in range(1, 13):
+            for jd in range(1, 31):
+                dy, dm, dd = jalali_to_dilami(jy, jm, jd)
+                assert 1584 <= dy <= 1589
+                assert 0 <= dm <= 12
+                assert 0 <= dd <= 30
+
+
+def test_dilami_to_jalali():
+    jy, jm, jd = dilami_to_jalali(1592, 12, 15)
+    assert jy == 1398
+    assert jm == 5
+    assert jd == 1
+
+    for dy in range(1584, 1589):
+        for dm in range(1, 12):
+            for dd in range(1, 30):
+                jy, jm, jd = dilami_to_jalali(dy, dm, dd)
+                assert 1389 <= jy <= 1395
+                assert 0 <= jm <= 12
+                assert 0 <= jd <= 31
+
+    # Test panjik days for leap years
+    jy, jm, jd = dilami_to_jalali(1587, 0, 2)
+    assert jy == 1393
+    assert jm == 1
+    assert jd == 17
+
+    jy, jm, jd = dilami_to_jalali(1587, 0, 0)
+    assert jy == 1393
+    assert jm == 1
+    assert jd == 15
---
+++ packages/dilami-calendar/dilami_calendar-0.2.1.tar.gz/dilami_calendar-0.2.1/setup.py
@@ -0,0 +1,36 @@
+import re
+import os
+from setuptools import find_packages, setup
+
+# reading package version (same way sqlalchemy does)
+with open(os.path.join(os.path.dirname(__file__), 'dilami_calendar', '__init__.py')) as v_file:
+    package_version = re.compile(r".*__version__ = '(.*?)'", re.S).match(v_file.read()).group(1)
+
+
+setup(
+    name="dilami_calendar",
+    version=package_version,
+    description='Dilami (Gilaki) calendar for python',
+    keywords='dilami deylami daylami gilaki calendar datetime date time conversion',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='http://github.com/jangal/python-dilami-calendar',
+    author="Jangal",
+    install_requires=[
+        'khayyam~=3.0.17'
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Localization"
+    ],
+    packages=find_packages()
+)
