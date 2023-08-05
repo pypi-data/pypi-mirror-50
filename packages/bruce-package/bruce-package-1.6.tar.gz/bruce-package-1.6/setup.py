795b2048c992be868ad1077924e801241c509420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:07 +0000

    Add package bruce-package/1.6/bruce-package-1.6.tar.gz

---
+++ packages/bruce-package/bruce-package-1.6.tar.gz/bruce-package-1.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, Extension
+
+setup(
+    name = 'bruce-package',
+    version = '1.6',
+    description = 'GPU-accelerated binary star model',
+    url = None,
+    author = 'Samuel Gill et al',
+    author_email = 'samuel.gill@warwick.ac.uk',
+    license = 'GNU',
+    packages=['bruce','bruce/binarystar'],
+    scripts=['Utils/ngtsfit/ngtsfit',
+             'Utils/lcbin/lcbin',
+             'Utils/tessget/tessget',
+             'Utils/tls/tls',
+             'Utils/tls/PAOtls',
+             'Utils/prewhiten/prewhiten',
+             'Utils/mbls/mbls',
+             'Utils/predict/predict',
+             'Utils/analyse/analyse',
+             'Utils/analyse/plot_nights',
+             'Utils/massfunc/massfunc',
+             'Utils/modulation/modulation',
+             'Utils/monofind/monofind'],
+    install_requires=['numba']
+)
