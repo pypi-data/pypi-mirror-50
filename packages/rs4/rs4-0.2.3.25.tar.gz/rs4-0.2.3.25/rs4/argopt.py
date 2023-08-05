24c47d3655d7deda6047d28490098af916b6f728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:59 +0000

    Add package rs4/0.2.3.25/rs4-0.2.3.25.tar.gz

---
+++ packages/rs4/rs4-0.2.3.25.tar.gz/rs4-0.2.3.25/rs4/argopt.py
@@ -0,0 +1,62 @@
+import getopt as optparse
+import sys
+
+long_opts = []
+short_opts = []
+
+def addopt (lname = None, sname = None):
+    global long_opts, short_opts    
+    if lname:
+        if lname.startswith ("--"):
+            lname = lname [2:]
+        long_opts.append (lname)
+
+    if sname:
+        if sname.startswith ("-"):
+            sname = sname [1:]        
+        short_opts.append (sname)
+
+def addopts (*names):
+    for name in names:
+        assert name and name [0] == "-"
+        if name.startswith ("--"):
+            addopt (name [2:])
+        else:
+            addopt (None, name [1:])
+
+class ArgumentOptions:
+    def __init__ (self, kopt = {}, argv = []):
+        self.__kopt = kopt
+        self.argv = argv
+
+    def items (self):
+        return self.__kopt.items ()
+
+    def __contains__ (self, k):
+        return k in self.__kopt
+
+    def get (self, k, v = None):
+        return self.__kopt.get (k, v)
+
+    def set (self, k, v = None):
+        self.__kopt [k] = v
+
+    def remove (self, k):
+        try:
+            del self.__kopt [k]
+        except KeyError:
+            pass  
+
+def getopt ():        
+    global long_opts, short_opts
+    
+    argopt = optparse.getopt (sys.argv [1:], "".join (short_opts).replace ("=", ":"), long_opts)
+    kopts_ = {}
+    for k, v in argopt [0]:
+        if k in kopts_:
+            if not isinstance (kopts_ [k], list):
+                kopts [k] = {kopts [k]}
+            kopts_ [k].add (v)
+        else:
+            kopts_ [k] = v    
+    return ArgumentOptions (kopts_, argopt [1])
