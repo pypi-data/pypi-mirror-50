7c9bc516be7166c81303a9f49a29256b95963dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:51 +0000

    Add package vtorch/0.1/vtorch-0.1.tar.gz

---
+++ packages/vtorch/vtorch-0.1.tar.gz/vtorch-0.1/vtorch/predictors/multilabel_classifier.py
@@ -0,0 +1,19 @@
+from typing import List
+from overrides import overrides
+from .predictor import Predictor
+from vtorch.data import Instance
+
+
+@Predictor.register("multi_label_classifier")
+class MultiLabelClassifier(Predictor):
+    def predict(self, inputs: List[Instance]):
+        prepared_instances = []
+        for sample in inputs:
+            prepared_instances.append(self._dataset_reader.text_to_instance(text=sample.get("text", None),
+                                                                            labels=sample.get("labels", None)))
+        return self.predict_batch_instance(prepared_instances)
+
+    @overrides
+    def _json_to_instance(self, json_dict: dict) -> Instance:
+        text = json_dict.get("text", "")
+        return self._dataset_reader.text_to_instance(text=text)
