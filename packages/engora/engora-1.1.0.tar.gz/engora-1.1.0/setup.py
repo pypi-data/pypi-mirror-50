8cbd2daaa4e017a87f5e09468adc1f4855687c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:14 +0000

    Add package engora/1.1.0/engora-1.1.0.tar.gz

---
+++ packages/engora/engora-1.1.0.tar.gz/engora-1.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='engora',
+      version='1.1.0',
+      description='Python client library for engora.tech',
+      url='https://engora.tech',
+      author='engora.tech',
+      author_email='dsa@engora.tech',
+      license='MIT',
+      packages=['engora'],
+      install_requires=[
+          'requests',
+          'pint'
+      ],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      zip_safe=False)
