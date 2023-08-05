9dc2f46fd88bc2e0383736693d3418b50a8f9eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:46 +0000

    Add package wat-cli/0.1.3/wat-cli-0.1.3.tar.gz

---
+++ packages/wat-cli/wat-cli-0.1.3.tar.gz/wat-cli-0.1.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name='wat-cli',
+    author='Philipp Hack',
+    author_email='philipp.hack@gmail.com',
+    url='http://github.com/phha/wat',
+    version='0.1.3',
+    license='MIT',
+    description='Get short answers from Wolfram Alpha.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    py_modules=['wat'],
+    install_requires=[
+        'click',
+        'click-config-file',
+        'requests',
+    ],
+    entry_points='''
+        [console_scripts]
+        wat=wat:wat
+    ''',
+)
