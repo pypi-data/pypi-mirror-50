4ee76e1985cf7ed99e831f13bdf10b6ac8b4e52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:30 +0000

    Add package odoo11-addon-fieldservice-sale/11.0.0.1.0.99.dev4/odoo11_addon_fieldservice_sale-11.0.0.1.0.99.dev4-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-fieldservice-sale/odoo11_addon_fieldservice_sale-11.0.0.1.0.99.dev4-py2.py3-none-any.whl/odoo/addons/fieldservice_sale/__manifest__.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Field Service - Sales',
+    'summary': 'Sales integration for Field Service',
+    'version': '11.0.0.1.0',
+    'category': 'Field Service',
+    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/field-service',
+    'depends': [
+        'fieldservice',
+        'fieldservice_recurring',
+        'sale_management',
+    ],
+    'data': [
+        'views/fsm_location.xml',
+        'views/fsm_order.xml',
+        'views/fsm_recurring.xml',
+        'views/product.xml',
+        'views/sale_order.xml',
+    ],
+    'license': 'AGPL-3',
+    'development_status': 'Beta',
+    'maintainers': [
+        'wolfhall',
+        'max3903',
+        'brian10048',
+    ],
+}
