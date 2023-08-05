9783145ac7e27cbf5dbae2c7bc67dac026add3df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:46 +0000

    Add package lite/0.1.3/lite-0.1.3.tar.gz

---
+++ packages/lite/lite-0.1.3.tar.gz/lite-0.1.3/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+modules = \
+['lite']
+install_requires = \
+['aiohttp>=3.5,<4.0']
+
+setup_kwargs = {
+    'name': 'lite',
+    'version': '0.1.3',
+    'description': 'Analouge Light.js on Python',
+    'long_description': None,
+    'author': 'mihett05',
+    'author_email': 'mihett05@yandex.ru',
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
