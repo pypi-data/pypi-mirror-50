c2220b454919fc91073d84f972467781be3921dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:39 +0000

    Add package awsaccountmgr/0.0.2/awsaccountmgr-0.0.2.tar.gz

---
+++ packages/awsaccountmgr/awsaccountmgr-0.0.2.tar.gz/awsaccountmgr-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+with open('HISTORY.md', 'r') as f:
+    history = f.read()
+
+setuptools.setup(
+    name="awsaccountmgr",
+    version="0.0.2",
+    author="Mathijs Mortimer",
+    keywords="AWS Accounts",
+    author_email="mathijs@mortimer.nl",
+    description="A command line tool for managing accounts within an AWS organization. Easy to integrate into AWS Deployment Framework",
+    long_description=readme + '\n\n' + history,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thiezn/awsaccountmgr/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=['bin/awsaccountmgr'],
+)
