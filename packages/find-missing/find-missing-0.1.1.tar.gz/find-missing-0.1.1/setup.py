64e3bfd3ca79a39a29a237d6edd350bc570d9dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:30 +0000

    Add package find-missing/0.1.1/find-missing-0.1.1.tar.gz

---
+++ packages/find-missing/find-missing-0.1.1.tar.gz/find-missing-0.1.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['find_missing']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.0,<8.0']
+
+entry_points = \
+{'console_scripts': ['find-missing = find_missing:main']}
+
+setup_kwargs = {
+    'name': 'find-missing',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'MB',
+    'author_email': 'mb@m1k.pw',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
