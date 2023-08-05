132bc3a1ed5bebc067af3962bec93e46f2bf9550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:30 +0000

    Add package HABApp/0.6.8/HABApp-0.6.8.tar.gz

---
+++ packages/HABApp/HABApp-0.6.8.tar.gz/HABApp-0.6.8/HABApp/openhab/__init__.py
@@ -0,0 +1,6 @@
+from .oh_interface import OpenhabInterface, get_openhab_interface
+from .oh_connection import OpenhabConnection
+from .http_connection import HttpConnection
+
+import HABApp.openhab.items
+from .items.map_items import map_items
---
+++ packages/HABApp/HABApp-0.6.8.tar.gz/HABApp-0.6.8/HABApp/openhab/items/rollershutter_item.py
@@ -0,0 +1,29 @@
+from HABApp.core.items import Item
+from .. import get_openhab_interface
+
+
+class RollershutterItem(Item):
+
+    def set_state(self, new_state) -> bool:
+        if new_state == 'UP':
+            new_state = 0.0
+        if new_state == 'DOWN':
+            new_state = 100.0
+        assert isinstance(new_state, (int, float)) or new_state is None, new_state
+        return super().set_state(new_state)
+
+    def up(self):
+        """Move shutter up"""
+        get_openhab_interface().send_command(self.name, 'UP')
+
+    def down(self):
+        """Move shutter down"""
+        get_openhab_interface().send_command(self.name, 'DOWN')
+
+    def percent(self, value: float):
+        """Command shutter to value (in percent)"""
+        assert 0 <= value <= 100
+        get_openhab_interface().send_command(self.name, str(value))
+
+    def __str__(self):
+        return self.state
