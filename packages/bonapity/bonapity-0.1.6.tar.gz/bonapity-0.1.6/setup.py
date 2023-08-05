c3e9aaf8b876a80a68c5a320eb90bc79018edb8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:35 +0000

    Add package bonapity/0.1.6/bonapity-0.1.6.tar.gz

---
+++ packages/bonapity/bonapity-0.1.6.tar.gz/bonapity-0.1.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name='bonapity',
+    version='0.1.6',
+    description='Get a simple HTTP (GET & POST) API with only this simple decorator : @bonapity !',
+    long_description="""
+# 👀 BONAPITY 👀
+
+`bonAPIty` is the simplest python3 package allowing you to create simple API 
+(GET & POST) for your functions without writting any complicated line of server code !
+
+You are the 👨‍🍳, just do what you do the best : cook code !
+don't loose your time to 💁, we do it for you... :)
+
+By type hinting your code we cast 
+the received inputs to the right type ! 
+so don't worry about it... 😀
+
+-------------------------------------------
+
+🚧: still in 👷 🏗 ⚠️, already great features implemented, 
+but the official git is not still openned... stay tuned ! 😃 🎞🎞🎞 
+    """,
+    long_description_content_type='text/markdown',
+    url='http://github.com/VieVie31/bonapity',
+    author='Olivier RISSER-MAROIX',
+    author_email='orissermaroix@gmail.com',
+    license='CC-BY',
+    packages=['bonapity'],
+    zip_safe=False
+)
+
