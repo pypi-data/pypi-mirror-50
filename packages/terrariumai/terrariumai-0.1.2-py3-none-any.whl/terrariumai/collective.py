78c6c488dd488b6aad7247e59fa370d55f356c1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:10 +0000

    Add package terrariumai/0.1.2/terrariumai-0.1.2-py3-none-any.whl

---
+++ packages/terrariumai/terrariumai-0.1.2-py3-none-any.whl/terrariumai/collective.py
@@ -0,0 +1,40 @@
+import threading
+import grpc
+import sys
+import time
+from queue import Queue
+from .collective_pb2 import *
+from .collective_pb2_grpc import *
+
+
+PROD_SERVER_ADDR = "34.67.243.23:9090" # TODO
+
+def defaultModelFunc(obsv):
+  raise Exception('You need to pass in a model function.')
+  
+def connectRemoteModel(secret, modelFunc, addr=PROD_SERVER_ADDR):
+  # Create the stub
+  channel = grpc.insecure_channel(addr)
+  stub = CollectiveStub(channel)
+  metadata = [('model-secret', secret)]
+  # Create the iterator
+  request_queue = Queue()
+  request_iter = iter(request_queue.get, object())
+  # Connect
+  responses = stub.ConnectRemoteModel(request_iter, metadata=metadata)
+  # Try/catch cleanup for Notebook
+  try:
+      for response in responses:
+        # Create the request
+        actions = []
+        for obsv in response.observations:
+          actions.append(modelFunc(obsv))
+        actionPacket = ActionPacket(actions=actions)
+        # Set the request to the iterator
+        request_queue.put(actionPacket)
+  except KeyboardInterrupt:
+    # Send cancel to the server
+    responses.cancel()
+
+def createAction(id, action, direction):
+  return Action(id=id, action=action, direction=direction)
