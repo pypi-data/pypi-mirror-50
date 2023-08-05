45d2fe598b7ea5b8435d75cc926027f30da99da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:08 +0000

    Add package hydraseq/0.0.25/hydraseq-0.0.25.tar.gz

---
+++ packages/hydraseq/hydraseq-0.0.25.tar.gz/hydraseq-0.0.25/hydraseq/__init__.py
@@ -0,0 +1,7 @@
+name = "hydraseq"
+__version__ = '0.0.25'
+from hydraseq.hydraseq import Node
+from hydraseq.hydraseq import Hydraseq
+from hydraseq.hydra import Hydra
+from hydraseq.minicolumn import MiniColumn
+from hydraseq.automata import DFAstate
