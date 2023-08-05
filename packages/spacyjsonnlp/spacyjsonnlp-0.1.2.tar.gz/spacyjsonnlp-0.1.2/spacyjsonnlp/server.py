03acdf933c465b939a2f3e5fcf9cf2d07aa9e98f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:56 +0000

    Add package spacyjsonnlp/0.1.2/spacyjsonnlp-0.1.2.tar.gz

---
+++ packages/spacyjsonnlp/spacyjsonnlp-0.1.2.tar.gz/spacyjsonnlp-0.1.2/spacyjsonnlp/server.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from spacyjsonnlp import SpacyPipeline
+from pyjsonnlp.microservices.flask_server import FlaskMicroservice
+
+app = FlaskMicroservice(__name__, SpacyPipeline(), base_route='/')
+app.with_constituents = True
+app.with_coreferences = True
+app.with_dependencies = True
+app.with_expressions = True
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
