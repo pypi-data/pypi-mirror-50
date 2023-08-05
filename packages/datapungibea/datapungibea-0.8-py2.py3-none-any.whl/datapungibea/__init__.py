501a64a39a39a82fb87cf450ef5c3b0bf082209c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:14 +0000

    Add package datapungibea/0.8/datapungibea-0.8-py2.py3-none-any.whl

---
+++ packages/datapungibea/datapungibea-0.8-py2.py3-none-any.whl/datapungibea/__init__.py
@@ -0,0 +1,11 @@
+"""Connects to BEA API and enriches data output; get vintage BEA data"""
+import pandas
+import requests
+
+from datapungibea.api import *
+import datapungibea.tests as tests
+
+__version__ = '0.8'
+#__all__ = ['api']
+
+
