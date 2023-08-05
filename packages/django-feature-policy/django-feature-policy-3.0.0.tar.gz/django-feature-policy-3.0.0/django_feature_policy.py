b0d5fae91fe950a7e3a102977cb644f6984ce877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:07 +0000

    Add package django-feature-policy/3.0.0/django-feature-policy-3.0.0.tar.gz

---
+++ packages/django-feature-policy/django-feature-policy-3.0.0.tar.gz/django-feature-policy-3.0.0/django_feature_policy.py
@@ -0,0 +1,98 @@
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.core.signals import setting_changed
+from django.dispatch import receiver
+from django.utils.functional import cached_property
+
+__version__ = "3.0.0"
+
+# Retrieved from Chrome document.featurePolicy.allowedFeatures()
+# with flag "Experimental Web Platform features" turned on
+FEATURE_NAMES = {
+    "accelerometer",
+    "ambient-light-sensor",
+    "autoplay",
+    "camera",
+    "document-domain",
+    "document-write",
+    "downloads-without-user-activation",
+    "encrypted-media",
+    "focus-without-user-activation",
+    "font-display-late-swap",
+    "forms",
+    "fullscreen",
+    "geolocation",
+    "gyroscope",
+    "hid",
+    "idle-detection",
+    "layout-animations",
+    "lazyload",
+    "loading-frame-default-eager",
+    "magnetometer",
+    "microphone",
+    "midi",
+    "modals",
+    "orientation-lock",
+    "oversized-images",
+    "payment",
+    "picture-in-picture",
+    "pointer-lock",
+    "popups",
+    "presentation",
+    "scripts",
+    "serial",
+    "speaker",
+    "sync-script",
+    "sync-xhr",
+    "top-navigation",
+    "unoptimized-lossless-images",
+    "unoptimized-lossless-images-strict",
+    "unoptimized-lossy-images",
+    "unsized-media",
+    "usb",
+    "vertical-scroll",
+    "vr",
+    "wake-lock",
+}
+
+
+class FeaturePolicyMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.header_value  # Access at setup so ImproperlyConfigured can be raised
+        receiver(setting_changed)(self.clear_header_value)
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        value = self.header_value
+        if value:
+            response["Feature-Policy"] = value
+        return response
+
+    @cached_property
+    def header_value(self):
+        setting = getattr(settings, "FEATURE_POLICY", {})
+        pieces = []
+        for feature, values in sorted(setting.items()):
+            if feature not in FEATURE_NAMES:
+                raise ImproperlyConfigured("Unknown feature {}".format(feature))
+            if isinstance(values, str):
+                values = (values,)
+
+            item = [feature]
+            for value in values:
+                if value == "none":
+                    item.append("'none'")
+                elif value == "self":
+                    item.append("'self'")
+                else:
+                    item.append(value)
+            pieces.append(" ".join(item))
+        return "; ".join(pieces)
+
+    def clear_header_value(self, setting, **kwargs):
+        if setting == "FEATURE_POLICY":
+            try:
+                del self.header_value
+            except AttributeError:
+                pass
