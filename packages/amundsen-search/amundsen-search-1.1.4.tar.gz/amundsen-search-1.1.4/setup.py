05bf86e93286e6c4a4d39642d5b15bad9708f53f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:59 +0000

    Add package amundsen-search/1.1.4/amundsen-search-1.1.4.tar.gz

---
+++ packages/amundsen-search/amundsen-search-1.1.4.tar.gz/amundsen-search-1.1.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+__version__ = '1.1.4'
+
+
+setup(
+    name='amundsen-search',
+    version=__version__,
+    description='Search Service for Amundsen',
+    url='https://github.com/lyft/amundsensearchlibrary.git',
+    maintainer='Lyft',
+    maintainer_email='dp-tools@lyft.com',
+    packages=find_packages(exclude=['tests*']),
+    dependency_links=[],
+    install_requires=[
+        'Flask-RESTFul==0.3.6',
+        'elasticsearch==6.2.0',
+        'elasticsearch-dsl==6.1.0',
+        'statsd==3.2.1',
+        'atlasclient==0.1.6'
+    ],
+    python_requires=">=3.6"
+)
