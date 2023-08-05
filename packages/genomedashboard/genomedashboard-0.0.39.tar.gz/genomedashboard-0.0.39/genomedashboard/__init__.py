dc5c9edf5aa92fb1826926885edb5e5bf87624c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:59 +0000

    Add package genomedashboard/0.0.39/genomedashboard-0.0.39.tar.gz

---
+++ packages/genomedashboard/genomedashboard-0.0.39.tar.gz/genomedashboard-0.0.39/genomedashboard/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Genome Dashboard"""
+
+# from os import path
+# from io import open
+# from genomedashboard import genomedashboard as gd
+# import cli as cli
+from .convert import convert as cv
+from .ds import ds as ds
+from .io import io as io
+
+
+# -->>> !!!! IMPORTANT: BUMP THE VERSION WITH EVERY COMMIT USING SEMVER CONVENTIONS  <Major.minor.patch> !!!! <<<--
+__version__ = '0.0.39'   # This value MUST be aligned with the value in .genome-dashboard-python/setup.py!!!
+__author__ = 'Zilong Li, Ran Sun, Thomas C. Bishop'
+__email__ = 'genome.dashboard@gmail.com'
