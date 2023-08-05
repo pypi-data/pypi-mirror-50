b369c8eb09193d67413efb1a23b0256182bae750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:44 +0000

    Add package pyObjective/0.1/pyObjective-0.1.tar.gz

---
+++ packages/pyObjective/pyObjective-0.1.tar.gz/pyObjective-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyObjective',
+      version='0.1',
+      description='A tool to easily pass complex objects to scipy.optimize',
+      url='http://github.com/storborg/funniest',
+      author='Devansh Agrawal',
+      author_email='devanshinspace@gmail.com',
+      license='MIT',
+      packages=['pyObjective'],
+      zip_safe=False)
