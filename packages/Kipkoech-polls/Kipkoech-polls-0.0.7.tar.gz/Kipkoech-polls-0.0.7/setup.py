0a6d09b52c529e50187ae4f2f31066a9a7c635fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:41 +0000

    Add package Kipkoech-polls/0.0.7/Kipkoech-polls-0.0.7.tar.gz

---
+++ packages/Kipkoech-polls/Kipkoech-polls-0.0.7.tar.gz/Kipkoech-polls-0.0.7/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools.command.install import install
+
+VERSION = "0.0.7"
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+class VerifyVersionCommand(install):
+    description = "Simple Django PollsApp"
+
+
+setup(
+    name="Kipkoech-polls",
+    version=VERSION,
+    description="Simple Django Polls app",
+    long_description=readme(),
+    url="https://github.com/DenisBiwott/PollsApp",
+    author="Denis Kipkoech",
+    author_email="deniskipkoech@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords='',
+    packages=['.'],
+    install_requires=[
+        'requests==2.20.0',
+    ],
+    python_requires='>=3',
+)
