099b431999c70f88054f9952a08037aa9b0c36cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:42 +0000

    Add package PanaceaMobile/1.0.0/PanaceaMobile-1.0.0.tar.gz

---
+++ packages/PanaceaMobile/PanaceaMobile-1.0.0.tar.gz/PanaceaMobile-1.0.0/PanaceaMobile/__init__.py
@@ -0,0 +1,2 @@
+"""Send SMS via PanaceaMobile API"""
+from PanaceaMobile.PanaceaMobile import PanaceaMobile
---
+++ packages/PanaceaMobile/PanaceaMobile-1.0.0.tar.gz/PanaceaMobile-1.0.0/PanaceaMobile/const.py
@@ -0,0 +1,10 @@
+"""constants for PanaceaMobile."""
+VERSION = "1.0.0"
+
+CONFIG_FILE_CONTENT = """[PANACEA]
+PANACEA_LOGIN = 06501234567
+PANACEA_PASSWORD = password-or-api-key
+"""
+CONFIG_FILE_PATHS = ["/etc/panaceamobile.conf",
+                     "~/.config/panaceamobile.conf",
+                    ]
