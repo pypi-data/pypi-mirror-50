7cda5e2c4702e1b03029f94182eab3fa3e8ab402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:28 +0000

    Add package recept/0.0.7/recept-0.0.7-py3-none-any.whl

---
+++ packages/recept/recept-0.0.7-py3-none-any.whl/recept/errors.py
@@ -0,0 +1,16 @@
+class ReceptError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    @property
+    def name(self):
+        return self.__class__.__name__
+
+    def __str__(self):
+        return f"{self.name}({self.message})"
+
+    __repr__ = __str__
+
+
+class ImproperlyConfigured(ReceptError):
+    pass
