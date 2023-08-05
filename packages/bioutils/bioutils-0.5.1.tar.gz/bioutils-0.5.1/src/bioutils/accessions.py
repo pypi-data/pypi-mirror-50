99b2f7007774740e99d0de3306a75f14d1d45133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:25 +0000

    Add package bioutils/0.5.1/bioutils-0.5.1.tar.gz

---
+++ packages/bioutils/bioutils-0.5.1.tar.gz/bioutils-0.5.1/src/bioutils/accessions.py
@@ -0,0 +1,252 @@
+"""simple routines to deal with accessions, identifiers, etc.
+
+biocommons terminology: an identifier is composed of a *namespace* and
+an *accession*. The namespace is a string, composed of any character
+other than colon (:). The accession is a string without character set
+restriction.  An accession is expected to be unique within the
+namespace; there is no expectation of uniqueness of accessions across
+namespaces.
+
+Identifier := <Namespace, Accession>
+Namespace := [^:]+
+Accession := \\w+
+
+Some sample serializations of Identifiers:
+
+json: {"namespace": "RefSeq", "accession": "NM_000551.3"}
+xml: <Identifier namespace="RefSeq" accession="NM_000551.3"/>
+string: "RefSeq:NM_000551.3"
+
+The string form may be used as a CURIE, in which case the document in
+which the CURIE is used must contain a map of {namespace => uri}.
+
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import re
+
+from .exceptions import BioutilsError
+
+
+_ensembl_species_prefixes = "|".join("""ENS ENSACA ENSAME ENSAMX
+ENSANA ENSAPL ENSBTA ENSCAF ENSCAN ENSCAP ENSCAT ENSCCA ENSCEL ENSCGR
+ENSCGR ENSCHI ENSCHO ENSCIN ENSCJA ENSCLA ENSCPO ENSCSA ENSCSAV ENSDAR
+ENSDNO ENSDOR ENSEBU ENSECA ENSEEU ENSETE ENSFAL ENSFCA ENSFDA ENSGAC
+ENSGAL ENSGGO ENSGMO ENSHGLF ENSHGLM ENSJJA ENSLAC ENSLAF ENSLOC
+ENSMAU ENSMEU ENSMFA ENSMGA ENSMIC ENSMLE ENSMLU ENSMMU ENSMNE ENSMOC
+ENSMOD ENSMPU ENSMUS ENSNGA ENSNLE ENSOAN ENSOAR ENSOCU ENSODE ENSOGA
+ENSONI ENSOPR ENSORL ENSPAN ENSPCA ENSPCO ENSPEM ENSPFO ENSPMA ENSPPA
+ENSPPR ENSPPY ENSPSI ENSPTI ENSPTR ENSPVA ENSRBI ENSRNO ENSRRO ENSSAR
+ENSSBO ENSSCE ENSSHA ENSSSC ENSSTO ENSTBE ENSTGU ENSTNI ENSTRU ENSTSY
+ENSTTR ENSVPA ENSXET ENSXMA FB MGP_129S1SvImJ_ MGP_AJ_ MGP_AKRJ_
+MGP_BALBcJ_ MGP_C3HHeJ_ MGP_C57BL6NJ_ MGP_CAROLIEiJ_ MGP_CASTEiJ_
+MGP_CBAJ_ MGP_DBA2J_ MGP_FVBNJ_ MGP_LPJ_ MGP_NODShiLtJ_ MGP_NZOHlLtJ_
+MGP_PWKPhJ_ MGP_PahariEiJ_ MGP_SPRETEiJ_ MGP_WSBEiJ_""".split())
+_ensembl_feature_types_re = r"E|FM|G|GT|P|R|T"
+_ensembl_re = r"^(?:{})(?:{}){}$".format(
+    _ensembl_species_prefixes, _ensembl_feature_types_re, r"\d{11}(?:\.\d+)?")
+
+# map of regexp => namespace
+# TODO: make this namespace => [regexps] for clarity
+# namespaces follow convention of identifiers.org
+ac_namespace_regexps = {
+    # https://uswest.ensembl.org/info/genome/stable_ids/prefixes.html
+    # [species prefix][feature type prefix][a unique eleven digit number]
+    # N.B. The regexp at http://identifiers.org/ensembl appears broken:
+    # 1) Human only; 2) escaped backslashes (\\d rather than \d).
+    _ensembl_re: "ensembl",
+
+    # http://identifiers.org/insdc/
+    # P12345, a UniProtKB accession matches the miriam regexp but shouldn't (I think)
+    r"^([A-Z]\d{5}|[A-Z]{2}\d{6}|[A-Z]{4}\d{8}|[A-J][A-Z]{2}\d{5})(\.\d+)?$":
+    "insdc",
+
+    # http://identifiers.org/refseq/
+    # https://www.ncbi.nlm.nih.gov/books/NBK21091/table/ch18.T.refseq_accession_numbers_and_mole/
+    r"^((AC|AP|NC|NG|NM|NP|NR|NT|NW|XM|XP|XR|YP|ZP)_\d+|(NZ\_[A-Z]{4}\d+))(\.\d+)?$":
+    "refseq",
+
+    # Uniprot
+    # http://identifiers.org/uniprot/
+    # https://www.uniprot.org/help/accession_numbers
+    r"^(?:[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2})$":
+    "uniprot",
+}
+
+ac_namespace_regexps = {re.compile(k): v for k, v in ac_namespace_regexps.items()}
+
+
+def chr22XY(c):
+    """force to name from 1..22, 23, 24, X, Y, M 
+    to in chr1..chr22, chrX, chrY, chrM
+    str or ints accepted
+
+    >>> chr22XY('1')
+    'chr1'
+    >>> chr22XY(1)
+    'chr1'
+    >>> chr22XY('chr1')
+    'chr1'
+    >>> chr22XY(23)
+    'chrX'
+    >>> chr22XY(24)
+    'chrY'
+    >>> chr22XY("X")
+    'chrX'
+    >>> chr22XY("23")
+    'chrX'
+    >>> chr22XY("M")
+    'chrM'
+
+    """
+    c = str(c)
+    if c[0:3] == 'chr':
+        c = c[3:]
+    if c == '23':
+        c = 'X'
+    if c == '24':
+        c = 'Y'
+    return 'chr' + c
+
+
+def coerce_namespace(ac):
+    """given an accession, prefix with inferred namespace if not present
+
+    This function is intended to be used to promote an unqualified
+    identifier to one that is guaranteed to have an identifier.
+
+    >>> coerce_namespace("refseq:NM_01234.5")
+    'refseq:NM_01234.5'
+
+    >>> coerce_namespace("NM_01234.5")
+    'refseq:NM_01234.5'
+
+    >>> coerce_namespace("bogus:QQ_01234.5")
+    'bogus:QQ_01234.5'
+
+    >>> coerce_namespace("QQ_01234.5")
+    Traceback (most recent call last):
+    ...
+    ValueError: Could not infer namespace for QQ_01234.5
+
+    """
+    if ":" not in ac:
+        ns = infer_namespace(ac)
+        if ns is None:
+            raise ValueError(f"Could not infer namespace for {ac}")
+        ac = ns + ":" + ac
+    return ac
+
+
+def infer_namespace(ac):
+    """Infer the single namespace of the given accession
+
+    This function is convenience wrapper around infer_namespaces().
+    Returns:
+      * None if no namespaces are inferred
+      * The (single) namespace if only one namespace is inferred
+      * Raises an exception if more than one namespace is inferred
+
+    >>> infer_namespace("ENST00000530893.6")
+    'ensembl'
+
+    >>> infer_namespace("NM_01234.5")
+    'refseq'
+
+    >>> infer_namespace("A2BC19")
+    'uniprot'
+
+    N.B. The following test is disabled because Python 2 and Python 3
+    handle doctest exceptions differently. :-(
+    X>>> infer_namespace("P12345")
+    Traceback (most recent call last):
+    ...
+    bioutils.exceptions.BioutilsError: Multiple namespaces possible for P12345
+
+    >>> infer_namespace("BOGUS99") is None
+    True
+
+    """
+
+    namespaces = infer_namespaces(ac)
+    if not namespaces:
+        return None
+    if len(namespaces) > 1:
+        raise BioutilsError("Multiple namespaces possible for {}".format(ac))
+    return namespaces[0]
+
+
+def infer_namespaces(ac):
+    """infer possible namespaces of given accession based on syntax
+    Always returns a list, possibly empty
+
+    >>> infer_namespaces("ENST00000530893.6")
+    ['ensembl']
+    >>> infer_namespaces("ENST00000530893")
+    ['ensembl']
+    >>> infer_namespaces("ENSQ00000530893")
+    []
+    >>> infer_namespaces("NM_01234")
+    ['refseq']
+    >>> infer_namespaces("NM_01234.5")
+    ['refseq']
+    >>> infer_namespaces("NQ_01234.5")
+    []
+    >>> infer_namespaces("A2BC19")
+    ['uniprot']
+    >>> sorted(infer_namespaces("P12345"))
+    ['insdc', 'uniprot']
+    >>> infer_namespaces("A0A022YWF9")
+    ['uniprot']
+
+
+    """
+    return [v for k, v in ac_namespace_regexps.items() if k.match(ac)]
+
+
+def prepend_chr(chr):
+    """prefix chr with 'chr' if not present
+
+    Users are strongly encouraged to NOT use this function. Added a
+    'chr' prefix means that you're using a name that is not consistent
+    with authoritative assembly records.
+
+    >>> prepend_chr('22')
+    'chr22'
+
+    >>> prepend_chr('chr22')
+    'chr22'
+
+    """
+    return chr if chr[0:3] == 'chr' else 'chr' + chr
+
+
+def strip_chr(chr):
+    """remove 'chr' prefix if it exists
+
+    >>> strip_chr('22')
+    '22'
+
+    >>> strip_chr('chr22')
+    '22'
+
+    """
+    return chr[3:] if chr[0:3] == 'chr' else chr
+
+
+## <LICENSE>
+## Copyright 2014 Bioutils Contributors (https://bitbucket.org/biocommons/bioutils)
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+## </LICENSE>
---
+++ packages/bioutils/bioutils-0.5.1.tar.gz/bioutils-0.5.1/src/bioutils/sequences.py
@@ -0,0 +1,367 @@
+# -*- coding: utf-8 -*-
+"""simple functions and lookup tables for nucleic acid and amino acid sequences"""
+
+import logging
+import re
+from string import ascii_lowercase
+
+
+_logger = logging.getLogger(__name__)
+
+aa3_to_aa1_lut = {
+    "Ala": "A",
+    "Arg": "R",
+    "Asn": "N",
+    "Asp": "D",
+    "Cys": "C",
+    "Gln": "Q",
+    "Glu": "E",
+    "Gly": "G",
+    "His": "H",
+    "Ile": "I",
+    "Leu": "L",
+    "Lys": "K",
+    "Met": "M",
+    "Phe": "F",
+    "Pro": "P",
+    "Ser": "S",
+    "Thr": "T",
+    "Trp": "W",
+    "Tyr": "Y",
+    "Val": "V",
+    "Xaa": "X",
+    "Ter": "*",
+    "Sec": "",
+}
+
+aa1_to_aa3_lut = {v: k for k, v in aa3_to_aa1_lut.items()}
+
+dna_to_aa1_lut = {      # NCBI standard translation table
+    'AAA': 'K',
+    'AAC': 'N',
+    'AAG': 'K',
+    'AAT': 'N',
+    'ACA': 'T',
+    'ACC': 'T',
+    'ACG': 'T',
+    'ACT': 'T',
+    'AGA': 'R',
+    'AGC': 'S',
+    'AGG': 'R',
+    'AGT': 'S',
+    'ATA': 'I',
+    'ATC': 'I',
+    'ATG': 'M',
+    'ATT': 'I',
+    'CAA': 'Q',
+    'CAC': 'H',
+    'CAG': 'Q',
+    'CAT': 'H',
+    'CCA': 'P',
+    'CCC': 'P',
+    'CCG': 'P',
+    'CCT': 'P',
+    'CGA': 'R',
+    'CGC': 'R',
+    'CGG': 'R',
+    'CGT': 'R',
+    'CTA': 'L',
+    'CTC': 'L',
+    'CTG': 'L',
+    'CTT': 'L',
+    'GAA': 'E',
+    'GAC': 'D',
+    'GAG': 'E',
+    'GAT': 'D',
+    'GCA': 'A',
+    'GCC': 'A',
+    'GCG': 'A',
+    'GCT': 'A',
+    'GGA': 'G',
+    'GGC': 'G',
+    'GGG': 'G',
+    'GGT': 'G',
+    'GTA': 'V',
+    'GTC': 'V',
+    'GTG': 'V',
+    'GTT': 'V',
+    'TAA': '*',
+    'TAC': 'Y',
+    'TAG': '*',
+    'TAT': 'Y',
+    'TCA': 'S',
+    'TCC': 'S',
+    'TCG': 'S',
+    'TCT': 'S',
+    'TGA': '*',
+    'TGC': 'C',
+    'TGG': 'W',
+    'TGT': 'C',
+    'TTA': 'L',
+    'TTC': 'F',
+    'TTG': 'L',
+    'TTT': 'F',
+}
+
+
+complement_transtable = bytes.maketrans(b"ACGT", b"TGCA")
+
+
+def aa_to_aa1(seq):
+    """coerce string of 1- or 3-letter amino acids to 1-letter
+
+    >>> aa_to_aa1("CATSARELAME")
+    'CATSARELAME'
+
+    >>> aa_to_aa1("CysAlaThrSerAlaArgGluLeuAlaMetGlu")
+    'CATSARELAME'
+
+    >>> aa_to_aa1(None)
+
+    """
+    if seq is None:
+        return None
+    return aa3_to_aa1(seq) if looks_like_aa3_p(seq) else seq
+
+
+def aa_to_aa3(seq):
+    """coerce string of 1- or 3-letter amino acids to 3-letter
+
+    >>> aa_to_aa3("CATSARELAME")
+    'CysAlaThrSerAlaArgGluLeuAlaMetGlu'
+
+    >>> aa_to_aa3("CysAlaThrSerAlaArgGluLeuAlaMetGlu")
+    'CysAlaThrSerAlaArgGluLeuAlaMetGlu'
+
+    >>> aa_to_aa3(None)
+
+    """
+    if seq is None:
+        return None
+    return aa1_to_aa3(seq) if not looks_like_aa3_p(seq) else seq
+
+
+def aa1_to_aa3(seq):
+    """convert string of 1-letter amino acids to 3-letter amino acids
+
+    >>> aa1_to_aa3("CATSARELAME")
+    'CysAlaThrSerAlaArgGluLeuAlaMetGlu'
+
+    >>> aa1_to_aa3(None)
+
+    """
+    if seq is None:
+        return None
+    return "".join(aa1_to_aa3_lut[aa1] for aa1 in seq)
+
+
+def aa3_to_aa1(seq):
+    """convert string of 3-letter amino acids to 1-letter amino acids
+
+    >>> aa3_to_aa1("CysAlaThrSerAlaArgGluLeuAlaMetGlu")
+    'CATSARELAME'
+
+    >>> aa3_to_aa1(None)
+
+    """
+    if seq is None:
+        return None
+    return "".join(aa3_to_aa1_lut[aa3]
+                   for aa3 in [seq[i:i + 3] for i in range(0, len(seq), 3)])
+
+
+def complement(seq):
+    """return the complement of a sequence
+
+    >>> complement("ATCG")
+    'TAGC'
+
+    >>> complement(None)
+
+    """
+
+    if seq is None:
+        return None
+    return seq.translate(complement_transtable)
+
+
+def elide_sequence(s, flank=5, elision="..."):
+    """trim a sequence to include the left and right flanking sequences of
+    size `flank`, with the intervening sequence elided by `elision`.
+
+    >>> elide_sequence("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    'ABCDE...VWXYZ'
+
+    >>> elide_sequence("ABCDEFGHIJKLMNOPQRSTUVWXYZ", flank=3)
+    'ABC...XYZ'
+
+    >>> elide_sequence("ABCDEFGHIJKLMNOPQRSTUVWXYZ", elision="..")
+    'ABCDE..VWXYZ'
+
+    >>> elide_sequence("ABCDEFGHIJKLMNOPQRSTUVWXYZ", flank=12)
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    >>> elide_sequence("ABCDEFGHIJKLMNOPQRSTUVWXYZ", flank=12, elision=".")
+    'ABCDEFGHIJKL.OPQRSTUVWXYZ'
+
+    """
+
+    elided_sequence_len = flank + flank + len(elision)
+    if len(s) <= elided_sequence_len:
+        return s
+    return s[:flank] + elision + s[-flank:]
+
+
+def looks_like_aa3_p(seq):
+    """string looks like a 3-letter AA string"""
+    return (seq is not None and (len(seq) % 3 == 0) and
+            (len(seq) == 0 or seq[1] in ascii_lowercase))
+
+
+def normalize_sequence(seq):
+    """return normalized representation of sequence for hashing
+
+    This really means ensuring that the sequence is represented as a
+    binary blob and removing whitespace and asterisks and uppercasing.
+
+    >>> normalize_sequence("ACGT")
+    'ACGT'
+
+    >>> normalize_sequence("  A C G T * ")
+    'ACGT'
+
+    >>> normalize_sequence("ACGT1")
+    Traceback (most recent call last):
+    ...
+    RuntimeError: Normalized sequence contains non-alphabetic characters
+
+    """
+
+    nseq = re.sub(r"[\s\*]", "", seq).upper()
+    m = re.search("[^A-Z]", nseq)
+    if m:
+        _logger.debug("Original sequence: " + seq)
+        _logger.debug("Normalized sequence: " + nseq)
+        _logger.debug("First non-[A-Z] at {}".format(m.start()))
+        raise RuntimeError("Normalized sequence contains non-alphabetic characters")
+    return nseq
+
+
+def reverse_complement(seq):
+    """return the reverse complement of a sequence
+
+    >>> reverse_complement("ATCG")
+    'CGAT'
+
+    >>> reverse_complement(None)
+
+    """
+    if seq is None:
+        return None
+    return "".join(reversed(complement(seq)))
+
+
+def replace_t_to_u(seq):
+    """
+    >>> replace_t_to_u("ACGT")
+    'ACGU'
+
+    >>> replace_t_to_u(None)
+
+    """
+    if seq is None:
+        return None
+    return seq.replace("T", "U").replace("t", "u")
+
+
+def replace_u_to_t(seq):
+    """
+    >>> replace_u_to_t("ACGU")
+    'ACGT'
+
+    >>> replace_u_to_t(None)
+
+    """
+    if seq is None:
+        return None
+    return seq.replace("U", "T").replace("u", "t")
+
+
+def translate_cds(seq, full_codons=True, ter_symbol="*"):
+    """translate a DNA or RNA sequence into a single-letter amino acid sequence
+    using the standard translation table
+
+    If full_codons is True, a sequence whose length isn't a multiple of three
+    generates a ValueError; else an 'X' will be added as the last amino acid.
+    This matches biopython's behaviour when padding the last codon with 'N's.
+
+    >>> translate_cds("ATGCGA")
+    'MR'
+
+    >>> translate_cds("AUGCGA")
+    'MR'
+
+    >>> translate_cds(None)
+
+
+    >>> translate_cds("")
+    ''
+
+    >>> translate_cds("AUGCG")
+    Traceback (most recent call last):
+    ...
+    ValueError: Sequence length must be a multiple of three
+
+    >>> translate_cds("AUGCG", full_codons=False)
+    'M*'
+
+    >>> translate_cds("AUGCGQ")
+    Traceback (most recent call last):
+    ...
+    ValueError: Codon CGQ at position 4..6 is undefined in codon table
+
+    """
+    if seq is None:
+        return None
+
+    if len(seq) == 0:
+        return ""
+
+    if full_codons and len(seq) % 3 != 0:
+        raise ValueError("Sequence length must be a multiple of three")
+
+    seq = replace_u_to_t(seq)
+    seq = seq.upper()
+
+    protein_seq = list()
+    for i in range(0, len(seq) - len(seq) % 3, 3):
+        try:
+            aa = dna_to_aa1_lut[seq[i:i + 3]]
+        except KeyError:
+            raise ValueError("Codon {} at position {}..{} is undefined in codon table".format(
+                seq[i:i + 3], i+1, i+3))
+        protein_seq.append(aa)
+
+    # check for trailing bases and add the ter symbol if required
+    if not full_codons and len(seq) % 3 != 0:
+        protein_seq.append(ter_symbol)
+
+    return ''.join(protein_seq)
+
+
+## <LICENSE>
+## Copyright 2014 Bioutils Contributors
+## 
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+## 
+##     http://www.apache.org/licenses/LICENSE-2.0
+## 
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+## </LICENSE>
+
