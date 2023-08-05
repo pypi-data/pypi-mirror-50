eca2829a7f3a284f7373dd94f624b65e06c6321f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:42 +0000

    Add package franklab-msdrift/0.1.1.dev0/franklab_msdrift-0.1.1.dev0.tar.gz

---
+++ packages/franklab-msdrift/franklab_msdrift-0.1.1.dev0.tar.gz/franklab_msdrift-0.1.1.dev0/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+import subprocess
+
+symlink_cmd = 'ml-link-python-module franklab_msdrift $(ml-config package_directory)/franklab_msdrift'.split()
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+
+    def run(self):
+        subprocess.run(symlink_cmd, shell=True)
+        develop.run(self)
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def run(self):
+        subprocess.run(symlink_cmd, shell=True)
+        install.run(self)
+
+
+INSTALL_REQUIRES = ['mountainlab>=0.15', 'numpy', 'scikit-learn', 'scipy']
+TESTS_REQUIRE = []
+
+setup(
+    name='franklab_msdrift',
+    version='0.1.1.dev0',
+    license='',
+    description=(''),
+    author='',
+    author_email='',
+    url='',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
+)
