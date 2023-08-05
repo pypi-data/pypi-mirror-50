a668eb2786de7bd06ad541d705fd38cb95eed3a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:58 +0000

    Add package odoo10-addon-sale-commission-areamanager/10.0.1.1.0.99.dev4/odoo10_addon_sale_commission_areamanager-10.0.1.1.0.99.dev4-py2-none-any.whl

---
+++ packages/odoo10-addon-sale-commission-areamanager/odoo10_addon_sale_commission_areamanager-10.0.1.1.0.99.dev4-py2-none-any.whl/odoo/addons/sale_commission_areamanager/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Nicola Malcontenti - Agile Business Group
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Sales commission Area Manager',
+    'version': '10.0.1.1.0',
+    'author': 'Agile Business Group, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/commission',
+    'category': "Sales Management",
+    'license': 'LGPL-3',
+    'depends': ['sale_commission'],
+    'data': ["views/res_partner_view.xml"],
+    'installable': True
+}
