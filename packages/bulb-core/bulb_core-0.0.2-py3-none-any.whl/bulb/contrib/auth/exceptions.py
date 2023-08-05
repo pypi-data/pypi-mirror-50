fe35ffab41682246002830cb51fd40d803686242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:12 +0000

    Add package bulb-core/0.0.2/bulb_core-0.0.2-py3-none-any.whl

---
+++ packages/bulb-core/bulb_core-0.0.2-py3-none-any.whl/bulb/contrib/auth/exceptions.py
@@ -0,0 +1,43 @@
+class BULBPermissionDoesNotExist(Exception):
+    pass
+
+
+class BULBGroupDoesNotExist(Exception):
+    pass
+
+
+class BULBUserWarning(Warning):  # Useful
+    pass
+
+
+class BULBAnonymousUserWarning(Warning):  # Useful
+    pass
+
+
+class BULBPermissionWarning(Warning):  # Useful
+    pass
+
+
+class BULBGroupWarning(Warning):  # Useful
+    pass
+
+
+class BULBUserPropertyWarning(Warning):
+    pass
+
+
+class BULBPermissionPropertyWarning(Warning):
+    pass
+
+
+class BULBGroupPropertyWarning(Warning):
+    pass
+
+
+class BULBGetUserError(Exception):
+    pass
+
+
+class BULBLoginError(Exception):
+    pass
+
