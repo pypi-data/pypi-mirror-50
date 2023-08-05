4a102392df1ff561a51d3db6cbdedd8e7c0484ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:39 +0000

    Add package zlairflow/4.0.23/zlairflow-4.0.23-py3-none-any.whl

---
+++ packages/zlairflow/zlairflow-4.0.23-py3-none-any.whl/zlairflow/dag/data/data_zljianzhu.py
@@ -0,0 +1,22 @@
+para=[
+
+["zljianzhu_qiyexx_0",'minutes=240', ''],
+["zljianzhu_qiyexx_1",'minutes=240', ''],
+["zljianzhu_qiyexx_2",'minutes=240', ''],
+["zljianzhu_qiyexx_3",'minutes=240', ''],
+["zljianzhu_qiyexx_4",'minutes=240', ''],
+["zljianzhu_renyuanxx_0",'minutes=240', ''],
+["zljianzhu_renyuanxx_1",'minutes=240', ''],
+["zljianzhu_renyuanxx_2",'minutes=240', ''],
+["zljianzhu_renyuanxx_3",'minutes=240', ''],
+["zljianzhu_renyuanxx_4",'minutes=240', ''],
+["zljianzhu_xiangmuxx_0",'minutes=240', ''],
+["zljianzhu_xiangmuxx_1",'minutes=240', ''],
+["zljianzhu_xiangmuxx_2",'minutes=240', ''],
+["zljianzhu_xiangmuxx_3",'minutes=240', ''],
+["zljianzhu_xiangmuxx_4",'minutes=240', ''],
+["zljianzhu_gg",'minutes=240', ''],
+
+
+]
+
