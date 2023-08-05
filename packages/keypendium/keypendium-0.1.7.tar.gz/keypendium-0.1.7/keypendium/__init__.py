946d17a9d90821d78343044e2cda4026ad6d51bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:29 +0000

    Add package keypendium/0.1.7/keypendium-0.1.7.tar.gz

---
+++ packages/keypendium/keypendium-0.1.7.tar.gz/keypendium-0.1.7/keypendium/__init__.py
@@ -0,0 +1,10 @@
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+from Card import Card
+from Deck import Deck
+from FAQ import FAQ
+from Tag import Tag
+
+from keypendium import KeyPendium
