23f799bcc7cffbd7da4fe9889cd5ee787c2e6ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:48 +0000

    Add package liboidcagent/0.1.7/liboidcagent-0.1.7-py2-none-any.whl

---
+++ packages/liboidcagent/liboidcagent-0.1.7-py2-none-any.whl/liboidcagent/__init__.py
@@ -0,0 +1,7 @@
+"""liboidc-agent - A python library for requesting OpenID Connect access tokens from oidc-agent."""
+
+__version__ = '0.1.7'
+__author__ = 'Gabriel Zachmann'
+__author_email__ = 'oidc-agent-contact@lists.kit.edu'
+__all__ = []
+
