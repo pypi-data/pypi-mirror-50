faa81c3299dcb1b2a6558fb0edf4e92d4932903b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:34 +0000

    Add package autolens/0.25.6/autolens-0.25.6.tar.gz

---
+++ packages/autolens/autolens-0.25.6.tar.gz/autolens-0.25.6/autolens/pipeline/pipeline.py
@@ -0,0 +1,33 @@
+import autofit as af
+from autolens import exc
+
+
+class PipelineImaging(af.Pipeline):
+    def __init__(self, pipeline_name, *phases, hyper_mode=False):
+
+        super(PipelineImaging, self).__init__(pipeline_name, *phases)
+
+        self.hyper_mode = hyper_mode
+
+    def run(self, data, mask=None, positions=None, data_name=None):
+
+        if self.hyper_mode and mask is None:
+            raise exc.PhaseException(
+                "The pipeline is running in hyper mode, but has not received an input mask. Add"
+                "a mask to the run function of the pipeline (e.g. pipeline.run(data=data, mask=mask)"
+            )
+
+        def runner(phase, results):
+            return phase.run(data=data, results=results, mask=mask, positions=positions)
+
+        return self.run_function(runner, data_name)
+
+
+class PipelinePositions(af.Pipeline):
+    def run(self, positions, pixel_scale):
+        def runner(phase, results):
+            return phase.run(
+                positions=positions, pixel_scale=pixel_scale, results=results
+            )
+
+        return self.run_function(runner)
