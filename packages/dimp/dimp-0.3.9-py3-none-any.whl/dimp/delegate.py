77d1a3f29aa6c61abd662a17d2f2c10c5d6bc8f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:05 +0000

    Add package dimp/0.3.9/dimp-0.3.9-py3-none-any.whl

---
+++ packages/dimp/dimp-0.3.9-py3-none-any.whl/dimp/delegate.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+#
+#   DIMP : Decentralized Instant Messaging Protocol
+#
+#                                Written in 2019 by Moky <albert.moky@gmail.com>
+#
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2019 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+from abc import ABCMeta, abstractmethod
+
+from dkd import InstantMessage
+
+"""
+    Delegates
+    ~~~~~~~~~
+"""
+
+
+class ICallback(metaclass=ABCMeta):
+
+    def finished(self, result, error):
+        pass
+
+
+class ICompletionHandler(metaclass=ABCMeta):
+
+    def success(self):
+        pass
+
+    def failed(self, error):
+        pass
+
+
+class ITransceiverDelegate(metaclass=ABCMeta):
+
+    @abstractmethod
+    def send_package(self, data: bytes, handler: ICompletionHandler) -> bool:
+        """
+        Send out a data package onto network
+
+        :param data:    package data
+        :param handler: completion handler
+        :return:        False on data/delegate error
+        """
+        pass
+
+    @abstractmethod
+    def upload_data(self, data: bytes, msg: InstantMessage) -> str:
+        """
+        Upload encrypted data to CDN
+
+        :param data: encrypted file data
+        :param msg:  instant message
+        :return:     download URL
+        """
+        pass
+
+    @abstractmethod
+    def download_data(self, url: str, msg: InstantMessage) -> bytes:
+        """
+        Download encrypted data from CDN, and decrypt it when finished
+
+        :param url: download URL
+        :param msg: instant message
+        :return:    encrypted file data
+        """
+        pass
