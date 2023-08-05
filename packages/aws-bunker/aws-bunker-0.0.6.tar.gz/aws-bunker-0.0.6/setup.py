461eb9bd027040b0f2bac01a49bca29575c99dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:24 +0000

    Add package aws-bunker/0.0.6/aws-bunker-0.0.6.tar.gz

---
+++ packages/aws-bunker/aws-bunker-0.0.6.tar.gz/aws-bunker-0.0.6/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aws-bunker",
+    version="0.0.6",
+    author="Andy Klier, Gus Clemens",
+    author_email="andyklier@gmail.com",
+    description="bunker is a command line program for creating a dev/backup ec2 in AWS. It will install software, and clone your git repos, then it will transfer ignored files from your machine to the ec2.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/shindagger/bunker",
+    py_modules=["bunker.init", "bunker.install"],
+    packages=['bunker'],
+    include_package_data=True,
+    install_requires= ['setuptools'],
+    entry_points = {
+        'console_scripts': ['bunker=bunker.bunker:bunker'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
