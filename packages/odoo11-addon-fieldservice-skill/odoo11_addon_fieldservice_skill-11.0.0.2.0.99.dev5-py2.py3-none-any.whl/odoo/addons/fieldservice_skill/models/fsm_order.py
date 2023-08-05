486abe277818bdc68dad73a17e71a8f4743c79e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:40 +0000

    Add package odoo11-addon-fieldservice-skill/11.0.0.2.0.99.dev5/odoo11_addon_fieldservice_skill-11.0.0.2.0.99.dev5-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-fieldservice-skill/odoo11_addon_fieldservice_skill-11.0.0.2.0.99.dev5-py2.py3-none-any.whl/odoo/addons/fieldservice_skill/models/fsm_order.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields
+
+from odoo.addons.base_geoengine import geo_model
+
+
+class FSMOrder(geo_model.GeoModel):
+    _inherit = 'fsm.order'
+
+    skill_ids = fields.Many2many('hr.skill', string="Required Skills")
+
+    @api.onchange('category_ids')
+    def _onchange_category_ids(self):
+        if not self.template_id:
+            skill_ids = []
+            for category in self.category_ids:
+                skill_ids.extend([skill.id for skill in category.skill_ids])
+            self.skill_ids = [(6, 0, skill_ids)]
+
+    @api.onchange('template_id')
+    def _onchange_template_id(self):
+        if self.template_id:
+            super(FSMOrder, self)._onchange_template_id()
+            self.skill_ids = self.template_id.skill_ids
