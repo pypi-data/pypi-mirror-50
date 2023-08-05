3fc3f07ea07925e9984fdb544f53c1014a304f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:00 +0000

    Add package selenium-testing-module/1.0.1/selenium_testing_module-1.0.1.tar.gz

---
+++ packages/selenium-testing-module/selenium_testing_module-1.0.1.tar.gz/selenium_testing_module-1.0.1/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+setup(
+    name='selenium_testing_module',
+    packages=['selenium_testing_module'],
+    version='1.0.1',
+    license='MIT',
+    description='This is a module which can be used with Selenium to make testing scripts easier to read and write. The module automates some hectic parts of testing such as browser initiation and styles command line output for better, more understandable test results.',
+    author='Navaz Alani',
+    author_email='nalani@uwaterloo.ca',
+    url='https://github.com/navaz-alani/selenium-testing-module/',
+    download_url='https://github.com/navaz-alani/selenium-testing-module/archive/v1.0_Beta.tar.gz',
+    keywords=[
+        'testing',
+        'selenium',
+        'webdriver',
+        'automation'
+    ],
+    install_requires=[
+        'selenium',
+        'webdrivermanager',
+        'colorama'
+    ],
+    classifiers=[
+        # "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
