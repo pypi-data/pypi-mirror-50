cdba4db7b8e5ad9b6b9d9e6c4e47ddb44635dd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:52 +0000

    Add package genepattern-python/1.4.4/genepattern-python-1.4.4.tar.gz

---
+++ packages/genepattern-python/genepattern-python-1.4.4.tar.gz/genepattern-python-1.4.4/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+
+# Read version and other metadata from file
+__version__ = '1.4.4'
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='genepattern-python',
+    packages=['gp'],
+    version=__version__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description='Library for programmatically interacting with GenePattern from Python.',
+    author='Thorin Tabor',
+    author_email='tmtabor@cloud.ucsd.edu',
+    url='https://github.com/genepattern/genepattern-python',
+    download_url='https://github.com/genepattern/genepattern-python/archive/' + __version__ + '.tar.gz',
+    keywords=['genepattern', 'genomics', 'bioinformatics'],
+    license='BSD'
+)
