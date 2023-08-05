6f2f7d9e49eca7a644fdc32f4e7e57e6d117d007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:08 +0000

    Add package ot-probability/0.1/ot_probability-0.1.tar.gz

---
+++ packages/ot-probability/ot_probability-0.1.tar.gz/ot_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ot_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ot_probability'],
+      zip_safe=False)
