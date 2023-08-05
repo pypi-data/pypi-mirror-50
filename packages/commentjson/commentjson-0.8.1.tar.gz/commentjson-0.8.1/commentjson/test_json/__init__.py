52ea35ae318fc651bf39845c2022499963781ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:03 +0000

    Add package commentjson/0.8.1/commentjson-0.8.1.tar.gz

---
+++ packages/commentjson/commentjson-0.8.1.tar.gz/commentjson-0.8.1/commentjson/test_json/__init__.py
@@ -0,0 +1,61 @@
+import os
+import json
+import doctest
+import unittest
+
+from test import support
+
+import commentjson
+
+# import json with and without accelerations
+cjson = support.import_fresh_module('json', fresh=['_json'])
+pyjson = support.import_fresh_module('json', blocked=['_json'])
+# JSONDecodeError is cached inside the _json module
+cjson.JSONDecodeError = cjson.decoder.JSONDecodeError = json.JSONDecodeError
+
+# create two base classes that will be used by the other tests
+class PyTest(unittest.TestCase):
+    loads = staticmethod(commentjson.loads)
+    dumps = staticmethod(commentjson.dumps)
+    JSONDecodeError = staticmethod(pyjson.JSONDecodeError)
+
+class CommentJsonTest(unittest.TestCase):
+    loads = staticmethod(commentjson.loads)
+    dumps = staticmethod(commentjson.dumps)
+    JSONDecodeError = staticmethod(json.JSONDecodeError)
+
+@unittest.skipUnless(cjson, 'requires _json')
+class CTest(unittest.TestCase):
+    if cjson is not None:
+        loads = staticmethod(commentjson.loads)
+        dumps = staticmethod(commentjson.dumps)
+        JSONDecodeError = staticmethod(cjson.JSONDecodeError)
+
+# test PyTest and CTest checking if the functions come from the right module
+class TestPyTest(PyTest):
+    def test_pyjson(self):
+        self.assertEqual(self.json.scanner.make_scanner.__module__,
+                         'json.scanner')
+        self.assertEqual(self.json.decoder.scanstring.__module__,
+                         'json.decoder')
+        self.assertEqual(self.json.encoder.encode_basestring_ascii.__module__,
+                         'json.encoder')
+
+class TestCTest(CTest):
+    def test_cjson(self):
+        self.assertEqual(self.json.scanner.make_scanner.__module__, '_json')
+        self.assertEqual(self.json.decoder.scanstring.__module__, '_json')
+        self.assertEqual(self.json.encoder.c_make_encoder.__module__, '_json')
+        self.assertEqual(self.json.encoder.encode_basestring_ascii.__module__,
+                         '_json')
+
+
+def load_tests(loader, _, pattern):
+    suite = unittest.TestSuite()
+    for mod in (json, json.encoder, json.decoder):
+        suite.addTest(doctest.DocTestSuite(mod))
+    suite.addTest(TestPyTest('test_pyjson'))
+    suite.addTest(TestCTest('test_cjson'))
+
+    pkg_dir = os.path.dirname(__file__)
+    return support.load_package_tests(pkg_dir, loader, suite, pattern)
