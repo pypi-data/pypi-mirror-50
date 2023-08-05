c7140eca1bb465d31a8097f942016cc89573e930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:42 +0000

    Add package aws-cdk.aws-ecr-assets/1.3.0/aws-cdk.aws-ecr-assets-1.3.0.tar.gz

---
+++ packages/aws-cdk.aws-ecr-assets/aws-cdk.aws-ecr-assets-1.3.0.tar.gz/aws-cdk.aws-ecr-assets-1.3.0/setup.py
@@ -0,0 +1,50 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-ecr-assets",
+    "version": "1.3.0",
+    "description": "Docker image assets deployed to ECR",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_ecr_assets",
+        "aws_cdk.aws_ecr_assets._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_ecr_assets._jsii": [
+            "aws-ecr-assets@1.3.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_ecr_assets": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.14.3",
+        "publication>=0.0.3",
+        "aws-cdk.assets~=1.3,>=1.3.0",
+        "aws-cdk.aws-cloudformation~=1.3,>=1.3.0",
+        "aws-cdk.aws-ecr~=1.3,>=1.3.0",
+        "aws-cdk.aws-iam~=1.3,>=1.3.0",
+        "aws-cdk.aws-lambda~=1.3,>=1.3.0",
+        "aws-cdk.aws-s3~=1.3,>=1.3.0",
+        "aws-cdk.core~=1.3,>=1.3.0",
+        "aws-cdk.cx-api~=1.3,>=1.3.0"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
