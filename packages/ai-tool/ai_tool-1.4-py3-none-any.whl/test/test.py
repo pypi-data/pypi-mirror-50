3a1da370bdcd7c0a9498d3bb3f0caaf668d66dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:52 +0000

    Add package ai-tool/1.4/ai_tool-1.4-py3-none-any.whl

---
+++ packages/ai-tool/ai_tool-1.4-py3-none-any.whl/test/test.py
@@ -0,0 +1,23 @@
+# -*-coding:utf-8-*- 
+"""
+Author：yinshunyao
+Date:2019/7/31 7:50
+test
+"""
+import cv2
+import unittest
+
+
+class ImgTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.jpg = "test.jpg"
+
+    def test_slide_img(self):
+        from ai_tool.img_slide import yield_sub_img
+        # 使用切图功能
+        for bbox, sub_img in yield_sub_img(self.jpg, 0, 0, 180, 60):
+            clip = "-".join([str(x) for x in bbox])
+            print("slide:{}".format(clip))
+            cv2.imshow(clip, sub_img)
+            cv2.waitKey(0)
