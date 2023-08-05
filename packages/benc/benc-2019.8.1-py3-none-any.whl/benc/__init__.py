ce6cdef54504c0ef2ded34b7dd22b1953d0fbe07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:18 +0000

    Add package benc/2019.8.1/benc-2019.8.1-py3-none-any.whl

---
+++ packages/benc/benc-2019.8.1-py3-none-any.whl/benc/__init__.py
@@ -0,0 +1,9 @@
+from .benc import encode, decode
+
+VERSION = (2019, 8, 1, "")
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2018, Peter Tripp"
+__credits__ = ["Peter Tripp"]
+__email__ = "notpeter@notpeter.net"
+__version__ = "{0}.{1}.{2}{3}".format(*VERSION)
