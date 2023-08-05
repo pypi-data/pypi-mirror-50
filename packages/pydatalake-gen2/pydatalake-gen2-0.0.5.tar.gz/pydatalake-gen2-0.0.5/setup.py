eaafb2bb527afaf02325bde20a2b82f81285f1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:45 +0000

    Add package pydatalake-gen2/0.0.5/pydatalake-gen2-0.0.5.tar.gz

---
+++ packages/pydatalake-gen2/pydatalake-gen2-0.0.5.tar.gz/pydatalake-gen2-0.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements
+
+install_reqs = parse_requirements("requirements.txt", session="hack")
+
+version = "0.0.5"
+
+setup(
+    name="pydatalake-gen2",
+    description="REST API Client to Azure Data Lake Gen2",
+    author="Ivan Grunev",
+    author_email="ivan.grunev@gmail.com",
+    include_package_data=True,
+    version=version,
+    url="https://github.com/estatic/pydatalake-gen2",
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
