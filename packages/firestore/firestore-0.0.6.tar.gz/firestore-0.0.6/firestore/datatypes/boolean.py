76c6aaa287ae659613664164a1cc6b61f6099f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:57 +0000

    Add package firestore/0.0.6/firestore-0.0.6.tar.gz

---
+++ packages/firestore/firestore-0.0.6.tar.gz/firestore-0.0.6/firestore/datatypes/boolean.py
@@ -0,0 +1,13 @@
+from .datatype import Datatype
+
+
+class Boolean(Datatype):
+    """
+    Represents a boolean field in the firestore Document instance
+
+    .. py:function:: enumerate(sequence[, start=0])
+
+        Return an iterator that yields tubles of an index and an item of the
+        *sequence*. (And so on.)
+    """
+    pass
---
+++ packages/firestore/firestore-0.0.6.tar.gz/firestore-0.0.6/firestore/datatypes/reference.py
@@ -0,0 +1,8 @@
+from .datatype import Datatype
+
+
+class Reference(Datatype):
+    """Firestore referable elements i.e. Project ID,
+    Document ID etc.
+    """
+    pass
