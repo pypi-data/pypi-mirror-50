d06b2b209416a0222b087dda752a2b41e295540d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:39 +0000

    Add package stack-django-auditlog/0.4.6/stack-django-auditlog-0.4.6.tar.gz

---
+++ packages/stack-django-auditlog/stack-django-auditlog-0.4.6.tar.gz/stack-django-auditlog-0.4.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+setup(
+    name='stack-django-auditlog',
+    version='0.4.6',
+    packages=['auditlog', 'auditlog.migrations', 'auditlog.management', 'auditlog.management.commands'],
+    package_dir={'': 'src'},
+    url='https://github.com/jjkester/django-auditlog',
+    license='MIT',
+    author='Jan-Jelle Kester',
+    description='Audit log app for Django',
+
+    install_requires=[
+        'django-jsonfield>=1.0.0',
+        'python-dateutil>=2.6.0'
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
