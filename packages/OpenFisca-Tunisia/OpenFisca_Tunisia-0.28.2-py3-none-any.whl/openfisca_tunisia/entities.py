65896f4b3fb8cea74c35523c51f4730dd0ecd86a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:55 +0000

    Add package OpenFisca-Tunisia/0.28.2/OpenFisca_Tunisia-0.28.2-py3-none-any.whl

---
+++ packages/OpenFisca-Tunisia/OpenFisca_Tunisia-0.28.2-py3-none-any.whl/openfisca_tunisia/entities.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+
+from openfisca_core.entities import build_entity
+
+
+Individu = build_entity(
+    key = "individu",
+    plural = "individus",
+    label = u'Individu',
+    is_person = True
+    )
+
+FoyerFiscal = build_entity(
+    key = "foyer_fiscal",
+    plural = "foyers_fiscaux",
+    label = u'Déclaration d’impôts',
+    roles = [
+        {
+            'key': 'declarant',
+            'plural': 'declarants',
+            'label': u'Déclarants',
+            'subroles': ['declarant_principal', 'conjoint']
+            },
+        {
+            'key': 'personne_a_charge',
+            'plural': 'personnes_a_charge',
+            'label': u'Personnes à charge'
+            },
+        ]
+    )
+
+Menage = build_entity(
+    key = "menage",
+    plural = "menages",
+    label = u'Logement principal',
+    roles = [
+        {
+            'key': 'personne_de_reference',
+            'label': u'Personne de référence',
+            'max': 1
+            },
+        {
+            'key': 'conjoint',
+            'label': u'Conjoint',
+            'max': 1
+            },
+        {
+            'key': 'enfant',
+            'plural': 'enfants',
+            'label': u'Enfants',
+            'max': 2
+            },
+        {
+            'key': 'autre',
+            'plural': 'autres',
+            'label': u'Autres'
+            }
+        ]
+    )
+
+entities = [Individu, FoyerFiscal, Menage]
