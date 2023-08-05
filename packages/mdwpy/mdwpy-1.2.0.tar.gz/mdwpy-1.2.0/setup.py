d38b21876fc166b1c030f21ddd847ed13a52f261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:49 +0000

    Add package mdwpy/1.2.0/mdwpy-1.2.0.tar.gz

---
+++ packages/mdwpy/mdwpy-1.2.0.tar.gz/mdwpy-1.2.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+from setuptools import setup, find_packages
+ 
+import mdwpy
+ 
+setup(
+    name='mdwpy',
+    version='1.2.0',
+    packages=find_packages(),
+    author='Matthieu Bossennec',
+    author_email='mymail@protonmail.com',
+    description="Multi process downloader in python",
+    long_description=open('README.md').read(),
+    # install_requires= ,
+    include_package_data=True,
+    url='https://github.com/maazhe/mdwpy',
+    classifiers=[
+        "Natural Language :: French",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.5",
+        # "Topic :: Download",
+        # "Topic :: Python",
+        # "Topic :: Multiprocessing",
+        # "Topic :: Download fast",
+    ],
+    # entry_points = {
+    #     'console_scripts': [
+    #         'mdwpy = mdwpy.multi_process_downloader:Downloader',
+    #     ],
+    # },
+    license="BEERWARE"
+)
