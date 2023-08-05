9f26a428b7d43965b7424ff38ea630d19c4746d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:05 +0000

    Add package Simple-CLI/0.0.3/Simple-CLI-0.0.3.tar.gz

---
+++ packages/Simple-CLI/Simple-CLI-0.0.3.tar.gz/Simple-CLI-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Simple-CLI",
+    version="0.0.3",
+    author="Alex Manning",
+    author_email="alex.manning9904@gmail.com",
+    description="Python CLI Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alexmanning9904/simple-cli/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
