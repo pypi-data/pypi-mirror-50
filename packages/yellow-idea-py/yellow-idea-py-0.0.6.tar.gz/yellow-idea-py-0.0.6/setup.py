357ff9a3b6f2af2aeaad09a2da698671fa9cd810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:05 +0000

    Add package yellow-idea-py/0.0.6/yellow-idea-py-0.0.6.tar.gz

---
+++ packages/yellow-idea-py/yellow-idea-py-0.0.6.tar.gz/yellow-idea-py-0.0.6/setup.py
@@ -0,0 +1,20 @@
+# # https://packaging.python.org/tutorials/packaging-projects/
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yellow-idea-py",
+    version="0.0.6",
+    author="Kittiphong Jittanupagorn",
+    author_email="ragnarokmaster03@gmail.com",
+    description="Yellow Idea SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'mysql-connector'
+    ]
+)
