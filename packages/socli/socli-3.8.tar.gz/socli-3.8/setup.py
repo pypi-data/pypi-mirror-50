bb80d576c70cc19b36ce5533db9cbd10df98280c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:29 +0000

    Add package socli/3.8/socli-3.8.tar.gz

---
+++ packages/socli/socli-3.8.tar.gz/socli-3.8/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from codecs import open
+from sys import exit,version
+import sys
+if version < '1.0.0':
+    print("Python 1 is not supported...")
+    sys.exit(1)
+
+with open('README.rst') as f:
+    longd = f.read()
+
+setup(
+    name='socli',
+    include_package_data=True,
+    packages=["socli"],
+    data_files=[('socli', ['socli/user_agents.txt'])],
+    entry_points = {"console_scripts": ['socli = socli.socli:main']},
+    install_requires=['BeautifulSoup4','requests','colorama','Py-stackExchange', 'urwid'],
+    requires=['BeautifulSoup4','requests','colorama','PyStackExchange', 'urwid'],
+    version='3.8',
+    url='http://www.github.com/gautamkrishnar/socli',
+    keywords="stack overflow cli",
+    license='BSD',
+    author='Gautam krishna R',
+    author_email='r.gautamkrishna@gmail.com',
+    description='Stack overflow commnand line interface. SoCLI allows you to search and browse stack overflow from the terminal.',
+    long_description="\n\n"+longd
+    )
