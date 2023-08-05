0dc31066c8aa99f5546ab6d6bf0f081d95056078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:04 +0000

    Add package unscii/0.1.1/unscii-0.1.1-py3-none-any.whl

---
+++ packages/unscii/unscii-0.1.1-py3-none-any.whl/unscii/raw_unscii/__init__.py
@@ -0,0 +1,16 @@
+# File generate by unscii.py. Not intended to be read or modified by humans.
+
+raw_unscii_modules = ['unscii_16',
+ 'unscii_16_transposed',
+ 'unscii_8_alt',
+ 'unscii_8_alt_transposed',
+ 'unscii_8_fantasy',
+ 'unscii_8_fantasy_transposed',
+ 'unscii_8_mcr',
+ 'unscii_8_mcr_transposed',
+ 'unscii_8_tall',
+ 'unscii_8_tall_transposed',
+ 'unscii_8_thin',
+ 'unscii_8_thin_transposed',
+ 'unscii_8',
+ 'unscii_8_transposed']
