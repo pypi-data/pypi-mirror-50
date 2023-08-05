e7dab4d888c6d0545c9440aa8ee18b64b825cf51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:05 +0000

    Add package pypax/0.0.32/pypax-0.0.32-py3-none-any.whl

---
+++ packages/pypax/pypax-0.0.32-py3-none-any.whl/pypax/paxconfig.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+<Description>
+"""
+
+
+class PaxConfig:
+    def __init__(self):
+        self.api_endpoint = "None"
+        pass
+
+    def encode(self):
+        pass
+
+    def decode(self):
+        pass
