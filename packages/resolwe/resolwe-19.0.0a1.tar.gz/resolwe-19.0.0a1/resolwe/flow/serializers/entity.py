d494907faabd7d931e3c5641ff68cc964576f6bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:42 +0000

    Add package resolwe/19.0.0a1/resolwe-19.0.0a1.tar.gz

---
+++ packages/resolwe/resolwe-19.0.0a1.tar.gz/resolwe-19.0.0a1/resolwe/flow/serializers/entity.py
@@ -0,0 +1,27 @@
+"""Resolwe entity serializer."""
+from resolwe.flow.models import Collection, Entity
+
+from .collection import CollectionSerializer
+from .fields import DictRelatedField
+
+
+class EntitySerializer(CollectionSerializer):
+    """Serializer for Entity."""
+
+    collection = DictRelatedField(
+        queryset=Collection.objects.all(),
+        serializer=CollectionSerializer,
+        required=False,
+        write_permission='add',
+    )
+
+    class Meta(CollectionSerializer.Meta):
+        """EntitySerializer Meta options."""
+
+        model = Entity
+        fields = CollectionSerializer.Meta.fields + (
+            'collection',
+            'descriptor_completed',
+            'duplicated',
+            'type',
+        )
