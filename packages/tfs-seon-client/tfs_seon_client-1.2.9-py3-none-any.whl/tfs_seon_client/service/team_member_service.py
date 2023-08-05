43436bb258a2b545bd141c40333f28e4da027b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:19 +0000

    Add package tfs-seon-client/1.2.9/tfs_seon_client-1.2.9-py3-none-any.whl

---
+++ packages/tfs-seon-client/tfs_seon_client-1.2.9-py3-none-any.whl/tfs_seon_client/service/team_member_service.py
@@ -0,0 +1,11 @@
+import pika
+import uuid
+import os
+import json
+from pprint import pprint
+from .abstract_service import Abstract_Service
+
+class Team_Member_Service (Abstract_Service):
+    
+    def __init__(self):
+        Abstract_Service.__init__(self,'integration.tfs.team_member') 
