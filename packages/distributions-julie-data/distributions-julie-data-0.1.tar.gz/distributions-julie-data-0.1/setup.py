eb29b602ff2467f88fb1bf2094f374a973dce38d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:48 +0000

    Add package distributions-julie-data/0.1/distributions-julie-data-0.1.tar.gz

---
+++ packages/distributions-julie-data/distributions-julie-data-0.1.tar.gz/distributions-julie-data-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions-julie-data',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions-julie-data'],
+      author = 'Julie Trinco',
+      author_email = 'julie_trinco@hotmail.com',
+      zip_safe=False)
