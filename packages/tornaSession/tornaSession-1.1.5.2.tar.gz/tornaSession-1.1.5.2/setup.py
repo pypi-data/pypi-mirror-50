a8f25990186fb20d0e56299c1b00cd657ac6e4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:28 +0000

    Add package tornaSession/1.1.5.2/tornaSession-1.1.5.2.tar.gz

---
+++ packages/tornaSession/tornaSession-1.1.5.2.tar.gz/tornaSession-1.1.5.2/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright @ 2014 Mitchell Chu
+
+import io
+
+import torndsession
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as des_f:
+            buf.append(des_f.read())
+    return sep.join(buf)
+
+version = torndsession.version
+long_description = read("README.rst")
+
+setup(
+    name='tornaSession',
+    version=version,
+    description="Session extensions for Tornado",
+    long_description=long_description,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Internet :: WWW/HTTP :: Session',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords='torndsession tornado session redis memcached memory file python',
+    author="MitchellChu",
+    author_email="zxdjsj@126.com",
+    url="https://github.com/HQ1363/torndsession",
+    license="MIT",
+    packages=["torndsession"],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=['tornado', ],
+)
