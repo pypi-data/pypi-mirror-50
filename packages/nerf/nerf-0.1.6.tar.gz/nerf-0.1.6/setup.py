89400e31e7b35b629ff80bda9033456e333e8520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:02 +0000

    Add package nerf/0.1.6/nerf-0.1.6.tar.gz

---
+++ packages/nerf/nerf-0.1.6.tar.gz/nerf-0.1.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name="nerf",
+      version='0.1.6',
+      description='Second test version of NERF',
+      long_description='NERF is an interpretation tool for scikit-learn built random forest models'
+                       'NERF is designed to capture synergy effect between biological molecule.',
+      keywords= ("random forest", "interpretation"),
+      author="Yue Zhang",
+      author_email="yue.zhang@lih.lu",
+      packages=find_packages(),
+      include_package_data=True,
+      platforms="any",
+      install_requires=["numpy", "pandas", "networkx"]
+
+)
