3ba6bb2339308180af17cb1829482ee2cdde211f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:41 +0000

    Add package codepost/0.1.0/codepost-0.1.0.tar.gz

---
+++ packages/codepost/codepost-0.1.0.tar.gz/codepost-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from codepost import __version__
+from setuptools import setup, find_packages
+
+# The text of the README file
+README = open("README.md").read()
+
+# This call to setup() does all the work
+setup(
+    name="codepost",
+    version=__version__,
+    description="Python bindings for the codePost API",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/codepost-io/codepost-python",
+    author="codePost",
+    author_email="team@codepost.io",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "PyYAML"
+    ],
+    include_package_data=True,
+)
