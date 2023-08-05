6d361841417d5840a2366c04d7e23edc8c5a1b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:04 +0000

    Add package hidrokit/0.2.1/hidrokit-0.2.1.tar.gz

---
+++ packages/hidrokit/hidrokit-0.2.1.tar.gz/hidrokit-0.2.1/hidrokit/__init__.py
@@ -0,0 +1,9 @@
+"""hidrokit - analisis hidrologi dengan python
+"""
+
+__author__ = 'Taruma Sakti Megariansyah'
+__email__ = 'hi@taruma.info'
+
+from . import prep, analysis, viz
+
+__all__ = ['prep', 'analysis', 'viz', ]
