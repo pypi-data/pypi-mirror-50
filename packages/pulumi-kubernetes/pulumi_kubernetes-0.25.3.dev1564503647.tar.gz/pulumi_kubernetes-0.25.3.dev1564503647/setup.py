cef936e0997c510dd752e09805b1c2eac4b2fe43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:19 +0000

    Add package pulumi-kubernetes/0.25.3.dev1564503647/pulumi_kubernetes-0.25.3.dev1564503647.tar.gz

---
+++ packages/pulumi-kubernetes/pulumi_kubernetes-0.25.3.dev1564503647.tar.gz/pulumi_kubernetes-0.25.3.dev1564503647/setup.py
@@ -0,0 +1,39 @@
+from subprocess import check_call
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+class InstallPluginCommand(install):
+    def run(self):
+        install.run(self)
+        check_call(['pulumi', 'plugin', 'install', 'resource', 'kubernetes', 'v0.25.3-dev.1564503647+g29c5de9'])
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='pulumi_kubernetes',
+      version='0.25.3.dev1564503647',
+      description='A Pulumi package for creating and managing Kubernetes resources.',
+      long_description=readme(),
+      cmdclass={
+          'install': InstallPluginCommand,
+      },
+      keywords='pulumi kubernetes',
+      url='https://pulumi.io',
+      project_urls={
+          'Repository': 'https://github.com/pulumi/pulumi-kubernetes'
+      },
+      license='Apache-2.0',
+      packages=find_packages(),
+      install_requires=[
+          'pulumi>=0.17.18,<0.18.0',
+          'requests>=2.21.0,<2.22.0',
+          'pyyaml>=5.1,<5.2',
+          'semver>=2.8.1',
+          'parver>=0.2.1',
+      ],
+      zip_safe=False)
