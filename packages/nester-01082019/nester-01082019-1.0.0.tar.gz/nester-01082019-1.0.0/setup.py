16a5848bd83ccd001fe1f7a2c5f7f81428a20efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:01 +0000

    Add package nester-01082019/1.0.0/nester-01082019-1.0.0.tar.gz

---
+++ packages/nester-01082019/nester-01082019-1.0.0.tar.gz/nester-01082019-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'nester-01082019',
+    version         = '1.0.0',
+    py_modules      = ['nester-01082019'],
+    author          = 'Prakash Subramani',
+    author_email    = 'praka123@gmail.com',
+    url             = 'http://digitaldevops.co.uk',
+    description     = 'A Simper print of nested lists',
+)
