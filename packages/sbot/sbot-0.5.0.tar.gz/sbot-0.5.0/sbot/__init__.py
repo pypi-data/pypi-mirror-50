4fde9f6e0cc874be2d2dfa486714a4d28709c6bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:54 +0000

    Add package sbot/0.5.0/sbot-0.5.0.tar.gz

---
+++ packages/sbot/sbot-0.5.0.tar.gz/sbot-0.5.0/sbot/__init__.py
@@ -0,0 +1,22 @@
+"""SourceBots API."""
+
+from j5.components.gpio_pin import GPIOPinMode
+from j5.components.motor import MotorSpecialState
+from j5.components.piezo import Note
+
+from .logging import logger_setup
+from .robot import Robot, __version__
+
+logger_setup()
+
+COAST = MotorSpecialState.COAST
+BRAKE = MotorSpecialState.BRAKE
+
+__all__ = [
+    "__version__",
+    "BRAKE",
+    "COAST",
+    "GPIOPinMode",
+    "Note",
+    "Robot",
+]
