e4ac8c7e024cff07e4f83d20f9197343ad764d99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:34 +0000

    Add package tracepy/0.1.1/tracepy-0.1.1.tar.gz

---
+++ packages/tracepy/tracepy-0.1.1.tar.gz/tracepy-0.1.1/tracepy/__init__.py
@@ -0,0 +1,8 @@
+from .ray import ray
+from .geometry import geometry
+from .optimize import optimize
+from .geoplot import plotxz, plotyz, plot2d
+from .optplot import spotdiagram, plotobject, rayaberration
+from .iotables import *
+from .transforms import *
+from .raygroup import *
