d1ff06330e93eef26d0b6f78511042432b5de4d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:07 +0000

    Add package digitizer/0.0.3/digitizer-0.0.3.tar.gz

---
+++ packages/digitizer/digitizer-0.0.3.tar.gz/digitizer-0.0.3/digitizer/__init__.py
@@ -0,0 +1,1 @@
+from .parser import parseBinary as parse
---
+++ packages/digitizer/digitizer-0.0.3.tar.gz/digitizer-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="digitizer",
+    version="0.0.3",
+    author="William Wyatt",
+    author_email="wwyatt@ucsc.edu",
+    description="Paser for the caen DT5742",
+    long_description="Paser for the caen DT5742",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    url="https://github.com/Tsangares/",
+    include_package_data=True,
+    scripts=[
+    ],
+    install_requires=[
+        'numpy>=1.17.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
