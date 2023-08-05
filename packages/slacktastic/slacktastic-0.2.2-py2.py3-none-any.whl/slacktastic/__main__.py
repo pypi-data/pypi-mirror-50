6647c3d3c2e4f9c6aa2857f216573353d5cd5dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:22 +0000

    Add package slacktastic/0.2.2/slacktastic-0.2.2-py2.py3-none-any.whl

---
+++ packages/slacktastic/slacktastic-0.2.2-py2.py3-none-any.whl/slacktastic/__main__.py
@@ -0,0 +1,43 @@
+from slacktastic.client import SlackClient
+from slacktastic.template import (
+    Message, PieChart, BarChart, LineChart, RadarChart, DonutChart)
+
+client = SlackClient(
+    webhook_url='https://hooks.slack.com/services/'
+                'TBWAF1BH7/BLTGSMRNK/40X6YJP7czgUX4OgSfGvFlUR')
+
+test = PieChart(
+    "Test data", labels=['Ride', 'Reservation'], values=[22, 55])
+
+test_5 = DonutChart(
+    "Test data", labels=['Ride', 'Reservation'], values=[22, 55])
+
+test_2 = BarChart(
+    "Test data", labels=['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+    data={
+        'Test 1': [1, 2, 4, 8, 16],
+        'Test 2': [7, 3, 45, 1, 12],
+    })
+
+test_3 = LineChart(
+    "Test data", labels=['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+    data={
+        'Test 1': [1, 2, 4, 8, 16],
+        'Test 2': [7, 3, 45, 1, 12],
+    })
+
+test_4 = RadarChart(
+    "Test data", labels=['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+    data={
+        'Test 1': [1, 2, 4, 8, 16],
+        'Test 2': [7, 3, 45, 1, 12],
+    })
+
+message = Message(text='This is a *test*', attachments=[
+    test,
+    test_2,
+    test_3,
+    test_4,
+    test_5
+])
+client.send_message(message)
