0291e51239e2620613c5e230a615820faa2b422a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:36 +0000

    Add package hello-world-yaz/0.1.0/hello_world_yaz-0.1.0.tar.gz

---
+++ packages/hello-world-yaz/hello_world_yaz-0.1.0.tar.gz/hello_world_yaz-0.1.0/sayhelloworld.py
@@ -0,0 +1,6 @@
+def say_hello():
+    print("Hello world!")
+
+def say_hello_with_count(count):
+    for i in range(0,count):
+        print("Hello world")
---
+++ packages/hello-world-yaz/hello_world_yaz-0.1.0.tar.gz/hello_world_yaz-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='hello_world_yaz',
+    version='0.1.0',
+    packages=[''],
+    url='http://www.umutbayraktar.net/',
+    license='GPL',
+    author='umut bayraktar',
+    author_email='umt9735@gmail.com',
+    description='Say Hello World'
+)
