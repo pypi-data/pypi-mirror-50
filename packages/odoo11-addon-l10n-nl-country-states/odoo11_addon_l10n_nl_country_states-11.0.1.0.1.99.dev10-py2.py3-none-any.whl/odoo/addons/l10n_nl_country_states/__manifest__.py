cef2a045c81d60bc6fa8194e7b5a7dd8f2acb48e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:03 +0000

    Add package odoo11-addon-l10n-nl-country-states/11.0.1.0.1.99.dev10/odoo11_addon_l10n_nl_country_states-11.0.1.0.1.99.dev10-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-l10n-nl-country-states/odoo11_addon_l10n_nl_country_states-11.0.1.0.1.99.dev10-py2.py3-none-any.whl/odoo/addons/l10n_nl_country_states/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Dutch country states (Provincies)",
+    "version": "11.0.1.0.1",
+    "author": "Therp BV, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Localization",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/l10n_nl_country_states.xml",
+        "data/res_country_state_nl_zip.xml",
+    ],
+    "installable": True,
+}
---
+++ packages/odoo11-addon-l10n-nl-country-states/odoo11_addon_l10n_nl_country_states-11.0.1.0.1.99.dev10-py2.py3-none-any.whl/odoo/addons/l10n_nl_country_states/models/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2018 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import res_country_state_nl_zip
+from . import res_partner
