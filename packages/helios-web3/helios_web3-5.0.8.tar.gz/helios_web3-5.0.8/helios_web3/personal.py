da8877ee48106e1e3f961f4629e12081d0cd26bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:11 +0000

    Add package helios-web3/5.0.8/helios_web3-5.0.8.tar.gz

---
+++ packages/helios-web3/helios_web3-5.0.8.tar.gz/helios_web3-5.0.8/helios_web3/personal.py
@@ -0,0 +1,19 @@
+from web3.geth import GethPersonal
+from web3.method import (
+    Method,
+    default_root_munger,
+)
+
+class Personal(GethPersonal):
+    '''
+    Class to allow for overriding functions if necessary
+    '''
+    sendTransactions = Method(
+        "personal_sendTransactions",
+        mungers=[default_root_munger],
+    )
+
+    receiveTransactions = Method(
+        "personal_receiveTransactions",
+        mungers=[default_root_munger],
+    )
