886396e72a272b5c268f28250b681725d4fa54f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:09 +0000

    Add package osm-export-tool/0.0.2/osm_export_tool-0.0.2-py3.7.egg

---
+++ packages/osm-export-tool/osm_export_tool-0.0.2-py3.7.egg/osm_export_tool/cmd.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import sys
+import time
+import argparse
+from osm_export_tool import export
+from osm_export_tool.feature_selection import FeatureSelection
+
+def main():
+	parser = argparse.ArgumentParser(description='Export OSM data in other file formats.')
+	parser.add_argument('osm_file',  help='OSM .pbf or .xml input file')
+	parser.add_argument('output_name', help='Output prefix')
+	parser.add_argument('-f','--formats', dest='formats',default='gpkg',help='List of formats e.g. gpkg,shp,kml (default: gpkg)')
+	parser.add_argument('-m','--mapping', dest='mapping',help='YAML mapping of features schema. If not specified, a default is used.')
+	parser.add_argument('-v','--verbose', action='store_true')
+	parsed = parser.parse_args()
+	print(parsed.mapping)
+
+	formats = parsed.formats.split(',')
+	outputs = []
+	if 'gpkg' in formats:
+		outputs.append(export.Geopackage(parsed.output_name))
+	if 'shp' in formats:
+		outputs.append(export.Shapefile(parsed.output_name))
+	if 'kml' in formats:
+		outputs.append(export.Kml(parsed.output_name))
+	h = export.Handler(outputs)
+	start_time = time.time()
+	h.apply_file(parsed.osm_file, locations=True, idx='sparse_file_array')
+
+	for output in outputs:
+		output.finalize()
+	print(time.time() - start_time)
+
+if __name__ == '__main__':
+	main()
---
+++ packages/osm-export-tool/osm_export_tool-0.0.2-py3.7.egg/osm_export_tool/export.py
@@ -0,0 +1,122 @@
+import osmium as o
+try:
+    import ogr
+except ModuleNotFoundError:
+    print('ERROR: Install the version of python package GDAL that corresponds to gdalinfo --version on your system.')
+    exit(1)
+from base64 import b64decode
+from osm_export_tool import GeomType
+
+fab = o.geom.WKBFactory()
+create_geom = lambda b : ogr.CreateGeometryFromWkb(bytes.fromhex(b))
+
+class Shapefile:
+    class Layer:
+        def __init__(self,driver,name,ogr_geom_type):
+            self.cols = ['name']
+            self.ds = driver.CreateDataSource(name + '.shp')
+            self.ogr_layer = self.ds.CreateLayer('', None, ogr_geom_type)
+            field_name = ogr.FieldDefn('name', ogr.OFTString)
+            field_name.SetWidth(254)
+            self.ogr_layer.CreateField(field_name)
+            self.defn = self.ogr_layer.GetLayerDefn()
+
+    def __init__(self,output_name,mapping):
+        driver = ogr.GetDriverByName('ESRI Shapefile')
+
+        self.layers = {}
+        for theme in mapping.themes:
+            if 'points' in mapping.geom_types(theme):
+                self.layers[(theme,GeomType.POINT)] = Shapefile.Layer(driver,output_name + '_' + theme + '_points',ogr.wkbPoint)
+            if 'lines' in mapping.geom_types(theme):
+                self.layers[(theme,GeomType.LINE)] = Shapefile.Layer(driver,output_name + '_' + theme + '_lines',ogr.wkbLineString)
+            if 'polygons' in mapping.geom_types(theme):
+                self.layers[(theme,GeomType.POLYGON)] = Shapefile.Layer(driver,output_name + '_' + theme + '_polygons',ogr.wkbMultiPolygon)
+
+    def write(self,layer_name,geom_type,geom,tags):
+        layer = self.layers[(layer_name,geom_type)]
+        feature = ogr.Feature(layer.defn)
+        feature.SetGeometry(geom)
+        for col in layer.cols:
+            if col in tags:
+                feature.SetField(col,tags[col])
+        layer.ogr_layer.CreateFeature(feature)
+
+    def finalize(self):
+        pass
+
+class Geopackage:
+    class Layer:
+        def __init__(self,ds,name,ogr_geom_type):
+            self.cols = ['name']
+            self.ogr_layer = ds.CreateLayer(name, None, ogr_geom_type)
+            field_name = ogr.FieldDefn('name', ogr.OFTString)
+            field_name.SetWidth(254)
+            self.ogr_layer.CreateField(field_name)
+            self.defn = self.ogr_layer.GetLayerDefn()
+
+    def __init__(self,output_name,mapping):
+        driver = ogr.GetDriverByName('GPKG')
+        self.ds = driver.CreateDataSource(output_name + '.gpkg')
+        self.ds.StartTransaction()
+
+        self.layers = {}
+        for theme in mapping.themes:
+            if 'points' in mapping.geom_types(theme):
+                self.layers[(theme,GeomType.POINT)] = Geopackage.Layer(self.ds,theme + '_points',ogr.wkbPoint)
+            if 'lines' in mapping.geom_types(theme):
+                self.layers[(theme,GeomType.LINE)] = Geopackage.Layer(self.ds,theme + '_lines',ogr.wkbLineString)
+            if 'polygons' in mapping.geom_types(theme):
+                self.layers[(theme,GeomType.POLYGON)] = Geopackage.Layer(self.ds,theme + '_polygons',ogr.wkbMultiPolygon)
+
+    def write(self,layer_name,geom_type,geom,tags):
+        layer = self.layers[(layer_name,geom_type)]
+        feature = ogr.Feature(layer.defn)
+        feature.SetGeometry(geom)
+        for col in layer.cols:
+            if col in tags:
+                feature.SetField(col,tags[col])
+        layer.ogr_layer.CreateFeature(feature)
+
+    def finalize(self):
+        self.ds.CommitTransaction()
+        self.layers = None
+        self.ds = None
+
+class Handler(o.SimpleHandler):
+    def __init__(self,outputs,mapping):
+        super(Handler, self).__init__()
+        self.outputs = outputs
+        self.mapping = mapping
+
+    def node(self,n):
+        geom = None
+        for layer_name in self.mapping.match(n.tags,GeomType.POINT):
+            if not geom:
+                geom = create_geom(fab.create_point(n))
+            for output in self.outputs:
+                output.write(layer_name,GeomType.POINT,geom,n.tags)
+
+    def way(self, w):
+        try:
+            geom = None
+            for layer_name in self.mapping.match(w.tags,GeomType.LINE):
+                if not geom:
+                    geom = create_geom(fab.create_linestring(w))
+                for output in self.outputs:
+                    output.write(layer_name,GeomType.LINE,geom,w.tags)
+        except RuntimeError:
+            print("Incomplete way: {0}".format(w.id))
+
+    def area(self,a):
+        try:
+            geom = None
+            for layer_name in self.mapping.match(a.tags,GeomType.POLYGON):
+                if not geom:
+                    geom = create_geom(fab.create_multipolygon(a))
+                for output in self.outputs:
+                    output.write(layer_name,GeomType.POLYGON,geom,a.tags)
+        except RuntimeError:
+            print('Invalid area: {0}'.format(a.orig_id()))
+
+
