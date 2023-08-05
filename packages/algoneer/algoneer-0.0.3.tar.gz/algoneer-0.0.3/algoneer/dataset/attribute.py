4f8df323a9cfbec010c8b6c16bf7c9375479b030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:56 +0000

    Add package algoneer/0.0.3/algoneer-0.0.3.tar.gz

---
+++ packages/algoneer/algoneer-0.0.3.tar.gz/algoneer-0.0.3/algoneer/dataset/attribute.py
@@ -0,0 +1,55 @@
+import algoneer.dataset
+
+from typing import Optional, Iterable
+
+from ..dataschema import AttributeSchema
+
+import abc
+
+
+class Attribute(abc.ABC):
+    @abc.abstractmethod
+    def __init__(
+        self,
+        dataset: "algoneer.dataset.DataSet",
+        column: str,
+        schema: Optional[AttributeSchema],
+    ) -> None:
+        pass
+
+    def __getattr__(self, attr):
+        if attr.startswith("is_"):
+            _type = attr[3:]
+            if self.schema is not None and self.schema.type.name.lower() == _type:
+                return True
+            return False
+        return super().__getattr__(attr)
+
+    @property  # type: ignore
+    @abc.abstractmethod
+    def column(self) -> str:
+        pass
+
+    @column.setter  # type: ignore
+    @abc.abstractmethod
+    def column(self, column: str) -> None:
+        pass
+
+    @property  # type: ignore
+    @abc.abstractmethod
+    def roles(self) -> Iterable[str]:
+        pass
+
+    @property  # type: ignore
+    @abc.abstractmethod
+    def schema(self) -> Optional[AttributeSchema]:
+        pass
+
+    @schema.setter  # type: ignore
+    @abc.abstractmethod
+    def schema(self, schema: Optional[AttributeSchema]) -> None:
+        pass
+
+    @abc.abstractmethod
+    def __len__(self) -> int:
+        pass
