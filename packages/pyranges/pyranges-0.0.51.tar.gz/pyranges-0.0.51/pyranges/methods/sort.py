082b17c7e1cb2eb124b223c24f723948119bfd35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:47 +0000

    Add package pyranges/0.0.51/pyranges-0.0.51.tar.gz

---
+++ packages/pyranges/pyranges-0.0.51.tar.gz/pyranges-0.0.51/pyranges/methods/sort.py
@@ -0,0 +1,21 @@
+def _sort(df, kwargs):
+
+    if "by" in kwargs:
+        by = kwargs["by"]
+        # print("by " * 100)
+        # print(by)
+        return df.sort_values(by)
+    else:
+        # print("else " * 100)
+        # print(df)
+        df = sort_one_by_one(df, "Start", "End")
+        return df
+
+
+def sort_one_by_one(d, col1, col2):
+    """
+    Equivalent to pd.sort_values(by=[col1, col2]), but faster.
+    """
+
+    d = d.sort_values(by=[col2])
+    return d.sort_values(by=[col1], kind='mergesort')
