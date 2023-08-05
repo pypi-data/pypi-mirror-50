97297ad42b53dccd13d35df289b7aef6c432879d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:26 +0000

    Add package wisdoms/2.0.5/wisdoms-2.0.5.tar.gz

---
+++ packages/wisdoms/wisdoms-2.0.5.tar.gz/wisdoms-2.0.5/setup.py
@@ -0,0 +1,25 @@
+# Created by Q-ays.
+# whosqays@gmail.com
+
+
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wisdoms",
+    version="2.0.5",
+    author="tzxd group",
+    author_email="whosqays@qq.com",
+    description="添加异常处理父类-add parent of exception class",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/soft-project/wisdom-lib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
