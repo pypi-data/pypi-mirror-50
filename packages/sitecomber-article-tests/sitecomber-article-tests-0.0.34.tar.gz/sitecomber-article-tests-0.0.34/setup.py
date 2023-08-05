f1293f0d31bc02f413a09193895f8d59c3210656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:07 +0000

    Add package sitecomber-article-tests/0.0.34/sitecomber-article-tests-0.0.34.tar.gz

---
+++ packages/sitecomber-article-tests/sitecomber-article-tests-0.0.34.tar.gz/sitecomber-article-tests-0.0.34/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+# this is a test
+setup(name='sitecomber-article-tests',
+      description='Article related tests for Sitecomber',
+      version='0.0.34',
+      url='https://github.com/ninapavlich/sitecomber-article-tests',
+      author='Nina Pavlich',
+      author_email='nina@ninalp.com',
+      license='MIT',
+      packages=find_packages(),
+      package_data={'sitecomber_article_tests': ['*.py', '*.html', '*.css', '*.js', '*.jpg', '*.png']},
+      include_package_data=True,
+      install_requires=['newspaper3k', 'readtime', 'pyspellchecker', 'textsearch', 'contractions'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python'
+      ]
+      )
