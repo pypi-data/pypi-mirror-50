20eddc76abf14d701014ebc47e7de3b804198363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package dopetools/0.1.0/dopetools-0.1.0.tar.gz

---
+++ packages/dopetools/dopetools-0.1.0.tar.gz/dopetools-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import os
+
+
+version = '0.1.0'
+
+setup(
+    name='dopetools',
+    version=version,
+    description='Useful tools for the DOPE project.',
+    author='Badruu',
+    author_email='badrus@propulsionacademy.com',
+    license='MIT',
+    packages=['dopetools'],
+    zip_safe=False
+)
