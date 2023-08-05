79cfcff5a911857fd8f39f18a395bbbef69d0c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:26 +0000

    Add package swh.web/0.0.204/swh.web-0.0.204.tar.gz

---
+++ packages/swh.web/swh.web-0.0.204.tar.gz/swh.web-0.0.204/swh/web/browse/urls.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2017-2019  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU Affero General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+from django.conf.urls import url
+from django.shortcuts import render
+
+import swh.web.browse.views.directory # noqa
+import swh.web.browse.views.content # noqa
+import swh.web.browse.views.origin_save # noqa
+import swh.web.browse.views.origin # noqa
+import swh.web.browse.views.person # noqa
+import swh.web.browse.views.release # noqa
+import swh.web.browse.views.revision # noqa
+import swh.web.browse.views.snapshot # noqa
+
+from swh.web.browse.browseurls import BrowseUrls
+from swh.web.browse.identifiers import swh_id_browse
+
+
+def _browse_help_view(request):
+    return render(request, 'browse/help.html',
+                  {'heading': 'How to browse the archive ?'})
+
+
+def _browse_search_view(request):
+    return render(request, 'browse/search.html',
+                  {'heading': 'Search software origins to browse'})
+
+
+def _browse_vault_view(request):
+    return render(request, 'browse/vault-ui.html',
+                  {'heading': 'Download archive content from the Vault'})
+
+
+def _browse_origin_save_view(request):
+    return render(request, 'browse/origin-save.html',
+                  {'heading': 'Request the saving of a software origin into the archive'}) # noqa
+
+
+urlpatterns = [
+    url(r'^$', _browse_search_view),
+    url(r'^help/$', _browse_help_view, name='browse-help'),
+    url(r'^search/$', _browse_search_view, name='browse-search'),
+    url(r'^vault/$', _browse_vault_view, name='browse-vault'),
+    url(r'^origin/save/$', _browse_origin_save_view,
+        name='browse-origin-save'),
+    # for backward compatibility
+    url(r'^(?P<swh_id>swh:[0-9]+:[a-z]+:[0-9a-f]+.*)/$', swh_id_browse)
+]
+
+urlpatterns += BrowseUrls.get_url_patterns()
---
+++ packages/swh.web/swh.web-0.0.204.tar.gz/swh.web-0.0.204/swh/web/urls.py
@@ -0,0 +1,74 @@
+# Copyright (C) 2017-2019  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU Affero General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import json
+
+from django.conf import settings
+from django.conf.urls import (
+    url, include, handler400, handler403, handler404, handler500
+)
+from django.contrib.staticfiles import finders
+from django.contrib.staticfiles.views import serve
+from django.shortcuts import render
+from django.views.generic.base import RedirectView
+
+from django_js_reverse.views import urls_js
+
+from swh.web.browse.identifiers import swh_id_browse
+from swh.web.config import get_config
+from swh.web.common.exc import (
+    swh_handle400, swh_handle403, swh_handle404, swh_handle500
+)
+from swh.web.misc.coverage import swh_coverage
+
+swh_web_config = get_config()
+
+favicon_view = RedirectView.as_view(url='/static/img/icons/swh-logo-32x32.png',
+                                    permanent=True)
+
+
+def default_view(request):
+    return render(request, "homepage.html")
+
+
+def jslicenses(request):
+    jslicenses_file = finders.find('jssources/jslicenses.json')
+    jslicenses_data = json.load(open(jslicenses_file))
+    jslicenses_data = sorted(jslicenses_data.items(),
+                             key=lambda item: item[0].split('/')[-1])
+    return render(request, "jslicenses.html",
+                  {'jslicenses_data': jslicenses_data})
+
+
+urlpatterns = [
+    url(r'^admin/', include('swh.web.admin.urls')),
+    url(r'^favicon\.ico$', favicon_view),
+    url(r'^api/', include('swh.web.api.urls')),
+    url(r'^browse/', include('swh.web.browse.urls')),
+    url(r'^$', default_view, name='swh-web-homepage'),
+    url(r'^jsreverse/$', urls_js, name='js_reverse'),
+    url(r'^(?P<swh_id>swh:[0-9]+:[a-z]+:[0-9a-f]+.*)/$',
+        swh_id_browse, name='browse-swh-id'),
+    url(r'^coverage/$', swh_coverage, name='swh-coverage'),
+    url(r'^jslicenses/$', jslicenses, name='jslicenses'),
+]
+
+
+# allow to serve assets through django staticfiles
+# even if settings.DEBUG is False
+def insecure_serve(request, path, **kwargs):
+    return serve(request, path, insecure=True, **kwargs)
+
+
+# enable to serve compressed assets through django development server
+if swh_web_config['serve_assets']:
+    static_pattern = r'^%s(?P<path>.*)$' % settings.STATIC_URL[1:]
+    urlpatterns.append(url(static_pattern, insecure_serve))
+
+
+handler400 = swh_handle400 # noqa
+handler403 = swh_handle403 # noqa
+handler404 = swh_handle404 # noqa
+handler500 = swh_handle500 # noqa
