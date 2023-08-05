bc4de87e3793d2fa90ecf953851fc9b39c8f83f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:36 +0000

    Add package express-py/2.5.0/express-py-2.5.0.tar.gz

---
+++ packages/express-py/express-py-2.5.0.tar.gz/express-py-2.5.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='express-py',
+    version='2.5.0',
+    description='Exabyte Property Ex(ss)tractor, Sourcer, Serializer class.',
+    url='https://github.com/Exabyte-io/exabyte-express',
+    author='Exabyte Inc.',
+    author_email='info@exabyte.io',
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    install_requires=[
+        "mock==1.3.0",
+        "bunch==1.0.1",
+        "numpy==1.16.4",
+        "xmltodict==0.9.2",
+        "pymatgen==2018.5.3",
+        "ase==3.17.0",
+        "esse==2.2.0",
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: Apache Software License'
+    ]
+)
