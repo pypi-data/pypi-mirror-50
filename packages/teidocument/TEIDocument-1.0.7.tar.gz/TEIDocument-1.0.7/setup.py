97d12bb5407d757d905239b088b96a3d7c169d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:26 +0000

    Add package teidocument/1.0.7/TEIDocument-1.0.7.tar.gz

---
+++ packages/teidocument/TEIDocument-1.0.7.tar.gz/TEIDocument-1.0.7/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['teidocument']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['lxml>=4.3,<5.0', 'xmltodict>=0.12.0,<0.13.0']
+
+setup_kwargs = {
+    'name': 'teidocument',
+    'version': '1.0.7',
+    'description': 'Extract text and metadata from TEI-documents',
+    'long_description': None,
+    'author': 'proprefenetre',
+    'author_email': 'proprefenetre@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
