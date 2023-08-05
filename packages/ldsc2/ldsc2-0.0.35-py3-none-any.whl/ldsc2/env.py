da0654a0b57a7fd9e5ebda628ddc07292b87f4e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:22 +0000

    Add package ldsc2/0.0.35/ldsc2-0.0.35-py3-none-any.whl

---
+++ packages/ldsc2/ldsc2-0.0.35-py3-none-any.whl/ldsc2/env.py
@@ -0,0 +1,45 @@
+#===============================================================================
+# env.py
+#===============================================================================
+
+# Imports ======================================================================
+
+import os
+import os.path
+
+
+
+
+# Constants ====================================================================
+
+DIR = os.environ.get(
+    'LDSC2_DIR',
+    os.path.dirname(__file__)
+)
+ANACONDA_DIR = os.environ.get(
+    'LDSC2_ANACONDA_DIR',
+    os.path.join(os.path.dirname(__file__), 'anaconda2')
+)
+ANACONDA_PATH = os.path.join(ANACONDA_DIR, 'envs', 'ldsc', 'bin', 'python')
+HAPMAP3_SNPS = os.environ.get(
+    'LDSC2_HAPMAP3_SNPS',
+    os.path.join(DIR, 'hapmap3_snps')
+)
+PLINKFILES = os.environ.get(
+    'LDSC2_PLINKFILES',
+    os.path.join(DIR, '1000G_EUR_Phase3_plink')
+)
+PLINKFILES_EAS = os.environ.get(
+    'LDSC2_PLINKFILES_EAS',
+    os.path.join(DIR, '1000G_Phase3_EAS_plinkfiles')
+)
+BASELINE = os.environ.get(
+    'LDSC2_BASELINE',
+    os.path.join(DIR, '1000G_Phase3_baseline')
+)
+BASELINE_EAS = os.environ.get(
+    'LDSC2_BASELINE_EAS',
+    os.path.join(DIR, '1000G_Phase3_EAS_baseline')
+)
+SNPS = os.environ.get('LDSC2_SNPS', os.path.join(DIR, 'snps'))
+SNPS_EAS = os.environ.get('LDSC2_SNPS_EAS', os.path.join(DIR, 'snps_eas'))
