77d9e3a8107863d204c3fa48b35c6c4bcedd26b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:24 +0000

    Add package popt/0.4/popt-0.4.tar.gz

---
+++ packages/popt/popt-0.4.tar.gz/popt-0.4/popt/popt.py
@@ -0,0 +1,167 @@
+from __future__ import print_function
+import subprocess
+import pkg_resources
+from xml.etree import cElementTree as ET
+from datetime import datetime
+from argparse import ArgumentParser
+
+
+def in_plain_text(filename, skip_timestamps=False, width=None):
+    converter = RobotXmlToTextConverter(skip_timestamps)
+    converter.set_width(width)
+
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    return converter.convert(root)
+
+
+class RobotXmlToTextConverter(object):
+
+    _SKIPPED_ELEMENTS = ('doc', 'status', 'arguments', 'tags')
+
+    def __init__(self, skip_timestamps=False):
+        self._width = self._get_default_width()
+        self._timestamp_formatter = EmptyTimestampFormatter() if skip_timestamps else TimestampFormatter()
+
+    def _get_default_width(self):
+        try:
+            _, width = subprocess.check_output('stty size', stderr=subprocess.STDOUT, shell=True).split()
+            width = int(width)
+        except:
+            width = 120
+        return width
+
+    def skip_timestamps(self):
+        self._timestamp_formatter = EmptyTimestampFormatter()
+
+    def set_width(self, width):
+        if width:
+            self._width = width
+
+    def convert(self, root):
+        result = self.print_double_line()
+        result += self.print_children(root, 0)
+        result += self.print_double_line()
+        return result
+
+    def print_line(self):
+        return '{}\n'.format('-' * self._width)
+
+    def print_double_line(self):
+        return '{}\n'.format('=' * self._width)
+
+    def print_children(self, element, indent):
+        if element.tag == 'statistics':
+            return ''
+        result = self.print_element(element, indent)
+        for child in element:
+            result += self.print_children(child, indent + 2)
+        return result
+
+    def print_element(self, element, indent):
+        return {'robot': self.print_robot,
+                'kw': self.print_kw,
+                'test': self.print_test,
+                'suite': self.print_suite,
+                'msg': self.print_msg,
+                'arg': self.print_arg,
+                'tag': self.print_tag}.get(element.tag, self.print_generic_element)(element, indent)
+
+    def print_robot(self, element, indent):
+        result = u''
+        for key, value in element.attrib.iteritems():
+            result += u'{}{}: {}\n'.format(' ' * indent, key, value)
+        return result
+
+    def print_msg(self, element, indent):
+        timestamp = self._timestamp_formatter.msg(element)
+        level = element.get('level')
+        text = self.indent_lines(element.text, indent + len(timestamp) + 5 + 2)
+        return u'{:>{indent}}{:<5}  {}\n'.format(timestamp, level, text, indent=indent + len(timestamp))
+
+    def indent_lines(self, text, indent):
+        indent_spaces = ' ' * indent
+        return indent_spaces.join(line for line in text.splitlines(True))
+
+    def print_kw(self, element, indent):
+        name = element.get('name')
+        library = element.get('library', '')
+        name = u'{}.{}'.format(library, name) if library else name
+        return self.print_suite_test_kw(name, element, indent)
+
+    def print_suite_test_kw(self, name, element, indent):
+        len_of_first_part = indent + len(name)
+        status = element.find('status')
+        padding = ' ' * (self._width - 26 - len_of_first_part)
+        return u'{:>{indent}}{}{}  {}\n'.format(name, padding,
+                                                status.get('status'), self._timestamp_formatter.ts_and_duration(status),
+                                                indent=indent + len(name))
+
+    def print_test(self, element, indent):
+        result = self.print_line()
+        result += self.print_suite_test_kw(element.get('name'), element, indent)
+        return result
+
+    def print_suite(self, element, indent):
+        result = self.print_double_line()
+        result += self.print_suite_test_kw(element.get('name'), element, indent)
+        return result
+
+    def print_arg(self, element, indent):
+        return self.print_text_element(element, indent, 'arg')
+
+    def print_tag(self, element, indent):
+        return self.print_text_element(element, indent, 'tag')
+
+    def print_text_element(self, element, indent, element_name):
+        return u'{:>{indent}} {}\n'.format(element_name + ':', element.text, indent=(indent + len(element_name + ':')))
+
+    def print_generic_element(self, element, indent):
+        text = element.text.strip() if element.text is not None else ''
+        if element.tag in self._SKIPPED_ELEMENTS:
+            return ''
+        return u'{:>{indent}} {} {}\n'.format(element.tag, element.attrib, text,
+                                              indent=indent + len(element.tag))
+
+
+class TimestampFormatter(object):
+
+    def msg(self, element):
+        return element.get('timestamp').split()[-1] + '  '
+
+    def ts_and_duration(self, status):
+        starttime = status.get('starttime')
+        endtime = status.get('endtime')
+        start_dt = datetime.strptime(starttime + '000', '%Y%m%d %H:%M:%S.%f')
+        end_dt = datetime.strptime(endtime + '000', '%Y%m%d %H:%M:%S.%f')
+        duration = end_dt - start_dt
+        return u'{}  {:0>2}.{:0<3}'.format(starttime.split()[-1],
+                                           duration.seconds, duration.microseconds / 1000)
+
+
+class EmptyTimestampFormatter(object):
+
+    def msg(self, element):
+        return ''
+
+    def ts_and_duration(self, status):
+        return ''
+
+
+def get_version():
+    return pkg_resources.get_distribution('popt').version
+
+
+def read_arguments():
+    p = ArgumentParser(description='Convert Robot Framework output.xml to human-readable textual log')
+    p.add_argument('filename', type=str, help='Path to output.xml file')
+    p.add_argument('--skip-timestamps', '-T', action='store_true', help='Omit all timestamps from textual log (helps in diffing logs)')
+    p.add_argument('--width', type=int, help='Display width in characters. Default is screen width or 120.')
+    p.add_argument('--version', '-v', action='version', help='Print version', version=get_version())
+    args = p.parse_args()
+
+    print(in_plain_text(args.filename, skip_timestamps=args.skip_timestamps, width=args.width))
+
+
+if __name__ == '__main__':
+    read_arguments()
---
+++ packages/popt/popt-0.4.tar.gz/popt-0.4/test/test_popt.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+import unittest
+import os
+import difflib
+import xml.etree.cElementTree as ET
+import popt
+import subprocess
+from mock import patch
+
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+GOLDEN_TXT = os.path.join(THIS_DIR, 'golden.txt')
+GOLDEN_XML = os.path.join(THIS_DIR, 'golden.xml')
+
+
+class _WithPrintouts(object):
+
+    def assert_printout(self, actual, expected):
+        if(actual != expected):
+            differences = list(difflib.Differ().compare(expected.splitlines(True), actual.splitlines(True)))
+            self.fail(u'\n' + u''.join(differences))
+
+    def assert_element(self, xml, text):
+        t = ET.fromstring(xml)
+        self.assert_printout(self.converter.print_children(t, 0), text)
+
+
+class TestGoldenPopt(unittest.TestCase, _WithPrintouts):
+
+    def test_golden_basic(self):
+        result = popt.in_plain_text(GOLDEN_XML)
+        expected = open(GOLDEN_TXT).read()
+        self.assert_printout(result.strip(), expected.strip())
+
+
+class TestPopt(unittest.TestCase, _WithPrintouts):
+
+    def setUp(self):
+        self.converter = popt.RobotXmlToTextConverter()
+
+    def test_robot(self):
+        self.assert_element('''<robot generated="20160105 13:37:33.973" generator="Robot 3.0 (Python 2.7.6 on linux2)">
+</robot>''', '''\
+generated: 20160105 13:37:33.973
+generator: Robot 3.0 (Python 2.7.6 on linux2)
+''')
+
+    def test_msg_with_indenting(self):
+        self.assert_element('''<msg timestamp="20160105 13:37:34.031" level="INFO">The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+</msg>''', '''\
+13:37:34.031  INFO   The Zen of Python, by Tim Peters
+                     
+                     Beautiful is better than ugly.
+                     Explicit is better than implicit.
+
+''')
+
+    def test_msg_with_unicode(self):
+            self.assert_element(u'''<msg timestamp="20160105 13:37:34.031" level="INFO">Süper</msg>'''.encode("utf8"),
+                                u'''\
+13:37:34.031  INFO   Süper
+''')
+
+    def test_basic_keyword(self):
+        self.assert_element('''<kw name="Log" library="BuiltIn">
+<doc>Logs the given message with the given level.</doc>
+<arguments>
+<arg>We are doing some strange setup actions here!</arg>
+</arguments>
+<msg timestamp="20160105 13:37:34.030" level="INFO">We are doing some strange setup actions here!</msg>
+<status status="PASS" endtime="20160105 13:37:34.030" starttime="20160105 13:37:34.030"></status>
+</kw>''', '''\
+BuiltIn.Log                                                                                   PASS  13:37:34.030  00.000
+    arg: We are doing some strange setup actions here!
+  13:37:34.030  INFO   We are doing some strange setup actions here!
+''')
+
+    def test_complex_keyword(self):
+        self.assert_element('''<kw name="Test 2 keyword 1">
+<doc>Arguments are really ignored. I do not know why :-).</doc>
+<arguments>
+<arg>foo</arg>
+<arg>bar</arg>
+<arg>dii</arg>
+<arg>daa</arg>
+</arguments>
+<assign>
+<var>${foo}</var>
+</assign>
+<kw name="Log" library="BuiltIn">
+<doc>Logs the given message with the given level.</doc>
+<arguments>
+<arg>Test 2 keyword 1</arg>
+</arguments>
+<msg timestamp="20160105 13:37:34.034" level="INFO">Test 2 keyword 1</msg>
+<status status="PASS" endtime="20160105 13:37:34.034" starttime="20160105 13:37:34.034"></status>
+</kw>
+<msg timestamp="20160105 13:37:34.034" level="INFO">${foo} = foo</msg>
+<status status="PASS" endtime="20160105 13:37:34.034" starttime="20160105 13:37:34.033"></status>
+</kw>
+''', '''\
+Test 2 keyword 1                                                                              PASS  13:37:34.033  00.100
+    arg: foo
+    arg: bar
+    arg: dii
+    arg: daa
+  assign {} 
+    var {} ${foo}
+  BuiltIn.Log                                                                                 PASS  13:37:34.034  00.000
+      arg: Test 2 keyword 1
+    13:37:34.034  INFO   Test 2 keyword 1
+  13:37:34.034  INFO   ${foo} = foo
+''')
+
+    def test_test(self):
+        self.assert_element('''<test id="s1-t1" name="Simple test">
+<kw name="Log" library="BuiltIn">
+<doc>Logs the given message with the given level.</doc>
+<arguments>
+<arg>hello</arg>
+</arguments>
+<msg timestamp="20160107 10:52:37.429" level="INFO">hello</msg>
+<status status="PASS" endtime="20160107 10:52:37.429" starttime="20160107 10:52:37.429"></status>
+</kw>
+<status status="PASS" endtime="20160107 10:52:37.429" critical="yes" starttime="20160107 10:52:37.429"></status>
+</test>
+''', '''\
+------------------------------------------------------------------------------------------------------------------------
+Simple test                                                                                   PASS  10:52:37.429  00.000
+  BuiltIn.Log                                                                                 PASS  10:52:37.429  00.000
+      arg: hello
+    10:52:37.429  INFO   hello
+''')
+
+    def test_suite(self):
+        self.assert_element('''<suite source="/home/jkohvakk/Documents/popt/simple_suite.robot" id="s1" name="Simple Suite">
+<test id="s1-t1" name="Simple test">
+<status status="PASS" endtime="20160107 10:52:37.429" critical="yes" starttime="20160107 10:52:37.429"></status>
+</test>
+<status status="PASS" endtime="20160107 10:52:37.430" starttime="20160107 10:52:37.414"></status>
+</suite>
+''', '''\
+========================================================================================================================
+Simple Suite                                                                                  PASS  10:52:37.414  00.160
+------------------------------------------------------------------------------------------------------------------------
+  Simple test                                                                                 PASS  10:52:37.429  00.000
+''')
+
+    def test_tag(self):
+        self.assert_element('''<tags>
+<tag>Feature1</tag>
+<tag>Feature2</tag>
+</tags>
+''', '''\
+  tag: Feature1
+  tag: Feature2
+''')
+
+    def test_setting_width(self):
+        self.converter.set_width(60)
+        self.assert_element('''<test id="s1-t1" name="Simple test">
+<status status="PASS" endtime="20160107 10:52:37.429" critical="yes" starttime="20160107 10:52:37.429"></status>
+</test>
+''', '''\
+------------------------------------------------------------
+Simple test                       PASS  10:52:37.429  00.000
+''')
+
+    def test_skip_timestamps(self):
+        self.converter.skip_timestamps()
+        self.assert_element('''<kw name="Log" library="BuiltIn">
+<doc>Logs the given message with the given level.</doc>
+<arguments>
+<arg>We are doing some strange setup actions here!</arg>
+</arguments>
+<msg timestamp="20160105 13:37:34.030" level="INFO">We are doing some strange setup actions here!</msg>
+<status status="PASS" endtime="20160105 13:37:34.030" starttime="20160105 13:37:34.030"></status>
+</kw>''', '''\
+BuiltIn.Log                                                                                   PASS  
+    arg: We are doing some strange setup actions here!
+  INFO   We are doing some strange setup actions here!
+''')
+
+    @patch('popt.subprocess.check_output')
+    def test_default_width_is_by_screen_size_on_linux(self, check_output_mock):
+        check_output_mock.return_value = '40 79'
+        c = popt.RobotXmlToTextConverter()
+        self.assertEqual(c._width, 79)
+        check_output_mock.assert_called_once_with('stty size', stderr=subprocess.STDOUT, shell=True)
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
