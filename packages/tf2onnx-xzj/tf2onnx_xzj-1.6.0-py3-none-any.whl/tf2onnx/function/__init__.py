a21797ad3c7221f5fbc7691f7ad587950c53023c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:44 +0000

    Add package tf2onnx-xzj/1.6.0/tf2onnx_xzj-1.6.0-py3-none-any.whl

---
+++ packages/tf2onnx-xzj/tf2onnx_xzj-1.6.0-py3-none-any.whl/tf2onnx/function/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+"""tf2onnx.function module"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from .gathernd import gathernd_op
+from .range import range_op7
+from .select import select_op8
+from .sparse_softmax_cross_entropy_with_logits import sparse_softmax_cross_entropy_with_logits_op
+
+__all__ = ["gathernd_op", "range_op7", "select_op8", "sparse_softmax_cross_entropy_with_logits_op"]
---
+++ packages/tf2onnx-xzj/tf2onnx_xzj-1.6.0-py3-none-any.whl/tf2onnx/version.py
@@ -0,0 +1,3 @@
+
+version = '1.6.0'
+git_version = 'b0d59eb85b437fba0eaff9ebe20f3676f6dd198c'
