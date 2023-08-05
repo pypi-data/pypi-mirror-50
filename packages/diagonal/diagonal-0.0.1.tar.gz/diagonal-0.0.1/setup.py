256f1fbe683e328de37358c1ca6960a98e450fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:45 +0000

    Add package diagonal/0.0.1/diagonal-0.0.1.tar.gz

---
+++ packages/diagonal/diagonal-0.0.1.tar.gz/diagonal-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name="diagonal",
+    version="0.0.1",
+    author="opxyc",
+    #author_email="xxxxxxxxxxxxx@gmail.com",
+    description="A small collection of functions to get various diagonals of a 2d matrix",
+    #url="https://github.com/pypa/example-project",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
