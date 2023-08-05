6b40bf49572182ad9d6d0d89960a298bb62f3d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:04 +0000

    Add package mangum/0.6.12/mangum-0.6.12.tar.gz

---
+++ packages/mangum/mangum-0.6.12.tar.gz/mangum-0.6.12/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+from mangum.__version__ import __version__
+
+
+def get_long_description():
+    return open("README.md", "r", encoding="utf8").read()
+
+
+setup(
+    name="mangum",
+    version=__version__,
+    packages=find_packages(),
+    license="MIT",
+    url="https://github.com/erm/mangum",
+    description="Serverless ASGI",
+    long_description=get_long_description(),
+    entry_points={"console_scripts": ["mangum = mangum.__main__:main"]},
+    long_description_content_type="text/markdown",
+    author="Jordan Eremieff",
+    author_email="jordan@eremieff.com",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
