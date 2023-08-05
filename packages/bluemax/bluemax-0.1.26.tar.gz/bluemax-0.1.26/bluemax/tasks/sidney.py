a4a9056968f00248ae75838e89b07eb5bf45f1c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:02 +0000

    Add package bluemax/0.1.26/bluemax-0.1.26.tar.gz

---
+++ packages/bluemax/bluemax-0.1.26.tar.gz/bluemax-0.1.26/bluemax/tasks/sidney.py
@@ -0,0 +1,12 @@
+from invoke import task
+from . import create
+
+
+@task
+def sidney(ctx, name):
+    """ all the jazz - creates everything."""
+    from bluemax.sa import tasks as database
+
+    create.project(ctx, name, with_config=True)
+    create.tests(ctx, name)
+    database.create(ctx, name)
