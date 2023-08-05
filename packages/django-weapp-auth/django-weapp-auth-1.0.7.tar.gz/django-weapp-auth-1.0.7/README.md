143fd741116780dbb0180ef2fe662dcc6e352b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:39 +0000

    Add package django-weapp-auth/1.0.7/django-weapp-auth-1.0.7.tar.gz

---
+++ packages/django-weapp-auth/django-weapp-auth-1.0.7.tar.gz/django-weapp-auth-1.0.7/README.md
@@ -0,0 +1,26 @@
+# Weapp Auth
+
+Weapp auth is a Django app that helps to handle wechat mini program login and user info updating logic.
+
+Quick start
+-----------
+0. Install using pip `pip install --upgrade django-weapp-auth`
+
+1. Add "weapp_auth" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = [
+        ...
+        'weapp_auth',
+    ]
+
+2. Include the weapp_auth URLconf in your project urls.py like this::
+
+    path('weapp_auth/', include('weapp_auth.urls')),
+
+3. Run `python manage.py migrate` to create the models.
+
+4. These two APIs are avaliable! `weapp_auth/wechatLogin/` and `weapp_auth/wechatUpdateUserInfo/`
+
+5. Call these two APIs in your wechat mini app!
+
+6. Authenticate your request with `from weapp_auth.authenticator.OpenIdAuthenticator import OpenIdAuthenticator`
---
+++ packages/django-weapp-auth/django-weapp-auth-1.0.7.tar.gz/django-weapp-auth-1.0.7/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-weapp-auth',
+    version='1.0.7',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',
+    description='A helper Django app that handles wechat mini program login and user info updating logic',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://blog.mimimi.fun/',
+    author='Meowzz95',
+    author_email='info@mimimi.fun',
+    install_requires=[
+          'requests',
+          'pycrypto'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
