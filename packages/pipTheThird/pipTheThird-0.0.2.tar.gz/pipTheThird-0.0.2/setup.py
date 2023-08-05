35f1cc58269b2ef8728d206b98c4fe3065167271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:19 +0000

    Add package pipTheThird/0.0.2/pipTheThird-0.0.2.tar.gz

---
+++ packages/pipTheThird/pipTheThird-0.0.2.tar.gz/pipTheThird-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pipTheThird",
+    version="0.0.2",
+    author="Sean",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SeanSRice/SeansPipTest.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
