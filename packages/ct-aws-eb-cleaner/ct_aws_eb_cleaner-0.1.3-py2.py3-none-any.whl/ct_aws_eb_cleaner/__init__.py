0252d28f088fff07a038658bd07c45da1ee1e9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:28 +0000

    Add package ct-aws-eb-cleaner/0.1.3/ct_aws_eb_cleaner-0.1.3-py2.py3-none-any.whl

---
+++ packages/ct-aws-eb-cleaner/ct_aws_eb_cleaner-0.1.3-py2.py3-none-any.whl/ct_aws_eb_cleaner/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Cinnecta - AWS Elastic Beanstalk Cleaner."""
+from .eb_cleaner import run
+
+__author__ = """Cinnecta"""
+__email__ = 'cinnecta.dev@gmail.com'
+__version__ = '0.1.3'
+
+
+__all__ = [
+    'run'
+]
+
+
+def main():
+    run()
