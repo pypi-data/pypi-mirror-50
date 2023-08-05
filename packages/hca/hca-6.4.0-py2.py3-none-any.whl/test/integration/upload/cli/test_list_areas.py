74157fee1df668111537080ecc89d2a4153604d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:46 +0000

    Add package hca/6.4.0/hca-6.4.0-py2.py3-none-any.whl

---
+++ packages/hca/hca-6.4.0-py2.py3-none-any.whl/test/integration/upload/cli/test_list_areas.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import sys
+import unittest
+from argparse import Namespace
+
+import six
+
+pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..'))  # noqa
+sys.path.insert(0, pkg_root)  # noqa
+
+import hca
+from hca.upload.cli.list_areas_command import ListAreasCommand
+from test import CapturingIO
+
+
+class TestUploadCliListAreasCommand(unittest.TestCase):
+
+    def test_it_lists_areas_when_there_are_some(self):
+        a_uuid = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
+        b_uuid = 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'
+        config = hca.get_config()
+        config.upload = {
+            'areas': {
+                a_uuid: {'uri': "s3://foo/{}/".format(a_uuid)},
+                b_uuid: {'uri': "s3://foo/{}/".format(b_uuid)},
+            },
+            'current_area': a_uuid
+        }
+        config.save()
+
+        with CapturingIO('stdout') as stdout:
+            ListAreasCommand(Namespace())
+
+        six.assertRegex(self, stdout.captured(), "%s <- selected" % a_uuid)
+        six.assertRegex(self, stdout.captured(), b_uuid)
+
+
+if __name__ == "__main__":
+    unittest.main()
