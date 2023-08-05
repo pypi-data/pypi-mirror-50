6d9db416061823669a80b8eee8fda908580570a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:27 +0000

    Add package PyLg/1.3.3/PyLg-1.3.3.tar.gz

---
+++ packages/PyLg/PyLg-1.3.3.tar.gz/PyLg-1.3.3/CHANGES.rst
@@ -0,0 +1,62 @@
+Changelog
+=========
+
+1.3.3
+-----
+
+- Fix python 3 issue with basestring type
+
+1.3.2
+-----
+
+- Update e-mail address.
+
+1.3.1
+-----
+
+- Fixed README.
+
+1.3.0
+-----
+
+- Added option to trace return value type.
+- Added options for tracing exception tracebacks.
+- Made all exception options possible to set individually for functions.
+- Added support for multi-line trace messages.
+
+1.2.2
+-----
+
+- Modified message wrapping code.
+
+1.2.1
+-----
+
+- Dummy implementation used when PyLg is disabled has been simplified.
+
+1.2.0
+-----
+
+- Fixed bug that didn't preserve exception backtrace correctly.
+
+- Function ``trace`` now automatically converts a message to a string.
+
+- Added several new options that can now be set in
+  ``pylg_settings.py``, see the README for details.
+
+- Improved dummy implementation for the case when PyLg is disabled.
+
+- User settings provided in ``pylg_settings.py`` are now checked for
+  errors. In the case of a failed import, PyLg will set all settings
+  to defaults. In case of an invalid individual value, only the
+  relevant setting will be reset to its default.
+
+1.1.0
+-----
+
+- PyLg can now be installed with pip.
+
+1.0.0
+-----
+
+- Initial PyLg version.
---
+++ packages/PyLg/PyLg-1.3.3.tar.gz/PyLg-1.3.3/pylg/dummy.py
@@ -0,0 +1,84 @@
+# -----------------------------------------------------------------------------
+# PyLg: module to facilitate and automate the process of writing runtime logs.
+# Copyright (C) 2017 Wojciech Kozlowski <wk@wojciechkozlowski.eu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
+
+from functools import partial
+from .pylg import TraceFunction
+
+
+class TraceFunctionDummy(TraceFunction):
+
+    """ Dummy implementation of TraceFunction.
+    """
+
+    def __init__(self, *args, **kwargs):
+
+        """ Constructor for dummy TraceFunction.
+        """
+
+        # ---------------------------------------------------------------------
+        # Rather than having an entirely empty dummy class, we call
+        # the superclass constructor to have input verification even
+        # when PyLg is disabled.
+        # ---------------------------------------------------------------------
+        super(TraceFunctionDummy, self).__init__(*args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+
+        """ The actual wrapper that is called when a call to a
+            decorated function is made. It also handles extra
+            initialisation when parameters are passed to
+            TraceFunction.
+
+            :return: The return value of the decorated function.
+        """
+
+        if self.function is None:
+            # -----------------------------------------------------------------
+            # For an explanation of the logic here, see the non-dummy
+            # implementations in pylg.py.
+            # -----------------------------------------------------------------
+            self.init_function(*args, **kwargs)
+            return self
+
+        # ---------------------------------------------------------------------
+        # The actual decorating. The dummy implementation doesn't do anything.
+        # ---------------------------------------------------------------------
+        return self.function(*args, **kwargs)
+
+    def init_function(self, *args, **kwargs):
+
+        """ Function to initialise the TraceFunctionStruct kept by the
+            decorator.
+        """
+
+        # ---------------------------------------------------------------------
+        # This function should only ever be called with one parameter
+        # - the function to be decorated. These checks are done here,
+        # rather than by the caller, as anything that calls this
+        # function should also have been called with the decorated
+        # function as its only parameter.
+        # ---------------------------------------------------------------------
+        assert not kwargs
+        assert len(args) == 1
+        assert callable(args[0])
+
+        self.function = args[0]
+
+
+def trace(message, function=None):
+    pass
