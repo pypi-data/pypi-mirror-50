dec36cf08e8a87d12aae4965199b8ec9fb3c7833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:15 +0000

    Add package Flask-RESTy-Tenants/0.7.3/Flask-RESTy-Tenants-0.7.3.tar.gz

---
+++ packages/Flask-RESTy-Tenants/Flask-RESTy-Tenants-0.7.3.tar.gz/Flask-RESTy-Tenants-0.7.3/setup.py
@@ -0,0 +1,58 @@
+from setuptools import Command, setup, find_packages
+import subprocess
+
+# -----------------------------------------------------------------------------
+
+
+def system(command):
+    class SystemCommand(Command):
+        user_options = []
+
+        def initialize_options(self):
+            pass
+
+        def finalize_options(self):
+            pass
+
+        def run(self):
+            subprocess.check_call(command, shell=True)
+
+    return SystemCommand
+
+
+# -----------------------------------------------------------------------------
+
+setup(
+    name="Flask-RESTy-Tenants",
+    version='0.7.3',
+    description='Flask Resty Authorization module for multitenancy',
+    url='https://github.com/4Catalyzer/flask-resty-tenants',
+    author='Giacomo Tagliabue',
+    author_email='giacomo.tag@gmail.com',
+    license='MIT',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Framework :: Flask',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ),
+    keywords='rest flask multitenancy',
+    packages=find_packages(exclude=('tests',)),
+    install_requires=(
+        'Flask >= 0.10',
+        'Flask-RESTy >= 0.13.0',
+        'SQLAlchemy >= 1.0.0',
+    ),
+    cmdclass={
+        'clean': system('rm -rf build dist *.egg-info'),
+        'package': system('python setup.py sdist bdist_wheel'),
+        'publish': system('twine upload dist/*'),
+        'release': system('python setup.py clean package publish'),
+        'test': system('tox'),
+    },
+)
