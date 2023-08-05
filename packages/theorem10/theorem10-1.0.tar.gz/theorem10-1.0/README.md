afe77c04c1af7ff36690575d2dc087e6e6533e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:23 +0000

    Add package theorem10/1.0/theorem10-1.0.tar.gz

---
+++ packages/theorem10/theorem10-1.0.tar.gz/theorem10-1.0/README.md
@@ -0,0 +1,20 @@
+# Theorem10 [A theorem in Mathematics]
+This repository includes a computer program for a mathematical theorem paper titled "A theorem on numbers of the form 10^x"
+
+Author: [Ravin Kumar](https://mr-ravin.github.io)
+
+### Demonstration
+
+```python
+import theorem10
+x=4 # we took x=4 as an example, for number of form 10^x 
+num_bits=theorem10.get_bits(x)  # here, we pass x to get_bits function, and it returns number of bits 
+                                # that will always represent total numbers greater than 10^x
+```
+
+### PyPi package installation
+
+```
+pip3 install theorem10
+```
+##### Note: This computer program is made available free to use. Please use this code at your own risk.
---
+++ packages/theorem10/theorem10-1.0.tar.gz/theorem10-1.0/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+     name='theorem10',  
+
+     version='1.0',
+
+     author="Ravin Kumar",
+
+     author_email="mr.ravin_kumar@hotmail.com",
+
+     description="This repository includes a computer program for a mathematical theorem paper titled A theorem on numbers of the form 10^x",
+
+     long_description=long_description,
+
+   long_description_content_type="text/markdown",
+
+     url="https://github.com/mr-ravin/theorem10",
+
+     packages=setuptools.find_packages(),
+
+     classifiers=[
+
+         "Programming Language :: Python :: 3",
+
+         "License :: OSI Approved :: GNU General Public License (GPL)",
+
+         "Operating System :: OS Independent",
+
+     ],
+
+ )
