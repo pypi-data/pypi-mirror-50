d5406cbb4b674c1d8dab078f5d989d0f946e2054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:52 +0000

    Add package otpy/0.0.2/otpy-0.0.2.tar.gz

---
+++ packages/otpy/otpy-0.0.2.tar.gz/otpy-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="otpy",
+    version="0.0.2",
+    author="l0rem1psum",
+    description="Python package for OTP (One-Time Password)",
+    long_description="This is a Python package to cater to all your needs for generatring and verifying OTP (One-Time Password).",
+    long_description_content_type="text/markdown",
+    url="https://github.com/l0rem1psum/OTPy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
