44eb00938adee1ca8b859d881019ed8ec944b4ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:17 +0000

    Add package jsonldschema/0.0.1/jsonldschema-0.0.1.tar.gz

---
+++ packages/jsonldschema/jsonldschema-0.0.1.tar.gz/jsonldschema-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='jsonldschema',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='Alejandra Gonzalez-Beltran',
+    author_email='alejandra.gonzalez.beltran@gmail.com',
+    packages=[],    
+    classifiers=[],
+)
