31cc565f397ae0b9399692fdd122caf710f4dc1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:24 +0000

    Add package pantsbuild.pants.contrib.go/1.17.0/pantsbuild.pants.contrib.go-1.17.0-py27.py36.py37-none-any.whl

---
+++ packages/pantsbuild.pants.contrib.go/pantsbuild.pants.contrib.go-1.17.0-py27.py36.py37-none-any.whl/pants/contrib/go/tasks/go_fmt.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from pants.task.fmt_task_mixin import FmtTaskMixin
+
+from pants.contrib.go.tasks.go_fmt_task_base import GoFmtTaskBase
+
+
+class GoFmt(FmtTaskMixin, GoFmtTaskBase):
+  """Format Go code using gofmt."""
+
+  def execute(self):
+    with self.go_fmt_invalid_targets(['-w']) as output:
+      if output:
+        self.context.logger.info(output)
