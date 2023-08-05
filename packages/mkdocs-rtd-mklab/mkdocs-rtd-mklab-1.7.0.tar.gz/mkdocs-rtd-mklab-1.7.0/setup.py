faea9e8d78af968633c8e064dc61eb17f501d973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:06 +0000

    Add package mkdocs-rtd-mklab/1.7.0/mkdocs-rtd-mklab-1.7.0.tar.gz

---
+++ packages/mkdocs-rtd-mklab/mkdocs-rtd-mklab-1.7.0.tar.gz/mkdocs-rtd-mklab-1.7.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: mklab
+# Mail: mklabs@163.com
+# Created Time:  2019-07-30 10:17:34
+#############################################
+
+from setuptools import setup, find_packages           
+
+setup(
+    name = "mkdocs-rtd-mklab",      
+    version = "1.7.0", 
+    keywords = ("pip", "readthedocs-mklab","theme"),
+    description = "The readthedocs theme for mkdocs,Replacement of Google fonts, etc.",
+    long_description = "The readthedocs theme for mkdocs,Replacement of Google fonts, etc.",
+    license = "MIT Licence",
+
+    #url = "",     
+    author = "mklab",
+    author_email = "mklabs@163.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = [],       
+    entry_points={
+        'mkdocs.themes': [
+            'rtd-mklab = mkdocs_rtd_mklab',
+        ]
+    }
+)
