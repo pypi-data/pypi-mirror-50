8dd20ff6663950746d3db41cf83a457899365300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:13 +0000

    Add package colibris/0.8.0/colibris-0.8.0.tar.gz

---
+++ packages/colibris/colibris-0.8.0.tar.gz/colibris-0.8.0/colibris/authentication/__init__.py
@@ -0,0 +1,63 @@
+
+import logging
+
+from colibris.conf import settings
+
+from .base import AuthenticationBackend
+from .exceptions import *
+
+
+_REQUEST_ACCOUNT_ITEM_NAME = 'account'
+
+
+logger = logging.getLogger(__name__)
+
+
+def require_authentication(required=True):
+    def decorator(handler):
+        handler.authentication_required = required
+
+        return handler
+
+    return decorator
+
+
+def get_authentication_required(handler):
+    return getattr(handler, 'authentication_required', None)
+
+
+def authenticate(request):
+    account = AuthenticationBackend.get_instance().authenticate(request)
+    request[_REQUEST_ACCOUNT_ITEM_NAME] = account
+
+    return account
+
+
+def get_account(request):
+    return request.get(_REQUEST_ACCOUNT_ITEM_NAME)
+
+
+def login(request, account, persistent):
+    logger.debug('logging in account "%s" (persistent=%s)', account, persistent)
+
+    request[_REQUEST_ACCOUNT_ITEM_NAME] = account
+    AuthenticationBackend.get_instance().login(request, persistent)
+
+
+def logout(request):
+    account = request.pop(_REQUEST_ACCOUNT_ITEM_NAME, None)
+    if account:
+        logger.debug('logging out account "%s"', account)
+        AuthenticationBackend.get_instance().logout(request)
+
+
+def process_request(request):
+    return AuthenticationBackend.get_instance().process_request(request)
+
+
+def process_response(request, response):
+    return AuthenticationBackend.get_instance().process_response(request, response)
+
+
+def setup():
+    AuthenticationBackend.configure(settings.AUTHENTICATION)
