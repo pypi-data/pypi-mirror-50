9f52da593d5e5a2c008c8ab7d81044b8337ac3b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:07 +0000

    Add package piperci/0.0.1/piperci-0.0.1.tar.gz

---
+++ packages/piperci/piperci-0.0.1.tar.gz/piperci-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'piperci',
+  packages = ['piperci'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'Piperci',
+  author = 'Nick Wilburn',
+  author_email = 'nwilburn@globalinfotek.com',
+  url = 'https://gitlab.com/dreamer-labs/piperci',
+  keywords = ['ci'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
