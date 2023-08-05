28eca1f6db58e026cdc5cfc3160c47fea5238fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:30 +0000

    Add package HexColors/0.0.2/HexColors-0.0.2.tar.gz

---
+++ packages/HexColors/HexColors-0.0.2.tar.gz/HexColors-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="HexColors",
+    version="0.0.2",
+    author="Reggles",
+    author_email="reginaldbeakes@gmail.com",
+    description="Organized Hex Color Tuples",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/Reggles44/hex-colors",
+    packages=['hexcolors'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
