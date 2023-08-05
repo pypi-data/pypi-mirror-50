6c7b065504c992cba610997132486bd8b850c8e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:33 +0000

    Add package keypendium/0.1.9/keypendium-0.1.9.tar.gz

---
+++ packages/keypendium/keypendium-0.1.9.tar.gz/keypendium-0.1.9/keypendium/__init__.py
@@ -0,0 +1,5 @@
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+from keypendium import *
---
+++ packages/keypendium/keypendium-0.1.9.tar.gz/keypendium-0.1.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='keypendium',
+    version='0.1.9',
+    author='caerulius',
+    author_email='cae@polywhack.com',
+    description='A Wrapper for the keyforge-compendium.com API',
+    long_description=long_description,
+    packages=['keypendium']
+)
