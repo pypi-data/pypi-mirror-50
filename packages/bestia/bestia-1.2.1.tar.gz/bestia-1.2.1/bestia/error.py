09fadf40ceef4e3190b68ee61d3a99d23b4008bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:47 +0000

    Add package bestia/1.2.1/bestia-1.2.1.tar.gz

---
+++ packages/bestia/bestia-1.2.1.tar.gz/bestia-1.2.1/bestia/error.py
@@ -0,0 +1,17 @@
+class UndefinedAnsiSequence(Exception):
+    pass
+
+class SttyBinMissing(Exception):
+    pass
+
+class SayBinMissing(Exception):
+    pass
+
+class FileBinMissing(Exception):
+    pass
+
+class CurlBinMissing(Exception):
+    pass
+
+class CurlFailed(Exception):
+    pass
