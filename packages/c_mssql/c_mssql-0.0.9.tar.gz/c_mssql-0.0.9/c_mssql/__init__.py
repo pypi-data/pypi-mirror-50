f23cdf4b3e158feb96b0699aebde02ec52e7b830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:56 +0000

    Add package c_mssql/0.0.9/c_mssql-0.0.9.tar.gz

---
+++ packages/c_mssql/c_mssql-0.0.9.tar.gz/c_mssql-0.0.9/c_mssql/__init__.py
@@ -0,0 +1,6 @@
+class DB_Config(object):
+    def __init__(self, server,user,password,database):
+        self.server=server
+        self.user=user
+        self.password=password
+        self.database=database
