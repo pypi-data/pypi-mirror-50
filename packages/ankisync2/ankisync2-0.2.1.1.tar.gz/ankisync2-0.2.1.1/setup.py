c32ca050c4eb8c62e0ca0be4d39e09ffea068719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:48 +0000

    Add package ankisync2/0.2.1.1/ankisync2-0.2.1.1.tar.gz

---
+++ packages/ankisync2/ankisync2-0.2.1.1.tar.gz/ankisync2-0.2.1.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['ankisync2']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['peewee>=3.9,<4.0', 'shortuuid>=0.5.0,<0.6.0']
+
+setup_kwargs = {
+    'name': 'ankisync2',
+    'version': '0.2.1.1',
+    'description': 'Doing in Anki what AnkiConnect cannot do',
+    'long_description': None,
+    'author': 'patarapolw',
+    'author_email': 'patarapolw@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
