fa3fadc2507d8cb777ed7a939f3e823e72968add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:49 +0000

    Add package zlairflow/4.0.19/zlairflow-4.0.19-py3-none-any.whl

---
+++ packages/zlairflow/zlairflow-4.0.19-py3-none-any.whl/zlairflow/dag/data/data_zljianzhu.py
@@ -0,0 +1,9 @@
+para=[
+
+["zljianzhu_gg", 'minutes=240', ''],
+["zljianzhu_qiyexx", 'minutes=240', ''],
+["zljianzhu_renyuanxx", 'minutes=240', ''],
+["zljianzhu_xiangmuxx", 'minutes=240', ''],
+
+]
+
