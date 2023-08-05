add647c3c5359d4fcf48854646e32f14fc61ec31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:53 +0000

    Add package janis-pipelines.bioinformatics/0.5.0/janis-pipelines.bioinformatics-0.5.0.tar.gz

---
+++ packages/janis-pipelines.bioinformatics/janis-pipelines.bioinformatics-0.5.0.tar.gz/janis-pipelines.bioinformatics-0.5.0/janis_bioinformatics/tools/common/__init__.py
@@ -0,0 +1,3 @@
+from .alignsortedbam import AlignSortedBam
+from .splitmultiallele import SplitMultiAllele
+from .bwamem_samtoolsview import BwaMem_SamToolsView
---
+++ packages/janis-pipelines.bioinformatics/janis-pipelines.bioinformatics-0.5.0.tar.gz/janis-pipelines.bioinformatics-0.5.0/janis_bioinformatics/tools/illumina/__init__.py
@@ -0,0 +1,6 @@
+from .strelkagermline.strelkagermline import StrelkaGermline_2_9_9, StrelkaGermline_2_9_10, StrelkaGermlineLatest
+from .strelkasomatic.strelkasomatic import StrelkaSomatic_2_9_9, StrelkaSomatic_2_9_10, StrelkaSomaticLatest
+
+from .manta.manta import Manta_1_4_0
+from .manta.manta import Manta_1_5_0
+from .manta.manta import MantaLatest
