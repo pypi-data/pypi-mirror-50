ddbc5111f7f51fa5bd337cde1e272a142870405e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:34 +0000

    Add package aws-es-query/0.1.0/aws-es-query-0.1.0.tar.gz

---
+++ packages/aws-es-query/aws-es-query-0.1.0.tar.gz/aws-es-query-0.1.0/README.md
@@ -0,0 +1,8 @@
+# aws-es-query
+
+Really 'dumb' utility command to query AWS elasticsearch endpoints which use
+IAM policies.
+
+
+    aws-es-query --role-arn=<iam-role> <endpoint>
+
---
+++ packages/aws-es-query/aws-es-query-0.1.0.tar.gz/aws-es-query-0.1.0/aws_es_query.py
@@ -0,0 +1,33 @@
+import json
+
+import click
+
+import boto3
+import requests
+from requests_aws4auth import AWS4Auth
+
+
+@click.command()
+@click.option("--role-arn", required=True, type=str)
+@click.argument("url")
+def main(role_arn, url):
+    sts = boto3.client("sts")
+    resp = sts.assume_role(RoleArn=role_arn, RoleSessionName="es-curl")
+    awsauth = AWS4Auth(
+        resp["Credentials"]["AccessKeyId"],
+        resp["Credentials"]["SecretAccessKey"],
+        "eu-west-1",
+        "es",
+        session_token=resp["Credentials"]["SessionToken"],
+    )
+
+    response = requests.get(url, auth=awsauth)
+
+    try:
+        print(json.dumps(response.json(), indent=4, sort_keys=True))
+    except json.JSONDecodeError:
+        print(response.content.decode("utf-8"))
+
+
+if __name__ == "__main__":
+    main()
