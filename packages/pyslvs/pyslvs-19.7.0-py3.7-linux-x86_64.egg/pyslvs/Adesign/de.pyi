4ac3f4752c7179e00dd183c6120e8c0081515cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:03 +0000

    Add package pyslvs/19.7.0/pyslvs-19.7.0-py3.7-linux-x86_64.egg

---
+++ packages/pyslvs/pyslvs-19.7.0-py3.7-linux-x86_64.egg/pyslvs/Adesign/de.pyi
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from typing import (
+    Tuple,
+    List,
+    Dict,
+    Callable,
+    Optional,
+    Any,
+)
+from .verify import Verification
+
+
+class Differential:
+
+    """Algorithm class."""
+
+    def __init__(
+        self,
+        func: Verification,
+        settings: Dict[str, Any],
+        progress_fun: Optional[Callable[[int, str], None]] = None,
+        interrupt_fun: Optional[Callable[[], bool]] = None
+    ):
+        """
+        settings = {
+            'strategy',
+            'NP',
+            'F',
+            'CR',
+            'max_gen', 'min_fit' or 'max_time',
+            'report'
+        }
+        """
+        ...
+
+    def run(self) -> Tuple[Any, List[Tuple[int, float, float]]]: ...
