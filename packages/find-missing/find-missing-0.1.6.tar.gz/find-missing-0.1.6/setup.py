3bc9e29150591b795984cc59b5fddfe54203dcfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:37 +0000

    Add package find-missing/0.1.6/find-missing-0.1.6.tar.gz

---
+++ packages/find-missing/find-missing-0.1.6.tar.gz/find-missing-0.1.6/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['find_missing']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.0,<8.0']
+
+setup_kwargs = {
+    'name': 'find-missing',
+    'version': '0.1.6',
+    'description': '',
+    'long_description': None,
+    'author': 'MB',
+    'author_email': 'mb@m1k.pw',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
