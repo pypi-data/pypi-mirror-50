d8a43fc5e2742c8ce39e92827f5bcc738737d82d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:06 +0000

    Add package bel-repository/0.0.8/bel_repository-0.0.8.tar.gz

---
+++ packages/bel-repository/bel_repository-0.0.8.tar.gz/bel_repository-0.0.8/src/bel_repository/version.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""Version information for :mod:`bel-repository`."""
+
+__all__ = [
+    'VERSION',
+    'get_version',
+]
+
+VERSION = '0.0.8'
+
+
+def get_version() -> str:
+    """Get the software verison of :mod:`bel-repository`."""
+    return VERSION
