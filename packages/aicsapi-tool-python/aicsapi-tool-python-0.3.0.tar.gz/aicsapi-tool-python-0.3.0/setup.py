cc1db5ce43b423aa9e92e0074b64bb880c0dc6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:56 +0000

    Add package aicsapi-tool-python/0.3.0/aicsapi-tool-python-0.3.0.tar.gz

---
+++ packages/aicsapi-tool-python/aicsapi-tool-python-0.3.0.tar.gz/aicsapi-tool-python-0.3.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="aicsapi-tool-python",
+    version="0.3.0",
+    author="Sean (Shih-Lun) Wu",
+    author_email="summer7sean@gmail.com",
+    description="Toolkit for Python backend template",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ASUS-AICS/aicsapi-tool-python",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
