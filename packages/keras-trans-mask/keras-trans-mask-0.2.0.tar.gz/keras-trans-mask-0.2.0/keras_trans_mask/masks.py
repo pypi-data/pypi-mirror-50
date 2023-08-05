bf27bf68c26df8d601f7540e293ee300781cbc12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:36 +0000

    Add package keras-trans-mask/0.2.0/keras-trans-mask-0.2.0.tar.gz

---
+++ packages/keras-trans-mask/keras-trans-mask-0.2.0.tar.gz/keras-trans-mask-0.2.0/keras_trans_mask/masks.py
@@ -0,0 +1,51 @@
+from .backend import keras
+from .backend import backend as K
+
+
+class RemoveMask(keras.layers.Layer):
+    """Remove mask from input tensor.
+
+    # Input shape
+        Tensor with shape: `(batch_size, ...)`.
+
+    # Output shape
+        Tensor with shape: `(batch_size, ...)`.
+    """
+
+    def __init__(self, **kwargs):
+        super(RemoveMask, self).__init__(**kwargs)
+        self.supports_masking = True
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+    def compute_mask(self, inputs, mask=None):
+        return None
+
+    def call(self, inputs, mask=None, **kwargs):
+        return K.identity(inputs)
+
+
+class RestoreMask(keras.layers.Layer):
+    """Restore mask from the second tensor.
+
+    # Input shape
+        Tensor with shape: `(batch_size, ...)`.
+        Tensor with mask and shape: `(batch_size, ...)`.
+
+    # Output shape
+        Tensor with shape: `(batch_size, ...)`.
+    """
+
+    def __init__(self, **kwargs):
+        super(RestoreMask, self).__init__(**kwargs)
+        self.supports_masking = True
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0]
+
+    def compute_mask(self, inputs, mask=None):
+        return mask[1]
+
+    def call(self, inputs, mask=None, **kwargs):
+        return K.identity(inputs[0])
