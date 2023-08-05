0b24b4ae1e169e5d9045e73bfdc0dc204773d2fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:24 +0000

    Add package zeus-client/0.1.7/zeus-client-0.1.7.tar.gz

---
+++ packages/zeus-client/zeus-client-0.1.7.tar.gz/zeus-client-0.1.7/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.client.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='zeus-client',
+
+    version='0.1.7',
+
+    description='Zeus client',
+
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+
+    url='https://github.com/grnet/zeus',
+    author='GRNET Zeus developers',
+    author_email='dev@zeus.grnet.gr',
+
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+    ],
+
+    py_modules=['zeus.core', 'zeus.client', 'zeus.zeus_sk'],
+
+    install_requires=['pycrypto>=2.6'],
+
+    entry_points={
+        'console_scripts': [
+            'zeus-client=zeus.client:main',
+            'zeus-core=zeus.core:main',
+        ],
+    },
+)
