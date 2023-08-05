b8c29e78e7bcf2dcfa8ea5da8b3129e320564b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:49 +0000

    Add package tisdk/0.0.7/tisdk-0.0.7.tar.gz

---
+++ packages/tisdk/tisdk-0.0.7.tar.gz/tisdk-0.0.7/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+long_description = """
+```
+pip install tisdk
+tireq to_9012345678_grs 3292a4f76c0d46bf post http://taiqiyun.wowfintech.cn/grs/v1/open_match name:美团,foo:bar
+```
+"""
+
+setup(
+    name='tisdk',
+    version='0.0.7',
+    description='python sdk of taiqiyun',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['request', 'pycrypto'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    py_modules=['ti'],
+    entry_points={
+        'console_scripts': ['tireq=ti:main'],
+    },
+    include_package_data=True,
+)
