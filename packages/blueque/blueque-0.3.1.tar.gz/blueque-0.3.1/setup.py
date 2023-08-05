96c9b674b2eea31279e83c7bf9e0368a8d5d94cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:15 +0000

    Add package blueque/0.3.1/blueque-0.3.1.tar.gz

---
+++ packages/blueque/blueque-0.3.1.tar.gz/blueque-0.3.1/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+install_requires = [
+    "argparse",
+    "redis"
+]
+
+setup(name="blueque",
+      version="0.3.1",
+      description="Simple job queuing for very long tasks",
+      url="https://github.com/ustudio/Blueque",
+      packages=["blueque"],
+      install_requires=install_requires)
