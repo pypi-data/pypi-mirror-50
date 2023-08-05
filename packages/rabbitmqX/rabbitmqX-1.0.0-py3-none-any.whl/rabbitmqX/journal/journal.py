46646848c5b080fc3e2050631a22cdab70b098e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:07 +0000

    Add package rabbitmqX/1.0.0/rabbitmqX-1.0.0-py3-none-any.whl

---
+++ packages/rabbitmqX/rabbitmqX-1.0.0-py3-none-any.whl/rabbitmqX/journal/journal.py
@@ -0,0 +1,9 @@
+import time
+
+class Journal (object):
+
+    def __init__(self, type, data, action):
+        self.type = type
+        self.datatime = time.time()
+        self.action = action
+        self.data = data
