5608440b8dac12462512c681c38b19eef7aa3c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:45 +0000

    Add package fst-pso/1.5.3/fst-pso-1.5.3.zip

---
+++ packages/fst-pso/fst-pso-1.5.3.zip/fst-pso-1.5.3/fstpso/__init__.py
@@ -0,0 +1,1 @@
+from fstpso_ import FuzzyPSO
---
+++ packages/fst-pso/fst-pso-1.5.3.zip/fst-pso-1.5.3/fstpso/example.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+from fstpso import FuzzyPSO	
+
+def example_fitness( particle ):
+	return sum(map(lambda x: x**2, particle))
+
+if __name__ == '__main__':
+	
+	dims = 10
+	FP = FuzzyPSO()
+	FP.set_search_space( [[-10, 10]]*dims )	
+	FP.set_fitness(example_fitness)	
+	result =  FP.solve_with_fstpso(max_iter=100)
+	print ("Best solution:", result[0])
+	print ("Whose fitness is:", result[1])
