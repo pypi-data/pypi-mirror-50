06997642266a44e1e84e48e7c5991b4aef46828e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:24 +0000

    Add package opencpaas-twilio/0.0.2/opencpaas_twilio-0.0.2.tar.gz

---
+++ packages/opencpaas-twilio/opencpaas_twilio-0.0.2.tar.gz/opencpaas_twilio-0.0.2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="opencpaas_twilio",
+    version="0.0.2",
+    author="Perri Smith",
+    author_email="dx@bandwidth.com",
+    description="A Twilio implementation of OpenCpaas, an SDK that allows flexibility between CPaaS providers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Bandwidth/OpenCpaas/",
+    packages=['opencpaas_twilio'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'certifi',
+        'chardet',
+        'idna',
+        'opencpaas',
+        'PyJWT',
+        'PySocks',
+        'pytz',
+        'requests',
+        'six',
+        'twilio',
+        'urllib3'
+    ]
+)
