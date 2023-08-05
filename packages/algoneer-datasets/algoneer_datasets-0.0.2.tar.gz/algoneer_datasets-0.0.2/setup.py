cecca4dfe7bf7cd134ad6b1709e1607655ce15c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:52 +0000

    Add package algoneer-datasets/0.0.2/algoneer_datasets-0.0.2.tar.gz

---
+++ packages/algoneer-datasets/algoneer_datasets-0.0.2.tar.gz/algoneer_datasets-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="algoneer_datasets",
+    python_requires=">=3",
+    version="0.0.2",
+    author="Andreas Dewes",
+    author_email="andreas.dewes@algoneer.org",
+    license="Creative-Commons (license of datasets varies)",
+    url="https://github.com/algoneer/datasets",
+    packages=find_packages(),
+    install_requires=["algoneer"],
+    zip_safe=False,
+    entry_points={},
+    description="Example datasets for Algoneer.",
+    long_description="""Example datasets for Algoneer.
+""",
+)
