0ce728989315b9424b3ee470c5c26331763f72a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:07 +0000

    Add package AsyncLine/1.5.7/AsyncLine-1.5.7.tar.gz

---
+++ packages/AsyncLine/AsyncLine-1.5.7.tar.gz/AsyncLine-1.5.7/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+	
+install_requires = [
+	'frugal',
+	'rsa',
+	'requests',
+	'async_timeout',
+	'aiohttp',
+	'aiostomp',
+	'asyncio-nats-client'
+]
+
+#packages = [
+	#'AsyncLine',
+	#'AsyncLine.lib',
+#]
+
+setup(
+	name='AsyncLine',
+	version='1.5.7',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	description='LINE Unofficial Python Library with Asyncio support and C++ Binding',
+	author="Anysz, Doodspav, Dyseo",
+	author_email="katro.coplax@gmail.com",
+	packages=find_packages(),
+	license='MIT',
+	install_requires=install_requires,
+	classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Multimedia :: Video",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
