0ca10c7d0d08f6cdaa5cbfc9a3677b6888d54b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:03 +0000

    Add package tb-cli/2.0.16/tb_cli-2.0.16-py3-none-any.whl

---
+++ packages/tb-cli/tb_cli-2.0.16-py3-none-any.whl/tb/job_execution.py
@@ -0,0 +1,51 @@
+import multiprocessing as mp
+import os
+import sys
+
+
+def serial_call(jobs):
+    results = []
+    try:
+        for job in jobs:
+            func, args = job[0], job[1:]
+            r = func(*args)
+            results.append(r)
+    except KeyboardInterrupt:
+        print("Caught KeyboardInterrupt, exiting")
+
+    return results
+
+
+def parallel_call(jobs, processes=8):
+    pool = mp.Pool(processes=processes)
+    output = None
+    try:
+        results = []
+        for job in jobs:
+            func, args = job[0], job[1:]
+            r = pool.apply_async(func, args)
+            results.append(r)
+        output = [res.get() for res in results]
+    except KeyboardInterrupt:
+        print("Caught KeyboardInterrupt, terminating workers")
+        pool.terminate()
+        pool.join()
+    else:
+        pool.close()
+        pool.join()
+
+    return output
+
+
+def restore_original_env():
+    if getattr(sys, 'frozen', False):
+        env = dict(os.environ)  # make a copy of the environment
+        lp_key = 'LD_LIBRARY_PATH'  # for Linux and *BSD.
+        lp_orig = env.get(lp_key + '_ORIG')  # pyinstaller >= 20160820 has this
+        if lp_orig is not None:
+            env[lp_key] = lp_orig  # restore the original, unmodified value
+        else:
+            env.pop(lp_key, None)  # last resort: remove the env var
+        return env
+    else:
+        return os.environ
