f7174c5a6824cc0508b403205b73c4c76c6a04e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:43 +0000

    Add package azure-storage-queue/2.1.0/azure-storage-queue-2.1.0.tar.gz

---
+++ packages/azure-storage-queue/azure-storage-queue-2.1.0.tar.gz/azure-storage-queue-2.1.0/setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+import sys
+
+from setuptools import setup, find_packages
+
+
+# azure v0.x is not compatible with this package
+# azure v0.x used to have a __version__ attribute (newer versions don't)
+try:
+    import azure
+
+    try:
+        ver = azure.__version__
+        raise Exception(
+            'This package is incompatible with azure=={}. '.format(ver) +
+            'Uninstall it with "pip uninstall azure".'
+        )
+    except AttributeError:
+        pass
+except ImportError:
+    pass
+
+# azure-storage v0.36.0 and prior are not compatible with this package
+try:
+    import azure.storage
+
+    try:
+        ver = azure.storage.__version__
+        raise Exception(
+            'This package is incompatible with azure-storage=={}. '.format(ver) +
+            ' Uninstall it with "pip uninstall azure-storage".'
+        )
+    except AttributeError:
+        pass
+except ImportError:
+    pass
+
+setup(
+    name='azure-storage-queue',
+    version='2.1.0',
+    description='Microsoft Azure Storage Queue Client Library for Python',
+    long_description=open('README.rst', 'r').read(),
+    license='MIT License',
+    author='Microsoft Corporation',
+    author_email='ascl@microsoft.com',
+    url='https://github.com/Azure/azure-storage-python',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+    ],
+    zip_safe=False,
+    packages=find_packages(exclude=[
+        # Exclude packages that will be covered by PEP420 or nspkg
+        'azure',
+        'azure.storage',
+    ]),
+    install_requires=[
+        'azure-common>=1.1.5',
+        'azure-storage-common~=2.1'
+    ],
+)
