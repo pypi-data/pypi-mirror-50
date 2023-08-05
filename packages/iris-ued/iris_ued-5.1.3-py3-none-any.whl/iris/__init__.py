57e9f988fab7913798796e82762d1186f90ece70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:34 +0000

    Add package iris-ued/5.1.3/iris_ued-5.1.3-py3-none-any.whl

---
+++ packages/iris-ued/iris_ued-5.1.3-py3-none-any.whl/iris/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+__author__ = "Laurent P. René de Cotret"
+__email__ = "laurent.renedecotret@mail.mcgill.ca"
+__license__ = "MIT"
+__version__ = "5.1.3"
+
+from .raw import AbstractRawDataset, check_raw_bounds, open_raw
+from .mcgill import McGillRawDataset, LegacyMcGillRawDataset
+from .dataset import DiffractionDataset, PowderDiffractionDataset
+from .meta import ExperimentalParameter
+from .plugins import install_plugin
+
+from . import plugins
