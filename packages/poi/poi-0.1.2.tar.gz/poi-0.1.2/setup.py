aa6ebb5addc5a0257474ffacd6822eee465f3179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:32 +0000

    Add package poi/0.1.2/poi-0.1.2.tar.gz

---
+++ packages/poi/poi-0.1.2.tar.gz/poi-0.1.2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['poi', 'poi.visitors']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['xlsxwriter>=1.1,<2.0']
+
+setup_kwargs = {
+    'name': 'poi',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Ryan Wang',
+    'author_email': 'hwwangwang@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
