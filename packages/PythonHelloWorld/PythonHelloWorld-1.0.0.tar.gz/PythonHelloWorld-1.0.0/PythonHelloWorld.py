1fdcb53c5799f6166fdba9338a86d7954c27f2cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:54 +0000

    Add package PythonHelloWorld/1.0.0/PythonHelloWorld-1.0.0.tar.gz

---
+++ packages/PythonHelloWorld/PythonHelloWorld-1.0.0.tar.gz/PythonHelloWorld-1.0.0/PythonHelloWorld.py
@@ -0,0 +1,6 @@
+print('hello world')
+super = ['gone with the wind',1876,'yesterday once more',1945,'when i was young',1922]
+star = ['jammy','musk','eric']
+superstar = [super,star]
+for i in superstar:
+    print(i)
---
+++ packages/PythonHelloWorld/PythonHelloWorld-1.0.0.tar.gz/PythonHelloWorld-1.0.0/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+       name = 'PythonHelloWorld',
+       version = '1.0.0',
+       py_modules = ['PythonHelloWorld'],
+       author = 'Eric Zhang',
+       author_email = 'silentstorm321f@qq.com',
+       url = 'http://www.iluoqian.cn',
+       description = 'Try to upload',
+      )
