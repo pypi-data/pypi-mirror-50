580ea057e162d9bcda91148860496ae6b7290107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:12 +0000

    Add package gfcc/0.2/gfcc-0.2.tar.gz

---
+++ packages/gfcc/gfcc-0.2.tar.gz/gfcc-0.2/gfcc/__init__.py
@@ -0,0 +1,1 @@
+from gfcc.gfcc import *
---
+++ packages/gfcc/gfcc-0.2.tar.gz/gfcc-0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='gfcc',
+    version='0.2',
+    description='A layer of automation for ClearCase with a touch of git flavour',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='git clearcase cleartool',
+    url='https://github.com/bmpenuelas/gfcc',
+    author='Borja Penuelas',
+    author_email='bmpenuelas@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+    scripts=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'gfcc=gfcc.gfcc:main',
+        ]
+    }
+)
