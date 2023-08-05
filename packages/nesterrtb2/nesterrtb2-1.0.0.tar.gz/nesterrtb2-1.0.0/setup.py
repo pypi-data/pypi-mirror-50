90fb699318fe9322eb51d97aeec82a6635af1d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:35 +0000

    Add package nesterrtb2/1.0.0/nesterrtb2-1.0.0.tar.gz

---
+++ packages/nesterrtb2/nesterrtb2-1.0.0.tar.gz/nesterrtb2-1.0.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from distutils.core import setup
+
+
+setup(
+      name = 'nesterrtb2',
+      version = '1.0.0',
+      py_modules = ['nesterrtb2'],
+      author = 'Ravi',
+      author_email = 'raviteja.bodla@gmail.com',
+      description = 'A simple printer of nested lists'
+      )
