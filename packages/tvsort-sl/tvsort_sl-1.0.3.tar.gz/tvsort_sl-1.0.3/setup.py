5c2bfe5e948c4bf45daab27af81d2922385c712f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:32 +0000

    Add package tvsort-sl/1.0.3/tvsort_sl-1.0.3.tar.gz

---
+++ packages/tvsort-sl/tvsort_sl-1.0.3.tar.gz/tvsort_sl-1.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='tvsort_sl',
+    packages=['tvsort_sl'],
+    version='1.0.3',
+    description='Sort movies and TV-shows files',
+    long_description=open('readme.md').read(),
+    author='Shlomi Lanton',
+    author_email='shlomilanton@gmail.com',
+    url='https://github.com/shlomiLan/tvsort_sl',
+    download_url='https://github.com/shlomiLan/tvsort_sl/archive/0.1.zip',
+    keywords=['sort', 'tv', 'show', 'movie', 'KODI', 'XBM1C'],
+    classifiers=[],
+    setup_requires=['wheel']
+)
