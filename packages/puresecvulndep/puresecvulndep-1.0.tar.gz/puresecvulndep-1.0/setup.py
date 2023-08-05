44a762c17284b90d055651c6dfc4eefca1d19bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:13 +0000

    Add package puresecvulndep/1.0/puresecvulndep-1.0.tar.gz

---
+++ packages/puresecvulndep/puresecvulndep-1.0.tar.gz/puresecvulndep-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='puresecvulndep',
+      version='1.0',
+      description='Package to test vulnerability',
+      author='Yuri Shapira',
+      author_email='yuri@puresec.io',
+      url='https://www.puresec.io/',
+      install_requires=[
+        'requests==0.2.2'
+        ],
+     )
