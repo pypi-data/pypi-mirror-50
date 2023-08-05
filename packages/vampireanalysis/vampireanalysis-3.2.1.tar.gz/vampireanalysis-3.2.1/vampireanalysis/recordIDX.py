270c5b2fd6f059643514ddffb35df02b8ec03294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:22 +0000

    Add package vampireanalysis/3.2.1/vampireanalysis-3.2.1.tar.gz

---
+++ packages/vampireanalysis/vampireanalysis-3.2.1.tar.gz/vampireanalysis-3.2.1/vampireanalysis/recordIDX.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# built-in libraries
+import os
+from time import sleep
+# external libraries
+import pandas as pd
+import numpy as np
+import csv
+
+def recordIDX(IDX,IDX_dist,UI,cellornuc,setpath):
+	print('## recordIDX.py')
+	if cellornuc == 'ch1':
+		ledgername = UI['ch1'][0] + '_registry.csv'
+		picklename =  UI['ch1'][0] + '_boundary_coordinate_stack.pickle'
+	else: 
+		ledgername =  UI['ch2'][0] + '_registry.csv'
+		picklename =  UI['ch2'][0] + '_boundary_coordinate_stack.pickle'
+
+	if os.path.exists(os.path.join(setpath,ledgername)): 
+		obj_ledger = pd.read_csv(os.path.join(setpath,ledgername))
+		obj_ledger['IDX']=pd.Series(IDX) #write
+		obj_ledger['Fitness']=pd.Series(IDX_dist)
+	else: 
+		d = {'IDX':pd.Series(IDX),'Fitness':pd.Series(IDX_dist)}
+		obj_ledger = pd.DataFrame(data=d)
+	obj_ledger.to_csv(os.path.join(setpath,ledgername), index=False)
+
+	
+
