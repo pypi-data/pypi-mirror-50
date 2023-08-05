e2f8fa4176252f940113547e313e328baa5126c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:16 +0000

    Add package ddtestlib/1.0.1/ddtestlib-1.0.1.zip

---
+++ packages/ddtestlib/ddtestlib-1.0.1.zip/ddtestlib-1.0.1/setup.py
@@ -0,0 +1,12 @@
+
+# from distutils.core import setup
+from setuptools import setup
+
+def readme_file():
+      with open("README.rst",encoding="utf-8") as rf:
+            return rf.read()
+
+setup(name="ddtestlib", version="1.0.1", description="this is a niubi lib2 666 dd",
+      packages=["ddtestlib"], py_modules=["Tool"], author="dd", author_email="3425296552@qq.com",
+      long_description=readme_file(),
+      url="https://github.com/dongdong2019/Python_code", license="MIT")
