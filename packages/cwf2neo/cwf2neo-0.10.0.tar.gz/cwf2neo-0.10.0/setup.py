8561191113717490d906037388e7a1dc2020bb4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:39 +0000

    Add package cwf2neo/0.10.0/cwf2neo-0.10.0.tar.gz

---
+++ packages/cwf2neo/cwf2neo-0.10.0.tar.gz/cwf2neo-0.10.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='cwf2neo',
+    version='0.10.0',
+    packages=['cwf2neo'],
+    license='Apache License 2.0',
+    description='cwf2neo is a Python library use to download, '
+    'parse and import the NICE Cybersecurity Workforce Framework '
+    'into a Neo4J graphing database.',
+    author_email='ckoroscil@circadence.com',
+    install_requires=[
+        'confuse',
+        'progress',
+        'py2neo',
+        'xlrd',
+        'bumpversion',
+        'twine'
+    ]
+)
