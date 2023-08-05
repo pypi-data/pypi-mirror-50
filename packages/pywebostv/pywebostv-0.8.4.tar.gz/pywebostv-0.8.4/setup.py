1d8c2da759c37a156c8cd97d2b17a20695ab5fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:12 +0000

    Add package pywebostv/0.8.4/pywebostv-0.8.4.tar.gz

---
+++ packages/pywebostv/pywebostv-0.8.4.tar.gz/pywebostv-0.8.4/setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='pywebostv',
+    version='0.8.4',
+    url='https://github.com/supersaiyanmode/PyWebOSTV',
+    author='Srivatsan Iyer',
+    author_email='supersaiyanmode.rox@gmail.com',
+    packages=[
+        'pywebostv',
+    ],
+    license='MIT',
+    description='Library to remote control LG Web OS TV',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        "ws4py",
+        "requests[security]",
+        "future",
+    ],
+)
