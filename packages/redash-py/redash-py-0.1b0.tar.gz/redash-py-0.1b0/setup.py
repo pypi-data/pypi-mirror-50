22e6b54059fa87a75a7f5dc26bcdb1130a7af808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:54 +0000

    Add package redash-py/0.1b0/redash-py-0.1b0.tar.gz

---
+++ packages/redash-py/redash-py-0.1b0.tar.gz/redash-py-0.1b0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from redash_py import __VERSION__
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license_txt = f.read()
+
+setup(
+    name='redash-py',
+    version=__VERSION__,
+    description='redash api client.',
+    entry_points={
+        "console_scripts": [
+            "redashpy = redash_py.command:main"
+        ]
+    },
+    long_description=readme,
+    author='denzow',
+    author_email='denzow@gmail.com',
+    url='https://github.com/denzow/redash-py',
+    license=license_txt,
+    packages=find_packages(exclude=('sample',)),
+    install_requires=['requests', 'fire'],
+)
