0da2c635a9cd2f324c68477f07d0479ac2ab3a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:05 +0000

    Add package bestnewmusic/0.4.2/bestnewmusic-0.4.2.tar.gz

---
+++ packages/bestnewmusic/bestnewmusic-0.4.2.tar.gz/bestnewmusic-0.4.2/bestnewmusic/util.py
@@ -0,0 +1,25 @@
+def render(query_url, page_load_timeout=30):
+    from selenium import webdriver
+    from selenium.common.exceptions import TimeoutException
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    browser = webdriver.Chrome(chrome_options=options)
+    browser.set_page_load_timeout(page_load_timeout)
+
+    try:
+        browser.get(query_url)
+        html_source = browser.page_source
+        browser.quit()
+        return html_source
+
+    except TimeoutException:
+        return render(query_url)
+
+
+def try_except(f, field):
+    try:
+        out = f()
+    except (AttributeError, TypeError, IndexError):
+        out = "Unknown {}".format(field)
+    return out
