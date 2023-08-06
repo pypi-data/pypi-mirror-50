e6edf949786048f0bd74e5afc77c11a84304397a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:14 +0000

    Add package mlautomator/1.1.2/mlautomator-1.1.2.tar.gz

---
+++ packages/mlautomator/mlautomator-1.1.2.tar.gz/mlautomator-1.1.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="mlautomator",
+    version="1.1.2",
+    author="Kevin Vecmanis",
+    author_email="kevinvecmanis@gmail.com",
+    description="A fast, simple way to train machine learning algorithms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/VanAurum/ML-automator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'sklearn',
+        'hyperopt',
+        'xgboost',
+        'joblib',
+    ]
+)
