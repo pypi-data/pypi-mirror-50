7f1b061faf5efccfd3b478c450f9b4d604d86b42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:51 +0000

    Add package vmi/0.2.4/vmi-0.2.4-py3-none-any.whl

---
+++ packages/vmi/vmi-0.2.4-py3-none-any.whl/vmi/__init__.py
@@ -0,0 +1,14 @@
+name = 'vmi'
+version = '0.2.4'
+
+from .vrc import *
+from .vapp import *
+from .vask import *
+from .view import *
+from .vicom import *
+from .vickle import *
+from .vath import *
+from .vocc import *
+from .vimpo import *
+from .vrop import *
+from .vroptool import *
