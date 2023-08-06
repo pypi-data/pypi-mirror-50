6c615778464c63e7af154503cfa46ccdead46639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:31 +0000

    Add package focused/1.0.0/focused-1.0.0.tar.gz

---
+++ packages/focused/focused-1.0.0.tar.gz/focused-1.0.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="focused",
+    version="1.0.0",
+    author="Federico A. Corazza",
+    author_email="federico.corazza@live.it",
+    description="Focused is a Python library that removes clutter from webpages. Read articles without distractions.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Imperator26/focused",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "requests==2.22.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
