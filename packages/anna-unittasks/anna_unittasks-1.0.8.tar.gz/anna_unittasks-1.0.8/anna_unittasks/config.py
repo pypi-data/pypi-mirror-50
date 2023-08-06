ebfbbf97dedf091f4dcd63399ac8d59b53fff22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:01 +0000

    Add package anna-unittasks/1.0.8/anna_unittasks-1.0.8.tar.gz

---
+++ packages/anna-unittasks/anna_unittasks-1.0.8.tar.gz/anna_unittasks-1.0.8/anna_unittasks/config.py
@@ -0,0 +1,15 @@
+url = "https://test.annahub.se/test"
+sequence = (
+	"send_keys",
+	"submit",
+	"click",
+	"hover",
+	"wait",
+	"select",
+	"switch_to",
+	"iframe.send_keys",
+	"iframe.submit",
+	"iframe.click",
+	"iframe.hover",
+	"iframe.wait"
+)
