8e0f44439157c8071b42400981b44fac3491a023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:52 +0000

    Add package xbart/0.1.7/xbart-0.1.7.tar.gz

---
+++ packages/xbart/xbart-0.1.7.tar.gz/xbart-0.1.7/src/predict_r.cpp
@@ -0,0 +1,88 @@
+#include <ctime>
+#include <RcppArmadillo.h>
+#include "tree.h"
+#include "forest.h"
+#include <chrono>
+#include "mcmc_loop.h"
+#include "utility.h"
+#include "json_io.h"
+
+// [[Rcpp::export]]
+Rcpp::List xbart_predict(arma::mat X, double y_mean, Rcpp::XPtr<std::vector<std::vector<tree>>> tree_pnt)
+{
+
+    // Size of data
+    size_t N = X.n_rows;
+    size_t p = X.n_cols;
+
+    // Init X_std matrix
+    Rcpp::NumericMatrix X_std(N, p);
+    for (size_t i = 0; i < N; i++)
+    {
+        for (size_t j = 0; j < p; j++)
+        {
+            X_std(i, j) = X(i, j);
+        }
+    }
+    double *Xpointer = &X_std[0];
+
+    // Trees
+    std::vector<std::vector<tree>> *trees = tree_pnt;
+
+    // Result Container
+    matrix<double> yhats_test_xinfo;
+    size_t N_sweeps = (*trees).size();
+    size_t M = (*trees)[0].size();
+    ini_xinfo(yhats_test_xinfo, N, N_sweeps);
+
+    NormalModel *model = new NormalModel();
+
+    // Predict
+    model->predict_std(Xpointer, N, p, M, N_sweeps,
+                yhats_test_xinfo, *trees);
+
+    // Convert back to Rcpp
+    Rcpp::NumericMatrix yhats(N, N_sweeps);
+    for (size_t i = 0; i < N; i++)
+    {
+        for (size_t j = 0; j < N_sweeps; j++)
+        {
+            yhats(i, j) = yhats_test_xinfo[j][i];
+        }
+    }
+
+    return Rcpp::List::create(Rcpp::Named("yhats") = yhats);
+}
+
+// [[Rcpp::export]]
+Rcpp::StringVector r_to_json(double y_mean, Rcpp::XPtr<std::vector<std::vector<tree>>> tree_pnt)
+{
+
+    Rcpp::StringVector result(1);
+    std::vector<std::vector<tree>> *trees = tree_pnt;
+    json j = get_forest_json(*trees, y_mean);
+    result[0] = j.dump(4);
+    return result;
+}
+
+// [[Rcpp::export]]
+Rcpp::List json_to_r(Rcpp::StringVector json_string_r)
+{
+
+    std::vector<std::string> json_string(json_string_r.size());
+    //std::string json_string = json_string_r(0);
+    json_string[0] = json_string_r(0);
+    double y_mean;
+
+    // Create trees
+    vector<vector<tree>> *trees2 = new std::vector<vector<tree>>();
+
+    // Load
+    from_json_to_forest(json_string[0], *trees2, y_mean);
+
+    // Define External Pointer
+    Rcpp::XPtr<std::vector<std::vector<tree>>> tree_pnt(trees2, true);
+
+    return Rcpp::List::create(Rcpp::Named("model_list") = Rcpp::List::create(Rcpp::Named("tree_pnt") = tree_pnt,
+                                                                             Rcpp::Named("y_mean") = y_mean));
+}
