e45c13679f7c995582ab1726edf5c1d910b830b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:44 +0000

    Add package mothpy/0.0.1/mothpy-0.0.1.tar.gz

---
+++ packages/mothpy/mothpy-0.0.1.tar.gz/mothpy-0.0.1/mothpy/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+mothpy - moth-inspired navigation models flying in `pompy` (Puff Odour-plume Model in Python)
+
+
+@author: Noam Benelli and Alex Liberzon
+"""
+name = "mothpy"
