aac716bc0078f3b49d82c4dac50e816a2d011631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:14 +0000

    Add package python-clu/0.3.0/python_clu-0.3.0-py3-none-any.whl

---
+++ packages/python-clu/python_clu-0.3.0-py3-none-any.whl/clu/dicts.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+from clu.predicates import haspyattr
+from clu.exporting import Exporter
+
+exporter = Exporter(path=__file__)
+export = exporter.decorator()
+
+# DICT FUNCTIONS: dictionary-merging
+
+@export
+def merge_two(one, two, cls=dict):
+    """ Merge two dictionaries into an instance of the specified class
+        Based on this docopt example source: https://git.io/fjCZ6
+    """
+    if not cls:
+        cls = type(one)
+    keys = frozenset(one) | frozenset(two)
+    merged = ((key, one.get(key, None) or two.get(key, None)) for key in keys)
+    return cls(merged)
+
+@export
+def merge_as(*dicts, **overrides):
+    """ Merge all dictionary arguments into a new instance of the specified class,
+        passing all additional keyword arguments to the class constructor as overrides
+    """
+    cls = overrides.pop('cls', dict)
+    if not cls:
+        cls = len(dicts) and type(dicts[0]) or dict
+    merged = cls(**overrides)
+    for d in dicts:
+        merged = merge_two(merged, d, cls=cls)
+    return merged
+
+@export
+def merge(*dicts, **overrides):
+    """ Merge all dictionary arguments into a new `dict` instance, using any
+        keyword arguments as item overrides in the final `dict` instance returned
+    """
+    if 'cls' in overrides:
+        raise NameError('Cannot override the `cls` value')
+    return merge_as(*dicts, cls=dict, **overrides)
+
+# DICT STUFF: asdict(…)
+
+@export
+def asdict(thing):
+    """ asdict(thing) → returns either thing, thing.__dict__, or dict(thing) as necessary """
+    if isinstance(thing, dict):
+        return thing
+    if haspyattr(thing, 'dict'):
+        return thing.__dict__
+    return dict(thing)
+
+# Assign the modules’ `__all__` and `__dir__` using the exporter:
+__all__, __dir__ = exporter.all_and_dir()
