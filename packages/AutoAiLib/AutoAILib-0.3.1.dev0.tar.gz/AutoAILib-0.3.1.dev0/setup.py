db8d1e2113c1fa607b75f7ae50bf85ad1031f10a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:38 +0000

    Add package AutoAiLib/0.3.1.dev0/AutoAILib-0.3.1.dev0.tar.gz

---
+++ packages/AutoAiLib/AutoAILib-0.3.1.dev0.tar.gz/AutoAILib-0.3.1.dev0/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, Extension, find_packages
+from bs4 import BeautifulSoup as soup
+from markdown import markdown
+description = markdown(open('README.md').read())
+#text = ''.join(soup(description, 'lxml').findAll(text=True))
+setup(name="AutoAILib",
+      version ='0.3.1dev',
+      packages=find_packages(),
+      license='GNU GPLv3',
+      long_description=description,
+      long_description_content_type = 'text/markdown',
+      author ='Matthew Mulhall',
+      author_email='matthewlmulhall@gmail.com',
+      install_requires=['scikit-image',  'progressbar2', 'pandas', 'keras', 'numpy', 'opencv-python','scipy',]
+      )
