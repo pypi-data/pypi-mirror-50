b677172c1e99ef164ba9e85caf95ec63620ef360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:49 +0000

    Add package smart-preprocess/0.0.3/smart_preprocess-0.0.3.tar.gz

---
+++ packages/smart-preprocess/smart_preprocess-0.0.3.tar.gz/smart_preprocess-0.0.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from os import listdir
+from os.path import isfile, join
+ttps = [f for f in listdir('smart_preprocess/help') if isfile(join('smart_preprocess/help', f))]
+ttps = [''.join(('smart_preprocess/',f)) for f in ttps]
+dm = [f for f in listdir('smart_preprocess/dmreader') if isfile(join('smart_preprocess/dmreader', f))]
+dm = [''.join(('smart_preprocess/',f)) for f in dm]
+print(ttps)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="smart_preprocess",
+    version="0.0.3",
+    author="Joshua Stuckner",
+    author_email="stuckner@vt.edu",
+    description="A package for denoising TEM video data.",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JStuckner/smart_preprocess/",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['numpy',
+                      'matplotlib',
+                      'h5py',
+                      'scipy',
+                      'scikit-image',
+                      'pillow'],
+    package_data={'help': ttps,
+                  'dmreader': dm,},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
