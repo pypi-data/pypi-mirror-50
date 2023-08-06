e8c9993505080070e1f90a27d137be5d2892c02f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:16 +0000

    Add package django-ios-storekit/1.0.6/django-ios-storekit-1.0.6.tar.gz

---
+++ packages/django-ios-storekit/django-ios-storekit-1.0.6.tar.gz/django-ios-storekit-1.0.6/storekit/receipt.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+from .appstore import AppStoreValidator
+
+
+def normal_receipt(receipt, sandbox):
+    try:
+        storekit_bundle_id = settings.STOREKIT_APP_BUNDLE_ID
+    except AttributeError:
+        raise AttributeError('Please set "STOREKIT_APP_BUNDLE_ID" in your settings.py')
+
+    validator = AppStoreValidator(
+        bundle_id=storekit_bundle_id,
+        sandbox=sandbox
+    )
+
+    return validator.validate(
+        receipt=receipt
+    )
+
+
+def subscribe_receipt(receipt, sandbox):
+    try:
+        storekit_app_bundle_id = settings.STOREKIT_APP_BUNDLE_ID
+    except AttributeError:
+        raise AttributeError('Please set "STOREKIT_APP_BUNDLE_ID" in your settings.py')
+
+    try:
+        storekit_purchased_secret = settings.STOREKIT_PURCHASED_SECRET
+    except AttributeError:
+        raise AttributeError('Please set "STOREKIT_PURCHASED_SECRET" in your settings.py')
+
+    validator = AppStoreValidator(
+        bundle_id=storekit_app_bundle_id,
+        sandbox=sandbox
+    )
+
+    return validator.validate(
+        receipt=receipt,
+        password=storekit_purchased_secret
+    )
