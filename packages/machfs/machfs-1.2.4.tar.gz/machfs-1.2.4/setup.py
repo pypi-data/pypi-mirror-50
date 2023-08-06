80a8df1cdee392df5d5bb2f6248954cabed09eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:32 +0000

    Add package machfs/1.2.4/machfs-1.2.4.tar.gz

---
+++ packages/machfs/machfs-1.2.4.tar.gz/machfs-1.2.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='machfs',
+    version='1.2.4',
+    author='Elliot Nunn',
+    author_email='elliotnunn@me.com',
+    description='Library for reading and writing Macintosh HFS volumes',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/elliotnunn/machfs',
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: System :: Filesystems',
+    ],
+    packages=['machfs'],
+    install_requires=['macresources'],
+    scripts=['bin/MakeHFS', 'bin/DumpHFS'],
+)
