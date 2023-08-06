c56db85a297910d2ccdb2421809e6c88ddf78889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:27 +0000

    Add package Draugr/0.3.0/draugr-0.3.0-py36-none-any.whl

---
+++ packages/Draugr/draugr-0.3.0-py36-none-any.whl/draugr/writers/tensorboard/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "cnheider"
+__doc__ = ""
+
+from .launcher import *
+from .tensorboard_pytorch_writer import *
+from .tensorboard_x_writer import *
