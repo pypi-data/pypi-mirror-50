60320dbbc279e599638ffba4cafe9765a91d0374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:24 +0000

    Add package artificery/0.0.2.3/artificery-0.0.2.3.tar.gz

---
+++ packages/artificery/artificery-0.0.2.3.tar.gz/artificery-0.0.2.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="artificery",
+    version="0.0.2.3",
+    author="Sergiy Popovych",
+    author_email="sergiy.popovich@gmail.com",
+    description="A tool for building pyrorch nets from spec files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/supersergiy/artificery",
+    install_requires=[
+      'scalenet',
+      'torch',
+      'torchvision'
+    ],
+    packages=setuptools.find_packages(),
+)
