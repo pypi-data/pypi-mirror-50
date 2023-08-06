2f7eaa9932e85460209f6232c6e64f51ca1f36e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:49 +0000

    Add package qprompt/0.15.3/qprompt-0.15.3.tar.gz

---
+++ packages/qprompt/qprompt-0.15.3.tar.gz/qprompt-0.15.3/setup.py
@@ -0,0 +1,22 @@
+from os.path import isfile
+from setuptools import setup, find_packages
+
+setup(
+    name = "qprompt",
+    version = "0.15.3",
+    author = "Jeff Rimko",
+    author_email = "jeffrimko@gmail.com",
+    description = "Library for quick CLI user prompts, input, and menus.",
+    license = "MIT",
+    keywords = "cli menu prompt input user library",
+    url = "https://github.com/jeffrimko/Qprompt",
+    py_modules=["qprompt"],
+    install_requires=["iterfzf"],
+    long_description=open("README.rst").read() if isfile("README.rst") else "",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3"
+    ],
+)
