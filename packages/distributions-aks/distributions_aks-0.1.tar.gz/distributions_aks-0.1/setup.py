b182ea2e2f2f6e0494b616787ad8dccf45c7d907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:37 +0000

    Add package distributions-aks/0.1/distributions_aks-0.1.tar.gz

---
+++ packages/distributions-aks/distributions_aks-0.1.tar.gz/distributions_aks-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_aks',
+      version='0.1',
+      description='Gaussian and Binomial Distributions',
+      packages=['distributions_aks'],
+      zip_safe=False)
