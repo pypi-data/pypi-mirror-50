e395b30d7eba008e2ec47f2dd174a8061fb740f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:05 +0000

    Add package refinery/2.2.2/refinery-2.2.2-py3.5.egg

---
+++ packages/refinery/refinery-2.2.2-py3.5.egg/refinery/__init__.py
@@ -0,0 +1,12 @@
+# ##############
+#   metadata   #
+# ##############
+__author__ = "Devin Bobadilla"
+#           YYYY.MM.DD
+__date__ = "2019.08.15"
+__version__ = (2, 2, 2)
+__all__ = (
+    'defs', 'heuristic_deprotection', 'repl', 'tag_index', 'widgets', 'windows',
+    'constants', 'core', 'crc_functions', 'exceptions', 'main',
+    'queue_item', 'util', 
+    )
