5a88a7eed1ca23fef9f0c3b8d8699160d605f87a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:45 +0000

    Add package drever/0.0.5/drever-0.0.5.tar.gz

---
+++ packages/drever/drever-0.0.5.tar.gz/drever-0.0.5/drever/__init__.py
@@ -0,0 +1,21 @@
+'''
+Created on Apr 12, 2019
+
+Python __init__
+
+@author: baumannt
+'''
+import os
+from vunit import VUnit
+
+from drever import pkg_info
+
+__version__ = pkg_info.get_version()
+
+VHDL_LIBRARY_NAME = "drever"
+VHDL_SRC_DIR = os.path.join(__path__[0], "vhdl/**/*.vhd")
+
+
+def add_drever_hdl_sources(vunit):  # pragma: no cover
+    drever_lib = vunit.add_library(VHDL_LIBRARY_NAME)
+    drever_lib.add_source_files(VHDL_SRC_DIR, vhdl_standard="2008")
