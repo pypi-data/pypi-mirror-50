2586c2c196af325d4111c17757c98ed6aec218c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:30 +0000

    Add package jiaxin/1.5.2.0/jiaxin-1.5.2.0-py3-none-any.whl

---
+++ packages/jiaxin/jiaxin-1.5.2.0-py3-none-any.whl/jiaxin/__init__.py
@@ -0,0 +1,5 @@
+## from __future__ import absolute_import
+name = "jiaxin"
+
+def work():
+	print("愚公爱夹心呀")
---
+++ packages/jiaxin/jiaxin-1.5.2.0-py3-none-any.whl/jx/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import
+from .jx import *
+name = "jiaxin.py"
