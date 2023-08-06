27b7796680caf810c0f1e1d7115dca525ace8731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:15 +0000

    Add package bpnet/0.0.11/bpnet-0.0.11.tar.gz

---
+++ packages/bpnet/bpnet-0.0.11.tar.gz/bpnet-0.0.11/bpnet/__main__.py
@@ -0,0 +1,34 @@
+import argh
+
+from bpnet.cli.train import bpnet_train, dataspec_stats
+from bpnet.cli.contrib import bpnet_contrib, list_contrib
+from bpnet.cli.export_bw import bpnet_export_bw
+from bpnet.cli.modisco import bpnet_modisco_run, cwm_scan
+
+# logging
+import pkg_resources
+import logging
+import logging.config
+logging.config.fileConfig(pkg_resources.resource_filename(__name__, "logging.conf"))
+logger = logging.getLogger(__name__)
+
+
+def ipynb_render(input_ipynb, output_ipynb, params=""):
+    from bpnet.utils import render_ipynb, kwargs_str2kwargs
+    render_ipynb(input_ipynb, output_ipynb, kwargs_str2kwargs(params))
+
+
+def main():
+    parser = argh.ArghParser()
+    parser.add_commands([
+        # available commands
+        bpnet_train,
+        dataspec_stats,
+        bpnet_contrib,
+        list_contrib,
+        bpnet_export_bw,
+        bpnet_modisco_run,
+        cwm_scan,
+        ipynb_render,
+    ])
+    argh.dispatch(parser)
