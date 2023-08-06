06fb9dcde2b5bd49d35f7d3bfe7f1966a8fed6f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:10 +0000

    Add package ajenti.plugin.auth-users/0.29/ajenti.plugin.auth_users-0.29.tar.gz

---
+++ packages/ajenti.plugin.auth-users/ajenti.plugin.auth_users-0.29.tar.gz/ajenti.plugin.auth_users-0.29/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+import os
+
+__requires = filter(None, open('requirements.txt').read().splitlines())
+
+setup(
+    name='ajenti.plugin.auth_users',
+    version='0.29',
+    install_requires=__requires,
+    description='Custom users authentication',
+    long_description='A Custom users authentication plugin for Ajenti panel',
+    author='Ajenti project',
+    author_email='e@ajenti.org',
+    url='http://ajenti.org',
+    packages=find_packages(),
+    include_package_data=True,
+)
