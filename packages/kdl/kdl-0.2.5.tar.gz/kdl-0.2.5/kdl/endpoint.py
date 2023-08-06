83ad327c885d09c4d599034e06acd0941f2d4c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:04 +0000

    Add package kdl/0.2.5/kdl-0.2.5.tar.gz

---
+++ packages/kdl/kdl-0.2.5.tar.gz/kdl-0.2.5/kdl/endpoint.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from enum import Enum, unique
+
+
+@unique
+class EndPoint(Enum):
+    """ 各个api的主机+路径 """
+    GetOrderExpireTime = "dev.kdlapi.com/api/getorderexpiretime"
+    GetIpWhitelist = "dev.kdlapi.com/api/getipwhitelist"
+    SetIpWhitelist = "dev.kdlapi.com/api/setipwhitelist"
+    GetKpsProxy = "kps.kdlapi.com/api/getkps"
+    GetDpsProxy = "dps.kdlapi.com/api/getdps"
+    GetOpsProxyNormalOrVip = "dev.kdlapi.com/api/getproxy"
+    GetOpsProxySvip = "svip.kdlapi.com/api/getproxy"
+    GetOpsProxyEnt = "ent.kdlapi.com/api/getproxy"
+    CheckDpsValid = "dps.kdlapi.com/api/checkdpsvalid"
+    CheckOpsValid = "dev.kdlapi.com/api/checkopsvalid"
+    GetIpBalance = "dps.kdlapi.com/api/getipbalance"
+    GetDpsValidTime = "dps.kdlapi.com/api/getdpsvalidtime"
