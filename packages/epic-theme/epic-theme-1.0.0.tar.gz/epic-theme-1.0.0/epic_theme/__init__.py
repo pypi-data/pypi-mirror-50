4bd9b0b9cdf2960dc9eab40eb5083045187f4dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:21:03 +0000

    Add package epic-theme/1.0.0/epic-theme-1.0.0.tar.gz

---
+++ packages/epic-theme/epic-theme-1.0.0.tar.gz/epic-theme-1.0.0/epic_theme/__init__.py
@@ -0,0 +1,8 @@
+from . import version
+
+__name__ = 'epic-theme'
+__version__ = '1.0.0'
+__build__ = version.VERSION
+__author__  = 'Rizki Sasri Dwitama'
+
+VERSION = __version__ # synonim
---
+++ packages/epic-theme/epic-theme-1.0.0.tar.gz/epic-theme-1.0.0/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import find_packages, setup
+import epic_theme
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name= epic_theme.__name__,
+    version= epic_theme.__version__,
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Epic admin theme',
+    long_description=README,
+    url='https://github.com/sasri-djproject/epic_theme',
+    author=epic_theme.__author__,
+    author_email='sasri.djproject@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
