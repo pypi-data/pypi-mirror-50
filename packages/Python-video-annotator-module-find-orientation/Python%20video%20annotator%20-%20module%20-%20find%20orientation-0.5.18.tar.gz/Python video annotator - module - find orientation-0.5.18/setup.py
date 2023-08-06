3ae057f96074140afa73e2feb5ecca49ee37f36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:43 +0000

    Add package Python-video-annotator-module-find-orientation/0.5.18/Python%20video%20annotator%20-%20module%20-%20find%20orientation-0.5.18.tar.gz

---
+++ packages/Python-video-annotator-module-find-orientation/Python%20video%20annotator%20-%20module%20-%20find%20orientation-0.5.18.tar.gz/Python video annotator - module - find orientation-0.5.18/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import re
+
+with open('README.md', 'r') as fd:
+    long_description = fd.read()
+
+import os, re;
+with open(os.path.join(os.path.dirname(__file__), 'pythonvideoannotator_module_findorientation','__init__.py')) as fd:
+	version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
+
+
+setup(
+	name='Python video annotator - module - find orientation',
+	version=version,
+	description="""""",
+	author=['Ricardo Ribeiro'],
+	author_email='ricardojvr@gmail.com',
+	url='https://bitbucket.org/fchampalimaud/pythonvideoannotator-module-findorientation',
+	long_description = long_description,
+    long_description_content_type = 'text/markdown',
+	packages=find_packages(),	
+)
