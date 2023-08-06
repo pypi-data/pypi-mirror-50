a499cb8b5c2a5b9783b86f84069de84795decffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:36 +0000

    Add package ab/1.0/ab-1.0.tar.gz

---
+++ packages/ab/ab-1.0.tar.gz/ab-1.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    name='ab',
+    author='Dan Masquelier',
+    author_email='dancrew32+ab@gmail.com',
+    url='https://github.com/dancrew32/ab/',
+    version='1.0',
+    packages=['ab'],
+    license='Unlicense',
+    description='A/B Testing Framework for Python with optional Multi-armed bandit implementation',
+    long_description='See usage here: https://github.com/dancrew32/ab/',
+)
