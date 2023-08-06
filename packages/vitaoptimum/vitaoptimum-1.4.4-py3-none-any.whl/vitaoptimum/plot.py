0c28b88e19e70d96ddddd281a818002d28aa1ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:35 +0000

    Add package vitaoptimum/1.4.4/vitaoptimum-1.4.4-py3-none-any.whl

---
+++ packages/vitaoptimum/vitaoptimum-1.4.4-py3-none-any.whl/vitaoptimum/plot.py
@@ -0,0 +1,209 @@
+import os
+import numpy as np
+import matplotlib.image as image
+import matplotlib.pyplot as plt
+import matplotlib.cbook as cbook
+
+from operator import sub
+from pathlib import Path
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib._png import read_png
+from matplotlib.cbook import get_sample_data
+
+
+class Logotype:
+    ''' Class that keeps parameters of a logotype that
+        should be added to different graphs.
+    '''
+    def __init__(self, file_path, x=0, y=0, alpha=0.5):
+        my_dir = os.path.dirname(os.path.realpath(__file__))
+        path = Path(os.path.join(my_dir, file_path)).resolve()
+        if not path.is_file():
+            raise FileNotFoundError("logo file is not found: " + file_path)
+        self._file_path = path
+        if x >= 0:
+            self._x = x
+        else:
+            raise ValueError("Logo position coordinate x must be positive")
+
+        if y >= 0:
+            self._y = y
+        else:
+            raise ValueError("Logo position coordinate y must be positive")
+
+        if alpha > 0 and alpha <= 1:
+            self._alpha = alpha
+        else:
+            raise ValueError("Logo alpha value must be in (0, 1]")
+
+    @property
+    def file_path(self):
+        return self._file_path
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def alpha(self):
+        return self._alpha
+
+    def add_to_3d_graph(self, range_x, range_y, ax):
+        # TODO: implement 2d logo on 3d graphs
+        with cbook.get_sample_data(self._file_path) as file:
+            img = image.imread(file)
+            xrng = plt.xlim(range_x[0], range_x[1])
+            yrng = plt.ylim(range_y[0], range_y[1])
+            ax.imshow(img,
+                    extent=[1, 0.1, 10, 0.1],
+                    origin='upper',
+                    alpha=self._alpha,
+                    aspect='auto')
+
+    def add_to_2d_graph(self, fig):
+        with cbook.get_sample_data(self._file_path) as file:
+            img = image.imread(file)
+            fig.figimage(img,
+                        self._x,
+                        self._y,
+                        zorder=1,
+                        alpha=self._alpha)
+
+
+def _add_logo_2d(logo, fig):
+    if logo:
+        if isinstance(logo, bool):
+            logo = Logotype("logo64.png", x=400, y=300, alpha=0.4)
+        if isinstance(logo, Logotype):
+            logo.add_to_2d_graph(fig)
+
+
+def _add_logo_3d(logo, ax):
+    if logo:
+        raise AttributeError("Logotypes are not supported for d>1")
+        logo.add_to_3d_graph(range_x, range_y, ax)
+
+
+def convergence(convergence, title, logo, save_to=None):
+    '''Plot convergence to a solution (result: 2d graph)
+    '''
+    fig, ax = plt.subplots()
+    _add_logo_2d(logo, fig)
+
+    ax.set_xlabel('Number of function evaluations')
+    ax.set_ylabel('Objective function')
+    ax.set_title(title)
+    ax.plot(convergence, 'b', label='line')
+    ax.grid()
+
+    if save_to:
+        plt.savefig(save_to, bbox_inches='tight')
+    else:
+        plt.show()
+
+
+def fobj_2d(fobj, title, range_x, range_y, logo, save_to=None):
+    '''Plot 2d objective function (result: 3d graph)
+    '''
+    x = np.arange(range_x[0], range_x[1], 0.01)
+    y = np.arange(range_y[0], range_y[1], 0.01)
+    X,Y = np.meshgrid(x,y)
+    zs = np.array([fobj([x,y]) for x,y in zip(np.ravel(X), np.ravel(Y))])
+    Z = zs.reshape(X.shape)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.grid(b=False, which='major', color='#D3D3D3', linestyle='-')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('fobj')
+    ax.set_title(title)
+    surf = ax.plot_surface(X, Y, Z, cmap='viridis', antialiased=False, )
+    plt.show()
+    #_add_logo_3d(logo, ax)
+
+    if save_to:
+        plt.savefig(save_to, bbox_inches='tight')
+    else:
+        plt.show()
+
+
+def fobj_1d(fobj, title, range_x, logo, save_to=None):
+    '''Plot 1d objective function (result: 2d graph)
+    '''
+    fig, ax = plt.subplots()
+    _add_logo_2d(logo, fig)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('fobj')
+    ax.set_title(title)
+
+    x = np.arange(range_x[0], range_x[1], 0.01)
+    y = fobj(x)
+
+    ax.plot(x, y, label='line')
+    ax.grid()
+
+    if save_to:
+        plt.savefig(save_to, bbox_inches='tight')
+    else:
+        plt.show()
+
+
+def optimum_1d(title, fobj, solution, eps, logo, save_to=None):
+    '''Plot optimum area for 1d objective function (result: 2d graph)
+    '''
+    fig, ax = plt.subplots()
+    _add_logo_2d(logo, fig)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('fobj')
+    ax.set_title(title)
+
+    x = np.arange(solution-eps, solution+eps, eps/100)
+    y = fobj(x)
+
+    ax.grid()
+    for i in range(9):
+        ax.plot(solution, fobj(solution),'ro', fillstyle='none', markersize=i)
+    ax.plot(x, y, label='line')
+
+    if save_to:
+        plt.savefig(save_to, bbox_inches='tight')
+    else:
+        plt.show()
+
+
+def optimum_2d(title, fobj, solution, eps, logo, save_to=None):
+    '''Plot optimum area for 1d objective function (result: 2d graph)
+    '''
+    x = np.arange(solution[0]-eps, solution[0]+eps, eps/100)
+    y = np.arange(solution[1]-eps, solution[1]+eps, eps/100)
+    X,Y = np.meshgrid(x,y)
+    zs = np.array([fobj([x,y]) for x,y in zip(np.ravel(X), np.ravel(Y))])
+    Z = zs.reshape(X.shape)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.grid(b=False, which='major', color='#D3D3D3', linestyle='-')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('fobj')
+    ax.set_title(title)
+    surf = ax.plot_surface(X, Y, Z, cmap='viridis', antialiased=False, )
+
+    ax.plot([solution[0]], [solution[1]], [fobj(solution)],'ro', markersize=7)
+
+    plt.show()
+    #_add_logo_3d(logo, ax)
+
+    if save_to:
+        plt.savefig(save_to, bbox_inches='tight')
+    else:
+        plt.show()
---
+++ packages/vitaoptimum/vitaoptimum-1.4.4-py3-none-any.whl/vitaoptimum/result.py
@@ -0,0 +1,455 @@
+import sys
+from abc import abstractmethod
+
+import vitaoptimum.plot as plot
+
+##############################################################################
+#                      ALGORITHMS RESULT MAIN BASE CLASS                     #
+##############################################################################
+
+class _ResultBase:
+
+    @abstractmethod
+    def to_string(self, separator):
+        pass
+
+    def __str__(self):
+        return self.to_string(separator="\n")
+
+    def print(self, device=sys.stdout):
+        if device:
+            device.write(self.to_string(separator="\n"))
+            device.write("\n")
+        else:
+            print(sys.stderr, "Print device is not defined")
+
+    @abstractmethod
+    def plot_convergence(self, title, logo):
+        pass
+
+    def plot_fobj_2d(self, title, range_x, range_y, logo=True, save_to=None):
+        '''Plot 2d objective function within boundaries constraints'''
+        plot.fobj_2d(self._fobj, title, range_x, range_y, logo, save_to)
+
+    def plot_fobj_1d(self, title, range_x, logo=True, save_to=None):
+        '''Plot 1d objective function within boundaries constraints'''
+        plot.fobj_1d(self._fobj, title, range_x, logo, save_to)
+
+    def plot_optimum_1d(self, title, eps, logo=True, save_to=None):
+        '''Plot found optimum for 1d objective function
+           (point on fobj within a small area)'''
+        plot.optimum_1d(title, self._fobj, self._solution, eps, logo, save_to)
+
+    def plot_optimum_2d(self, title, eps, logo=True, save_to=None):
+        '''Plot found optimum for 1d objective function
+           (point on fobj within a small area)'''
+        plot.optimum_2d(title, self._fobj, self._solution, eps, logo, save_to)
+
+
+##############################################################################
+#                      VITA OPTIMUM RESULTS BASE CLASSES                     #
+##############################################################################
+
+
+class _VitaOptimumResult(_ResultBase):
+    """Base result for all VitaOptimum Methods"""
+
+    def __init__(self, fobj, best_fobj, solution, convergence):
+        self._fobj = fobj
+        self._best_fobj = best_fobj
+        self._solution = solution
+        self._convergence = convergence
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Best objective function value: {self._best_fobj}",
+            f"Found solution: {self._solution}",
+            f"Convergence: {self._convergence}",
+        ]
+        return separator.join(lines)
+
+    def plot_convergence(self, title, logo=True):
+        '''Plot convergence of the solution search by VitaOptimum'''
+        plot.convergence(self._convergence, title, logo)
+
+    @property
+    def solution(self):
+        return self._solution
+
+    @property
+    def best_fobj(self):
+        return self._best_fobj
+
+    @property
+    def convergence(self):
+        return self._convergence
+
+
+class _VitaOptimumUnconstrainedResult(_VitaOptimumResult):
+    """Base result for all VitaOptimum Unconstrained Methods"""
+
+    def to_string(self, separator='\n'):
+        lines = [super().to_string(separator)]
+        return separator.join(lines)
+
+
+class _VitaOptimumConstrainedResult(_VitaOptimumResult):
+    """Base result for all VitaOptimum Constrained Methods"""
+
+    def __init__(self, fobj, best_fobj, solution, best_constrains, convergence):
+        self._best_constrains = best_constrains
+        _VitaOptimumResult.__init__(self, fobj,  best_fobj, solution, convergence)
+
+    def to_string(self, separator='\n'):
+        lines = [super().to_string(separator)]
+        lines.append(f"Best constraint values: {self._best_constrains}")
+        return separator.join(lines)
+
+    @property
+    def constrains(self):
+        return self._best_constrains
+
+
+
+class _VitaOptimumMixedResult(_VitaOptimumResult):
+    """Mixed Unonstrained Method Result"""
+
+    def __init__(self, fobj, best_fobj, continuous_solution, integer_solution,
+             binary_solution, permutations_solution, convergence):
+        mixed_solution = [
+            continuous_solution,
+            integer_solution,
+            binary_solution,
+            permutations_solution
+        ]
+        _VitaOptimumUnconstrainedResult.__init__(self, fobj, best_fobj,
+                mixed_solution, convergence)
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Best objective function value: {self._best_fobj}",
+            f"Found continuous solution: {self._solution[0]}",
+            f"Found integer solution: {self._solution[1]}",
+            f"Found binary solution: {self._solution[2]}",
+            f"Found permutations solution: {self._solution[3]}",
+            f"Convergence: {self._convergence}",
+        ]
+        return separator.join(lines)
+
+
+##############################################################################
+#                            VITA OPTIMUM RESULTS                            #
+##############################################################################
+
+class VitaOptimumBcsResult(_VitaOptimumConstrainedResult):
+    """Binary Constrained Method Result"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Binary Constrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumBusResult(_VitaOptimumUnconstrainedResult):
+    """Binary Unconstrained Global Optimization Method Result"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Binary Unconstrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumCcsResult(_VitaOptimumConstrainedResult):
+    """Continuous Constrained Method Result"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Continuous Constrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumCusResult(_VitaOptimumUnconstrainedResult):
+    """Continuous Unconstrained Method Result"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Continuous Unconstrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumIcsResult(_VitaOptimumConstrainedResult):
+    """Integer Constrained Method Result"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Integer Constrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumIusResult(_VitaOptimumUnconstrainedResult):
+    """Integer Unconstrained Method Result"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Integer Unconstrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumMcsResult(_VitaOptimumConstrainedResult):
+    """Mixed Constrained Method Result"""
+
+    def __init__(self, fobj, best_fobj, continuous_solution, integer_solution,
+                 binary_solution, permutations_solution, best_constrains,
+                 convergence):
+        self._best_constrains = best_constrains
+        _VitaOptimumMixedResult.__init__(
+            self, fobj, best_fobj, continuous_solution,
+            integer_solution,  binary_solution,
+            permutations_solution, convergence)
+
+    @property
+    def best_constrains(self):
+        return self._best_constrains
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Mixed Constrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        lines.append(f"Best constraint values: {self._best_constrains}")
+        return separator.join(lines)
+
+
+class VitaOptimumMusResult(_VitaOptimumUnconstrainedResult):
+    """Mixed Unconstrained Method Result"""
+
+    def __init__(self, fobj,  best_fobj, continuous_solution, integer_solution,
+                 binary_solution, permutations_solution, convergence):
+        _VitaOptimumMixedResult.__init__(
+            self, fobj, best_fobj, continuous_solution,
+            integer_solution,  binary_solution,
+            permutations_solution, convergence)
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Mixed Unconstrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumPcsResult(_VitaOptimumConstrainedResult):
+    """Permutation Constrained Method Result"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Permutation Constrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumPusResult(_VitaOptimumUnconstrainedResult):
+    """Permutation Unconstrained Method Result"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Permutation Unconstrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+##############################################################################
+#                            VITA OPTIMUM PLUS RESULTS                       #
+##############################################################################
+
+class _VitaOptimumPlusResult(_ResultBase):
+    """Base result for all VitaOptimum Plus Global Optimization Methods"""
+
+    def __init__(self, fobj, best_fobj, solution, is_converged, quality):
+        self._fobj = fobj
+        self._best_fobj = best_fobj
+        self._solution = solution
+        self._is_converged = is_converged
+        self._quality = quality
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Is converged: {self._is_converged[0]}",
+            f"Best objective function value: {self._best_fobj}",
+            f"Found solution: {self._solution}",
+            f"Quality: {self._quality}",
+        ]
+        return separator.join(lines)
+
+    def plot_convergence(self, title, logo=True):
+        '''Plot convergence of the solution search by VitaOptimum Plus'''
+        print("Not implemented")
+
+    @property
+    def solution(self):
+        return self._solution
+
+    @property
+    def quality(self):
+        return self._quality
+
+    @property
+    def best_fobj(self):
+        return self._best_fobj
+
+    @property
+    def is_converged(self):
+        return self._is_converged
+
+
+class _VitaOptimumPlusConstrainedResult(_VitaOptimumPlusResult):
+
+    def __init__(self, fobj, best_fobj, best_constrains, solution, is_converged, quality):
+        self._best_constrains = best_constrains
+        _VitaOptimumPlusResult.__init__(self, fobj, best_fobj, solution, is_converged, quality)
+
+    @property
+    def best_constrains(self):
+        return self._best_constrains
+
+    def to_string(self, separator='\n'):
+        lines = [super().to_string(separator)]
+        lines.append(f"Best constraint values: {self._best_constrains}")
+        return separator.join(lines)
+
+
+class _VitaOptimumPlusUnconstrainedResult(_VitaOptimumPlusResult):
+
+    def to_string(self, separator='\n'):
+        lines = [super().to_string(separator)]
+        return separator.join(lines)
+
+
+class VitaOptimumPlusBcsResult(_VitaOptimumPlusConstrainedResult):
+    """Binary Constrained Method Result (VO+)"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Binary Constrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumPlusBusResult(_VitaOptimumPlusUnconstrainedResult):
+    """Binary Unconstrained Method Result (VO+)"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Binary Unconstrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumPlusCcsResult(_VitaOptimumPlusConstrainedResult):
+    """Continuous Constrained Method Result (VO+)"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Continuous Constrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumPlusCusResult(_VitaOptimumPlusUnconstrainedResult):
+    """Continuous Unconstrained Method Result (VO+)"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Continuous Unconstrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumPlusIcsResult(_VitaOptimumPlusConstrainedResult):
+    """Integer Constrained Method Result (VO+)"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Integer Constrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumPlusIusResult(_VitaOptimumPlusUnconstrainedResult):
+    """Integer Unconstrained Method Result (VO+)"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Integer Unconstrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumPlusPcsResult(_VitaOptimumPlusConstrainedResult):
+    """Permutation Constrained Method Result (VO+)"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Permutation Constrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumPlusPusResult(_VitaOptimumPlusUnconstrainedResult):
+    """Permutation Unconstrained Method Result (VO+)"""
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Permutation Unconstrained Global Optimization Method Result:",
+        ]
+        lines.append(super().to_string(separator))
+        return separator.join(lines)
+
+
+class VitaOptimumPlusMusResult(_VitaOptimumPlusUnconstrainedResult):
+    def __init__(self, fobj, best_fobj, continuous_solution,
+                 integer_solution, binary_solution,
+                 permutations_solution, is_converged,
+                 quality):
+        mixed_solution = [
+            continuous_solution,
+            integer_solution,
+            binary_solution,
+            permutations_solution
+        ]
+        _VitaOptimumPlusUnconstrainedResult.__init__(
+            self, fobj, best_fobj, mixed_solution, is_converged, quality)
+
+    def to_string(self, separator='\n'):
+        lines = [
+            f"Mixed Unconstrained Global Optimization Method Result:",
+            f"Best objective function value: {self._best_fobj}",
+            f"Found continuous solution: {self._solution[0]}",
+            f"Found integer solution: {self._solution[1]}",
+            f"Found binary solution: {self._solution[2]}",
+            f"Found permutations solution: {self._solution[3]}",
+            f"Is converged: {self._convergence}",
+            f"Quality: {self._quality}",
+        ]
+        return separator.join(lines)
