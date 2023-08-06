a65102095f1a4bc9ce8d09afb615ed3a585aaecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:33 +0000

    Add package ngslite/1.0.0/ngslite-1.0.0.tar.gz

---
+++ packages/ngslite/ngslite-1.0.0.tar.gz/ngslite-1.0.0/ngslite/__init__.py
@@ -0,0 +1,42 @@
+"""
+Light-weight functions for next-generation sequencing (NGS) data analysis
+
+Python API for NGS-related command line tools
+Python functions for manipulating NGS-related data and files
+"""
+
+
+__version__ = '1.0.0'
+
+
+from .arrfunc import *
+from .bedtools import *
+from .bowtie2 import *
+from .bwa import *
+from .count import *
+from .data_class import *
+from .dnatools import *
+from .enrichment import *
+from .fasta import *
+from .fasta_gtf import *
+from .fastq import *
+from .file_conversion import *
+from .filetools import *
+from .genbank_parse import *
+from .genbank_write import *
+from .glimmer import *
+from .gtftools import *
+from .hmmer import *
+from .jellyfish import *
+from .kmertools import *
+from .locus_extractor import *
+from .merge_pfam_orf import *
+from .metaspades import *
+from .multiple_sequence_alignment import *
+from .orf_finder import *
+from .prokka import *
+from .samtools import *
+from .subsample import *
+from .synteny import *
+from .trim import *
+from .vcftools import *
