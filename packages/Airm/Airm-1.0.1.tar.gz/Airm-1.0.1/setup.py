ba73aa373b19c51f6855d8cba9f927ed75f3ed3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:45 +0000

    Add package Airm/1.0.1/Airm-1.0.1.tar.gz

---
+++ packages/Airm/Airm-1.0.1.tar.gz/Airm-1.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="Airm",
+    version='1.0.1',
+    author='Ashvini Jangid',
+    author_email="ashvinijangid@gmail.com",
+    license="MIT", install_requires=['numpy']
+)
