4366168699d7bfbb431c5a9284f158b4b398750e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:46 +0000

    Add package lambdata-chancedurr/0.0.6/lambdata_chancedurr-0.0.6-py3-none-any.whl

---
+++ packages/lambdata-chancedurr/lambdata_chancedurr-0.0.6-py3-none-any.whl/lambdata_chancedurr/__init__.py
@@ -0,0 +1,13 @@
+name = 'lambdata-chancedurr'
+
+def checkNulls(dataframe):
+        df = dataframe
+        nulls = df.isnull().sum()
+        for col, null in nulls.items():
+                print(f'\'{col}\' has {null} null value(s).')
+
+def addListToDataframe(alist, dataframe, colName = 'new_column'):
+	import pandas as pd
+	newCol = pd.DataFrame(alist, columns=[colName])
+	dataframe = dataframe.join(newCol)
+	return dataframe
