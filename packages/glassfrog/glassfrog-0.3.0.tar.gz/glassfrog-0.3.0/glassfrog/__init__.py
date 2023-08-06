c885a1c795d88d3ae1951ce0d892b4cb6b68f8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:44 +0000

    Add package glassfrog/0.3.0/glassfrog-0.3.0.tar.gz

---
+++ packages/glassfrog/glassfrog-0.3.0.tar.gz/glassfrog-0.3.0/glassfrog/__init__.py
@@ -0,0 +1,29 @@
+from glassfrog.models import (
+    Organization,
+    Circle,
+    Role,
+    Accountability,
+    Domain,
+    Policy,
+    Project,
+)
+from glassfrog.exceptions import (
+    UnsupportedModelException,
+    TokenUndefinedException,
+)
+
+
+name = "glassfrog"
+version = '0.3.0'
+
+__all__ = (
+    'Accountability',
+    'Circle',
+    'Domain',
+    'Organization',
+    'Policy',
+    'Project',
+    'Role',
+    'TokenUndefinedException',
+    'UnsupportedModelException',
+)
