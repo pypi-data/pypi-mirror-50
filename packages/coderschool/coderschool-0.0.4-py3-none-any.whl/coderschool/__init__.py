7b2b84b4c2487a961302d42088d615683696e5f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:15 +0000

    Add package coderschool/0.0.4/coderschool-0.0.4-py3-none-any.whl

---
+++ packages/coderschool/coderschool-0.0.4-py3-none-any.whl/coderschool/__init__.py
@@ -0,0 +1,9 @@
+instructor = "Do Hai Minh"
+ceo = "Charles 'Sang' Lee"
+tiger = "Nguyen Hoang Nguyen"
+cool_boy = "Nguyen Minh Anh"
+korean_guy = "Tran Cong Tan"
+
+
+def welcome(name):
+    print("Welcome to Coderschool,", name)
