cf0aa16bc20565b0d3592db2c8f1b919da807885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:02 +0000

    Add package girder-monkeybrains/1.0.4/girder-monkeybrains-1.0.4.zip

---
+++ packages/girder-monkeybrains/girder-monkeybrains-1.0.4.zip/girder-monkeybrains-1.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name='girder-monkeybrains',
+    version='1.0.4',
+    description='Displays monkey neurodevelopmental data.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/girder/monkeybrains',
+    maintainer='Kitware, Inc.',
+    maintainer_email='kitware@kitware.com',
+    packages=['girder_monkeybrains'],
+    install_requires=['girder'],
+    include_package_data=True,
+    entry_points={
+        'girder.plugin': [
+            'monkeybrains = girder_monkeybrains:MonkeybrainsPlugin'
+        ]
+    }
+)
