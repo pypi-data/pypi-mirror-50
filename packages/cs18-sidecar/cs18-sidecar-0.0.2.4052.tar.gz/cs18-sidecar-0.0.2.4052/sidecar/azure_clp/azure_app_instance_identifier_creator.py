9ddd999b3b13647fd3fa11b8e1ffe0104b2d10b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:02 +0000

    Add package cs18-sidecar/0.0.2.4052/cs18-sidecar-0.0.2.4052.tar.gz

---
+++ packages/cs18-sidecar/cs18-sidecar-0.0.2.4052.tar.gz/cs18-sidecar-0.0.2.4052/sidecar/azure_clp/azure_app_instance_identifier_creator.py
@@ -0,0 +1,17 @@
+from logging import Logger
+
+from sidecar.app_instance_identifier import AppInstanceIdentifier
+from sidecar.app_instance_identifier_creator import IAppInstanceIdentifierCreator
+
+
+class AzureAppInstanceIdentifierCreator(IAppInstanceIdentifierCreator):
+    def __init__(self, logger: Logger) -> None:
+        super().__init__(logger=logger)
+
+    def create(self, app_name: str, ip_address: str, additional_data: {}) -> AppInstanceIdentifier:
+        return self.create_from_instance_id(app_name=app_name, instance_id=additional_data['instance-id'])
+
+    def create_from_instance_id(self, app_name: str, instance_id: str) -> AppInstanceIdentifier:
+        if not instance_id:
+            raise Exception("Empty instance_id was provided")
+        return AppInstanceIdentifier(name=app_name, infra_id=instance_id)
