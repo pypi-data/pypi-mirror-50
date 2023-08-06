da73e64779413d9e99a32990a1e908d310218cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:12 +0000

    Add package stagesepx/0.4.0/stagesepx-0.4.0.tar.gz

---
+++ packages/stagesepx/stagesepx-0.4.0.tar.gz/stagesepx-0.4.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stagesepx',
+    version='0.4.0',
+    description='detect stages in video automatically',
+    author='williamfzc',
+    author_email='fengzc@vip.qq.com',
+    url='https://github.com/williamfzc/stagesepx',
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'opencv-python',
+        'opencv-contrib-python==3.4.2.17',
+        'numpy',
+        'loguru',
+        'scikit-image',
+        'scikit-learn',
+        'pyecharts',
+        'findit',
+        'jinja2',
+    ]
+)
