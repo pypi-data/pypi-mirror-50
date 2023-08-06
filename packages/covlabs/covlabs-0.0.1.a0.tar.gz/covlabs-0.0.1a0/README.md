55a378d29a254be60165e65a8c3599642057ba6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:38 +0000

    Add package covlabs/0.0.1a0/covlabs-0.0.1.a0.tar.gz

---
+++ packages/covlabs/covlabs-0.0.1.a0.tar.gz/covlabs-0.0.1a0/README.md
@@ -0,0 +1,2 @@
+# covlabs-python-uploader
+ 
---
+++ packages/covlabs/covlabs-0.0.1.a0.tar.gz/covlabs-0.0.1a0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="covlabs",
+    version="0.0.1.alpha0",
+    url="https://github.com/jeongukjae/covlabs-python-uploader",
+    author="Ukjae Jeong",
+    author_email="jeongukjae@gmail.com",
+    description="",
+    packages=find_packages(exclude=["tests"]),
+    zip_safe=False,
+    install_requires=[],
+)
