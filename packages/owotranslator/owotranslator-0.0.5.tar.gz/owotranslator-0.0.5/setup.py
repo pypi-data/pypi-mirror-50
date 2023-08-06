a0e65e54641cddb42f574247d1d8c77505847579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:41 +0000

    Add package owotranslator/0.0.5/owotranslator-0.0.5.tar.gz

---
+++ packages/owotranslator/owotranslator-0.0.5.tar.gz/owotranslator-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="owotranslator",
+    version="0.0.5",
+    author="Vimal",
+    author_email="usvimal@gmail.com",
+    description="A package that owoifies your text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/usvimal/owotranslator",
+    download_url = 'https://github.com/usvimal/owotrans/archive/0.0.5.tar.gz',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
