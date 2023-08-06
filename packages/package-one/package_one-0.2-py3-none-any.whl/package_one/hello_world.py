35ebb67bf6ceea093d1961b638e251c6e8d7fa8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:06 +0000

    Add package package-one/0.2/package_one-0.2-py3-none-any.whl

---
+++ packages/package-one/package_one-0.2-py3-none-any.whl/package_one/hello_world.py
@@ -0,0 +1,9 @@
+HELLO_WORLD_MESSAGE = f'Hello Peeps! This is my first package and print method!'
+
+
+def get_message():
+    return HELLO_WORLD_MESSAGE
+
+
+def print_hello_world():
+    print(get_message())
