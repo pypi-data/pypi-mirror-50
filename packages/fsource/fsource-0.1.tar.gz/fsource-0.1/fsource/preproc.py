ceac29b2b54b0cd9e8974f947548b0775a05cc8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:12 +0000

    Add package fsource/0.1/fsource-0.1.tar.gz

---
+++ packages/fsource/fsource-0.1.tar.gz/fsource-0.1/fsource/preproc.py
@@ -0,0 +1,8 @@
+# Copyright 2019 Markus Wallerberger
+# Released under the GNU Lesser General Public License, Version 3 only.
+# See LICENSE.txt for permissions on usage, modification and distribution
+"""Auxiliary routines"""
+from __future__ import print_function
+
+from . import lexer
+from . import parser
