32697ea96604f8b78302ae8795693b3abca522f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:50 +0000

    Add package sumologic-appclient-sdk/1.0.9/sumologic-appclient-sdk-1.0.9.tar.gz

---
+++ packages/sumologic-appclient-sdk/sumologic-appclient-sdk-1.0.9.tar.gz/sumologic-appclient-sdk-1.0.9/sumoappclient/pkggenerator/utils.py
@@ -0,0 +1,21 @@
+# -*- coding: future_fstrings -*-
+import os
+import shutil
+import boto3
+
+
+def upload_code_in_S3(filepath, bucket_name, region):
+    print("Uploading zip file in S3", region)
+    s3 = boto3.client('s3', region)
+    filename = os.path.basename(filepath)
+    s3.upload_file(filepath, bucket_name, filename,
+                   ExtraArgs={'ACL': 'public-read'})
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
