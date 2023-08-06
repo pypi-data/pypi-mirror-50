651ed892ad16d9c3de08f510b97e3eb10a8fd926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:43 +0000

    Add package bilispider/0.5.1/bilispider-0.5.1.tar.gz

---
+++ packages/bilispider/bilispider-0.5.1.tar.gz/bilispider-0.5.1/bilispider/__init__.py
@@ -0,0 +1,11 @@
+
+def main():
+    from .start import start
+    start()
+
+from .version import version
+__version__ = version
+name = 'bilispider'
+__all__ = ['bilispider']
+
+from .bilispider import *
