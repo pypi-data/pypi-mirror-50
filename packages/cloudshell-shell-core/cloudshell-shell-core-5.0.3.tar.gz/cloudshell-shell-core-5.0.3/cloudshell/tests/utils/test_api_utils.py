1ad26e6f4126cfb10f2d0046a20aaaa3ca836ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:03 +0000

    Add package cloudshell-shell-core/5.0.3/cloudshell-shell-core-5.0.3.tar.gz

---
+++ packages/cloudshell-shell-core/cloudshell-shell-core-5.0.3.tar.gz/cloudshell-shell-core-5.0.3/cloudshell/tests/utils/test_api_utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from mock import MagicMock
+from unittest import TestCase
+
+from cloudshell.shell.core.api_utils import decrypt_password_from_attribute
+
+
+class TestApiUtils(TestCase):
+    def setUp(self):
+        self.context = MagicMock()
+
+    def test_decrypt_password_by_attribute_name(self):
+        password = 'test_password'
+        attribute_name = 'Password'
+
+        api = MagicMock()
+        self.context.resource.attributes = {attribute_name: password}
+
+        api.DecryptPassword.return_value.Value = password
+        self.assertEqual(password, decrypt_password_from_attribute(api=api,
+                                                                   context=self.context,
+                                                                   password_attribute_name=attribute_name))
