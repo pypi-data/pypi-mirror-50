46cebb41ca95eed6ffbdd7a175ccc109eecbe1bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:43 +0000

    Add package oatspower/0.0.10/oatspower-0.0.10.tar.gz

---
+++ packages/oatspower/oatspower-0.0.10.tar.gz/oatspower-0.0.10/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(name='oatspower',
+      version='0.0.10',
+      description='OATS: Optimisation and Analysis Toolbox for Power Systems',
+      url='https://github.com/bukhsh/oats',
+      author='Waqquas Bukhsh',
+      author_email='wbukhsh@gmail.com',
+      license='GPLv3+',
+      packages=['oats','oats.models','oats.testcases'],
+      package_data={'oats.testcases': ['*']},
+      install_requires =[
+        'pyomo',
+        'tabulate',
+        'PyYAML',
+        'ipython[notebook]',
+        'openpyxl',
+        'pymysql',
+        'pypyodbc',
+        'pyro4',
+        'sympy',
+        'xlrd',
+        'pandas',
+      ],
+      zip_safe=False)
