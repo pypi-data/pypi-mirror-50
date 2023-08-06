0ce5947d48522af81a4af715f14ac357ed497197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:40 +0000

    Add package datatable/0.9.0/datatable-0.9.0-cp37-cp37m-macosx_10_7_x86_64.whl

---
+++ packages/datatable/datatable-0.9.0-cp37-cp37m-macosx_10_7_x86_64.whl/datatable/__git__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# © H2O.ai 2018; -*- encoding: utf-8 -*-
+#   This Source Code Form is subject to the terms of the
+#   Mozilla Public License, v2.0. If a copy of the MPL was
+#   not distributed with this file, You can obtain one at
+#   http://mozilla.org/MPL/2.0/.
+# ----------------------------------------------------------
+# This file was auto-generated from ci/setup_utils.py
+
+__git_revision__ = '6cdfbce1ce8daa6d4d1612b1f6ff73442ea21af9'
