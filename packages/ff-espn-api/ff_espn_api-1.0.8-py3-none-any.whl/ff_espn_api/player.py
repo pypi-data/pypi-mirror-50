50cf12f6f28aaeb3f0c4a621f250a9153a044e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:55 +0000

    Add package ff-espn-api/1.0.8/ff_espn_api-1.0.8-py3-none-any.whl

---
+++ packages/ff-espn-api/ff_espn_api-1.0.8-py3-none-any.whl/ff_espn_api/player.py
@@ -0,0 +1,24 @@
+from .constant import POSITION_MAP
+
+class Player(object):
+    '''Player are part of team'''
+    def __init__(self, data):
+        player = data['playerPoolEntry']['player']
+        self.name = player['fullName']
+        self.playerId = data['playerId']
+        if 'ratings' in data['playerPoolEntry']:
+            self.posRank = data['playerPoolEntry']['ratings']['0']['positionalRanking']
+        else:
+            self.posRank = 0
+        self.position = ''
+        self.eligibleSlots = [POSITION_MAP[pos] for pos in player['eligibleSlots']]
+        self.acquisitionType = data['acquisitionType']
+
+        # Get players main position
+        for pos in player['eligibleSlots']:
+            if (pos != 25 and '/' not in POSITION_MAP[pos]) or '/' in self.name:
+                self.position = POSITION_MAP[pos]
+                break
+    def __repr__(self):
+        return 'Player(%s)' % (self.name, )
+        
