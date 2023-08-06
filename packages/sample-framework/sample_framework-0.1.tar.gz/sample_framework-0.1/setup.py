0566412776f20b23e2ccd2e38a4407974e3c6d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:32 +0000

    Add package sample-framework/0.1/sample_framework-0.1.tar.gz

---
+++ packages/sample-framework/sample_framework-0.1.tar.gz/sample_framework-0.1/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+     name='sample_framework',    # This is the name of your PyPI-package.
+     version='0.1',                          # Update the version number for new releases
+     scripts=['sample_framework-0.1']                  # The name of your scipt, and also the command you'll be using for calling it
+ )
