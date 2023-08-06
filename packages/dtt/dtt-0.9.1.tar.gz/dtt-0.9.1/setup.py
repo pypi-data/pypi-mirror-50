9c31a5d09af2c20c9ca54629ac794081241f6fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:08 +0000

    Add package dtt/0.9.1/dtt-0.9.1.tar.gz

---
+++ packages/dtt/dtt-0.9.1.tar.gz/dtt-0.9.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(
+        name='dtt',
+        version='0.9.1',
+        description='the text-mode interface for docker command',
+        author='ymizushi',
+        author_email='mizushi@gmail.com',
+        url='https://github.com/ymizushi/dtt',
+        packages=find_packages(),
+        install_requires=[
+          'docker',
+          'docopt',
+          'kubernetes',
+          'toml',
+        ],
+        entry_points={
+            'console_scripts':[
+                'dtt = dtt.main:main',
+            ],
+        },
+        platforms=['POSIX'],
+        keywords=['docker', 'k8s'],
+        classifiers=[
+            'License :: OSI Approved :: MIT License',
+            "Programming Language :: Python",
+            "Development Status :: 4 - Beta",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "Topic :: Utilities",
+            "Topic :: Software Development",
+        ],
+        long_description= """dtt is the text-mode interface for docker command.
+
+the ui of dtt is inspired by vim, ranger and tig.
+
+A main usecase for dtt is to login container."""
+    )
