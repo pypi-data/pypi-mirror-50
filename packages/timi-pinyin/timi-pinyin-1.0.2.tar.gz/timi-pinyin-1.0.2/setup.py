574334717bfbdc85d9dfa947156bbfda05f3fd2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:29 +0000

    Add package timi-pinyin/1.0.2/timi-pinyin-1.0.2.tar.gz

---
+++ packages/timi-pinyin/timi-pinyin-1.0.2.tar.gz/timi-pinyin-1.0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name="timi-pinyin",
+    version="1.0.2",
+    url='http://github.com/lxl0928/timi_pinyin',
+    author='Timi long',
+    author_email='lixiaolong@smuer.cn',
+    description='根据传入的input_str="费家村设备01", 返回该字符串的所有字符的拼音首字目组成的字符串: "fjcsb01".',
+    long_description="""
+    input_str: "费家村设备01"
+    return: "fjcsb01".
+    
+    文档:  https://github.com/lxl0928/timi_pinyin/blob/master/README.md 
+    """,
+    packages=['timi_pinyin'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    extras_require={
+        "pypinyin": ["pypinyin"],
+    },
+    install_requires=["pypinyin==0.35.4"]
+)
