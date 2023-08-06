aef385a3101e238b88fa1d89b10752b8a7995230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:20 +0000

    Add package jours-feries-france/0.5.1/jours_feries_france-0.5.1.tar.gz

---
+++ packages/jours-feries-france/jours_feries_france-0.5.1.tar.gz/jours_feries_france-0.5.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name="jours_feries_france",
+    license="MIT",
+    packages=["jours_feries_france"],
+    version="0.5.1",
+    description="Get bank holidays for France",
+    author="Antoine Augusti",
+    author_email="hi@antoine-augusti.fr",
+    url="https://github.com/AntoineAugusti/jours-feries-france",
+    keywords=["bank-holidays", "jours-feries", "france"],
+    extras_require={"dev": ["nose"]},
+    python_requires=">=2.7,<4",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
