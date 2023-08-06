593adeec467f2c3e9eaae4e2570b77b146e931b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:17 +0000

    Add package frameworkilens/0.1/frameworkilens-0.1.tar.gz

---
+++ packages/frameworkilens/frameworkilens-0.1.tar.gz/frameworkilens-0.1/setup.py
@@ -0,0 +1,23 @@
+# from setuptools import find_packages, setup
+# import sys
+# import pprint
+# pprint.pprint(sys.path)
+# from package import Package
+#
+# setup(
+#     author="Ami Mahloof",
+#     author_email="author@email.com",
+#     packages=find_packages(),
+#     include_package_data=True,
+#     cmdclass={
+#         "package": Package
+#     }
+# )
+
+from setuptools import setup
+
+setup(
+    name='frameworkilens',      # name of PyPI package
+    version='0.1',          # version number, update with new releases
+    packages=['bin'] # names of packages directories
+)
