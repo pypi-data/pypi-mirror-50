6e68495f606f5bb8e6c58664a4b370ddd9fa1bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:10 +0000

    Add package hearthstone-card-generator/1.2/hearthstone_card_generator-1.2-py3-none-any.whl

---
+++ packages/hearthstone-card-generator/hearthstone_card_generator-1.2-py3-none-any.whl/hearthstone_card_generator/hearthstone_card.py
@@ -0,0 +1,31 @@
+class HearthstoneCard:
+    def __init__(self, name, description, manaCost, attack, health, durability, playerClass, cardType, rarity, race, expansion, image, goldImage):
+        self.name = name
+        self.description = description
+        self.manaCost = manaCost
+        self.attack = attack
+        self.health = health
+        self.durability = durability
+        self.playerClass = playerClass
+        self.cardType = cardType
+        self.rarity = rarity
+        self.race = race
+        self.expansion = expansion
+        self.image = image
+        self.goldImage = goldImage
+    def __eq__(self, other):
+        if isinstance(other, HearthstoneCard):
+            return\
+                self.name == other.name and\
+                self.description == other.description and\
+                self.manaCost == other.manaCost and\
+                self.attack == other.attack and\
+                self.health == other.health and\
+                self.durability == other.durability and\
+                self.playerClass == other.playerClass and\
+                self.cardType == other.cardType and\
+                self.rarity == other.rarity and\
+                self.race == other.race and\
+                self.expansion == other.expansion and\
+                self.image == other.image and\
+                self.goldImage == other.goldImage
