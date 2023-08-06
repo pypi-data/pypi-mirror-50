5a7f3bcdbe1227525b4622830b6ca5061f2d656c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:07 +0000

    Add package clidog/0.0.1565613748/clidog-0.0.1565613748-py3-none-any.whl

---
+++ packages/clidog/clidog-0.0.1565613748-py3-none-any.whl/clidog/cli.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : tql-CLI.
+# @File         : cli
+# @Time         : 2019-07-11 15:34
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  :
+
+import fire
+# from clis.spark import spark
+# from clis.info import ip
+
+
+from .clis import *
+
+for py in dir():
+    print(py)
+    if py.startswith("cli_"):
+
+        py = getattr(eval(py), py[4:-3])
+
+# print(model_class())
+# getattr("clis.info", "info_local")
+# for (k, v) in self.scen2processorNames.items():
+#     self.scenarios2processor[k] = []
+#     for name in v:
+#         __import__("models." + name + "_model")
+#         model_py = getattr(models, name + "_model")
+#         model_class = getattr(model_py, name + "Model")
+#         self.scenarios2processor[k].append(model_class())
+
+
+def main():
+    fire.Fire()
+
+
+if __name__ == '__main__':
+    fire.Fire()
