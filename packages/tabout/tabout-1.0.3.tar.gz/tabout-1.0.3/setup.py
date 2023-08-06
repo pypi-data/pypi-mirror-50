a764e60bc920a4db454c34af0103c8eae22dc4cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:36 +0000

    Add package tabout/1.0.3/tabout-1.0.3.tar.gz

---
+++ packages/tabout/tabout-1.0.3.tar.gz/tabout-1.0.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tabout",
+    version="1.0.3",
+    author="Dan Averbuj",
+    author_email="dan.averbuj@gmail.com",
+    description="Create sound and/or slack notifications within your script",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/daverbuj/Tabout",
+    packages=setuptools.find_packages(),
+    install_requires=['playsound', 'slackclient', 'pox']
+)
