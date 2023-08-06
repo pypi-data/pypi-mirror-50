d783b5937ee7dea793f75f2e4aa13a7587bed808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:22 +0000

    Add package anna-unittasks/1.0.4/anna_unittasks-1.0.4.tar.gz

---
+++ packages/anna-unittasks/anna_unittasks-1.0.4.tar.gz/anna_unittasks-1.0.4/anna_unittasks/current_url.py
@@ -0,0 +1,11 @@
+from anna_lib.selenium import events
+from anna_lib.task.abstract_task import AbstractTask
+
+
+class CurrentUrl(AbstractTask):
+	def __execute__(self):
+		events.click(self.driver, '#test-current-url')
+
+	def after_execute(self):
+		self.assert_url_equals('http://annahub.se:8000/test/switchto')
+		self.assert_in_url('test/switchto')
