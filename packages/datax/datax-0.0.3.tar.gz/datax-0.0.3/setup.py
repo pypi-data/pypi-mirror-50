9c82b4f17aff6afeadfcdcbaf5ab4598015176e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:00 +0000

    Add package datax/0.0.3/datax-0.0.3.tar.gz

---
+++ packages/datax/datax-0.0.3.tar.gz/datax-0.0.3/setup.py
@@ -0,0 +1,44 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
+requires = [
+    'dxql == 0.0.6',
+    'click >= 7.0',
+]
+
+test_require = [
+    'pytest >= 5.0.1',
+    'pytest-cov >= 2.7.1',
+    'codecov >= 2.0.15',
+]
+
+setup(
+    name='datax',
+    version='0.0.3',
+    description='DataXplorer ',
+    long_description=README,
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+    author='Eric Frechette',
+    author_email='frechetta93@gmail.com',
+    url='https://github.com/Frechetta/DataXplorer',
+    keywords='data search query',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'test': test_require,
+    },
+    install_requires=requires,
+    python_requires='>= 3.7',
+    long_description_content_type='text/markdown',
+    entry_points = {
+        'console_scripts': ['dx=datax.runner:run']
+    },
+)
