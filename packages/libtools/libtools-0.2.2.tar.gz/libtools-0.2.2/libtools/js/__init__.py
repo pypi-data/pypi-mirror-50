fe6784af3b6315e1d36fc820f2f0b4d245f3b4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:11 +0000

    Add package libtools/0.2.2/libtools-0.2.2.tar.gz

---
+++ packages/libtools/libtools-0.2.2.tar.gz/libtools-0.2.2/libtools/js/__init__.py
@@ -0,0 +1,9 @@
+"""
+Javascript Obj Notation Operations
+"""
+
+from libtools.js.parse_json import ParameterSet
+from libtools.js.export import export_iterobject
+
+# legacy pyaws.utils support
+from libtools.js.export import export_iterobject as export_json_object
