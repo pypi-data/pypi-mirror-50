6122b97418d102bcefd12983ed6137695188d97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:36 +0000

    Add package blur-clip-board-image-cli/0.0.3/blur-clip-board-image-cli-0.0.3.tar.gz

---
+++ packages/blur-clip-board-image-cli/blur-clip-board-image-cli-0.0.3.tar.gz/blur-clip-board-image-cli-0.0.3/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="blur-clip-board-image-cli",
+    url='https://github.com/kenchan0130/blur-clip-board-image-for-macOS',
+    version="0.0.3",
+    description="Blur clip board image command line tool for macOS",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Tadayuki Onishi",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "blur-clip-board-image=blur_clip_board_image.app:main"
+        ]
+    },
+    license="MIT"
+)
