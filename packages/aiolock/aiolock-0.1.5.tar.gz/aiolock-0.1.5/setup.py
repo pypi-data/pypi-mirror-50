14a5c36512657f9f84066c7a66ce4b4069e6db64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:53 +0000

    Add package aiolock/0.1.5/aiolock-0.1.5.tar.gz

---
+++ packages/aiolock/aiolock-0.1.5.tar.gz/aiolock-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+desc = """
+do not try
+"""
+
+setup(
+      name='aiolock',
+      version='0.1.5',
+      packages=find_packages(),
+      install_requires=[
+            "aioredis>=1.2.0",  # 基于这个版本写的
+      ],
+      description=desc,
+      author='bigpangl\n\r',
+      author_email='bigpangl@163.com\n\r',
+      url="https://github.com/bigpangl \n\r",
+      license="MIT",
+)
