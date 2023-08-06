9e6c477ca4ec6e6319c0a7aa4bf119c49fb05cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:37 +0000

    Add package sopel-modules.github/0.2.0a16/sopel_modules.github-0.2.0a16.tar.gz

---
+++ packages/sopel-modules.github/sopel_modules.github-0.2.0a16.tar.gz/sopel_modules.github-0.2.0a16/sopel_modules/github/__init__.py
@@ -0,0 +1,14 @@
+# coding=utf8
+"""Sopel GitHub
+
+GitHub module for Sopel
+"""
+from __future__ import unicode_literals, absolute_import, division, print_function
+
+from .github import *
+
+__author__ = 'maxpowa'
+__email__ = 'maxpowa@outlook.com'
+__version__ = '0.2.0a16'
+__repo__ = 'https://github.com/sopel-irc/sopel-github'
+
