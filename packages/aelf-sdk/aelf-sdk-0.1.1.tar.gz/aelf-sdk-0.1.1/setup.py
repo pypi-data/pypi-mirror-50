81ed88d8e2a5c6b655b74e12a4c0066516e449c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:45 +0000

    Add package aelf-sdk/0.1.1/aelf-sdk-0.1.1.tar.gz

---
+++ packages/aelf-sdk/aelf-sdk-0.1.1.tar.gz/aelf-sdk-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aelf-sdk',
+    version='0.1.1',
+    description='Python SDK for AElf',
+    url='https://github.com/rosona/aelf-sdk.py',
+    packages=['aelf'],
+    install_requires=['requests', 'protobuf', 'base58', 'coincurve']
+)
