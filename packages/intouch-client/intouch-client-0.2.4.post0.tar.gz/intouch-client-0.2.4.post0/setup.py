16a75e188300749f1f660fec253f436c31388d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:07 +0000

    Add package intouch-client/0.2.4.post0/intouch-client-0.2.4.post0.tar.gz

---
+++ packages/intouch-client/intouch-client-0.2.4.post0.tar.gz/intouch-client-0.2.4.post0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+VERSION = "0.2.4.post0"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="intouch-client",
+    version=VERSION,
+    author="David Bonnes",
+    author_email="zxdavb@gmail.com",
+    description="A aiohttp-based client for Intergas Intouch Lan2RF systems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zxdavb/intouch-client",
+    download_url = 'https://github.com/zxdavb/intouch-client/archive/VERSION.tar.gz',
+    packages=setuptools.find_packages(),
+    keywords = ['intergas', 'intouch', 'lan2rf'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 7 - Inactive",
+    ],
+)
