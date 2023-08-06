af9faaba0c7a44ac25f07bdd5969a97ab5ae26d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:39 +0000

    Add package pypax/0.0.73/pypax-0.0.73.tar.gz

---
+++ packages/pypax/pypax-0.0.73.tar.gz/pypax-0.0.73/pypax/__init__.py
@@ -0,0 +1,22 @@
+__version__ = "0.0.73"
+
+from .pypax import Pax
+from . import pax_deploy
+from . import pax_global
+from . import pax_initialize
+
+
+def instance():
+    return Pax.instance()
+
+
+def configure():
+    pax_global.configure(instance())
+
+
+def initialize():
+    pax_initialize.initialize(instance())
+
+
+def deploy(tag: str="default"):
+    pax_deploy.deploy(instance(), tag)
