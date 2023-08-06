ff3ff1f97f8f2575363d6d19a389922acc60c140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:58 +0000

    Add package caleb/0.6.2/caleb-0.6.2-py3-none-any.whl

---
+++ packages/caleb/caleb-0.6.2-py3-none-any.whl/caleb/app.py
@@ -0,0 +1,102 @@
+import logging
+import os
+from typing import Set
+
+from .file_handler import AuxHandler, BibHandler
+from .reference import Reference
+
+
+class Application:
+    """Glues together all the functionality of this package.
+
+    This class is intended to be called by the `cmdline` module. Afterwards, it
+    uses the `reference` module to obtain the bibtex entries. Then it uses
+    `file_handler` to append the entries to .bib file.
+
+    Args:
+        input_name (str): Path to tex file whose bib entries we need to add.
+    """
+
+    def __init__(
+        self, input_name: str, take_first: bool = False, method: str = "crossref"
+    ):
+        # Normalize name by removing .tex and .aux, if necessary.
+        filename, file_extension = os.path.splitext(input_name)
+        if file_extension in [".tex", ".aux"]:
+            input_name = filename
+
+        # Set filenames
+        aux_file = input_name + ".aux"
+        aux_h = AuxHandler(aux_file)
+        bib_file_name = aux_h.bibdata() + ".bib"
+
+        dirname = os.path.dirname(aux_file)
+        bib_file = os.path.join(dirname, bib_file_name)
+
+        logging.info(f"aux file: {aux_file}")
+        logging.info(f"bib file: {bib_file}")
+        self.aux_file = aux_file
+        self.bib_file = bib_file
+
+        logging.info(f"Using {method} for citations")
+        self.method = method
+
+        logging.info(f"Taking first citation found: {take_first}")
+        self.take_first = take_first
+
+    def go(self, dry_run: bool = False) -> None:
+        """Fill in the bibtex entries.
+
+        Args:
+            take_first (bool): Whether to just take the first bibtex entry.
+            method (str): Determines whether we use `ams` or `crossref`.
+        """
+        aux_h = AuxHandler(self.aux_file)
+        bib_h = BibHandler(self.bib_file)
+
+        requested_citation_keys = aux_h.citation_keys()
+        existing_bibs = bib_h.citation_keys()
+        missing_cits = requested_citation_keys.difference(existing_bibs)
+        logging.info(f"List of missing citations: {missing_cits}")
+
+        entries_to_append = self.get_all(missing_cits)
+        if dry_run:
+            print(entries_to_append)
+
+        bib_h.append(entries_to_append)
+
+    def get_all(self, missing_cits: Set[str]) -> str:
+        """Retrieve all citations given the keys.
+
+        Args:
+            missing_cits (list of str): a list of citation keys
+
+        Returns:
+            str: all bibtex entries as a single string
+        """
+        ans = ""
+        for key in missing_cits:
+            bibtex = self.get_single(key)
+            logging.info(f"Appending: \n{bibtex}")
+            ans = ans + bibtex + "\n"
+        return ans
+
+    def get_single(self, key: str) -> str:
+        """Retrieve a citation given the key.
+
+        Args:
+            key (str): the citation key
+
+        Returns:
+            str: a bibtex citation
+        """
+        logging.info(f"Working on: {key}")
+        new_bib = Reference(key, method=self.method)
+        if not new_bib.exists():
+            logging.warning(f"No results found for: {key}")
+            return ""
+        elif not new_bib.is_unique():
+            logging.warning(f"Multiple results found for: {key}")
+            if not self.take_first:
+                return ""
+        return new_bib.bibtex()
---
+++ packages/caleb/caleb-0.6.2-py3-none-any.whl/caleb/reference.py
@@ -0,0 +1,169 @@
+"""This module is used obtaining citations from references. Currently, this
+module can use both the crossref api and can scrape the AMS page. Eventually,
+more sources will be available.
+"""
+
+import requests
+from crossref_commons.iteration import iterate_publications_as_json
+from crossref_commons.retrieval import get_publication_as_refstring
+
+
+class Reference:
+    """Given a search key, this class will be able to retrieve data relating to
+    the corresponding bibtex entry.
+
+    Note:
+        The key is assumed to be the form `author:title:year` or
+        `author:title`. Underscores are replaced with a space.
+    """
+
+    def __init__(self, key: str, method: str = "crossref") -> None:
+        self.key = key
+
+        # replace underscore by space
+        spaced_key = key.replace("_", " ")
+
+        # data for query
+        self.pieces = spaced_key.split(":")
+
+        self.method = method
+
+    def _get_bibtex(self) -> None:
+        """Calls the correct version of _get_bibtex_XXX given self.method
+
+        Returns:
+            str: A bibtex entry.
+        """
+        if self.method == "crossref":
+            self._get_bibtex_crossref()
+        elif self.method == "ams":
+            self._get_bibtex_ams()
+        else:
+            raise NotImplementedError()
+
+    def _get_bibtex_ams(self) -> None:
+        """Fetch the bibtex entry from amsmrlookup.
+
+        Note:
+            Results are cached.
+
+        Returns:
+            str: A bibtex entry.
+        """
+        self.payload = dict(zip(["au", "ti", "year"], self.pieces))
+        self.payload["format"] = "bibtex"
+        r = requests.get("https://mathscinet.ams.org/mrlookup", params=self.payload)
+        output = r.text
+
+        # set existence and uniqueness
+        num_results = output.count("<pre>")
+        if num_results == 0:
+            self._exists = False
+            return
+        elif num_results == 1:
+            self._exists = True
+            self._is_unique = True
+        else:
+            self._exists = True
+            self._is_unique = False
+
+        # This is *almost* correct. We just have to fix the key.
+        bibtex = output.split("<pre>")[1].split("</pre>")[0].strip("\n")
+
+        # Here we assume the first line is always
+        # @something {OLD_CITATION,\n
+        # Replace OLD_CITATION with citation
+        # Use { and ,\n to find and replace
+        a, b = bibtex.split("{", 1)
+        self._bibtex = a + "{" + self.key + ",\n" + b.split(",\n", 1)[1]
+        return
+
+    def _get_bibtex_crossref(self) -> None:
+        """Internal function to fetch the bibtex entry and determine existence
+        and uniqueness.
+
+        Note:
+            Results are cached.
+
+        Returns:
+            str: A bibtex entry.
+        """
+        queries = dict(
+            zip(["query.author", "query.title", "query.bibliographic"], self.pieces)
+        )
+        queries["sort"] = "relevance"
+
+        iter_pub = iterate_publications_as_json(queries=queries)
+        try:
+            doi = next(iter_pub)["DOI"]
+            self._exists = True
+        except StopIteration:
+            self._exists = False
+            return
+        try:
+            next(iter_pub)
+            self._is_unique = False
+        except StopIteration:
+            self._is_unique = True
+
+        # This is almost correct! We just need to change the citation key to
+        # self.key
+        raw_bibtex: str = get_publication_as_refstring(doi, "bibtex")
+
+        # Here we assume the first line is always
+        # @something {OLD_CITATION,
+        # Replace OLD_CITATION with citation
+        # Use { and , to find and replace
+        a, b = raw_bibtex.split("{", 1)
+        self._bibtex = a + "{" + self.key + "," + b.split(",", 1)[1]
+
+        # Remove leading whitespace if necessary
+        if self._bibtex[0] == " ":
+            self._bibtex = self._bibtex[1:]
+        return
+
+    def bibtex(self) -> str:
+        """Return the first bibtex entry found given the citation. Return the
+        empty string if no bibtex entry exists.
+
+        Note:
+            This calls _get_bibtex if it has not been ran already.
+
+        Returns:
+            str: bibtex entry
+        """
+        if not self.exists():
+            return ""
+        return self._bibtex
+
+    def exists(self) -> bool:
+        """Return whether a citation for this reference exists.
+
+        Note:
+            This calls _get_bibtex if it has not been ran already.
+
+        Returns:
+            bool: True if citations exists and False otherwise.
+        """
+        try:
+            return self._exists
+        except AttributeError:
+            self._get_bibtex()
+        return self._exists
+
+    def is_unique(self) -> bool:
+        """Return whether a citation for this reference is unique.
+
+        Note:
+            Since self.exists() calls self._get_bibtex(), we don't need to make
+            sure to call self._get_bibtex()
+
+            Uniqueness only makes sense given existence.
+
+        Returns:
+            bool: True if citations is unique and False otherwise.
+        """
+        if not self.exists():
+            raise ValueError("Determining uniqueness requires existence.")
+
+        return self._is_unique
