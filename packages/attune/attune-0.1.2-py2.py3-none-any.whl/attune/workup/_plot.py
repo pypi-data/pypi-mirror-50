85dd65abee53f860ecb74ecce07afc8305fe74c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:10 +0000

    Add package attune/0.1.2/attune-0.1.2-py2.py3-none-any.whl

---
+++ packages/attune/attune-0.1.2-py2.py3-none-any.whl/attune/workup/_plot.py
@@ -0,0 +1,129 @@
+import WrightTools as wt
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_intensity(data, channel, dependent, curve, prior_curve=None, raw_offsets=None):
+    fig, gs = wt.artists.create_figure(
+        width="single", nrows=2, cols=[1, "cbar"], default_aspect=.5
+    )
+    ax = plt.subplot(gs[0, 0])
+    curve_plot_kwargs = {"lw": 5, "c": "k", "alpha": .5}
+    prior_curve_plot_kwargs = {"lw": 2, "c": "k"}
+    ax.plot(curve.setpoints[:], curve[dependent][:], **curve_plot_kwargs)
+    if prior_curve:
+        ax.plot(prior_curve.setpoints[:], prior_curve[dependent][:], **prior_curve_plot_kwargs)
+    ax.set_ylabel(dependent)
+    wt.artists.plot_gridlines()
+    ax.set_xlim(*curve.get_limits())
+    ax.xaxis.set_tick_params(label1On=False)
+
+    ax = plt.subplot(gs[1, 0])
+    graymap = "greyscale"
+    ax.pcolor(data, channel=f"{channel}_orig", cmap=graymap)
+    ax.pcolor(data, channel=channel)
+    if prior_curve:
+        ypoints = (
+            curve[dependent][:]
+            - prior_curve(curve.setpoints[:], curve.setpoints.units, full=False)[dependent]
+        )
+    else:
+        ypoints = curve[dependent][:]
+
+    if raw_offsets is not None:
+        ax.plot(curve.setpoints[:], raw_offsets, c="grey", lw=5, alpha=0.5)
+
+    ax.plot(curve.setpoints[:], ypoints, **curve_plot_kwargs)
+    ax.axhline(0, **prior_curve_plot_kwargs)
+    wt.artists.plot_gridlines()
+    ax.set_ylabel(r"$\mathsf{{\Delta {dependent}}}$".format(dependent=dependent))
+    ax.set_xlabel(f"Setpoint ({curve.setpoints.units})")
+    ax.set_xlim(*curve.get_limits())
+
+    cax = plt.subplot(gs[1, 1])
+    ticks = np.linspace(data[channel].null, data[channel].max(), 11)
+    wt.artists.plot_colorbar(
+        cax, clim=(data[channel].null, data[channel].max()), ticks=ticks, label=channel
+    )
+
+    return fig, gs
+
+def plot_setpoint(data, channel, dependent, curve, prior_curve=None, raw_offsets=None):
+    fig, gs = wt.artists.create_figure(
+        width="single", nrows=2, cols=[1, "cbar"], default_aspect=.5
+    )
+    ax = plt.subplot(gs[0, 0])
+    curve_plot_kwargs = {"lw": 5, "c": "k", "alpha": .5}
+    prior_curve_plot_kwargs = {"lw": 2, "c": "k"}
+    ax.plot(curve.setpoints[:], curve[dependent][:], **curve_plot_kwargs)
+    ax.set_xlim(*curve.get_limits())
+    ax.xaxis.set_tick_params(label1On=False)
+    if prior_curve:
+        ax.plot(prior_curve.setpoints[:], prior_curve[dependent][:], **prior_curve_plot_kwargs)
+    ax.set_ylabel(dependent)
+    wt.artists.plot_gridlines()
+
+    ax = plt.subplot(gs[1, 0])
+    #data[channel][:] -= data.axes[0][:]
+    data[channel].signed = True
+    limits = -0.05*data[channel].mag(), 0.05 * data[channel].mag()
+    ax.pcolor(data, channel=channel, vmin=limits[0], vmax=limits[1])
+    ax.set_xlim(*curve.get_limits())
+    ax.set_ylim(data.axes[1].min(), data.axes[1].max())
+    if prior_curve:
+        ypoints = (
+            curve[dependent][:]
+            - prior_curve(curve.setpoints[:], curve.setpoints.units, full=False)[dependent]
+        )
+    else:
+        ypoints = curve[dependent][:]
+
+    if raw_offsets is not None:
+        ax.plot(curve.setpoints[:], raw_offsets, c="grey", lw=5, alpha=0.5)
+    ax.plot(curve.setpoints[:], ypoints, **curve_plot_kwargs)
+    ax.axhline(0, **prior_curve_plot_kwargs)
+    wt.artists.plot_gridlines()
+    ax.set_ylabel(fr"$\mathsf{{\Delta {dependent}}}$")
+    units = curve.setpoints.units
+    if units is None:
+        ax.set_xlabel("Setpoint")
+    else:
+        symbol = wt.units.get_symbol(units)
+        units_str = wt.units.dicts[wt.units.kind(units)][units][2]
+        label = fr"$\mathsf{{{symbol}_{{setpoint}}\,\left({units_str}\right)}}$"
+        ax.set_xlabel(label)
+    ax.set_xlim(*curve.get_limits())
+
+    cax = plt.subplot(gs[1, 1])
+    ticks = np.linspace(*limits, 11)
+    wt.artists.plot_colorbar(
+        cax, clim=limits, ticks=ticks, label=channel, cmap="signed"
+    )
+
+    return fig, gs
+
+
+def plot_tune_test(data, channel, curve, prior_curve, raw_offsets=None):
+    fig, gs = wt.artists.create_figure(default_aspect=0.5, cols=[1, "cbar"])
+    # heatmap
+    ax = plt.subplot(gs[0, 0])
+    ax.pcolor(data, channel=channel)
+    ax.set_xlim(data.axes[0].min(), data.axes[0].max())
+    # lines
+    if raw_offsets is not None:
+        ax.plot(curve.setpoints[:], raw_offsets, c="grey", lw=5, alpha=0.5)
+
+    ax.plot(curve.setpoints[:], curve.setpoints[:] - prior_curve.setpoints[:], c="k", lw=5, alpha=0.5)
+    ax.axhline(c="k", lw=1)
+    ax.grid()
+
+    ax.set_xlabel(r"$\mathsf{{Setpoint, ({units})}}$".format(units=curve.setpoints.units))
+    ax.set_ylabel(r"$\mathsf{{\Delta {symbol}}}$".format(symbol=wt.units.get_symbol(curve.setpoints.units)))
+
+    # colorbar
+    cax = plt.subplot(gs[:, -1])
+    label = channel
+    ticks = np.linspace(0, data[channel].max(), 7)
+    wt.artists.plot_colorbar(cax=cax, label=label, ticks=ticks)
+
+    return fig, gs
---
+++ packages/attune/attune-0.1.2-py2.py3-none-any.whl/attune/workup/_tune_test.py
@@ -0,0 +1,108 @@
+"""Methods for processing OPA 800 tuning data."""
+
+
+import pathlib
+
+import numpy as np
+
+import WrightTools as wt
+
+from ._plot import plot_tune_test
+
+__all__ = ["tune_test"]
+
+def _offsets(data, channel_name, tune_points, *, spline=True, **spline_kwargs):
+    data.moment(axis=1, channel=channel_name, moment=1)
+    offsets = data[f"{channel_name}_1_moment_1"].points
+
+    if spline:
+        spline = wt.kit.Spline(data.axes[0].points, offsets, **spline_kwargs)
+        return spline(tune_points).clip(data.axes[1].min(), data.axes[1].max())
+    if np.allclose(data.axes[0].points, tune_points):
+        return offsets.clip(data.axes[1].min(), data.axes[1].max())
+    else:
+        raise ValueError("Data points and curve points do not match, and splining disabled")
+
+
+def tune_test(
+    data, channel, curve=None, *, level=False, gtol=0.01, ltol=0.1, autosave=True, save_directory=None
+):
+    """Workup a Tune Test.
+
+    Parameters
+    ----------
+    data : wt.data.Data
+        should be in (setpoint, dependent)
+    channel: wt.data.Channel or int or str
+        channel to process
+    curve: attune.Curve, optional
+        curve object to modify (Default None: make a new curve)
+    level: bool, optional
+        toggle leveling data (Defalts to False)
+    gtol: float, optional
+        global tolerance for rejecting noise level relative to global maximum
+    ltol: float, optional
+        local tolerance for rejecting data relative to slice maximum
+    autosave: bool, optional
+        toggles saving of curve file and images (Defaults to True)
+    save_directory: Path-like
+        where to save (Defaults to current working directory)
+
+    Returns
+    -------
+    attune.Curve
+        New curve object.
+    """
+    data = data.copy()
+    data.convert("wn")
+    # make data object
+    if curve is not None:
+        old_curve = curve.copy()
+        old_curve.convert("wn")
+        setpoints = old_curve.setpoints
+    else:
+        old_curve = None
+        setpoints = Setpoints(data.axes[0].points, data.axes[0].expression, data.axes[0].units)
+
+    if isinstance(channel, (int, str)):
+        channel = data.channels[wt.kit.get_index(data.channel_names, channel)]
+        orig_channel = data.create_channel(f"{channel.natural_name}_orig", channel, units=channel.units)
+
+    # TODO: check if level does what we want
+    if level:
+        data.level(channel.natural_name, 0, -3)
+
+    # TODO: gtol/ltol should maybe be moved to wt
+    cutoff = channel.max() * gtol
+    channel.clip(min=cutoff)
+    max_axis = tuple(i for i, v in enumerate(data.axes[0].shape) if v > 1)
+    cutoff = np.amax(channel[:], axis=1, keepdims=True) * ltol
+    channel.clip(min=cutoff)
+
+    offsets = _offsets(data, channel.natural_name, setpoints[:])
+    try:
+        raw_offsets = _offsets(data, channel.natural_name, data.axes[0].points, spline=False)
+    except ValueError:
+        raw_offsets = None
+
+    # make curve ----------------------------------------------------------------------------------
+    new_curve = old_curve.copy()
+    new_curve.setpoints.positions += offsets
+    new_curve.interpolate()
+
+    # plot ----------------------------------------------------------------------------------------
+
+    fig, _ = plot_tune_test(data, channel.natural_name, new_curve, prior_curve=old_curve, raw_offsets=raw_offsets)
+
+    new_curve.map_setpoints(setpoints[:], units=setpoints.units)
+    new_curve.convert(curve.setpoints.units)
+    data.axes[0].convert(curve.setpoints.units)
+    # finish --------------------------------------------------------------------------------------
+    if autosave:
+        if save_directory is None:
+            save_directory = "."
+        save_directory = pathlib.Path(save_directory)
+        new_curve.save(save_directory=save_directory, full=True)
+        p = save_directory / "tune_test.png"
+        wt.artists.savefig(p, fig=fig)
+    return new_curve
