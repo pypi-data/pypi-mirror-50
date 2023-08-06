ca9ff08704e1bec1354384e1ef3b0c92bdcfff6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:23 +0000

    Add package holdouts-generator/0.0.29/holdouts_generator-0.0.29.tar.gz

---
+++ packages/holdouts-generator/holdouts_generator-0.0.29.tar.gz/holdouts_generator-0.0.29/holdouts_generator/__init__.py
@@ -0,0 +1,12 @@
+from .random_holdout import random_holdout, random_holdouts
+from .chromosomal_holdout import chromosomal_holdout, chromosomal_holdouts
+from .holdouts_generator import holdouts_generator, clear_cache, cached_holdouts_generator
+from .store_results import store_keras_result, store_result, load_result, load_results, delete_results, delete_deprecated_results, merge_results, merge_all_results
+from .store_results import delete_all_deprecated_results, delete_all_duplicate_results
+from .work_in_progress import add_work_in_progress, clear_work_in_progress, skip, clear_all_work_in_progress
+
+__all__ = ["holdouts_generator", "cached_holdouts_generator",
+           "clear_cache", "chromosomal_holdouts", "random_holdouts",
+           "skip", "store_keras_result", "store_result", "load_result", "load_results",
+           "delete_results", "delete_deprecated_results", "merge_results", "merge_all_results", "add_work_in_progress", "clear_work_in_progress",
+           "delete_all_deprecated_results", "delete_all_duplicate_results", "clear_all_work_in_progress"]
