48b56b78e93e1a392b0216daa053d486cc7f8efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:22 +0000

    Add package easycli/1.4.0/easycli-1.4.0.tar.gz

---
+++ packages/easycli/easycli-1.4.0.tar.gz/easycli-1.4.0/easycli/root.py
@@ -0,0 +1,42 @@
+import sys
+import argparse
+import traceback
+from os import path
+
+from .command import Command
+
+
+class Root(Command):
+    __completion__ = None
+    __abbreviation__ = None
+
+    def __init__(self):
+        if self.__completion__:
+            from .completion import Completion
+            self.__arguments__.append(Completion)
+
+        super().__init__()
+
+        if self.__completion__:
+            import argcomplete
+            argcomplete.autocomplete(self._parser)
+
+    @classmethod
+    def _create_parser(self):
+        return argparse.ArgumentParser(
+            prog=path.basename(self.__command__ or sys.argv[0]),
+            description=self.__help__
+        )
+
+    def _execute_subcommand(self, args):
+        return args.func(args)
+
+    def main(self, argv=None):
+        # Actual argument parsing
+        args = self._parser.parse_args(argv)
+
+        if hasattr(args, 'func'):
+            return self._execute_subcommand(args)
+        else:
+            return self(args)
+
