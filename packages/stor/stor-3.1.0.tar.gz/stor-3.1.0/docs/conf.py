afcb066e01a3618f231c59d9eb2939ff2639a80c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:53 +0000

    Add package stor/3.1.0/stor-3.1.0.tar.gz

---
+++ packages/stor/stor-3.1.0.tar.gz/stor-3.1.0/docs/conf.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+#
+# stor documentation build configuration file
+import os
+import subprocess
+
+
+def get_version():
+    """
+    Extracts the version number from setup.py
+    """
+    proc = subprocess.Popen(['make', '--no-print-directory', 'version'], cwd='..',
+                            stdout=subprocess.PIPE)
+    return str(proc.communicate()[0].strip())
+
+
+# -- General configuration ------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'toc'
+
+# default role for "`" (makes it attempt to match against references within project)
+default_role = 'any'
+
+# General information about the project.
+project = u'stor'
+copyright = u'2015, Counsyl Inc'
+
+# The short X.Y version.
+version = get_version()
+# The full version, including alpha/beta/rc tags.
+release = version
+
+exclude_patterns = ['_build']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+intersphinx_mapping = {
+    'python': ('http://python.readthedocs.org/en/v2.7.2/', None),
+}
+
+# -- Options for HTML output ----------------------------------------------
+
+html_theme = 'default'
+#html_theme_path = []
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ['_static']
+
+# Custom sidebar templates, maps document names to template names.
+#html_sidebars = {}
+
+# Additional templates that should be rendered to pages, maps page names to
+# template names.
+#html_additional_pages = {}
+
+# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+html_show_sphinx = False
+
+# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
+html_show_copyright = True
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'stor-doc'
+
+
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+    ('index', 'stor.tex', u'stor Documentation',
+     u'Counsyl Inc.', 'manual'),
+]
+
+# -- Options for manual page output ---------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    ('index', 'stor', u'stor Documentation',
+     [u'Counsyl Inc.'], 1)
+]
+
+# -- Options for Texinfo output -------------------------------------------
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+    ('index', 'stor', u'stor Documentation',
+     u'Counsyl Inc.', 'stor', 'A short description',
+     'Miscellaneous'),
+]
