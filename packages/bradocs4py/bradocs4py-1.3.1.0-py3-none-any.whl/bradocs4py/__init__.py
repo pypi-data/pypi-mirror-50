8519bb2b6b1d595819ee17113f298e2b6a97c140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:18 +0000

    Add package bradocs4py/1.3.1.0/bradocs4py-1.3.1.0-py3-none-any.whl

---
+++ packages/bradocs4py/bradocs4py-1.3.1.0-py3-none-any.whl/bradocs4py/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+__SIGLAS_ESTADOS_BRASILEIROS__ = 'AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RR|RO|RJ|RN|RS|SC|SP|SE|TO'
+
+from .cnpj import Cnpj, ValidadorCnpj, GeradorCnpj, validar_cnpj
+from .cpf import CPF, ValidadorCpf, GeradorCpf, validar_cpf
+from .inscricaosuframa import InscricaoSuframa, ValidadorSuframa, GeradorSuframa, validar_inscricao_suframa
+from .inscricaoestadual import InscricaoEstadual, ValidadorInscricaoEstadual, validar_inscricao_estadual
+from .chaveacessonfe import ChaveAcessoNFe, ValidadorChaveAcessoNFe, GeradorChaveAcessoNFe, validarChaveAcessoNFe, gerarChaveAcessoNFe
+from .gtin import GTIN, ValidadorGTIN, GeradorGTIN, gerar_gtin, validar_gtin
+
+__version__ = '1.3.1.0'
+
+
