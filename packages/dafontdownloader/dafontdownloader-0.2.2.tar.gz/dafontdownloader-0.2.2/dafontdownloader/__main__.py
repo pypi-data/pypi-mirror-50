b3de45759cc27a3c0b19f548e0c74c9f83fc2d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:31 +0000

    Add package dafontdownloader/0.2.2/dafontdownloader-0.2.2.tar.gz

---
+++ packages/dafontdownloader/dafontdownloader-0.2.2.tar.gz/dafontdownloader-0.2.2/dafontdownloader/__main__.py
@@ -0,0 +1,29 @@
+"""dafontdownloader - making it easy to download fonts.
+
+Usage:
+  dafontdownloader <name-font>...
+  dafontdownloader (-h | --help)
+  dafontdownloader --version
+Options:
+  -h --help   Show help screen
+  --version   Show current version
+"""
+from dafontdownloader import Dafont, CompactFont
+import docopt
+
+
+def main():
+    args = docopt.docopt(__doc__, version='dafontdownloader v0.2.2')
+    df_obj = Dafont()
+    if args['<name-font>']:
+        try:
+            name = " ".join(args['<name-font>'])
+            font_to_download = df_obj.search_font(name)
+            font_to_download.install_font_file()
+        except ValueError:
+            print(f"{name} nonexistent in dafont.com")
+        else:
+            print("[DONE]")
+
+if __name__ == "__main__":
+    main()
