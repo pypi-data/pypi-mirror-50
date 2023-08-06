f0d500224e210874a348bf7fcd839d925858f111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:56 +0000

    Add package hypertuner/1.0.2/hypertuner-1.0.2.tar.gz

---
+++ packages/hypertuner/hypertuner-1.0.2.tar.gz/hypertuner-1.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hypertuner",
+    version="1.0.2",
+    author="Klemen Kotar, Dian Niu",
+    author_email="klemen@cs.washington.edu, dian.niu.11@gmail.com",
+    description="A library for passing arguments and graphing with HyperTuner",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hypertuner/hypertuner-pythonlib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'torch>=1.0.0',
+    ]
+)
