522b39920d951e1a2a46f4f7e38e839959266867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:19 +0000

    Add package smoothfit/0.1.1/smoothfit-0.1.1.tar.gz

---
+++ packages/smoothfit/smoothfit-0.1.1.tar.gz/smoothfit-0.1.1/smoothfit/__init__.py
@@ -0,0 +1,13 @@
+from .__about__ import __author__, __author_email__, __version__, __website__
+from .main import fit, fit1d, fit2d
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__author_email__",
+    "__website__",
+    #
+    "fit",
+    "fit1d",
+    "fit2d",
+]
