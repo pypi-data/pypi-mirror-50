cc2dde4c6d96d81be3ca69e88f9cd00c1dae2987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:59 +0000

    Add package etherscan/0.3.0/etherscan-0.3.0.tar.gz

---
+++ packages/etherscan/etherscan-0.3.0.tar.gz/etherscan-0.3.0/setup.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="etherscan",
+    version="0.3.0",
+    author="@neoctobers",
+    author_email="neoctobers@gmail.com",
+    description="Etherscan.io API wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/neoctobers/etherscan",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests_cache',
+    ],
+)
