2c37407fe3e3f721a4334f3c93a9f081c6f0ff0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:45 +0000

    Add package retrievepankey/0.1.4/retrievepankey-0.1.4.tar.gz

---
+++ packages/retrievepankey/retrievepankey-0.1.4.tar.gz/retrievepankey-0.1.4/retrievepankey/__init__.py
@@ -0,0 +1,37 @@
+import os.path
+import yaml
+import ipaddress
+
+
+def read_key_file(host, keyfile='.panconfkeystore', splitchar=":", searchpos=0, retpos=1, includehomedir=True):
+    if includehomedir == True:
+        pathstring = os.path.expanduser('~') + "/" + str(keyfile)
+    else:
+        pathstring = keyfile
+    with open(pathstring) as f:
+        for line in f.readlines():
+            if line.split(splitchar)[searchpos] == host:
+                return line.split(":")[retpos].strip()
+    raise Exception("Unable to find firewall in file, exiting now.")
+
+
+def fetch_deviceinfo_file(fwstr, devicemap={}, devicefile='.pandevice', includehomedir=True, field='ip'):
+    if includehomedir == True:
+        pathstring = os.path.expanduser('~') + "/" + str(devicefile)
+    else:
+        pathstring = devicefile
+
+    if devicemap == {}:
+        try:
+            with open(pathstring, "r") as ch:
+                devicemap = yaml.safe_load(ch.read())
+        except:
+            print("Unable to read %s" % devicefile)
+
+    if fwstr in devicemap and field in devicemap[fwstr]:
+        fwstr = str(devicemap[fwstr][field])
+
+    if field == 'ip':
+        return str(ipaddress.IPv4Address(fwstr))
+    else:
+        return fwstr
