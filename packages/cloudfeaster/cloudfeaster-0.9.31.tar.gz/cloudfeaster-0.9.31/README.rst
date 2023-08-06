28fb9620456ab6886baf53bb326073d75c0a86a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:46 +0000

    Add package cloudfeaster/0.9.31/cloudfeaster-0.9.31.tar.gz

---
+++ packages/cloudfeaster/cloudfeaster-0.9.31.tar.gz/cloudfeaster-0.9.31/README.rst
@@ -0,0 +1,50 @@
+cloudfeaster
+============
+
+|Maintained| |license| |PythonVersions| |status| |PyPI| |Requirements|
+|CircleCI| |codecov| |Vulnerabilities|
+|docker-simonsdave/cloudfeaster-xenial-dev-env|
+
+Cloudfeaster is a web crawling and business process automation
+infrastructure that leverages modern software engineering tools,
+services and trends to create:
+
+-  (this repo is) `a spider authoring and maintenance
+   environment <https://github.com/simonsdave/cloudfeaster>`__
+-  a service infrastructure for discovering and running spiders via a
+   RESTful API
+
+`This <https://github.com/simonsdave/cloudfeaster/tree/release-0.9.31/docs/story.md>`__
+is the story behind Cloudfeaster.
+
+What Next
+---------
+
+-  `this <https://github.com/simonsdave/cloudfeaster/tree/release-0.9.31/docs/spider_authors.md>`__
+   describes how to author spiders using Cloudfeaster
+-  see
+   `these <https://github.com/simonsdave/cloudfeaster/tree/release-0.9.31/docs/contributing.md>`__
+   instructions describe how to setup your development environment and
+   start contributing to Cloudfeaster
+-  take a look at `cloudfeaster/samples <cloudfeaster/samples/>`__ for
+   some spiders that illustrate the core concepts
+-  and
+   `simonsdave/gaming-spiders <https://github.com/simonsdave/gaming-spiders>`__
+   is an entire repo of spiders so you can get a sense of the end-to-end
+
+.. |Maintained| image:: https://img.shields.io/maintenance/yes/2019.svg?style=flat
+.. |license| image:: https://img.shields.io/pypi/l/cloudfeaster.svg?style=flat
+.. |PythonVersions| image:: https://img.shields.io/pypi/pyversions/cloudfeaster.svg?style=flat
+.. |status| image:: https://img.shields.io/pypi/status/cloudfeaster.svg?style=flat
+.. |PyPI| image:: https://img.shields.io/pypi/v/cloudfeaster.svg?style=flat
+   :target: https://pypi.python.org/pypi/cloudfeaster
+.. |Requirements| image:: https://requires.io/github/simonsdave/cloudfeaster/requirements.svg?branch=master
+   :target: https://requires.io/github/simonsdave/cloudfeaster/requirements/?branch=master
+.. |CircleCI| image:: https://circleci.com/gh/simonsdave/cloudfeaster/tree/release-0.9.31.svg?style=shield
+   :target: https://circleci.com/gh/simonsdave/cloudfeaster/tree/release-0.9.31
+.. |codecov| image:: https://codecov.io/gh/simonsdave/cloudfeaster/branch/master/graph/badge.svg
+   :target: https://codecov.io/gh/simonsdave/cloudfeaster
+.. |Vulnerabilities| image:: https://snyk.io/test/github/simonsdave/cloudfeaster/badge.svg
+   :target: https://snyk.io/test/github/simonsdave/cloudfeaster
+.. |docker-simonsdave/cloudfeaster-xenial-dev-env| image:: https://img.shields.io/badge/dockerhub-simonsdave%2Fcloudfeaster--xenial--dev-blue.svg
+   :target: https://hub.docker.com/r/simonsdave/cloudfeaster-xenial-dev-env
---
+++ packages/cloudfeaster/cloudfeaster-0.9.31.tar.gz/cloudfeaster-0.9.31/setup.py
@@ -0,0 +1,88 @@
+import re
+from setuptools import setup
+
+#
+# this approach used below to determine ```version``` was inspired by
+# https://github.com/kennethreitz/requests/blob/master/setup.py#L31
+#
+# why this complexity? wanted version number to be available in the
+# a runtime.
+#
+# the code below assumes the distribution is being built with the
+# current directory being the directory in which setup.py is stored
+# which should be totally fine 99.9% of the time. not going to add
+# the coode complexity to deal with other scenarios
+#
+reg_ex_pattern = r"__version__\s*=\s*['\"](?P<version>[^'\"]*)['\"]"
+reg_ex = re.compile(reg_ex_pattern)
+version = ""
+with open("cloudfeaster/__init__.py", "r") as fd:
+    for line in fd:
+        match = reg_ex.match(line)
+        if match:
+            version = match.group("version")
+            break
+if not version:
+    raise Exception("Can't locate cloudfeaster's version number")
+
+
+_author = "Dave Simons"
+_author_email = "simonsdave@gmail.com"
+
+
+def _long_description():
+    try:
+        with open('README.rst', 'r') as f:
+            return f.read()
+    except IOError:
+        # simple fix for avoid failure on "source cfg4dev"
+        return "a long description"
+
+
+setup(
+    name="cloudfeaster",
+    packages=[
+        "cloudfeaster",
+        "cloudfeaster.samples",
+        "cloudfeaster_extension",
+    ],
+    scripts=[
+        'bin/check-circleci-config.sh',
+        'bin/generate-circleci-config.py',
+        'bin/check-consistent-clf-version.sh',
+        'bin/run-all-spiders.sh',
+        'bin/run-spider.sh',
+    ],
+    install_requires=[
+        "colorama>=0.3.5",
+        "jsonschema>=2.3.0",
+        "python-dateutil==2.8.0",
+        # selenium versions @ https://pypi.org/project/selenium/#history
+        "selenium==3.14.0",
+    ],
+    include_package_data=True,
+    version=version,
+    description="Cloudfeaster",
+    long_description=_long_description(),
+    author=_author,
+    author_email=_author_email,
+    maintainer=_author,
+    maintainer_email=_author_email,
+    license="MIT",
+    url="https://github.com/simonsdave/cloudfeaster",
+    download_url="https://github.com/simonsdave/cloudfeaster/tarball/v%s" % version,
+    keywords=[
+        'selenium',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
