7df8d159e45015a65637043b75c9f744344ee23f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:11 +0000

    Add package keras-radam/0.1.0/keras-radam-0.1.0.tar.gz

---
+++ packages/keras-radam/keras-radam-0.1.0.tar.gz/keras-radam-0.1.0/demo/mnist.py
@@ -0,0 +1,63 @@
+import keras
+import keras.backend as K
+import numpy as np
+from keras.datasets import fashion_mnist
+from keras_radam import RAdam
+
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+x_train = np.expand_dims(x_train.astype(K.floatx()) / 255, axis=-1)
+x_test = np.expand_dims(x_test.astype(K.floatx()) / 255, axis=-1)
+
+y_train, y_test = np.expand_dims(y_train, axis=-1), np.expand_dims(y_test, axis=-1)
+
+train_num = round(x_train.shape[0] * 0.9)
+x_train, x_valid = x_train[:train_num, ...], x_train[train_num:, ...]
+y_train, y_valid = y_train[:train_num, ...], y_train[train_num:, ...]
+
+
+def get_model(optimizer):
+    model = keras.models.Sequential()
+    model.add(keras.layers.Dropout(input_shape=(28, 28, 1), rate=0.3, name='Input-Dropout'))
+    model.add(keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu', padding='same', name='Conv-1'))
+    model.add(keras.layers.MaxPool2D(pool_size=2, name='Pool-1'))
+    model.add(keras.layers.Dropout(rate=0.2, name='Dropout-1'))
+    model.add(keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', padding='same', name='Conv-2'))
+    model.add(keras.layers.MaxPool2D(pool_size=2, name='Pool-2'))
+    model.add(keras.layers.Dropout(rate=0.2, name='Dropout-2'))
+    model.add(keras.layers.Flatten(name='Flatten'))
+    model.add(keras.layers.Dense(units=256, activation='relu', name='Dense'))
+    model.add(keras.layers.Dropout(rate=0.2, name='Dense-Dropout'))
+    model.add(keras.layers.Dense(units=10, activation='softmax', name='Softmax'))
+    model.compile(
+        optimizer=optimizer,
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy'],
+    )
+    return model
+
+
+radam_model = get_model(RAdam())
+radam_model.fit(
+    x=x_train,
+    y=y_train,
+    epochs=10,
+    validation_data=(x_valid, y_valid),
+    callbacks=[keras.callbacks.EarlyStopping(monitor='val_acc', patience=2)]
+)
+radam_score = radam_model.evaluate(x_test, y_test)
+print('Score of RAdam:\t%.4f' % radam_score[1])
+
+
+adam_model = get_model('adam')
+adam_model.fit(
+    x=x_train,
+    y=y_train,
+    epochs=10,
+    validation_data=(x_valid, y_valid),
+    callbacks=[keras.callbacks.EarlyStopping(monitor='val_acc', patience=2)]
+)
+adam_score = adam_model.evaluate(x_test, y_test)
+print('Score of Adam:\t%.4f' % adam_score[1])
+print('Score of RAdam:\t%.4f' % radam_score[1])
