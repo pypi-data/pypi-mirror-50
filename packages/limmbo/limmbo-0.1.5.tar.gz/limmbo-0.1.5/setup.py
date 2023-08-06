54b2446027558f2554dac1379368cbc33cb20d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:10 +0000

    Add package limmbo/0.1.5/limmbo-0.1.5.tar.gz

---
+++ packages/limmbo/limmbo-0.1.5.tar.gz/limmbo-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+    readme = open('README.md').read()
+    try:
+        import pypandoc
+        long_description = pypandoc.convert_text(
+            readme, 'rst', format='markdown_github')
+    except (ImportError, RuntimeError, OSError):
+        long_description = readme
+
+    setup(long_description=long_description,
+          long_description_content_type='text/markdown',
+          entry_points = {
+              'console_scripts': [
+                  'runAssociation=limmbo.bin.runAssociation:entry_point',
+                  'runVarianceEstimation=limmbo.bin.runVarianceEstimation:entry_point'
+                  ]
+              }
+          )
