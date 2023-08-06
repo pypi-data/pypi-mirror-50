61b1ae6f69abae351b2dc266f7281e20dee867e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:59 +0000

    Add package cannerflow-python-client-development/0.4.0/cannerflow-python-client-development-0.4.0.tar.gz

---
+++ packages/cannerflow-python-client-development/cannerflow-python-client-development-0.4.0.tar.gz/cannerflow-python-client-development-0.4.0/cannerflow/workspace.py
@@ -0,0 +1,34 @@
+from cannerflow.request import Request
+from cannerflow.saved_query import SavedQuery
+from cannerflow.statement import Statement
+from cannerflow.file import File
+
+__all__ = ["Workspace"]
+
+class Workspace(object):
+    def __init__(
+        self,
+        endpoint,
+        headers,
+        workspaceId
+    ):
+        self.endpoint = endpoint
+        self.headers = headers
+        self.workspaceId = workspaceId
+        request = Request(
+            headers=headers,
+            endpoint=endpoint
+        )
+        self.request = request
+        self.saved_query = SavedQuery(
+            request=request,
+            workspaceId=workspaceId
+        )
+        self.statement = Statement(
+            request=request,
+            workspaceId=workspaceId
+        )
+        self.file = File(
+            request=request,
+            workspaceId=workspaceId
+        )
