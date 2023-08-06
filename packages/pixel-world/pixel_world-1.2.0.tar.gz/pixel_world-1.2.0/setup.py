e077c5ce8b3916297627eb67286b4b0dbc8d05b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:46 +0000

    Add package pixel-world/1.2.0/pixel_world-1.2.0.tar.gz

---
+++ packages/pixel-world/pixel_world-1.2.0.tar.gz/pixel_world-1.2.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='pixel_world',
+      version='1.2.0',
+      install_requires=['gym'],  # And any other dependencies foo needs,
+      include_package_data=True,
+      packages=find_packages()
+)  
