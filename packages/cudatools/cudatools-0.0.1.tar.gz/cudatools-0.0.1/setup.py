803e720da9a233d00e1ca8cd55f59eaa5a60364c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:16 +0000

    Add package cudatools/0.0.1/cudatools-0.0.1.tar.gz

---
+++ packages/cudatools/cudatools-0.0.1.tar.gz/cudatools-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='cudatools',
+      version='0.0.1',
+      description='CUDA utils accessible via python',
+      url='http://github.com/krzysg/cudautils',
+      author='Krzysztof Gonciarz',
+      author_email='krzysztof.gonciarz@gmail.com',
+      license='MIT',
+      packages=['cudatools'],
+      zip_safe=False)
+
