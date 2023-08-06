0b7cf439f7258b14224a0e510bb13ade19c76997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:19 +0000

    Add package zhlite/1.1.1/zhlite-1.1.1.tar.gz

---
+++ packages/zhlite/zhlite-1.1.1.tar.gz/zhlite-1.1.1/zhlite/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from zhlite.zhlite import *
+
+__author__ = "zhangbo"
+__email__ = "deplives@deplives.com"
+
+__license__ = "MIT"
+
+__version__ = "1.1.1"
+__date__ = "2019-08-11"
+__release__ = "release 1.1.1"
+
+
+class Version(object):
+
+    def __init__(self):
+        self.info = {
+            "version": __version__,
+            "date": __date__,
+            "release": __release__
+        }
+
+    def __str__(self):
+        return " ".join([f"{k}: {v}" for k, v in self.info.items()])
+
+
+version = Version()
