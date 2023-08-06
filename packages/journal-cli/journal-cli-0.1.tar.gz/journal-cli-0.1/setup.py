62932644f1f14fcb4aafcbbe7a5fa6898ef4443d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:30 +0000

    Add package journal-cli/0.1/journal-cli-0.1.tar.gz

---
+++ packages/journal-cli/journal-cli-0.1.tar.gz/journal-cli-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+	name='journal-cli',             # This is the name of your PyPI-package.
+	version='0.1',                  # Update the version number for new release
+	scripts=['journal']             # The name of your scipt, and also the command you'll be using for calling it
+)
