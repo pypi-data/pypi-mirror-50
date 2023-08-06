bbe7bbacae7b820378f28a62a449a5fecf3fdda6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:41 +0000

    Add package blueCup/0.0.3/blueCup-0.0.3.tar.gz

---
+++ packages/blueCup/blueCup-0.0.3.tar.gz/blueCup-0.0.3/setup.py
@@ -0,0 +1,23 @@
+# !/usr/bin/env python
+# coding: utf-8
+
+from setuptools import find_packages
+from setuptools import setup
+
+install_requires = [
+    'celery',
+    'jsonschema',
+    'nezha',
+    'muzha'
+]
+
+setup(
+    name='blueCup',
+    version='0.0.3',
+    author='kougazhang',
+    author_email='kougazhang@gmail.com',
+    url='https://github.com/kougazhang',
+    description="Little blue cup, who do not love?",
+    packages=find_packages(),
+    install_requires=install_requires,
+)
