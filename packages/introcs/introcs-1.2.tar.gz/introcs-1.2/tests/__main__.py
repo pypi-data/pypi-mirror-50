b5a835e20ff549da0183be70c79a49a6b17a14fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:43 +0000

    Add package introcs/1.2/introcs-1.2.tar.gz

---
+++ packages/introcs/introcs-1.2.tar.gz/introcs-1.2/tests/__main__.py
@@ -0,0 +1,23 @@
+"""
+Unit tests for the introc module
+
+:author:  Walker M. White (wmw2)
+:version: June 9, 2019
+"""
+import unittest
+
+
+def suite():
+    """
+    Creates the test suite for all packages except turtle (which is graphical)
+    """
+    modules  = ( 'test_testcase','test_strings','test_tuples','test_colors',
+                 'test_geom','test_filetools','test_urltools','test_modlib')
+    alltests = unittest.TestSuite()
+    for module in map(__import__, modules):
+        alltests.addTest(unittest.findTestCases(module))
+    return alltests
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
