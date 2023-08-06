27ae81f38ba2bf4a667f261dd7f65b5bb75459d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:48 +0000

    Add package steamtags/0.1.1/steamtags-0.1.1.tar.gz

---
+++ packages/steamtags/steamtags-0.1.1.tar.gz/steamtags-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+# noinspection PyUnresolvedReferences
+import setuptools
+
+setup(
+    name='steamtags',
+    packages=['steamtags'],
+    install_requires=[
+        'steamspypi',
+    ],
+    version='0.1.1',
+    description='Steam Tags on PyPI',
+    long_description='Steam Tags: an API to match Steam tags with appIDs, written in Python 3.',
+    long_description_content_type='text/x-rst',
+    author='Wok',
+    author_email='wok@tuta.io',
+    url='https://github.com/woctezuma/match-steam-tags',
+    download_url='https://github.com/woctezuma/match-steam-tags/archive/0.1.1.tar.gz',
+    keywords=['steam', 'steamtags', 'tags', 'api'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Games/Entertainment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
