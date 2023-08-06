43a5d0ff9536ebb3d9312a1e4dfb6f7418f349b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:20 +0000

    Add package deeptrade/0.0.2/deeptrade-0.0.2-py3-none-any.whl

---
+++ packages/deeptrade/deeptrade-0.0.2-py3-none-any.whl/deeptrade/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, division, print_function
+
+
+# configuration variables
+
+api_key = None
+api_base = "http://206.81.21.48/"
+
+
+
+# API sentiment
+from deeptrade.sentiment import *
+
+# API stocks
+from deeptrade.stocks import *
