2269aa69393272aa4d2f2589423cbe396de6325e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:40 +0000

    Add package api-client/1.1.5/api-client-1.1.5.tar.gz

---
+++ packages/api-client/api-client-1.1.5.tar.gz/api-client-1.1.5/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import setuptools
+
+
+application_dependencies = ["requests>=2.16", "pyyaml", "tenacity"]
+prod_dependencies = []
+test_dependencies = ["pytest", "pytest-env", "pytest-cov", "vcrpy"]
+lint_dependencies = ["flake8", "flake8-docstrings", "black", "isort"]
+docs_dependencies = []
+dev_dependencies = test_dependencies + lint_dependencies + docs_dependencies + ["ipdb"]
+deploy_dependencies = ["requests", "twine"]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="api-client",
+    version="1.1.5",
+    description="Separate the high level client implementation from the underlying CRUD.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Mike Wooster",
+    author_email="",
+    url="https://github.com/MikeWooster/api-client",
+    python_requires=">=3.6",
+    packages=["apiclient"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+    ],
+    install_requires=application_dependencies,
+    extras_require={
+        "production": prod_dependencies,
+        "test": test_dependencies,
+        "lint": lint_dependencies,
+        "docs": dev_dependencies,
+        "dev": dev_dependencies,
+        "deploy": deploy_dependencies,
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
