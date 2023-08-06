d8901e5fba1ce7ce93a61eac7c788e9a2db769fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:20 +0000

    Add package NSoL/0.1.14/NSoL-0.1.14.tar.gz

---
+++ packages/NSoL/NSoL-0.1.14.tar.gz/NSoL-0.1.14/tests/run_tests.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+##
+# \file run_tests.py
+# \brief      main-file to run specified unit tests
+#
+# \author     Michael Ebner (michael.ebner.14@ucl.ac.uk)
+# \date       July 2017
+#
+
+
+import unittest
+
+# Import modules for unit testing
+from kernels_test import *
+from loss_functions_test import *
+from similarity_measures_test import *
+from solvers_test import *
+from run_denoising_test import *
+from run_deconvolution_test import *
+from run_denoising_study_test import *
+from run_deconvolution_study_test import *
+
+if __name__ == '__main__':
+    print("\nUnit tests:\n--------------")
+    unittest.main()
