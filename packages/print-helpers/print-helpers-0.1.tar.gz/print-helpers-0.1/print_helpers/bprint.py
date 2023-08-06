ee689e5702f19726d774e8cc04edf9a8ac32fa5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:56 +0000

    Add package print-helpers/0.1/print-helpers-0.1.tar.gz

---
+++ packages/print-helpers/print-helpers-0.1.tar.gz/print-helpers-0.1/print_helpers/bprint.py
@@ -0,0 +1,8 @@
+def upper_print(message):
+    print(message.upper())
+
+
+def main():
+    print("Hello little child.")
+    upper_print("You have run bprint as a script.")
+    print("That seems great. Goodnight.")
