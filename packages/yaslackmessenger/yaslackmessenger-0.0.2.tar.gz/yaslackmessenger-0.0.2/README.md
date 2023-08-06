88cf796198b1f819af7e804bac96c438a79372bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:57 +0000

    Add package yaslackmessenger/0.0.2/yaslackmessenger-0.0.2.tar.gz

---
+++ packages/yaslackmessenger/yaslackmessenger-0.0.2.tar.gz/yaslackmessenger-0.0.2/README.md
@@ -0,0 +1,5 @@
+# Yet Another Slack Messenger
+
+I have a slacker class I use to post into slack webhooks from different applications.
+
+This repository goal is to store it, feel free to use.
---
+++ packages/yaslackmessenger/yaslackmessenger-0.0.2.tar.gz/yaslackmessenger-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+        name='yaslackmessenger',
+        packages=find_packages(),
+        version='0.0.2',
+        description='Yet Another Slack Messenger',
+        author='Cristiano W. Araujo',
+        author_email='cristianowerneraraujo@gmail.com',
+        url='https://gitlab.com/cristianowa/yaslackmessenger',
+        download_url='https://gitlab.com/cristianowa/yaslackmessenger/-/archive/0.0.2/yaslackmessenger-0.0.2.zip',
+        keywords=['slack', 'webhooks'],
+        classifiers=[],
+     )
