506227d956b2c9d9b1db0d3d739fddef9ae66bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:16 +0000

    Add package django-ios-storekit/1.0.6/django_ios_storekit-1.0.6-py3-none-any.whl

---
+++ packages/django-ios-storekit/django_ios_storekit-1.0.6-py3-none-any.whl/storekit/fields.py
@@ -0,0 +1,22 @@
+from django.db.models import BigIntegerField
+from django.utils.translation import gettext_lazy as _
+
+
+class PositiveBigIntegerField(BigIntegerField):
+
+    empty_strings_allowed = False
+    description = _('Big (8 byte) positive integer')
+
+    def db_type(self, connection):
+        return 'bigint UNSIGNED'
+
+    def get_internal_type(self):
+        return 'PositiveSmallIntegerField'
+
+    def formfield(self, **kwargs):
+        defaults = {
+            'min_value': 0,
+            'max_value': BigIntegerField.MAX_BIGINT
+        }
+        defaults.update(kwargs)
+        return super(PositiveBigIntegerField, self).formfield(**defaults)
