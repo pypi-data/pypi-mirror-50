0d31d3302dcbaa574dfc191bd5d56b0a7864b9d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:52 +0000

    Add package dekigokoro/0.3/dekigokoro-0.3.tar.gz

---
+++ packages/dekigokoro/dekigokoro-0.3.tar.gz/dekigokoro-0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+  name = 'dekigokoro',
+  version = '0.3',
+  license='MIT',
+  description = 'An asynchronous wrapper for the Dekigokoro API.',
+  author = 'broman',
+  author_email = 'ryan@broman.dev',
+  url = 'https://github.com/broman/dekigokoro.py',
+  download_url = 'https://github.com/broman/dekigokoro.py/archive/v0.3.tar.gz',
+  install_requires= [
+          'aiohttp>=3.4',
+          'asyncio',
+      ],
+  classifiers= [
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
