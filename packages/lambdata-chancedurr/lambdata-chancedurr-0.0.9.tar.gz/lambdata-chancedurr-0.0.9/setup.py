e833e488a29a2b4e031377cda753cc39bb6a2399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:13 +0000

    Add package lambdata-chancedurr/0.0.9/lambdata-chancedurr-0.0.9.tar.gz

---
+++ packages/lambdata-chancedurr/lambdata-chancedurr-0.0.9.tar.gz/lambdata-chancedurr-0.0.9/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lambdata-chancedurr",
+    version="0.0.9",
+    author="Chance Dare",
+    author_email="chancedurr@gmail.com",
+    description="A small helper package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chancedurr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
