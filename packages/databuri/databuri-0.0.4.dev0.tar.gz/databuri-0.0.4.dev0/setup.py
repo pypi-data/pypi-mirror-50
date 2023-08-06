7802f9f58ba8af6e7fd504dbaa1d8db3941b3ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:29 +0000

    Add package databuri/0.0.4.dev0/databuri-0.0.4.dev0.tar.gz

---
+++ packages/databuri/databuri-0.0.4.dev0.tar.gz/databuri-0.0.4.dev0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    dependencies = list(map(lambda x: x.strip(), fh.readlines()))
+
+setuptools.setup(
+    name="databuri",
+    version="0.0.4-dev",
+    packages=setuptools.find_packages(),
+    author="Pattarawat Chormai and friends",
+    author_email="author@example.com",
+    install_requires=dependencies,
+    # @todo #1 add description
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/codeforthailand/databuri",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # @todo #1 add license
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["opendata", "public-apis", "thailand"],
+)
