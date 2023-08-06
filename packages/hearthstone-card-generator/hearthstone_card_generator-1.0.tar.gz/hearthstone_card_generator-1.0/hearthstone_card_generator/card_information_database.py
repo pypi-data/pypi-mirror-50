2a23471c99d17c9136da5ea2bc0e776534a8dac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:19 +0000

    Add package hearthstone-card-generator/1.0/hearthstone_card_generator-1.0.tar.gz

---
+++ packages/hearthstone-card-generator/hearthstone_card_generator-1.0.tar.gz/hearthstone_card_generator-1.0/hearthstone_card_generator/card_information_database.py
@@ -0,0 +1,34 @@
+import json
+import urllib.request
+from .hearthstone_card import HearthstoneCard
+
+class CardInformationDatabase:
+    def __init__(self):
+        self.__card_information = []
+
+    def get_cards(self):
+        if self.__card_information == []:
+            headers = {"X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com", "X-RapidAPI-Key": "Lfe8TETbojmshf0pd9N1Gvqd3G1vp1tjf9DjsnWoteo40kBZEq"}
+            request = urllib.request.Request("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards?collectible=1", headers=headers)
+            with urllib.request.urlopen(request) as response:
+                content = response.read()
+            jsonContent = json.loads(content)
+            unflattenedCards = map(lambda key: jsonContent[key], filter(lambda key: len(jsonContent[key]) > 0, jsonContent))
+            cards = list(x for y in unflattenedCards for x in y)
+            self.__card_information = list(map(lambda card: HearthstoneCard(
+                lookup(card, 'name'),
+                lookup(card, 'text'),
+                lookup(card, 'cost'),
+                lookup(card, 'attack'),
+                lookup(card, 'health'),
+                lookup(card, 'durability'),
+                lookup(card, 'playerClass'),
+                lookup(card, 'type'),
+                lookup(card, 'rarity'),
+                lookup(card, 'race'),
+                lookup(cards, 'img'),
+                lookup(cards, 'imgGold')), cards))
+        return self.__card_information
+
+def lookup(dictionary, field):
+    return dictionary[field] if field in dictionary else ''
