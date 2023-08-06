2d2cebae1a206c59f6ca3b70abede1f1db7fcedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:52 +0000

    Add package cmu-multimodal-sdk/0.0.1/cmu-multimodal-sdk-0.0.1.tar.gz

---
+++ packages/cmu-multimodal-sdk/cmu-multimodal-sdk-0.0.1.tar.gz/cmu-multimodal-sdk-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cmu-multimodal-sdk',
+    version='0.0.1',
+    description='a copy of cmu-multimodal-sdk',
+    author='Lec',
+    author_email='2524463910@qq.com',
+    url='https://github.com/lizhaoliu-Lec/CMU-MultimodalSDK',
+    packages=find_packages(),
+    # install_requires=['requests'],
+)
