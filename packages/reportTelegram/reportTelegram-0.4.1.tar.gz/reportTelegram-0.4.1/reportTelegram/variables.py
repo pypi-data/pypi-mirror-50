2409a88801caecedb9a3adc7f3be6823ffd57231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:41 +0000

    Add package reportTelegram/0.4.1/reportTelegram-0.4.1.tar.gz

---
+++ packages/reportTelegram/reportTelegram-0.4.1.tar.gz/reportTelegram-0.4.1/reportTelegram/variables.py
@@ -0,0 +1,21 @@
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+link = config['Telegram']['link']
+group_id = int(config['Telegram']['group_id'])
+admin_id = int(config['Telegram']['admin_id'])
+sticker = config['Telegram']['sticker']
+
+
+DB_HOST = config['Database']['DB_HOST']
+DB_USER = config['Database']['DB_USER']
+DB_PASS = config['Database']['DB_PASS']
+DB_NAME = config['Database']['DB_NAME']
+DB_PORT = int(config['Database']['DB_PORT'])
+
+user_data_dict = dict()
+
+num_reports = 5
+ban_time = 300
