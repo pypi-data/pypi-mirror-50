bf6fe1a14c59f744b2aa749e466d11602be7c84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:15 +0000

    Add package census-data-aggregator/0.0.6/census-data-aggregator-0.0.6.tar.gz

---
+++ packages/census-data-aggregator/census-data-aggregator-0.0.6.tar.gz/census-data-aggregator-0.0.6/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+setup(
+    name='census-data-aggregator',
+    version='0.0.6',
+    description="Combine U.S. census data responsibly",
+    long_description=read('README.rst'),
+    author='Los Angeles Times Data Desk',
+    author_email='datadesk@latimes.com',
+    url='http://www.github.com/datadesk/census-data-aggregator',
+    license="MIT",
+    packages=("census_data_aggregator",),
+    install_requires=[
+          'numpy',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+    ],
+    project_urls={
+        'Maintainer': 'https://github.com/datadesk',
+        'Source': 'https://github.com/datadesk/census-data-aggregator',
+        'Tracker': 'https://github.com/datadesk/census-data-aggregator/issues'
+    },
+)
