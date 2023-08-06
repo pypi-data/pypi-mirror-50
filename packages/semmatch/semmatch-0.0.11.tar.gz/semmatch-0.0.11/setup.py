992105e84447510027c3ae3c82da1445035e29f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:02 +0000

    Add package semmatch/0.0.11/semmatch-0.0.11.tar.gz

---
+++ packages/semmatch/semmatch-0.0.11.tar.gz/semmatch-0.0.11/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="semmatch",
+    version="0.0.11",
+    author="Albert Xu",
+    author_email="albert.t.xu@gmail.com",
+    description="template matching tool for SerialEM",
+    packages=setuptools.find_packages(),
+    install_requires=required,
+    entry_points={"console_scripts": ["semmatch = semmatch.__main__:main"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
