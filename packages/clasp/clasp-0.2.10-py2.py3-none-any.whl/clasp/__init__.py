6ac91de4e7a8181330df1f8264374483730fed2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:25 +0000

    Add package clasp/0.2.10/clasp-0.2.10-py2.py3-none-any.whl

---
+++ packages/clasp/clasp-0.2.10-py2.py3-none-any.whl/clasp/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for clasp."""
+
+__author__ = """Stephen Wasilewski"""
+__email__ = 'stephanwaz@gmail.com'
+__version__ = '0.2.10'
+__all__ = ['script_tools', 'click_ext', 'sphinx_click_ext']
+
+import click
+import clasp.click_ext
+
+click = clasp.click_ext.click_ext(click)
