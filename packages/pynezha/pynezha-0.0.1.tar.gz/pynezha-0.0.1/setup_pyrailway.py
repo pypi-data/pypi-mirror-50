7b791926435366a1046e5e3abac18df9ed2843c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:28 +0000

    Add package pynezha/0.0.1/pynezha-0.0.1.tar.gz

---
+++ packages/pynezha/pynezha-0.0.1.tar.gz/pynezha-0.0.1/setup_pyrailway.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import find_packages
+from setuptools import setup
+
+install_requires = [
+    'pycryptodome',
+    'requests',
+]
+
+setup(
+    name='pynezha',
+    version='0.0.1',
+    author='kougazhang',
+    author_email='kougazhang@gmail.com',
+    url='https://github.com/kougazhang',
+    description="pyrailway, pysuway's railway",
+    packages=find_packages(include='pyrailway'),
+    install_requires=install_requires,
+    include_package_data=True
+)
