84902873a5197b8ff2a33e0f6198e33474e513f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:10 +0000

    Add package kedro-viz/2.1.0/kedro_viz-2.1.0-py3.6.egg

---
+++ packages/kedro-viz/kedro_viz-2.1.0-py3.6.egg/kedro_viz/html/precache-manifest.6e729e091e65deaf7e52e26e694d6c6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897c670ae47372f3eaae50ca4bb43ecc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06659679e33ff25020b3",
+    "url": "./static/css/2.d438ea0b.chunk.css"
+  },
+  {
+    "revision": "c2dab9123ea552ca8cc7",
+    "url": "./static/css/main.a72b7630.chunk.css"
+  },
+  {
+    "revision": "06659679e33ff25020b3",
+    "url": "./static/js/2.c9ab056c.chunk.js"
+  },
+  {
+    "revision": "c2dab9123ea552ca8cc7",
+    "url": "./static/js/main.e77050fc.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "7498697619bed8bcf479fc4d8648b5b4",
+    "url": "./static/media/delete.74986976.svg"
+  }
+]);
