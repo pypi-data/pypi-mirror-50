75f27fe2e07f64ce70fd2ad4da9ab6f8b31bd792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:48 +0000

    Add package pydero-jonlindg/1.0/pydero-jonlindg-1.0.tar.gz

---
+++ packages/pydero-jonlindg/pydero-jonlindg-1.0.tar.gz/pydero-jonlindg-1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pydero-jonlindg",
+    version="1.0",
+    author="Jonathan Lindgren",
+    author_email="ejonathanlindgren@gmail.com",
+    description="Python wrapper for interacting with the DERO blockdag",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonlindg/pydero",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
