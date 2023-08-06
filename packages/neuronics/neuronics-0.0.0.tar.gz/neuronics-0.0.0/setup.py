fc7e27dae4e6f8acd13105dbf8e6fea6600a55b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:24 +0000

    Add package neuronics/0.0.0/neuronics-0.0.0.tar.gz

---
+++ packages/neuronics/neuronics-0.0.0.tar.gz/neuronics-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='neuronics',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='',
+    author='Aly Shmahell',
+    author_email='aly.shmahell@gmail.com',
+    packages=[],
+)
