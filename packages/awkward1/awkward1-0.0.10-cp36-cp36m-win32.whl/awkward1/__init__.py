f9b1ad66063ef099bb20e0e2d468852600c05f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:36 +0000

    Add package awkward1/0.0.10/awkward1-0.0.10-cp36-cp36m-win32.whl

---
+++ packages/awkward1/awkward1-0.0.10-cp36-cp36m-win32.whl/awkward1/__init__.py
@@ -0,0 +1,17 @@
+import os
+import ctypes
+import platform
+
+import awkward1.layout
+
+__version__ = awkward1.layout.__version__
+
+if platform.system() == "Darwin":
+    lib1 = ctypes.cdll.LoadLibrary(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libawkward-cpu-kernels.dylib"))
+elif platform.system() == "Windows":
+    lib1 = ctypes.cdll.LoadLibrary(os.path.join(os.path.dirname(os.path.abspath(__file__)), "awkward-cpu-kernels.dll"))
+else:
+    lib1 = ctypes.cdll.LoadLibrary(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libawkward-cpu-kernels.so"))
+
+dummy1 = lib1.dummy1
+dummy3 = layout.dummy3
