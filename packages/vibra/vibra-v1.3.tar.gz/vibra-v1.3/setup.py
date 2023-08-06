93a5964c0a9c7fc304b2b0ce7f44df2a58616783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:20 +0000

    Add package vibra/v1.3/vibra-v1.3.tar.gz

---
+++ packages/vibra/vibra-v1.3.tar.gz/vibra-v1.3/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+setup(
+  name = 'vibra',
+  packages = ['vibra'],
+  version = 'v1.3',#####
+  license='MIT',
+  description = 'A non-finished add-on to Tensorflow',
+  author = 'PerceptronV',
+  author_email = 'perceptronv@gmail.com',
+  url = 'https://github.com/PerceptronV/vibra', #######
+  download_url = 'https://github.com/PerceptronV/vibra/archive/v1.3.tar.gz', ######
+  keywords = ['Tensorflow', 'add-on'],
+  install_requires=[
+      'numpy',
+    'os',
+    'tensorflow',
+    'time',
+    'random',
+  ],
+  classifiers=[  # Optional
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 3 - Alpha',
+
+    # Pick your license as you wish
+    'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3.7',
+  ],
+)
