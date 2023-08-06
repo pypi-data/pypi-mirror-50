9041e966e2661b2d2c2f8a89fb73196a2d8dc237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:05 +0000

    Add package igcoccebot/0.2/igcoccebot-0.2.tar.gz

---
+++ packages/igcoccebot/igcoccebot-0.2.tar.gz/igcoccebot-0.2/igcoccebot/__init__.py
@@ -0,0 +1,8 @@
+from .IGCocceBot import IGCocceBot
+from .IGNavigation import IGNavigation
+from .IGData import IGData
+from .IGAction import IGAction
+from .IGAlgo import IGAlgo
+from .tools import *
+from .yolo import *
+__version__ = '0.1'
