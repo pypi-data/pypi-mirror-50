37a2fccae9d5a3b668dab8d06d6e6086d686652a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:54 +0000

    Add package temp-abd/1/temp_abd-1.tar.gz

---
+++ packages/temp-abd/temp_abd-1.tar.gz/temp_abd-1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="temp_abd",
+    version="1",
+    author="Bilal",
+    author_email="bilal.aziz9680@gmail.com",
+    description="A small example package",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),)
