50efd6131f5937bae8c93602e940f3b99283c79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:25 +0000

    Add package synbio/0.4.0/synbio-0.4.0.tar.gz

---
+++ packages/synbio/synbio-0.4.0.tar.gz/synbio-0.4.0/synbio/__init__.py
@@ -0,0 +1,9 @@
+"""Library for synthetic biology design and build."""
+
+from .design import Combinatorial
+from .design import CombinatorialBins
+from .design import Plasmid
+from .protocol import Protocol
+from .mix import Mix
+from .reagents import Reagent
+from .species import Species
