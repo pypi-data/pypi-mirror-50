e407f16282c29e29dd93fb88a5e327e47daa1327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:25 +0000

    Add package kube-web-view/19.8.0/kube-web-view-19.8.0.tar.gz

---
+++ packages/kube-web-view/kube-web-view-19.8.0.tar.gz/kube-web-view-19.8.0/kube_web/templates/assets/kube-web.js
@@ -0,0 +1,45 @@
+// https://bulma.io/documentation/components/navbar/
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $toggleButtons = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger, .aside-burger, .toggle-tools'), 0);
+
+  // Check if there are any navbar burgers
+  if ($toggleButtons.length > 0) {
+
+    // Add a click event on each of them
+    $toggleButtons.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+  const $unselectButtons = Array.prototype.slice.call(document.querySelectorAll('.unselect'), 0);
+
+  if ($unselectButtons.length > 0) {
+
+    $unselectButtons.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+
+        const $inputs = Array.prototype.slice.call(document.querySelectorAll('#' + target + ' input[type=checkbox]'), 0);
+        $inputs.forEach( inp => {
+            inp.checked = false;
+        });
+
+      });
+    });
+  }
+
+});
