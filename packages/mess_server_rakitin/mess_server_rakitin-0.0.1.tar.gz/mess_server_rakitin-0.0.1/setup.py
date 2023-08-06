089cfe3d73067e5a8fcc5cdfbe1759a56ebb114d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:48 +0000

    Add package mess_server_rakitin/0.0.1/mess_server_rakitin-0.0.1.tar.gz

---
+++ packages/mess_server_rakitin/mess_server_rakitin-0.0.1.tar.gz/mess_server_rakitin-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="mess_server_rakitin",
+      version="0.0.1",
+      description="message_server",
+      author="Alexey Rakitin",
+      author_email="amrakitin@gmail.com",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome']
+      )
