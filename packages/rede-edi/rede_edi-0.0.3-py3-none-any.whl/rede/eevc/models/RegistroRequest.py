9935784d0d16f4d2497323e9385cafe83421a5eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:29 +0000

    Add package rede-edi/0.0.3/rede_edi-0.0.3-py3-none-any.whl

---
+++ packages/rede-edi/rede_edi-0.0.3-py3-none-any.whl/rede/eevc/models/RegistroRequest.py
@@ -0,0 +1,16 @@
+COLUMNS = [
+    'tipo_registro',
+    'nro_pv',
+    'nro_rv',
+    'nro_cartao',
+    'vl_transacao_cvnsu',
+    'dt_transacao_cvnsu',
+    'nro_referencia',
+    'nro_processo',
+    'nro_cvnsu',
+    'nro_autorizacao',
+    'cod_request',
+    'dt_limite_envio_docs',
+    'bandeira',
+    'livre'
+]
