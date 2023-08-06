ad3cd91b11ba7ae689c2c92b2d95c9e53f5bdeac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:34 +0000

    Add package jw-test/1.0.3/jw_test-1.0.3.tar.gz

---
+++ packages/jw-test/jw_test-1.0.3.tar.gz/jw_test-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name="jw_test",
+      version="1.0.3",
+      description="jw module",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="jw",
+      author_email="jw397@126.com",
+      py_modules=["jw.__init__", "jw.test"])
