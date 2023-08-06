bf64fd20bb8c4f90b35fc4c2cc386350237faff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:53 +0000

    Add package fecho/1.0.4/fecho-1.0.4.tar.gz

---
+++ packages/fecho/fecho-1.0.4.tar.gz/fecho-1.0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='fecho',
+    version='1.0.4',
+    packages=['fecho'],
+    url='https://github.com/thehappydinoa/fecho',
+    license='MIT',
+    author='thehappydinoa',
+    author_email='thehappydinoa@gmail.com',
+    description='Requests wrapper that uses Facebook Developer tool echo.',
+    entry_points={'console_scripts': ['fecho = fecho.cli:main']},
+    install_requires=['requests']
+)
