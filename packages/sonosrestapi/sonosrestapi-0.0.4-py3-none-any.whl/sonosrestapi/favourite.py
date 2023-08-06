46204a92a225cab379ea0bcc145d6ab964c0b40e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:15 +0000

    Add package sonosrestapi/0.0.4/sonosrestapi-0.0.4-py3-none-any.whl

---
+++ packages/sonosrestapi/sonosrestapi-0.0.4-py3-none-any.whl/sonosrestapi/favourite.py
@@ -0,0 +1,8 @@
+
+class Favourite:
+
+    def __init__ (self, id, name, description, image_url):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.image_url = image_url
