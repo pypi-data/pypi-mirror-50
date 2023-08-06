6dc56aee087069d7e0fd8d55435161b21d254595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:59 +0000

    Add package jw-test/1.0.1/jw_test-1.0.1.tar.gz

---
+++ packages/jw-test/jw_test-1.0.1.tar.gz/jw_test-1.0.1/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="jw_test",
+      version="1.0.1",
+      description="jw module",
+      author="jw",
+      author_email="jw397@126.com",
+      py_modules=["__init__", "test"])
