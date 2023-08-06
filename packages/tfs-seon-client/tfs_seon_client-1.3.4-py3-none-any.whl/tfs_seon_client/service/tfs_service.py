065926d1484a28d4424679e185b1a5946d701895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:44 +0000

    Add package tfs-seon-client/1.3.4/tfs_seon_client-1.3.4-py3-none-any.whl

---
+++ packages/tfs-seon-client/tfs_seon_client-1.3.4-py3-none-any.whl/tfs_seon_client/service/tfs_service.py
@@ -0,0 +1,24 @@
+import pika
+import uuid
+import os
+import json
+from pprint import pprint
+from rabbitmqX.patterns.client.rpc_client import RPC_Client
+from rabbitmqX.journal.journal import Journal
+
+class TFS_Service (RPC_Client):
+    
+    def __init__(self, type):
+        RPC_Client.__init__(self,'integration.tfs')
+        self.type = type
+
+    def integrate(self, user_key, tfs_url, organization_url):
+
+        data = {'key': user_key, 
+                'tfs_url': tfs_url,
+                'organization_url': organization_url}       
+
+        journal = Journal(self.type,data,"integration")
+
+        return self.do(journal.__dict__)
+
