33c44ddecd77c802720116e8937394f72c0e5e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:56 +0000

    Add package splunk-saml-cli-auth/0.1.2/splunk-saml-cli-auth-0.1.2.tar.gz

---
+++ packages/splunk-saml-cli-auth/splunk-saml-cli-auth-0.1.2.tar.gz/splunk-saml-cli-auth-0.1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+VERSION = "0.1.2"
+
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="splunk-saml-cli-auth",
+    version=VERSION,
+    author="Steve McMaster",
+    author_email="mcmaster@hurricanelabs.com",
+    py_modules=["splunksecrets"],
+    description="Helper for authenticating to Splunk on CLI when using SAML auth",
+    long_description=long_description,
+    install_requires=[
+        "pycookiecheat",
+        "requests"
+    ],
+    entry_points={
+        "console_scripts": [
+            "splunk-saml-cli-auth = splunk_saml_cli_auth:main",
+        ]
+    },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    bugtrack_url="https://github.com/HurricaneLabs/splunk-saml-cli-auth/issues",
+)
