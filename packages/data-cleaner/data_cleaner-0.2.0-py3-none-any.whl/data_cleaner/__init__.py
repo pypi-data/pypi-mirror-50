d9d1f350f0d38bfe8b848c963b125e076bd144a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:14 +0000

    Add package data-cleaner/0.2.0/data_cleaner-0.2.0-py3-none-any.whl

---
+++ packages/data-cleaner/data_cleaner-0.2.0-py3-none-any.whl/data_cleaner/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+from .data_cleaner import DataCleaner
+from .georef_api import GeorefWrapper
+
+__author__ = """Datos Argentina"""
+__email__ = 'datos@modernizacion.gob.ar'
+__version__ = '0.2.0'
