202dfce7ef818df74e75d4de60a9900b095203df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:37 +0000

    Add package AIOrqlite/0.1.7/AIOrqlite-0.1.7.tar.gz

---
+++ packages/AIOrqlite/AIOrqlite-0.1.7.tar.gz/AIOrqlite-0.1.7/aiorqlite/constants.py
@@ -0,0 +1,14 @@
+
+from __future__ import unicode_literals
+
+__version__ = "HEAD"
+__project__ = "aiorqlite"
+
+__author__ = "Terra Brown"
+__email__ = "superloach42@gmail.com"
+
+__copyright__ = None
+__license__ = "MIT"
+__description__ = "asynchronous Python client for rqlite"
+
+UNLIMITED_REDIRECTS = -1
