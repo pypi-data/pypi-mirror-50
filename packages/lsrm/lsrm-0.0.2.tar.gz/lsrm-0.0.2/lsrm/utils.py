4cb9e15826cd08c03e41319e746962af1e3cdbc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:38 +0000

    Add package lsrm/0.0.2/lsrm-0.0.2.tar.gz

---
+++ packages/lsrm/lsrm-0.0.2.tar.gz/lsrm-0.0.2/lsrm/utils.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 11:12:19 2019
+
+@author: michaelek
+"""
+import os
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+from pdsql import mssql
+import yaml
+
+###################################################
+### Parameters
+
+base_dir = os.path.realpath(os.path.dirname(__file__))
+
+with open(os.path.join(base_dir, 'parameters.yml')) as param:
+    param = yaml.safe_load(param)
+
+
+###################################################
+### Functions
+
+
+def ds_AET(pet, A, paw_now, paw_max):
+    """
+    Minhas et al. (1974) function used by David Scott to estimate 'actual ET' from PET and PAW. All inputs must be as floats.
+    """
+
+    aet = pet * ((1 - np.exp(-A*paw_now/paw_max))/(1 - 2*np.exp(-A) + np.exp(-A*paw_now/paw_max)))
+    return aet
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
