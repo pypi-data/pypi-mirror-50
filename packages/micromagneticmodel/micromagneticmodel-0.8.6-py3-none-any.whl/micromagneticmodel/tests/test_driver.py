18f20b84e75ac503e263434ab6e390c0167ba74b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:27 +0000

    Add package micromagneticmodel/0.8.6/micromagneticmodel-0.8.6-py3-none-any.whl

---
+++ packages/micromagneticmodel/micromagneticmodel-0.8.6-py3-none-any.whl/micromagneticmodel/tests/test_driver.py
@@ -0,0 +1,20 @@
+import pytest
+import micromagneticmodel as mm
+
+
+class TestDriver:
+    def setup(self):
+        self.driver = mm.Driver(a=1, b=2, c='c')
+
+    def test_init(self):
+        assert self.driver.a == 1
+        assert self.driver.b == 2
+        assert self.driver.c == "c"
+
+    def test_drive(self):
+        with pytest.raises(NotImplementedError):
+            self.driver.drive()
+
+    def test_script(self):
+        with pytest.raises(NotImplementedError):
+            script = self.driver._script
