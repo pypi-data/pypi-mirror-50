4431121ec5cd3be0bf37376bea7229b264bd58fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:17 +0000

    Add package jupyterlab-nvdashboard/0.1.2/jupyterlab-nvdashboard-0.1.2.tar.gz

---
+++ packages/jupyterlab-nvdashboard/jupyterlab-nvdashboard-0.1.2.tar.gz/jupyterlab-nvdashboard-0.1.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="jupyterlab-nvdashboard",
+    version='0.1.2',
+    url="https://github.com/jacobtomlinson/jupyterlab-nvdashboard",
+    author="NV Dashbaord contributors",
+    description="A JupyterLab extension for displaying dashboards of GPU usage.",
+    packages=setuptools.find_packages(),
+        keywords=['Jupyter'],
+        classifiers=['Framework :: Jupyter'],
+    install_requires=list(open("requirements.txt").read().strip().split("\n")),
+    entry_points={
+        'jupyter_serverproxy_servers': [
+            'nvdashboard = jupyterlab_nvdashboard:launch_server',
+        ]
+    },
+    package_data={
+        'jupyterlab_nvdashboard': ['icons/*'],
+    },
+)
