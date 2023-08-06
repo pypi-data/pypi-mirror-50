088c27f649e6c75b354ce73e993c5fc3bf065d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:35 +0000

    Add package drf-channels-oneway-ws/0.3.0/drf-channels-oneway-ws-0.3.0.tar.gz

---
+++ packages/drf-channels-oneway-ws/drf-channels-oneway-ws-0.3.0.tar.gz/drf-channels-oneway-ws-0.3.0/README.md
@@ -0,0 +1,98 @@
+[![PyPI version](https://badge.fury.io/py/drf-channels-oneway-ws.svg)](https://badge.fury.io/py/drf-channels-oneway-ws)
+[![Build Status](https://travis-ci.org/evocount/drf-channels-oneway-ws.svg?branch=master)](https://travis-ci.org/evocount/drf-channels-oneway-ws)
+[![codecov](https://codecov.io/gh/evocount/drf-channels-oneway-ws/branch/master/graph/badge.svg)](https://codecov.io/gh/evocount/drf-channels-oneway-ws)
+
+# DRF channels one-way WS
+
+Simple one-way bindings for django-channels with some specific support for django-rest-framework serializers and websockets. I.e. enrich your existing API with push notifications.
+
+## Installation
+
+* `pipenv install drf-channels-oneway-ws` or `pip install drf-channels-oneway-ws`
+
+## Usage
+
+### Bindings
+```python
+from django.db import models
+from rest_framework import serializers
+from channels_oneway.bindings import Binding
+
+class Family(models.Model):
+    name = models.CharField(max_length=255)
+
+class Bird(models.Model):
+    name = models.CharField(max_length=255)
+    family = models.ForeignKey('Family', models.CASCADE)
+
+
+class BirdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ('__all__')
+
+
+class BirdBinding(Binding):
+    model = Bird
+    stream = 'birds'
+    serializer = BirdSerializer
+
+    @classmethod
+    def group_names(cls, instance):
+        return [instance.family.name]
+
+class FamilyBinding(Binding):
+    """
+    example of a binding not using a drf serializer
+    """
+    model = Family
+    stream = 'bird-families'
+
+    @classmethod
+    def group_names(cls, instance):
+        return [instance.name]
+
+    def serialize_data(self, instance):
+        return {'id': instance.id, 'name': instance.name}
+```
+
+Now you make sure you have a WebsocketConsumer, which does something like `self.channel_layer.group_add('thrushes', self.channel_name)` in its connect coroutine.
+`Family.objects.create(name='thrushes')` will then cause the following to be sent over the associated websocket:
+
+```json
+{
+    "stream": "bird-families",
+    "payload": {
+        "action": "create",
+        "data": {"id": 1, "name": "thrushes"},
+        "model": "your_app.family",
+        "pk": 1
+    }
+}
+```
+
+Upon modification (`"action": "update"`) or deletion (`"action": "delete"`) you will receive messages with an equal structure.
+
+
+#### Registration
+In order to let the bindings register their signals make sure they are imported at some point. In case you use a dedicated file, import it from [`AppConfig.ready`](https://docs.djangoproject.com/en/2.2/ref/applications/#django.apps.AppConfig.ready) just like your signals.
+
+
+### Helpers
+In order to send a ws message from outside a binding, but using the same format (stream + payload) (and also the drf json encoder) use the async `channels_oneway.utils.groupSend(group, stream, payload)` or its sync equivalent `groupSendSync`.
+
+## Contributing
+
+### Installation
+
+* `git clone git@github.com:evocount/drf-channels-oneway-ws.git`
+* `cd drf-channels-oneway-ws`
+* `pipenv install --dev`
+
+### Running tests
+
+* `pipenv run pytest --cov`
+
+## License
+
+This project is licensed under the [MIT License](LICENSE.md).
---
+++ packages/drf-channels-oneway-ws/drf-channels-oneway-ws-0.3.0.tar.gz/drf-channels-oneway-ws-0.3.0/channels_oneway/bindings.py
@@ -0,0 +1,171 @@
+from django.db.models.signals import (
+    post_delete,
+    post_save,
+    pre_delete,
+    pre_save,
+    m2m_changed
+)
+from django.db import transaction
+from .utils import groupSendSync
+
+"""
+This is heavly inspired from the bindings that existed in channels v1.
+"""
+
+CREATE = 'create'
+UPDATE = 'update'
+DELETE = 'delete'
+
+
+class BindingMetaClass(type):
+    def __new__(cls, clsname, bases, attrs, **kwargs):
+        newclass = super().__new__(cls, clsname, bases, attrs, **kwargs)
+
+        if newclass.model is not None:
+            newclass.register()
+
+        return newclass
+
+
+class Binding(object, metaclass=BindingMetaClass):
+    """
+    Represents a one-way data binding from a django model to channels/groups.
+    Outgoing binding sends model events to zero or more groups
+    (websockets only).
+    """
+
+    model = None
+    stream = None
+    serializer = None
+    # optionally also connect to releated m2m fields
+    m2m_senders = []
+
+    # the kwargs the triggering signal (e.g. post_save) was emitted with
+    signal_kwargs = None
+
+    @classmethod
+    def register(cls):
+        """
+        Connet model signals.
+        """
+        pre_save.connect(cls.pre_save_receiver, sender=cls.model)
+        post_save.connect(cls.post_save_receiver, sender=cls.model)
+        pre_delete.connect(cls.pre_delete_receiver, sender=cls.model)
+        post_delete.connect(cls.post_delete_receiver, sender=cls.model)
+
+        for sender in cls.m2m_senders:
+            m2m_changed.connect(cls.m2m_changed_receiver, sender=sender)
+
+        cls.model_label = f'{cls.model._meta.app_label.lower()}.{cls.model._meta.object_name.lower()}'
+
+    # Outbound binding
+
+    @classmethod
+    def m2m_changed_receiver(cls, instance, action, **kwargs):
+        if action.startswith('pre_'):
+            cls.pre_change_receiver(instance, UPDATE, **kwargs)
+        else:
+            cls.post_change_receiver(instance, UPDATE, **kwargs)
+
+    @classmethod
+    def pre_save_receiver(cls, instance, **kwargs):
+        creating = instance._state.adding
+        cls.pre_change_receiver(instance, CREATE if creating else UPDATE)
+
+    @classmethod
+    def post_save_receiver(cls, instance, created, **kwargs):
+        # If this is called upon saving a drf serializer which included
+        # ManyToMany relationships those will only be available once the
+        # commit has finished. As those manytomany fields might be included
+        # in the serializer we using for ws messages, let's wait for on_commit.
+        transaction.on_commit(
+            lambda: cls.post_change_receiver(
+                instance,
+                CREATE if created else UPDATE,
+                **kwargs
+            )
+        )
+
+    @classmethod
+    def pre_delete_receiver(cls, instance, **kwargs):
+        cls.pre_change_receiver(instance, DELETE)
+
+    @classmethod
+    def post_delete_receiver(cls, instance, **kwargs):
+        cls.post_change_receiver(instance, DELETE, **kwargs)
+
+    @classmethod
+    def pre_change_receiver(cls, instance, action, **kwargs):
+        """
+        Entry point for triggering the binding from save signals.
+        """
+        if action == CREATE:
+            group_names = set()
+        else:
+            group_names = set(cls.group_names(instance))
+
+        if not hasattr(instance, '_binding_group_names'):
+            instance._binding_group_names = {}
+        instance._binding_group_names[cls] = group_names
+
+    @classmethod
+    def post_change_receiver(cls, instance, action, **kwargs):
+        """
+        Triggers the binding to possibly send to its group.
+        """
+        old_group_names = instance._binding_group_names[cls]
+        if action == DELETE:
+            new_group_names = set()
+        else:
+            new_group_names = set(cls.group_names(instance))
+
+        # if post delete, new_group_names should be []
+        self = cls()
+        self.instance = instance
+
+        # Django DDP had used the ordering of DELETE, UPDATE then CREATE for good reasons.
+        self.send_messages(instance, old_group_names - new_group_names, DELETE, **kwargs)
+        self.send_messages(instance, old_group_names & new_group_names, UPDATE, **kwargs)
+        self.send_messages(instance, new_group_names - old_group_names, CREATE, **kwargs)
+
+    def send_messages(self, instance, group_names, action, **kwargs):
+        """
+        Serializes the instance and sends it to all provided group names.
+        """
+        if not group_names:
+            return  # no need to serialize, bail.
+        self.signal_kwargs = kwargs
+        payload = self.serialize(instance, action)
+        if payload == {}:
+            return  # nothing to send, bail.
+
+        assert self.stream is not None
+        for group_name in group_names:
+            groupSendSync(group_name, self.stream, payload)
+
+    @classmethod
+    def group_names(cls, instance):
+        """
+        Returns the iterable of group names to send the object to based on the
+        instance and action performed on it.
+        """
+        raise NotImplementedError()
+
+    def serialize(self, instance, action):
+        payload = {
+            "action": action,
+            "pk": instance.pk,
+            "data": self.serialize_data(instance),
+            "model": self.model_label,
+        }
+        return payload
+
+    def serialize_data(self, instance):
+        """
+        Serializes model data into JSON-compatible types.
+        Overwrite this if you do not want to use a django-rest-framework
+        serializer.
+        """
+        assert self.serializer is not None
+
+        return self.serializer(instance).data
