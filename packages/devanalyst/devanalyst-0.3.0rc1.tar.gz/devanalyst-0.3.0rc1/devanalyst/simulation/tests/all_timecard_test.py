0d8e74277fdc98718eb64625650e592e8278ef10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:21 +0000

    Add package devanalyst/0.3.0rc1/devanalyst-0.3.0rc1.tar.gz

---
+++ packages/devanalyst/devanalyst-0.3.0rc1.tar.gz/devanalyst-0.3.0rc1/devanalyst/simulation/tests/all_timecard_test.py
@@ -0,0 +1,118 @@
+import unittest
+from unittest import TestCase
+
+import devanalyst.simulation.tests.test_timecard # Import needed to populate tu_.ACTUAL and tu_.EXPECTED
+
+import devanalyst.test_utils.test_utils as tu_
+
+# --------------------------------- Timecard tests ----------------------------------
+class test_uss(TestCase):
+    def test(self):
+        self.assertTrue(tu_.testOK('uss'))
+
+class test_createTeams(TestCase):
+    def test(self):
+        self.assertTrue(tu_.testOK('createTeams'))
+
+
+class test_userStoryCreate(TestCase):
+    def test_stories_df(self):
+        self.assertTrue(tu_.testOK('test_userStoryCreate.stories_df'))
+    def test_estimates_df(self):
+        self.assertTrue(tu_.testOK('test_userStoryCreate.estimates_df'))    
+    def test_workload_df(self):
+        self.assertTrue(tu_.testOK('test_userStoryCreate.workload_df'))   
+    def test_crosscheck_df(self):
+        self.assertTrue(tu_.testOK('test_userStoryCreate.crosscheck_df'))    
+    def test_avg_estimates_df(self):
+        self.assertTrue(tu_.testOK('test_userStoryCreate.avg_estimates_df'))
+
+class test_WorkAssignments(TestCase):
+    def test_Initial(self):
+        self.assertTrue(tu_.testOK('test_WorkAssignments.Initial'))
+    def test_Final(self):
+        self.assertTrue(tu_.testOK('test_WorkAssignments.Final'))  
+
+class test_oneSprint(TestCase):
+    def test_Start_Committed(self):
+        self.assertTrue(tu_.testOK('test_oneSprint.Start_Committed'))
+    def test_Start_Tasks(self):
+        self.assertTrue(tu_.testOK('test_oneSprint.Start_Tasks'))  
+    def test_End_Committed(self):
+        self.assertTrue(tu_.testOK('test_oneSprint.End_Committed'))
+    def test_End_Tasks(self):
+        self.assertTrue(tu_.testOK('test_oneSprint.End_Tasks'))  
+
+class test_multipleSprints(TestCase):
+    def test_start_committed(self):
+        self.assertTrue(tu_.testOK('test_multipleSprints.start_committed'))
+    def test_start_tasks(self):
+        self.assertTrue(tu_.testOK('test_multipleSprints.start_tasks'))  
+    def test_end_committed(self):
+        self.assertTrue(tu_.testOK('test_multipleSprints.end_committed'))
+    def test_end_tasks(self):
+        self.assertTrue(tu_.testOK('test_multipleSprints.end_tasks'))
+
+class test_releaseCycle(TestCase):
+    def test_Entries(self):
+        self.assertTrue(tu_.testOK('test_releaseCycle.Entries'))
+    def test_Burnout(self):
+        self.assertTrue(tu_.testOK('test_releaseCycle.Burnout'))
+    def test_planned_Start_CURRENT_SPRINT(self):
+        self.assertTrue(tu_.testOK('test_releaseCycle.planned_Start_CURRENT_SPRINT'))  
+    def test_planned_End_CURRENT_SPRINT(self):
+        self.assertTrue(tu_.testOK('test_releaseCycle.planned_End_CURRENT_SPRINT'))  
+    def test_planned_Start_NEXT_SPRINT(self):
+        self.assertTrue(tu_.testOK('test_releaseCycle.planned_Start_NEXT_SPRINT'))  
+    def test_planned_End_NEXT_SPRINT(self):
+        self.assertTrue(tu_.testOK('test_releaseCycle.planned_End_NEXT_SPRINT'))  
+    def test_backlog(self):
+        self.assertTrue(tu_.testOK('test_releaseCycle.backlog'))  
+    def test_Resourcing(self):
+        self.assertTrue(tu_.testOK('test_releaseCycle.Resourcing'))   
+    def test_Outcome(self):
+        self.assertTrue(tu_.testOK('test_releaseCycle.Outcome')) 
+
+class test_buggyReleaseCycle(TestCase):
+    def test_Entries(self):
+        self.assertTrue(tu_.testOK('test_buggyReleaseCycle.Entries'))
+    def test_User_Stories(self):
+        self.assertTrue(tu_.testOK('test_buggyReleaseCycle.User_Stories'))
+    def test_Tickets(self):
+        self.assertTrue(tu_.testOK('test_buggyReleaseCycle.Tickets'))
+    def test_Burnout(self):
+        self.assertTrue(tu_.testOK('test_buggyReleaseCycle.Burnout'))
+    def test_planned_Start_CURRENT_SPRINT(self):
+        self.assertTrue(tu_.testOK('test_buggyReleaseCycle.planned_Start_CURRENT_SPRINT'))  
+    def test_planned_End_CURRENT_SPRINT(self):
+        self.assertTrue(tu_.testOK('test_buggyReleaseCycle.planned_End_CURRENT_SPRINT'))  
+    def test_planned_Start_NEXT_SPRINT(self):
+        self.assertTrue(tu_.testOK('test_buggyReleaseCycle.planned_Start_NEXT_SPRINT'))  
+    def test_planned_End_NEXT_SPRINT(self):
+        self.assertTrue(tu_.testOK('test_buggyReleaseCycle.planned_End_NEXT_SPRINT'))  
+    def test_backlog(self):
+        self.assertTrue(tu_.testOK('test_buggyReleaseCycle.backlog'))  
+    def test_Resourcing(self):
+        self.assertTrue(tu_.testOK('test_buggyReleaseCycle.Resourcing'))   
+    def test_Outcome(self):
+        self.assertTrue(tu_.testOK('test_buggyReleaseCycle.Outcome'))   
+
+
+class test_workStealing(TestCase):
+    def test_burnout(self):
+        self.assertTrue(tu_.testOK('timecard.test_workStealing.burnout'))
+    def test_utilization(self):
+        self.assertTrue(tu_.testOK('timecard.test_workStealing.utilization'))
+    def test_works_stolen(self):
+        self.assertTrue(tu_.testOK('timecard.test_workStealing.works_stolen'))
+    def test_userStory1_work_stolen_explanation(self):
+        self.assertTrue(tu_.testOK('timecard.test_workStealing.userStory1_work_stolen_explanation'))
+    def test_userStory70_work_stolen_explanation(self):
+        self.assertTrue(tu_.testOK('timecard.test_workStealing.userStory70_work_stolen_explanation'))
+    def test_laggard_candidates_to_steal_sprint13(self):
+        self.assertTrue(tu_.testOK('timecard.test_workStealing.laggard_candidates_to_steal_sprint13'))
+    def test_balance_candidates_to_steal_sprint13(self):
+        self.assertTrue(tu_.testOK('timecard.test_workStealing.balance_candidates_to_steal_sprint13'))
+    def test_actual_steal_sprint13(self):
+        self.assertTrue(tu_.testOK('timecard.test_workStealing.actual_steal_sprint13'))
+
---
+++ packages/devanalyst/devanalyst-0.3.0rc1.tar.gz/devanalyst-0.3.0rc1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='devanalyst',
+      version='0.3.0.rc1',
+      description='Tools to simulate and analyze software development lifecycles',
+      long_description=long_description,
+      long_description_content_type="text/markdown",      
+      url='http://github.com/ChateauClaudia-Labs/devanalyst',
+      author='Alejandro Hernandez',
+      author_email='alejandro@chateauclaudia-labs.com',
+      license='MIT',
+      #packages=setuptools.find_packages(),      
+      packages=['devanalyst', 'devanalyst-examples'],
+      install_requires=[
+          'pandas', 'numpy', 'IPython', 'nbformat',
+      ],
+      setup_requires=["pytest-runner", ],
+      tests_require=["pytest",],
+      include_package_data=True,
+      zip_safe=False,
+      keywords='development efficiency analysis process simulation',
+      classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Software Development',
+      ],
+)
+
+    
+
+
