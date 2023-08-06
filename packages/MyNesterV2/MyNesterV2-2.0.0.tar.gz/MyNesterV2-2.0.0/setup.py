3a6c201cb5d416f0821e00797a87e9234f3641d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:44 +0000

    Add package MyNesterV2/2.0.0/MyNesterV2-2.0.0.tar.gz

---
+++ packages/MyNesterV2/MyNesterV2-2.0.0.tar.gz/MyNesterV2-2.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'MyNesterV2',
+version = '2.0.0',
+py_modules = ['MyNesterV2'],
+author = 'kira02',
+author_email = 'hfpython@headfirstlabs.com',
+url = 'http://www.headfirstlabs.com',
+description = 'A complex printer of nested lists',
+)
