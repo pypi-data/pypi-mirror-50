fbb315cface3ee703c7581cf161af4beaa6fff60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:40 +0000

    Add package csi-tai/0.0.14/csi-tai-0.0.14.tar.gz

---
+++ packages/csi-tai/csi-tai-0.0.14.tar.gz/csi-tai-0.0.14/README.md
@@ -0,0 +1,32 @@
+[![Build Status](https://travis-ci.com/dtaivpp/CSI_API.svg?branch=master)](https://travis-ci.com/dtaivpp/CSI_API)
+[![BCH compliance](https://bettercodehub.com/edge/badge/dtaivpp/CSI_API?branch=master)](https://bettercodehub.com/)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+# Using the CSI API Library 
+
+Please note this module was created in order to simplify bulk data pulling from the Virtual Observer API. This project is in no way affiliated with [CSI World / Virtual Observer](www.csiworld.com).
+
+## Getting Started
+
+To get started install the requests module using the following command.  
+
+`python -m pip install csi-tai`
+
+
+Basic Get Useage:  
+  ```
+  baseURL = "https://cloud.csiworld.com/VOWebAPI/v5"  
+  csi = CsiConnector(token, baseURL)  
+  params = {'filter': 'f.FName|o.eq|v.Tippett',  
+                'fields': 'FName, LName',  
+                'perpage':100}  
+  data = csi.query(endpoints.AgentInfo, params)  
+  ```
+
+Basic Post Useage:  
+  ```
+  baseURL = "https://cloud.csiworld.com/VOWebAPI/v5"  
+  csi = CsiConnector(token, baseURL)  
+  data = {'User': 'jsmith', 'Function': 'Pause'}  
+  csi.query(endpoints.lightstout, data)  
+  ```
---
+++ packages/csi-tai/csi-tai-0.0.14.tar.gz/csi-tai-0.0.14/csi/csv_writer.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+
+def _contains_special_char(_check_str):
+  special_char = ['\n', ',']
+  return any(st in _check_str for st in special_char)
+
+def process_row(_obj):
+  temp_row = ""
+  for key in _obj:
+    if _contains_special_char(_obj[key]):
+      temp_row += "\"" +_obj[key] + "\"" + ","
+    else:
+      temp_row += _obj[key] + ","
+
+  temp_row += "\n"
+
+  return temp_row
+
+def export_csv(_data, parameters):
+  """
+  Export CSV
+  ~~~~~~~~~~~~~
+
+  Exports Flat Json to CSV File
+  
+  Basic Useage:
+    >>> data = {'datapoint1': "data1", 'datapoint2': "data, two", 'datapoint3': "data3"}
+    >>> export_csv(data, {'path':'C:\\DropLocation\\ ', 'filename':"csvFile", 'overwrite':False})
+
+  :copyright: (c) 2019 by David Tippett.
+  :license: MIT License, see LICENSE for more details.
+  """
+
+  if parameters['overwrite']:
+    _path = parameters['path'] + parameters['filename'] + ".csv"
+  else:
+    now = datetime.now
+    timestr = datetime.utcnow().strftime('%Y-%m-%d-%f')
+    
+    _path = parameters['path'] + parameters['filename'] + timestr + ".csv"
+
+  _location = open(_path, 'w+')
+  count = 0
+  _filestr = ""
+
+  header = _data[0].keys()
+  _filestr += process_row(header)
+
+  for _obj in _data:
+    _filestr += process_row(_obj)
+
+  _location.write(_filestr)
+  _location.close()
+
+
+
