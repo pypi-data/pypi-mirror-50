d76ed8ddbd7663115372e65e79aceebb1f30dcfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:00 +0000

    Add package fast-configuration/1.0.0/fast_configuration-1.0.0.tar.gz

---
+++ packages/fast-configuration/fast_configuration-1.0.0.tar.gz/fast_configuration-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fast_configuration',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Dmitry Miasnenko',
+    author_email='d2ms2nk@gmail.com',
+    url='https://github.com/YoungMeatBoy/fast_configuration',
+)
