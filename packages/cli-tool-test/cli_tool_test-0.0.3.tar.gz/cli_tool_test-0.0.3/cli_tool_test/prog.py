fa16a368ffa82bfad261291f804542d9efe7ddff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:05 +0000

    Add package cli-tool-test/0.0.3/cli_tool_test-0.0.3.tar.gz

---
+++ packages/cli-tool-test/cli_tool_test-0.0.3.tar.gz/cli_tool_test-0.0.3/cli_tool_test/prog.py
@@ -0,0 +1,27 @@
+import argparse
+import sys
+import click
+import pyfiglet
+
+
+__version__ = "0.2.0"
+
+
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name', help='The person to greet.')
+def greet(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo('Hello %s!' % name)
+        
+    ascii_banner = pyfiglet.figlet_format("Hello!!")
+    print(ascii_banner)
+
+@click.command()
+@click.option('--text', default='Hello', help='Generates ascii art')
+def generate_ascii_art(text):
+    ascii_banner = pyfiglet.figlet_format(text)
+    print(ascii_banner)
+
+
