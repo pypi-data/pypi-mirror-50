ebe5434afcc45a291208c4b2820fc4893ed48a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:36 +0000

    Add package responsibly/0.1.0/responsibly-0.1.0.tar.gz

---
+++ packages/responsibly/responsibly-0.1.0.tar.gz/responsibly-0.1.0/responsibly/tests/conftest.py
@@ -0,0 +1,19 @@
+"""Unit tests configuration file."""
+
+
+def pytest_configure(config):
+    """Disable verbose output when running tests."""
+
+    terminal = config.pluginmanager.getplugin('terminal')
+    base = terminal.TerminalReporter
+
+    class QuietReporter(base):
+        """Reporter that only shows dots when running tests."""
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.verbosity = 0
+            self.showlongtestinfo = False
+            self.showfspath = False
+
+    terminal.TerminalReporter = QuietReporter
