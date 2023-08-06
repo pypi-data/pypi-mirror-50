4bd3fde550166d2f9e5a52a3e752027e108ff792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:58 +0000

    Add package gecos/0.2.3/gecos-0.2.3-py3-none-any.whl

---
+++ packages/gecos/gecos-0.2.3-py3-none-any.whl/gecos/__init__.py
@@ -0,0 +1,11 @@
+# This source code is part of the Gecos package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+__version__ = "0.2.3"
+__author__ = "Patrick Kunzmann"
+
+from .colors import *
+from .space import *
+from .optimizer import *
+from .file import *
