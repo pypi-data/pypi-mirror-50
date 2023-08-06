2c577d3c01aa5dda528b6d830752112c302df669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:59 +0000

    Add package pystencils-autodiff/0.1.3/pystencils_autodiff-0.1.3.tar.gz

---
+++ packages/pystencils-autodiff/pystencils_autodiff-0.1.3.tar.gz/pystencils_autodiff-0.1.3/src/pystencils_autodiff/__init__.py
@@ -0,0 +1,22 @@
+import sys
+
+import pystencils_autodiff.backends  # NOQA
+from pystencils_autodiff._field_to_tensors import (  # NOQA
+    tf_constant_from_field, tf_placeholder_from_field, tf_scalar_variable_from_field,
+    tf_variable_from_field, torch_tensor_from_field)
+from pystencils_autodiff.adjoint_field import AdjointField
+from pystencils_autodiff.autodiff import (
+    AutoDiffAstPair, AutoDiffOp, create_backward_assignments, get_jacobian_of_assignments)
+
+__all__ = ['backends',
+           'AdjointField',
+           'get_jacobian_of_assignments',
+           'create_backward_assignments',
+           'AutoDiffOp',
+           'AutoDiffAstPair',
+           "tf_constant_from_field", " tf_placeholder_from_field",
+           "tf_scalar_variable_from_field", " tf_variable_from_field",
+           "torch_tensor_from_field"]
+
+sys.modules['pystencils.autodiff'] = pystencils_autodiff
+sys.modules['pystencils.autodiff.backends'] = pystencils_autodiff.backends
