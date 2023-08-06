c9fa204bcb6368cf487471e5d9c2ab458b300c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:28 +0000

    Add package whoohoo.co.uk/1.0/whoohoo.co.uk-1.0.tar.gz

---
+++ packages/whoohoo.co.uk/whoohoo.co.uk-1.0.tar.gz/whoohoo.co.uk-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='whoohoo.co.uk',
+      version='1.0',
+      description='Allows you to translate texts into different dialects via whoohoo.co.uk',
+      long_description='Allows you to translate texts into different dialects via whoohoo.co.uk',
+      author='sl4v',
+      author_email='iamsl4v@protonmail.com',
+      url='https://github.com/sl4vkek/python-whoohoo',
+      packages=['whoohoo'],
+      install_requires=['requests', 'BeautifulSoup4'],
+      license="WTFPL"
+)
