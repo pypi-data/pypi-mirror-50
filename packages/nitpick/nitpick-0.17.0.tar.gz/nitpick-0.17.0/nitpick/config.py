950eecea11be742a2187a16b0dd0728e4d38ce7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:17 +0000

    Add package nitpick/0.17.0/nitpick-0.17.0.tar.gz

---
+++ packages/nitpick/nitpick-0.17.0.tar.gz/nitpick-0.17.0/nitpick/config.py
@@ -0,0 +1,132 @@
+"""Configuration of the plugin."""
+import itertools
+import logging
+from pathlib import Path
+from shutil import rmtree
+from typing import Optional
+
+from nitpick.constants import (
+    CACHE_DIR_NAME,
+    MANAGE_PY,
+    NITPICK_MINIMUM_VERSION_JMEX,
+    PROJECT_NAME,
+    ROOT_FILES,
+    ROOT_PYTHON_FILES,
+    TOOL_NITPICK_JMEX,
+)
+from nitpick.files.pyproject_toml import PyProjectTomlFile
+from nitpick.files.setup_cfg import SetupCfgFile
+from nitpick.formats import TomlFormat
+from nitpick.generic import climb_directory_tree, search_dict, version_to_tuple
+from nitpick.mixin import NitpickMixin
+from nitpick.style import Style
+from nitpick.typedefs import JsonDict, PathOrStr, StrOrList, YieldFlake8Error
+
+LOGGER = logging.getLogger(__name__)
+
+
+class NitpickConfig(NitpickMixin):
+    """Plugin configuration, read from the project config."""
+
+    error_base_number = 200
+
+    _singleton_instance = None  # type: Optional["NitpickConfig"]
+
+    def __init__(self) -> None:
+        """Init instance."""
+        self.cache_dir = None  # type: Optional[Path]
+
+        self.pyproject_toml = None  # type: Optional[TomlFormat]
+        self.tool_nitpick_dict = {}  # type: JsonDict
+        self.style_dict = {}  # type: JsonDict
+        self.nitpick_dict = {}  # type: JsonDict
+        self.files = {}  # type: JsonDict
+
+    @classmethod
+    def get_singleton(cls) -> "NitpickConfig":
+        """Init the global singleton instance of the plugin configuration, needed by all file checkers."""
+        if cls._singleton_instance is None:
+            cls._singleton_instance = cls()
+        return cls._singleton_instance
+
+    @classmethod
+    def reset_singleton(cls):
+        """Reset the singleton instance. Useful on automated tests, to simulate ``flake8`` execution."""
+        cls._singleton_instance = None
+
+    def find_root_dir(self, starting_file: PathOrStr) -> bool:
+        """Find the root dir of the Python project: the dir that has one of the `ROOT_FILES`.
+
+        Also clear the cache dir the first time the root dir is found.
+        """
+        if hasattr(self, "root_dir"):
+            return True
+
+        found_files = climb_directory_tree(
+            starting_file, ROOT_FILES + (PyProjectTomlFile.file_name, SetupCfgFile.file_name)
+        )
+        if not found_files:
+            # If none of the root files were found, try again with manage.py.
+            # On Django projects, it can be in another dir inside the root dir.
+            found_files = climb_directory_tree(starting_file, [MANAGE_PY])
+            if not found_files:
+                LOGGER.error("No files found while climbing directory tree from %s", str(starting_file))
+                return False
+
+        self.root_dir = found_files[0].parent
+        self.clear_cache_dir()
+        return True
+
+    def find_main_python_file(self) -> bool:
+        """Find the main Python file in the root dir, the one that will be used to report Flake8 warnings.
+
+        The search order is:
+        1. Python files that belong to the root dir of the project (e.g.: ``setup.py``, ``autoapp.py``).
+        2. ``manage.py``: they can be on the root or on a subdir (Django projects).
+        3. Any other ``*.py`` Python file.
+        """
+        if not self.root_dir:
+            return False
+        for the_file in itertools.chain(
+            [self.root_dir / root_file for root_file in ROOT_PYTHON_FILES],
+            self.root_dir.glob("*/{}".format(MANAGE_PY)),
+            self.root_dir.glob("*/*.py"),
+        ):
+            if the_file.exists():
+                self.main_python_file = Path(the_file)
+                LOGGER.info("Found the file %s", the_file)
+                return True
+        return False
+
+    def clear_cache_dir(self) -> None:
+        """Clear the cache directory (on the project root or on the current directory)."""
+        if not self.root_dir:
+            return
+        cache_root = self.root_dir / CACHE_DIR_NAME  # type: Path
+        self.cache_dir = cache_root / PROJECT_NAME
+        rmtree(str(self.cache_dir), ignore_errors=True)
+
+    def merge_styles(self) -> YieldFlake8Error:
+        """Merge one or multiple style files."""
+        pyproject_path = self.root_dir / PyProjectTomlFile.file_name  # type: Path
+        if pyproject_path.exists():
+            self.pyproject_toml = TomlFormat(path=pyproject_path)
+            self.tool_nitpick_dict = search_dict(TOOL_NITPICK_JMEX, self.pyproject_toml.as_data, {})
+
+        configured_styles = self.tool_nitpick_dict.get("style", "")  # type: StrOrList
+        style = Style(self)
+        style.find_initial_styles(configured_styles)
+        self.style_dict = style.merge_toml_dict()
+
+        from nitpick.plugin import NitpickChecker
+
+        minimum_version = search_dict(NITPICK_MINIMUM_VERSION_JMEX, self.style_dict, None)
+        if minimum_version and version_to_tuple(NitpickChecker.version) < version_to_tuple(minimum_version):
+            yield self.flake8_error(
+                3,
+                "The style file you're using requires {}>={}".format(PROJECT_NAME, minimum_version)
+                + " (you have {}). Please upgrade".format(NitpickChecker.version),
+            )
+
+        self.nitpick_dict = self.style_dict.get("nitpick", {})
+        self.files = self.nitpick_dict.get("files", {})
---
+++ packages/nitpick/nitpick-0.17.0.tar.gz/nitpick-0.17.0/nitpick/plugin.py
@@ -0,0 +1,75 @@
+"""Flake8 plugin to check files."""
+import itertools
+import logging
+from pathlib import Path
+
+import attr
+
+from nitpick import __version__
+from nitpick.config import NitpickConfig
+from nitpick.constants import PROJECT_NAME, ROOT_PYTHON_FILES
+from nitpick.files.base import BaseFile
+from nitpick.generic import get_subclasses
+from nitpick.mixin import NitpickMixin
+from nitpick.typedefs import YieldFlake8Error
+
+LOGGER = logging.getLogger(__name__)
+
+
+@attr.s(hash=False)
+class NitpickChecker(NitpickMixin):
+    """Main plugin class."""
+
+    # Plugin config
+    name = PROJECT_NAME
+    version = __version__
+
+    # NitpickMixin
+    error_base_number = 100
+
+    # Plugin arguments passed by Flake8
+    tree = attr.ib(default=None)
+    filename = attr.ib(default="(none)")
+
+    def run(self) -> YieldFlake8Error:
+        """Run the check plugin."""
+        self.config = NitpickConfig().get_singleton()
+        if not self.config.find_root_dir(self.filename):
+            yield self.flake8_error(1, "No root dir found (is this a Python project?)")
+            return
+
+        if not self.config.find_main_python_file():
+            yield self.flake8_error(
+                2,
+                "None of those Python files was found in the root dir"
+                + " {}: {}".format(self.config.root_dir, ", ".join(ROOT_PYTHON_FILES)),
+            )
+            return
+
+        current_python_file = Path(self.filename)
+        if current_python_file.absolute() != self.config.main_python_file.absolute():
+            # Only report warnings once, for the main Python file of this project.
+            LOGGER.info("Ignoring file: %s", self.filename)
+            return
+        LOGGER.info("Nitpicking file: %s", self.filename)
+
+        yield from itertools.chain(self.config.merge_styles(), self.check_absent_files())
+
+        for checker_class in get_subclasses(BaseFile):
+            checker = checker_class()
+            yield from checker.check_exists()
+
+        return []
+
+    def check_absent_files(self) -> YieldFlake8Error:
+        """Check absent files."""
+        for file_name, delete_message in self.config.files.get("absent", {}).items():
+            file_path = self.config.root_dir / file_name  # type: Path
+            if not file_path.exists():
+                continue
+
+            full_message = "File {} should be deleted".format(file_name)
+            if delete_message:
+                full_message += ": {}".format(delete_message)
+
+            yield self.flake8_error(3, full_message)
