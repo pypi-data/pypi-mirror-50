00cb9c35b1d9ac39e3adc94edbc4188d0d041551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:30 +0000

    Add package seamm-util/0.2.1/seamm_util-0.2.1.tar.gz

---
+++ packages/seamm-util/seamm_util-0.2.1.tar.gz/seamm_util-0.2.1/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'datetime',
+    'pint',
+    'pprint',
+]
+
+setup_requirements = [
+    'pytest-runner',
+    # TODO(paulsaxe): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    'pytest',
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='seamm_util',
+    version='0.2.1',
+    description="Utility classes and functions",
+    long_description=readme + '\n\n' + history,
+    author="Paul Saxe",
+    author_email='seamm@molssi.org',
+    url='https://github.com/molssi-seamm/seamm_util',
+    packages=find_packages(include=['seamm_util']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD license",
+    zip_safe=False,
+    keywords='seamm_util',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
