fe981854c1a0c6234a676aecf8aef574b5027c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:27 +0000

    Add package tutti-aisthetics/1.0.0/tutti_aisthetics-1.0.0.tar.gz

---
+++ packages/tutti-aisthetics/tutti_aisthetics-1.0.0.tar.gz/tutti_aisthetics-1.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+from src.tutti_aisthetics import __version__
+
+setup(
+    name='tutti_aisthetics',
+    version=__version__,
+    description='A tutti.ch image asthetics AI scorer.',
+    long_description='Package based on https://github.com/idealo/image-quality-assessment.',
+    author='Oscar Saleta',
+    author_email='oscar@tutti.ch',
+    license='Proprietary',
+    keywords=['nima', 'CNN', 'neural net', 'MobileNet', 'aesthetics'],
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    python_requires='>=3.5.2',
+    install_requires=[
+        'keras==2.2.*',
+        'absl-py==0.7.*',
+        'tensorflow==1.14.*',
+        'pillow==6.1.*',
+        'plac==1.1.*',
+        'boto3==1.9.*',
+    ],
+    zip_safe=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
+    package_data={
+        'tutti_aisthetics': ['model/*.hdf5']
+    },
+    include_package_data=True
+)
