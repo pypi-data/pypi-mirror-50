c73f64c3d570f9ccc1955b09d1b065e91225a93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:41 +0000

    Add package matialvarezs_node_configurations/0.0.1/matialvarezs_node_configurations-0.0.1.tar.gz

---
+++ packages/matialvarezs_node_configurations/matialvarezs_node_configurations-0.0.1.tar.gz/matialvarezs_node_configurations-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'matialvarezs_node_configurations',
+  packages = ['matialvarezs_node_configurations'], # this must be the same as the name above
+  version = '0.0.1',
+  install_requires = [
+
+  ],
+  include_package_data = True,
+  description = 'Nodes configurations - model and api',
+  author = 'Matias Alvarez Sabate',
+  author_email = 'matialvarezs@gmail.com',  
+  classifiers = [
+    'Programming Language :: Python :: 3.5',
+  ],
+)
