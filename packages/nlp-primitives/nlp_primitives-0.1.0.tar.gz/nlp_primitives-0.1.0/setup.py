43a546243ab8bb658399438edb80e9f1546e8669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:38 +0000

    Add package nlp-primitives/0.1.0/nlp_primitives-0.1.0.tar.gz

---
+++ packages/nlp-primitives/nlp_primitives-0.1.0.tar.gz/nlp_primitives-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='nlp_primitives',
+    version='v0.1.0',
+    author='Feature Labs, Inc.',
+    author_email='support@featurelabs.com',
+    license='BSD 3-clause',
+    url='http://www.featurelabs.com/',
+    install_requires=open('requirements.txt').readlines(),
+    tests_require=open('test-requirements.txt').readlines(),
+    packages=find_packages(),
+)
