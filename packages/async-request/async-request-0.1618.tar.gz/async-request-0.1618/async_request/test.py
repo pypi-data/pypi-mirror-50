c1236667ada2a1f450d9931a46006ffea837e5a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:56 +0000

    Add package async-request/0.1618/async-request-0.1618.tar.gz

---
+++ packages/async-request/async-request-0.1618.tar.gz/async-request-0.1618/async_request/test.py
@@ -0,0 +1,33 @@
+from async_request import Request, crawl, fetch
+
+
+def parse_baidu(response):
+    print(response.url, response.status_code)
+    yield Request('https://cn.bing.com/', callback=parse_bing)
+
+
+def parse_bing(response):
+    print(response.url, response.status_code)
+    print(response.xpath('//a/@href').get())
+    yield Request('https://www.360.cn/', callback=parse_github)
+
+
+def parse_github(response):
+    print(response.url, response.status_code)
+    yield {'hello': 'github'}
+
+
+def process_result(result):
+    print(result)
+
+
+def parse():
+    response = fetch('https://www.bing.com')
+    response2 = fetch('https://www.baidu.com')
+    print(response, response2)
+
+
+if __name__ == '__main__':
+    # request_list = [Request(url='https://www.baidu.com', callback=parse_baidu)]
+    # crawl(request_list, result_back=process_result, handle_cookies=False, download_delay=0)
+    parse()
