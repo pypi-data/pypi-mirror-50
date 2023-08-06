96a7cd38dcdd57f5069ab5c18d2f1a84c0b5197c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:44 +0000

    Add package pysweeper/1.0.3/pysweeper-1.0.3.tar.gz

---
+++ packages/pysweeper/pysweeper-1.0.3.tar.gz/pysweeper-1.0.3/setup.py
@@ -0,0 +1,28 @@
+"""PySweeper installation."""
+
+import pathlib
+
+from setuptools import setup
+
+import versioneer
+
+directory = pathlib.Path(__file__).parent
+
+setup(
+    name="pysweeper",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    url="https://github.com/cpcloud/pysweeper",
+    author="Phillip Cloud",
+    author_email="cpcloud@gmail.com",
+    maintainer="Phillip Cloud",
+    maintainer_email="cpcloud@gmail.com",
+    description="Your favorite mine sweeping game, console style.",
+    long_description=directory.joinpath("README.md").read_text(),
+    install_requires=(
+        directory.joinpath("requirements.txt").read_text().splitlines()
+    ),
+    python_requires=">=3.7",
+    license="MIT",
+    entry_points={"console_scripts": ["pysweeper = pysweeper.__main__:main"]},
+)
