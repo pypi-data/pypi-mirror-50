d75560725cf4cb79938bd62a6d21fd97f0792651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:56 +0000

    Add package colour-printing/0.0.6/colour-printing-0.0.6.tar.gz

---
+++ packages/colour-printing/colour-printing-0.0.6.tar.gz/colour-printing-0.0.6/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="colour-printing",
+    version="0.0.6",
+    author="faithforus",
+    author_email="ljunf817@163.com",
+    description="colour-printing",
+    long_description="以不同颜色区分终端输出信息类型，标识出重要信息",
+    keywords="python package print",
+    url="https://github.com/Faithforus/Colour-printing",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
