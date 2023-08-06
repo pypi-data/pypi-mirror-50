35c4fc5a545f94df054ed8822b93b5559a37d983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:40 +0000

    Add package plantlabeller/0.0.2/plantlabeller-0.0.2.tar.gz

---
+++ packages/plantlabeller/plantlabeller-0.0.2.tar.gz/plantlabeller-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+
+setup(
+        name="plantlabeller",
+        version="0.0.2",
+        keywords=("pip","plantlabeller","droneimage","yangh"),
+        description="label plants",
+        long_description="label plants in drone images",
+        license = "MIT Licence",
+
+        url="http://zzlab.net/yanghu/index.html",
+        author="yangh",
+        author_email="yang.hu@wsu.edu",
+
+        packages=find_packages(),
+        include_package_data=True,
+        platforms="any",
+        #install_requires=["tkinter","PIL","cv2","csv","gdal","matplotlib","scipy","sklearn","functools","time","skimage"]
+        )
