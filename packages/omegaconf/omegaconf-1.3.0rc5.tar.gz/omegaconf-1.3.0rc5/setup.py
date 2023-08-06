10d513032a7645268c95f209240a8a18a4970248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:12 +0000

    Add package omegaconf/1.3.0rc5/omegaconf-1.3.0rc5.tar.gz

---
+++ packages/omegaconf/omegaconf-1.3.0rc5.tar.gz/omegaconf-1.3.0rc5/setup.py
@@ -0,0 +1,36 @@
+"""
+OmegaConf setup
+    Instructions:
+    # Build:
+    rm -rf dist/ omegaconf.egg-info/
+    python3 setup.py sdist bdist_wheel
+    # Upload:
+    python3 -m twine upload dist/*
+"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    LONG_DESC = fh.read()
+    setuptools.setup(
+        name="omegaconf",
+        version="1.3.0rc5",
+        author="Omry Yadan",
+        author_email="omry@yadan.net",
+        description="A flexible configuration library",
+        long_description=LONG_DESC,
+        long_description_content_type="text/markdown",
+        setup_requires=["pytest-runner"],
+        tests_require=["pytest"],
+        url="https://github.com/omry/omegaconf",
+        keywords='yaml configuration config',
+        packages=['omegaconf'],
+        classifiers=[
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+        ],
+        install_requires=['six', 'PyYAML']
+    )
