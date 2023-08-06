5f047c43e16cb443c0d8ba2cfe0a3555c45b4fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:33 +0000

    Add package django-for-runners/0.10.1/django_for_runners-0.10.1-py2.py3-none-any.whl

---
+++ packages/django-for-runners/django_for_runners-0.10.1-py2.py3-none-any.whl/for_runners/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+from .version import __version__  # noqa
+
+# https://docs.djangoproject.com/en/2.0/ref/applications/#configuring-applications-ref
+default_app_config = "for_runners.apps.ForRunnersConfig"
