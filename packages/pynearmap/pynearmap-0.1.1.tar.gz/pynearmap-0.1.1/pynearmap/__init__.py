62a8749c7da1be545207d271f7f786be1d81d94d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:22 +0000

    Add package pynearmap/0.1.1/pynearmap-0.1.1.tar.gz

---
+++ packages/pynearmap/pynearmap-0.1.1.tar.gz/pynearmap-0.1.1/pynearmap/__init__.py
@@ -0,0 +1,30 @@
+from pynearmap.Coverage.Point import Point as CoveragePoint
+from pynearmap.Coverage.Polygon import Polygon as CoveragePolygon
+from pynearmap.Coverage.TileCoordinate import TileCoordinate as CoverageTileCoordinate
+
+from pynearmap.Image.Bounded import Bounded as ImageBounded
+from pynearmap.Image.Centered import Centered as ImageCentered
+
+from pynearmap.Tile.Tile import Tile
+from pynearmap.Tile.SpecifiedSurvey import SpecifiedSurvey as TileSpecifiedSurvey
+
+# class CoveragePoint(Point):
+#     pass
+
+# class CoveragePolygon(Polygon):
+#     pass
+
+# class CoverageTileCoordinate(TileCoordinate):
+#     pass
+
+# class ImageBounded(Bounded):
+#     pass
+
+# class ImageCentered(Centered):
+#     pass
+
+# class Tile(StandardTile):
+#     pass
+
+# class TileSpecifiedSurvey(SpecifiedSurvey):
+#     pass
