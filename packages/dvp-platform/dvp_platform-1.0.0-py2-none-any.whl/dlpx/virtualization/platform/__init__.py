fb5c15bb9513cbe2596c4a7d074717379801a0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:45 +0000

    Add package dvp-platform/1.0.0/dvp_platform-1.0.0-py2-none-any.whl

---
+++ packages/dvp-platform/dvp_platform-1.0.0-py2-none-any.whl/dlpx/virtualization/platform/__init__.py
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2019 by Delphix. All rights reserved.
+#
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+from dlpx.virtualization.platform._plugin_classes import *
+from dlpx.virtualization.platform._plugin import *
