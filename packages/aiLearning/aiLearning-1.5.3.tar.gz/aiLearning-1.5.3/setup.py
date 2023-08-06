d0ac59869ae7d4f163117939ac1f0f814ea118bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:05 +0000

    Add package aiLearning/1.5.3/aiLearning-1.5.3.tar.gz

---
+++ packages/aiLearning/aiLearning-1.5.3.tar.gz/aiLearning-1.5.3/setup.py
@@ -0,0 +1,44 @@
+import codecs
+import os
+import sys
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+"""
+打包的用的setup必须引入，
+
+"""
+
+NAME = "aiLearning"
+
+PACKAGES = ["aiLearning", ]
+
+DESCRIPTION = "this is a package for packing python machine learning."  # 模块描述，可选改
+
+url =  'https://github.com/apachecn/AiLearning'
+KEYWORDS = "python package"  # 关键字，可选改
+
+VERSION = "1.5.3"
+
+LICENSE = "MIT"
+
+
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords = KEYWORDS,
+    license = LICENSE,
+    packages = PACKAGES,
+    include_package_data = True,
+    zip_safe = True,
+    install_requires =['paramiko']
+)
