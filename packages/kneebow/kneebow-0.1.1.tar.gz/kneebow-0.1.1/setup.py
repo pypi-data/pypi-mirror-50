855293df526d68eee744838b461d964dd277d48e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:18 +0000

    Add package kneebow/0.1.1/kneebow-0.1.1.tar.gz

---
+++ packages/kneebow/kneebow-0.1.1.tar.gz/kneebow-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='kneebow',
+    version='0.1.1',
+    packages=['kneebow'],
+    url='https://github.com/georg-un/kneebow',
+    license='MIT',
+    author='Georg Unterholzner',
+    author_email='georg.unterholzner.coding@gmail.com',
+    description='Knee or elbow detection for curves',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['numpy', 'matplotlib', 'scikit-learn'],
+    download_url='https://github.com/georg-un/kneebow/archive/v0.1.1.tar.gz',
+    python_requires='>=3.4',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
