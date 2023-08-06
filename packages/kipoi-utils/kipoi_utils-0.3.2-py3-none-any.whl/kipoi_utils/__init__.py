53413b15b09ef7ecb46b90e22f582b26615034d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:35 +0000

    Add package kipoi-utils/0.3.2/kipoi_utils-0.3.2-py3-none-any.whl

---
+++ packages/kipoi-utils/kipoi_utils-0.3.2-py3-none-any.whl/kipoi_utils/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+__author__ = 'Kipoi team'
+__email__ = 'thorsten.beier@embl.de'
+
+from ._version import __version__
+
+
+# available modules
+from . import external
+from . utils import *
+
+from . utils import _get_arg_name_values
