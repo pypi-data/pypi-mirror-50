dc444b0dd5a5fdc4fca448acb919c2e291a7c724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:51 +0000

    Add package MacPackageTest/0.1/MacPackageTest-0.1-py2.py3-none-any.whl

---
+++ packages/MacPackageTest/MacPackageTest-0.1-py2.py3-none-any.whl/MacPackageTest/main.py
@@ -0,0 +1,15 @@
+# -*- coding：utf-8 -*-
+"""
+@author: macmao
+@email:mac.mao@kpmg.com
+@time:06/08/2019 9:49 am
+"""
+
+def max_number(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+# print(max_number(2,1))
