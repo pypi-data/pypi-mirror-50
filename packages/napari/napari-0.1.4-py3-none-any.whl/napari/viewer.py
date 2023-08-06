bf972337d8f09f0a4bd49feea5ba7cc73f065bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:52 +0000

    Add package napari/0.1.4/napari-0.1.4-py3-none-any.whl

---
+++ packages/napari/napari-0.1.4-py3-none-any.whl/napari/viewer.py
@@ -0,0 +1,21 @@
+from ._qt.qt_viewer import QtViewer
+from ._qt.qt_main_window import Window
+from .components import ViewerModel
+
+
+class Viewer(ViewerModel):
+    """Napari ndarray viewer.
+
+    Parameters
+    ----------
+    title : string
+        The title of the viewer window.
+    """
+
+    def __init__(self, title='napari'):
+        super().__init__(title=title)
+        qt_viewer = QtViewer(self)
+        self.window = Window(qt_viewer)
+        self.screenshot = self.window.qt_viewer.screenshot
+        self.camera = self.window.qt_viewer.view.camera
+        self.update_console = self.window.qt_viewer.console.push
