e5909d9c93f4b61ff72bd309be4dcb6acb8f8198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:15 +0000

    Add package larisinpy/0.0.2/larisinpy-0.0.2-py3-none-any.whl

---
+++ packages/larisinpy/larisinpy-0.0.2-py3-none-any.whl/larisinpy/cli.py
@@ -0,0 +1,5 @@
+def cli():
+    pass
+    
+def main():
+    cli()
---
+++ packages/larisinpy/larisinpy-0.0.2-py3-none-any.whl/larisinpy/hello_world.py
@@ -0,0 +1,8 @@
+class HelloWorld:
+
+    def __init__(self):
+    
+        pass
+    
+    def run(self):
+        print("Success")
