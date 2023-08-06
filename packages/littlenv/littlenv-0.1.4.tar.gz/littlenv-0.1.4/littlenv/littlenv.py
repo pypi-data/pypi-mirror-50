4e6ab3c4b219ef8bf941c2b8911316648dd153b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:27 +0000

    Add package littlenv/0.1.4/littlenv-0.1.4.tar.gz

---
+++ packages/littlenv/littlenv-0.1.4.tar.gz/littlenv-0.1.4/littlenv/littlenv.py
@@ -0,0 +1,19 @@
+import os
+import re
+
+"""
+A simple script for manage .env in django, is very easy, 
+you just have to import this library in your manage.py
+"""
+
+
+def load():
+    try:
+        env_file = open(".env", "r")
+        for linea in env_file.readlines():
+            if linea != '\n' and not re.search(r'^(#.*)$', linea):
+                variables = linea.split("=")
+                if not os.environ.get(variables[0].strip()):
+                    os.environ.setdefault(variables[0].strip(), variables[1].strip())
+    except Exception as e:
+        raise Exception('Not found .env or the file is wrong', e)
