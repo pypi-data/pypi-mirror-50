29de1b94ef3af6785ea491b726be0fc6c98171d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:54 +0000

    Add package sunscreen/0.0.4/sunscreen-0.0.4.tar.gz

---
+++ packages/sunscreen/sunscreen-0.0.4.tar.gz/sunscreen-0.0.4/setup.py
@@ -0,0 +1,29 @@
+from os import path
+from setuptools import setup
+
+this_dir = path.abspath(path.dirname(__file__))
+with open(path.join(this_dir, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="sunscreen",
+    version="0.0.4",
+    description="What's the UV forecast?",
+    author="Jon Miller",
+    author_email="jmill@jmill.xyz",
+    url="https://github.com/jmillxyz/sunscreen",
+    py_modules=["sunscreen"],
+    install_requires=["appdirs", "arrow", "colored", "click", "requests"],
+    entry_points="""
+        [console_scripts]
+        sunscreen=sunscreen:main
+    """,
+    classifiers=(
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
