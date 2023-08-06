84fb9cf9378e73be710b312e64c38b926ab8d51d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:21:04 +0000

    Add package json-to-dir/1.1/json-to-dir-1.1.tar.gz

---
+++ packages/json-to-dir/json-to-dir-1.1.tar.gz/json-to-dir-1.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="json-to-dir",
+    version="1.1",
+    author="fiatjaf",
+    author_email="fiatjaf@gmail.com",
+    license="MIT",
+    description="Turns a JSON file into a directory structure.",
+    packages=[],
+    scripts=["json-to-dir"],
+)
