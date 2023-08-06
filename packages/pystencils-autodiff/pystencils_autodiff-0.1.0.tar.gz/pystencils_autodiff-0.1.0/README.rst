969a4553e06a26cb5257f09c02572b5064830e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:15 +0000

    Add package pystencils-autodiff/0.1.0/pystencils_autodiff-0.1.0.tar.gz

---
+++ packages/pystencils-autodiff/pystencils_autodiff-0.1.0.tar.gz/pystencils_autodiff-0.1.0/README.rst
@@ -0,0 +1,25 @@
+.. image:: https://readthedocs.org/projects/pystencils-autodiff/badge/?version=latest
+    :target: https://pystencils-autodiff.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status===================
+
+.. image:: https://travis-ci.org/theHamsta/pystencils_autodiff.svg?branch=master
+    :target: https://travis-ci.org/theHamsta/pystencils_autodiff
+
+pystencils_autodiff
+===================
+
+
+Add a short description here!
+
+
+Description
+===========
+
+A longer description of your project goes here...
+
+
+Note
+====
+
+This project has been set up using PyScaffold 3.2. For details and usage
+information on PyScaffold see https://pyscaffold.org/.
---
+++ packages/pystencils-autodiff/pystencils_autodiff-0.1.0.tar.gz/pystencils_autodiff-0.1.0/src/pystencils_autodiff/__init__.py
@@ -0,0 +1,17 @@
+import pystencils_autodiff.backends
+from pystencils_autodiff._field_to_tensors import (
+    tf_constant_from_field, tf_placeholder_from_field, tf_scalar_variable_from_field,
+    tf_variable_from_field, torch_tensor_from_field)
+from pystencils_autodiff.adjoint_field import AdjointField
+from pystencils_autodiff.autodiff import (
+    AutoDiffAstPair, AutoDiffOp, create_backward_assignments, get_jacobian_of_assignments)
+
+__all__ = ['backends',
+           'AdjointField',
+           'get_jacobian_of_assignments',
+           'create_backward_assignments',
+           'AutoDiffOp',
+           'AutoDiffAstPair',
+           "tf_constant_from_field", " tf_placeholder_from_field",
+           "tf_scalar_variable_from_field", " tf_variable_from_field",
+           "torch_tensor_from_field"]
