af386da6d6040ad4b4296f6a9f4a54724c90af88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:46 +0000

    Add package waddle/0.8/waddle-0.8.tar.gz

---
+++ packages/waddle/waddle-0.8.tar.gz/waddle-0.8/waddle/__init__.py
@@ -0,0 +1,7 @@
+"""
+Contains a number of useful modules for handling
+configuration and encryption using aws parameter store
+and kms
+"""
+from .bunch import *  # noqa: F401, F403
+from .param_bunch import *  # noqa: F401, F403
