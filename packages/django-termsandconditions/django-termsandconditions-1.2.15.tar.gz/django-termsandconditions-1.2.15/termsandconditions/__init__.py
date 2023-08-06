b7362bf0f7a878a3ca6561c58f5800a90f14cde2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:47 +0000

    Add package django-termsandconditions/1.2.15/django-termsandconditions-1.2.15.tar.gz

---
+++ packages/django-termsandconditions/django-termsandconditions-1.2.15.tar.gz/django-termsandconditions-1.2.15/termsandconditions/__init__.py
@@ -0,0 +1,5 @@
+"""Django Terms and Conditions Module"""
+from __future__ import unicode_literals
+from future.standard_library import install_aliases
+install_aliases()
+default_app_config = 'termsandconditions.apps.TermsAndConditionsConfig'
