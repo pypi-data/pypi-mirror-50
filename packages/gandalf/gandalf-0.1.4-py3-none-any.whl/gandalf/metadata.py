c3f71b52477bfab6d67445474d2aafee0bef4252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:52 +0000

    Add package gandalf/0.1.4/gandalf-0.1.4-py3-none-any.whl

---
+++ packages/gandalf/gandalf-0.1.4-py3-none-any.whl/gandalf/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = "gandalf"
+project = "Providing Blazingly fast & beautifully expressive Web Apps and APIs"
+project_no_spaces = project.replace(" ", "")
+version = "0.1.4"
+description = "Blazingly fast & beautifully expressive Web Apps and APIs"
+authors = ["Infrascloudy"]
+authors_string = ", ".join(authors)
+emails = ["support@infrascloudy.io"]
+license = "MIT"
+copyright = "2019 " + authors_string
+url = "http://infrascloudy.io/"
