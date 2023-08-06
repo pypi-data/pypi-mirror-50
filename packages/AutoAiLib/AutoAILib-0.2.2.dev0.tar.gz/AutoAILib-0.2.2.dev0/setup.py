2c1919978a6828a6395ecd9c5043c368677b425a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:30 +0000

    Add package AutoAiLib/0.2.2.dev0/AutoAILib-0.2.2.dev0.tar.gz

---
+++ packages/AutoAiLib/AutoAILib-0.2.2.dev0.tar.gz/AutoAILib-0.2.2.dev0/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, Extension, find_packages
+from bs4 import BeautifulSoup as soup
+from markdown import markdown
+description = markdown(open('README.md').read())
+#text = ''.join(soup(description, 'lxml').findAll(text=True))
+setup(name="AutoAILib",
+      version ='0.2.2dev',
+      packages=find_packages(),
+      license='GNU GPLv3',
+      long_description=description,
+      long_description_content_type = 'text/markdown',
+      author ='Matthew Mulhall',
+      author_email='matthewlmulhall@gmail.com',
+      )
