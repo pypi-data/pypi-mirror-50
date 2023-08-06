4b7aa389365f86ceb68e8e2d90dc9231f3e052a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:06 +0000

    Add package tagmaps/0.19.0/tagmaps-0.19.0-py3-none-any.whl

---
+++ packages/tagmaps/tagmaps-0.19.0-py3-none-any.whl/tagmaps/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""TagMaps package import specifications"""
+
+from __future__ import absolute_import
+
+from .version import __version__
+from .tagmaps_ import TagMaps
+from .config.config import BaseConfig
+from .classes.load_data import LoadData
+from .classes.prepare_data import PrepareData
+from .classes.interface import UserInterface
+from .classes.shared_structure import (
+    EMOJI, LOCATIONS, TAGS, TOPICS, PostStructure, ClusterType)
