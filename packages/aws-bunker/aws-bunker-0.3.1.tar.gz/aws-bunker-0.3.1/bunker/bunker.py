6b3e95bc5f3986f9a537dc9a8fb41685e25071d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:05 +0000

    Add package aws-bunker/0.3.1/aws-bunker-0.3.1.tar.gz

---
+++ packages/aws-bunker/aws-bunker-0.3.1.tar.gz/aws-bunker-0.3.1/bunker/bunker.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+
+"""bunker is a command line program for setting up an ec2 in AWS for remote development or as a backup. It can clone your git repos, and transfer ignored files from your machine to the ec2."""
+
+import argparse
+from argparse import RawTextHelpFormatter as rawtxt
+import sys
+import signal
+import time
+import os
+from os.path import expanduser
+import shutil
+import json
+import subprocess
+from datetime import datetime
+from pathlib import Path
+import pkg_resources
+
+#from init import init_bunker
+#from install import install
+#from extras import extras
+#from prompt import prompt
+#from buildbunker import buildbunker
+from .init import init_bunker
+from .install import install
+from .extras import extras
+from .prompt import prompt
+from .buildbunker import buildbunker
+
+def signal_handler(sig, frame):
+    """handle control c"""
+    print(Bcolors.WARNING+"\nExit with "+Bcolors.FAIL+"Control C\n"+Bcolors.ENDC)
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+def query_yes_no(question, default="yes"):
+    '''confirm or decline'''
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("\nPlease respond with 'yes' or 'no' (or 'y' or 'n').\n")
+
+def is_tool(name):
+    """Check whether `name` is on PATH and marked as executable."""
+    from shutil import which
+    return which(name) is not None
+
+class Bcolors:
+    """console colors"""
+    CYAN = '\033[96m'
+    MAGENTA = '\033[95m'
+    GREY = '\033[90m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    YELLOW = '\033[33m'
+    RED = '\033[31m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    ORANGE = '\033[38;5;208m'
+    PINK = '\033[38;5;212m'
+    PALEYELLOW = '\033[38;5;228m'
+    PALEBLUE = '\033[38;5;111m'
+    GOLD = '\033[38;5;178m'
+
+def bunker():
+    """bunker is a command line program for setting up an ec2 in AWS for remote development or as a backup. It can clone your git repos, and transfer ignored files from your machine to the ec2."""
+    version = pkg_resources.require("aws-bunker")[0].version
+    parser = argparse.ArgumentParser(
+        description='bunker is a command line program for setting up an ec2 in AWS for remote development or as a backup. It can clone your git repos, and transfer ignored files from your machine to the ec2.',
+        prog='bunker',
+        formatter_class=rawtxt
+    )
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s '+version)
+    
+    subparsers = parser.add_subparsers(title='subcommands', required=True, dest="subcommand", help="""for more info: type `bunker subcommand -h`
+
+the """+Bcolors.PINK+"""install"""+Bcolors.ENDC+""", """+Bcolors.PINK+"""extra """+Bcolors.ENDC+"""and """+Bcolors.PINK+"""prompt """+Bcolors.ENDC+"""subcommands require custom bash scripts to exist on your EC2. The scripts also need to be executable.
+for example scripts please visit the project homepage:
+"""+Bcolors.GOLD+"""https://gitlab.com/shindagger/bunker **"""+Bcolors.ENDC)
+    #parser.print_help()
+    
+    parser_init = subparsers.add_parser('init', formatter_class=argparse.RawTextHelpFormatter, help='init bunker by writing ~/.config-bunker', description="init bunker by writing ~/.config-bunker")
+    parser_init.set_defaults(func=init_bunker)
+    parser_install = subparsers.add_parser('install', formatter_class=argparse.RawTextHelpFormatter, help="""install essential software
+"""+Bcolors.GOLD+"""** requires `install-essentials.sh`"""+Bcolors.ENDC, description="""install essential software
+requires a file named: """+Bcolors.GOLD+"""`install-essentials.sh`"""+Bcolors.ENDC+""" on the EC2.
+the file should be executable """+Bcolors.OKGREEN+"""`chmod a+x install-essentials.sh`"""+Bcolors.ENDC+"""
+for example files please visit: """+Bcolors.PALEBLUE+"""https://gitlab.com/shindagger/bunker"""+Bcolors.ENDC)
+    parser_install.add_argument('-y', '--yes', action='store_true', help='auto approve installing essentials.')
+    parser_install.add_argument('-s', action='store_true', help='startup EC2 first.')
+    parser_install.set_defaults(func=install)
+    parser_extras = subparsers.add_parser('extra', formatter_class=argparse.RawTextHelpFormatter, help="""install extra software
+"""+Bcolors.GOLD+"""** requires `install-extras.sh`"""+Bcolors.ENDC, description="""install extra software.
+requires a file named: """+Bcolors.GOLD+"""`install-extras.sh`"""+Bcolors.ENDC+""" on the EC2.
+the file should be executable """+Bcolors.OKGREEN+"""`chmod a+x install-extras.sh`"""+Bcolors.ENDC+"""
+for example files please visit: """+Bcolors.PALEBLUE+"""https://gitlab.com/shindagger/bunker"""+Bcolors.ENDC)
+    parser_extras.add_argument('-y', '--yes', action='store_true', help='auto approve installing extras.')
+    parser_extras.add_argument('-s', action='store_true', help='startup EC2 first.')
+    parser_extras.set_defaults(func=extras)
+    parser_prompt = subparsers.add_parser('prompt', formatter_class=argparse.RawTextHelpFormatter, help="""install a custom bash prompt
+"""+Bcolors.GOLD+"""** requires `prompt.sh`"""+Bcolors.ENDC, description="""install a custom bash prompt
+requires a file named: """+Bcolors.GOLD+"""`prompt.sh`"""+Bcolors.ENDC+""" on the EC2.
+the file should be executable """+Bcolors.OKGREEN+"""`chmod a+x prompt.sh`"""+Bcolors.ENDC+"""
+for example files please visit: """+Bcolors.PALEBLUE+"""https://gitlab.com/shindagger/bunker"""+Bcolors.ENDC)
+    parser_prompt.add_argument('-y', '--yes', action='store_true', help='auto approve customizing bash prompt.')
+    parser_prompt.add_argument('-s', action='store_true', help='startup EC2 first.')
+    parser_prompt.set_defaults(func=prompt)
+    parser_build = subparsers.add_parser('build', help='clone git repos and copy over ignored files', description="clone git repos and copy over ignored files")
+    parser_build.add_argument('-y', '--yes', action='store_true', help='auto approve cloning repos and backing up files.')
+    parser_build.add_argument('-S', action='store_true', help='stop EC2 after running.')
+    parser_build.add_argument('-s', action='store_true', help='startup EC2 first.')
+    parser_build.add_argument('-r', '--repo', nargs='?', default=None, help='Source directory to be duplicated to remote bunker')
+    parser_build.set_defaults(func=buildbunker)
+    args = parser.parse_args()
+    # error checking
+    if args.subcommand != 'init':
+        home = expanduser('~')
+        bunker_config = os.path.join(home, ".config-bunker.ini")
+        if not os.path.isfile(bunker_config):
+            print(Bcolors.WARNING+"no configuration file exists at: "+Bcolors.ORANGE+bunker_config+Bcolors.ENDC)
+            print("please run:")
+            print()
+            print(Bcolors.PINK+"   bunker init"+Bcolors.ENDC)
+            print()
+            time.sleep(1)
+            print(Bcolors.GREY+"... bunker --help ..."+Bcolors.ENDC)
+            time.sleep(1)
+            parser.print_help()
+            exit()
+    # auf gehts
+    if args.subcommand == 'build':
+        yes = args.yes
+        running = args.S
+        dostart = args.s
+        source = args.repo
+        args.func(running, dostart, source, yes)
+    elif args.subcommand == 'install':
+        yes = args.yes
+        dostart = args.s
+        args.func(dostart=dostart, yes=yes)
+    elif args.subcommand == 'extra':
+        yes = args.yes
+        dostart = args.s
+        args.func(dostart=dostart, yes=yes)
+    elif args.subcommand == 'prompt':
+        yes = args.yes
+        dostart = args.s
+        args.func(dostart=dostart, yes=yes)
+    else:
+        args.func()
+
+if __name__ == "__main__":
+    bunker()
---
+++ packages/aws-bunker/aws-bunker-0.3.1.tar.gz/aws-bunker-0.3.1/bunker/init.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+
+"""init bunker config file"""
+
+import configparser
+import sys
+import os
+from os.path import expanduser
+import json
+import time
+import signal
+import subprocess
+import inquirer
+import pkg_resources
+
+class Bcolors:
+    """console colors"""
+    CYAN = '\033[96m'
+    MAGENTA = '\033[95m'
+    GREY = '\033[90m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    YELLOW = '\033[33m'
+    RED = '\033[31m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    ORANGE = '\033[38;5;208m'
+    PINK = '\033[38;5;212m'
+    PALEYELLOW = '\033[38;5;228m'
+    PALEBLUE = '\033[38;5;111m'
+    GOLD = '\033[38;5;178m'
+
+def query_yes_no(question, default="yes"):
+    '''confirm or decline'''
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '': 
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("\nPlease respond with 'yes' or 'no' (or 'y' or 'n').\n")
+
+def init_bunker(instance_id=None):
+    #config here
+    home = expanduser('~')
+    bunker_config = os.path.join(home, ".config-bunker.ini")
+    config_exists = False
+    gitkeys_arr = []
+    if os.path.exists(bunker_config):
+        config = configparser.ConfigParser()
+        config.read(bunker_config)
+        if "gitkeys" in config['default']:
+            config_exists = True
+            prefix = config['default']['prefix']
+            pem = config['default']['pem']
+            if instance_id is None:
+                instance_id = config['default']['instance_id']
+            username = config['default']['username']
+            gitkeys = config['default']['gitkeys']
+            keycount = gitkeys.count(', ')
+            if keycount > 0:
+                gitkeys_arr = config['default']['gitkeys'].split(", ")
+            else:
+                gitkeys_arr.append(config['default']['gitkeys'])
+    if not config_exists:
+        print(Bcolors.PALEYELLOW+"deprecated config file or no config file found."+Bcolors.ENDC)
+        prefix, pem, instance_id, username, gitkeys = home+"/", "unknown", "unknown", "ubuntu", "unknown"
+
+    newconfig = """[default]
+prefix = {prefix}
+repos = {repos}
+ignored = {ignored}
+pem = {pem}
+instance_id = {iid}
+username = {username}
+gitkeys = {gitkeys}
+"""
+    instance_id = input(Bcolors.ENDC+"EC2 Instance ID ["+Bcolors.UNDERLINE+instance_id+Bcolors.ENDC+"] : "+Bcolors.PALEYELLOW) or instance_id
+    while True:
+        prefix = input(Bcolors.ENDC+"Directory where you will store repositories file ["+Bcolors.UNDERLINE+prefix+Bcolors.ENDC+"] : "+Bcolors.PALEYELLOW) or prefix
+        if not prefix.endswith("/"):
+            if config['default']['prefix']:
+                prefix = config['default']['prefix']
+            else:
+                prefix = "unknown"
+            print(Bcolors.FAIL+"ERROR: "+Bcolors.WARNING+"prefix must end with a /"+Bcolors.ENDC)
+            continue
+        elif not os.path.isdir(prefix):
+            if config['default']['prefix']:
+                prefix = config['default']['prefix']
+            else:
+                prefix = home+"/"
+            print(Bcolors.FAIL+"ERROR: "+Bcolors.WARNING+"directory does not exist"+Bcolors.ENDC)
+            continue
+        else:
+            break
+    while True:
+        repofile = input(Bcolors.ENDC+"Name of repository file ["+Bcolors.UNDERLINE+"repositories.txt"+Bcolors.ENDC+"] : "+Bcolors.PALEYELLOW) or "repositories.txt"
+        if not repofile.endswith(".txt"):
+            repofile = "repositories.txt"
+            print(Bcolors.FAIL+"ERROR: "+Bcolors.WARNING+"repository filename must be .txt"+Bcolors.ENDC)
+            continue
+        else:
+            break
+    while True:
+        ignorefile = input(Bcolors.ENDC+"Name of ignored-git-files file ["+Bcolors.UNDERLINE+"ignoredfiles.txt"+Bcolors.ENDC+"] : "+Bcolors.PALEYELLOW) or "ignoredfiles.txt"
+        if not ignorefile.endswith(".txt"):
+            ignorefile = "ignoredfiles.txt"
+            print(Bcolors.FAIL+"ERROR: "+Bcolors.WARNING+"ignored filename must be .txt"+Bcolors.ENDC)
+            continue
+        else:
+            break
+    while True:
+        pem = input(Bcolors.ENDC+"Full path to .pem keyfile ["+Bcolors.UNDERLINE+pem+Bcolors.ENDC+"] : "+Bcolors.PALEYELLOW) or pem
+        if not pem.endswith(".pem"):
+            if config['default']['pem']:
+                pem = config['default']['pem']
+            else:
+                pem = "unknown"
+            print(Bcolors.FAIL+"ERROR: "+Bcolors.WARNING+"keyfile name must be .pem"+Bcolors.ENDC)
+            continue
+        elif not os.path.isfile(pem):
+            if config['default']['pem']:
+                pem = config['default']['pem']
+            else:
+                pem = "unknown"
+            print(Bcolors.FAIL+"ERROR: "+Bcolors.WARNING+"file does not exist"+Bcolors.ENDC)
+            continue
+        else:
+            break
+    while True:
+        username = input(Bcolors.ENDC+"EC2 username ["+Bcolors.UNDERLINE+username+Bcolors.ENDC+"] : ") or username
+        if username != "ubuntu":
+            print(Bcolors.FAIL+"ERROR: "+Bcolors.WARNING+"bunker currently only supports ubuntu"+Bcolors.ENDC)
+            continue
+        else:
+            break
+    
+    f = []
+    d = []
+    for root, dirs, files in os.walk(os.path.join(home, ".ssh"), topdown=False):
+        for name in files:
+            if ".pub" in name:
+                name = name.replace(".pub", "")
+                if os.path.join(root, name) in gitkeys_arr:
+                    d.append(os.path.join(root, name))
+                f.append(os.path.join(root, name))
+    questions = [inquirer.Checkbox(
+        'keyfiles',
+        message="Select key files (use arrow keys) to copy to the EC2.\n"+Bcolors.GOLD+"NOTE: "+Bcolors.ENDC+"Select only keys you need for access to your git provider(s)",
+        choices=f,
+        default=d,
+    )]
+    answers = inquirer.prompt(questions)  # returns a dict
+    if len(answers['keyfiles']) < 1:
+        print(Bcolors.FAIL+"WARNING: "+Bcolors.WARNING+"you did not select a key file, so bunker build will not be able to clone your git repos."+Bcolors.ENDC)
+        print("if this was not your intention, please re-run: "+Bcolors.PINK+"bunker init"+Bcolors.ENDC)
+        newkeyfiles = []
+    else:
+        newkeyfiles = answers['keyfiles']
+    newgitkeys = ""
+    for item in newkeyfiles:
+        newgitkeys += item+", "
+    newgitkeys = newgitkeys[:-2]
+    print(Bcolors.OKGREEN+"bunker is now configured."+Bcolors.ENDC+" writing: "+Bcolors.PALEBLUE+"~/.config-bunker.ini"+Bcolors.ENDC)
+    print("if your ec2 is up and running, try: "+Bcolors.PINK+"bunker install"+Bcolors.ENDC)
+    newconfig = newconfig.format(prefix=prefix, repos=repofile, ignored=ignorefile, pem=pem, iid=instance_id, username=username, gitkeys=newgitkeys) 
+    with open(bunker_config, 'w+') as f:
+        f.write(newconfig)
+    exit()
+
+if __name__ == "__main__":
+    init_bunker()
