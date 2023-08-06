939afab08cc901cd240c4c1c988305cd69b7d383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:30 +0000

    Add package django-calvo-comments/0.0.12/django-calvo-comments-0.0.12.tar.gz

---
+++ packages/django-calvo-comments/django-calvo-comments-0.0.12.tar.gz/django-calvo-comments-0.0.12/comments/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+from . import views
+
+urlpatterns = [
+    url('', views.post_list, name='post_list'),
+    url('post/<int:pk>/', views.post_detail, name='post_detail'),
+    url('post/new', views.post_new, name='post_new'),
+    url('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+]
