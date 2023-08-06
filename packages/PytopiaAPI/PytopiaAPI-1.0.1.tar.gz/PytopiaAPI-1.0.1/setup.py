a94cc362b307d48b33fbaf7fb300bda9da57429a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:52 +0000

    Add package PytopiaAPI/1.0.1/PytopiaAPI-1.0.1.tar.gz

---
+++ packages/PytopiaAPI/PytopiaAPI-1.0.1.tar.gz/PytopiaAPI-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PytopiaAPI",
+    version="1.0.1",
+    author="Dest0re",
+    author_email="tihokaenglichman@gmail.com",
+    description="A python API for Utopia Ecosystem",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Dest0re/PytopiaAPI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
