abda974cb6470df696016fe9ca682b2145bc98ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:58 +0000

    Add package lazyeda/0.1.1/lazyeda-0.1.1.tar.gz

---
+++ packages/lazyeda/lazyeda-0.1.1.tar.gz/lazyeda-0.1.1/lazyeda/__init__.py
@@ -0,0 +1,27 @@
+"""
+Exploratory Analysis module for Python
+==================================
+
+"""
+import sys
+import re
+import warnings
+import logging
+import os
+
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.INFO)
+
+
+# Make sure that DeprecationWarning within this package always gets printed
+warnings.filterwarnings('always', category=DeprecationWarning,
+                        module=r'^{0}\.'.format(re.escape(__name__)))
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+
+__version__ = '0.1.1'
+
