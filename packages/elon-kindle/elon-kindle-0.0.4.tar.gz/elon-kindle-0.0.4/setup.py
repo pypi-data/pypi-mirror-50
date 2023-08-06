26c714686a007873ffa82e8dae228ff546bb1446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:43 +0000

    Add package elon-kindle/0.0.4/elon-kindle-0.0.4.tar.gz

---
+++ packages/elon-kindle/elon-kindle-0.0.4.tar.gz/elon-kindle-0.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    # pip install nnn
+    name="elon-kindle",
+    version="0.0.4",
+    keywords=("elon"),
+    description="send to kindle",
+    long_description="send to kindle",
+    # 协议
+    license="GPL Licence",
+
+    url="https://github.com/elon",
+    author="elon",
+    author_email="elon@126.com",
+
+    # 自动查询所有"__init__.py"
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    # 提示前置包
+    # install_requires=['pandas', 'numpy', 'sqlalchemy'],
+
+    entry_points={
+        'console_scripts': [
+            'kindle = kindle.kindle:send',
+        ]
+    },
+
+)
