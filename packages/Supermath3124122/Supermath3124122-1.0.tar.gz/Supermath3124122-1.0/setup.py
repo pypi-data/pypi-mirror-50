2912f5e431e4f6f7c6772424193d070e5656f012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:40 +0000

    Add package Supermath3124122/1.0/Supermath3124122-1.0.tar.gz

---
+++ packages/Supermath3124122/Supermath3124122-1.0.tar.gz/Supermath3124122-1.0/setup.py
@@ -0,0 +1,10 @@
+#encoding=utf-8
+from distutils.core import setup
+setup(
+name='Supermath3124122', # 对外我们模块的名字
+version='1.0', # 版本号
+description='这是第一个对外发布的模块，测试哦', #描述
+author='wangzhaoyang', # 作者
+author_email='1719640636@qq.com',
+py_modules=['Supermath3124122.demo1'] # 要发布的模块
+)
