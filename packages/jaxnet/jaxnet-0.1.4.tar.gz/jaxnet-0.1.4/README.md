49c6834ffd283fb0d0512d622928b8e0e2812bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:50 +0000

    Add package jaxnet/0.1.4/jaxnet-0.1.4.tar.gz

---
+++ packages/jaxnet/jaxnet-0.1.4.tar.gz/jaxnet-0.1.4/README.md
@@ -0,0 +1,169 @@
+# JAXnet [![Build Status](https://travis-ci.org/JuliusKunze/jaxnet.svg?branch=master)](https://travis-ci.org/JuliusKunze/jaxnet) [![PyPI](https://img.shields.io/pypi/v/jaxnet.svg)](https://pypi.python.org/pypi/jaxnet/#history)
+
+JAXnet is a neural net library for [JAX](https://github.com/google/jax).
+Different from popular neural net libraries, it is completely functional:
+- No mutable weights in modules
+- No global compute graph
+- No global random key
+
+**This is an early version. Expect breaking changes!** Install with
+```
+pip install jaxnet
+```
+
+To use GPU, first install the [right version of jaxlib](https://github.com/google/jax#installation).
+
+See JAXnet in action in these demos:
+[Mnist Classifier](https://colab.research.google.com/drive/18kICTUbjqnfg5Lk3xFVQtUj6ahct9Vmv),
+[Mnist VAE](https://colab.research.google.com/drive/19web5SnmIFglLcnpXE34phiTY03v39-g),
+[OCR with RNNs](https://colab.research.google.com/drive/1YuI6GUtMgnMiWtqoaPznwAiSCe9hMR1E),
+[ResNet](https://colab.research.google.com/drive/1q6yoK_Zscv-57ZzPM4qNy3LgjeFzJ5xN) and
+[WaveNet](https://colab.research.google.com/drive/111cKRfwYX4YFuPH3FF4V46XLfsPG1icZ).
+
+## Overview
+```python
+from jaxnet import *
+
+net = Sequential(Conv(2, (3, 3)), relu, flatten, Dense(4), softmax)
+```
+creates a neural net model.
+To initialize parameters, call `init_params` with a random key and example inputs:
+```python
+from jax import numpy as np, jit
+from jax.random import PRNGKey
+
+inputs = np.zeros((3, 5, 5, 1))
+params = net.init_params(PRNGKey(0), inputs)
+
+print(params.layers[3].bias) # [0.00212132 0.01169001 0.00331698 0.00460713]
+```
+
+Invoke the network with:
+```python
+output = net(params, inputs) # use "jit(net)(params, inputs)" for acceleration
+```
+
+## Defining modules
+
+Modules are functions decorated with `@parametrized`, with parameters defined through default values:
+```python
+def Dense(out_dim, kernel_init=glorot(), bias_init=randn()):
+    @parametrized
+    def dense(inputs,
+              kernel=Param(lambda inputs: (inputs.shape[-1], out_dim), kernel_init),
+              bias=Param(lambda _: (out_dim,), bias_init)):
+        return np.dot(inputs, kernel) + bias
+
+    return dense
+```
+
+`Param` specifies parameter shape and initialization.
+`@parametrized` transforms the function to allow usage as above.
+
+## Nesting modules
+
+Modules can be used in other modules through default arguments:
+
+```python
+@parametrized
+def encode(input, 
+           net=Sequential(Dense(512), relu),
+           mean_net=Dense(10),
+           variance_net=Sequential(Dense(10), softplus)):
+    input = net(input)
+    return mean_net(input), variance_net(input)
+```
+
+Use many modules at once with collections:
+```python
+def Sequential(*layers):
+    @parametrized
+    def sequential(inputs, layers=layers):
+        for module in layers:
+            inputs = module(inputs)
+        return inputs
+
+    return sequential
+```
+
+Nested `tuples`/`list`/`dicts` of modules work. The same is true for `Param`s.
+
+Using parameter-free functions is seamless:
+```python
+def relu(x):
+    return np.maximum(x, 0)
+
+layer = Sequential(Dense(10), relu)
+```
+
+## Parameter sharing
+
+Parameters can be shared by using module or parameter objects multiple times (**not yet implemented**):
+
+```python
+shared_net=Sequential(layer, layer)
+```
+
+This is equivalent to (already implemented):
+
+```python
+@parametrized
+def shared_net(input, layer=layer):
+    return layer(layer(input))
+```
+
+## Parameter reuse
+
+If you want to evaluate parts or extended versions of a trained network
+(i. e. to get accuracy, generate samples, or do introspection), you can use `apply_from`:
+
+```python
+predict = Sequential(Dense(1024), relu, Dense(10), logsoftmax)
+
+@parametrized
+def loss(inputs, targets, predict=predict):
+    return -np.mean(predict(inputs) * targets)
+
+@parametrized
+def accuracy(inputs, targets, predict=predict):
+    return np.mean(np.argmax(targets, axis=1) == np.argmax(predict(inputs), axis=1))
+
+params = loss.init_params(PRNGKey(0), inputs)
+
+# train params...
+
+test_acc = accuracy.apply_from({loss: params}, *test_inputs, jit=True)
+```
+
+It is a shorthand for:
+
+```python
+accuracy_params = accuracy.params_from({loss: params})
+test_acc = jit(accuracy)(accuracy_params, *test_inputs)
+```
+
+You can also reuse parts of your network while initializing the rest:
+
+```python
+inputs = np.zeros((1, 2))
+net = Dense(5)
+net_params = net.init_params(PRNGKey(0), inputs)
+
+# train net_params...
+
+transfer_net = Sequential(net, relu, Dense(2))
+transfer_net_params = transfer_net.init_params(PRNGKey(1), inputs, reuse={net: net_params})
+
+assert transfer_net_params.layers[0] is net_params
+
+# train transfer_net_params...
+```
+
+If you don't have a reference like `net`, `reuse={transfer_net.layers[0]: net_params}` also works.
+
+## What about [stax](https://github.com/google/jax/blob/master/jax/experimental/stax.py)?
+JAXnet is independent of stax.
+The main motivation over stax is to simplify nesting modules.
+Find details and porting instructions [here](STAX.md).
+
+Alternative design ideas are discussed [here](DESIGN.md).
---
+++ packages/jaxnet/jaxnet-0.1.4.tar.gz/jaxnet-0.1.4/jaxnet/jaxnet.py
@@ -0,0 +1,490 @@
+import functools
+import itertools
+from collections import namedtuple
+from inspect import signature
+from pathlib import Path
+
+import dill
+import jax
+import numpy as onp
+from jax import numpy as np, random, partial
+from jax.lax import lax, scan
+from jax.scipy.special import logsumexp, expit
+
+from jaxnet.tools import zip_nested, map_nested, enumerate_nested, set_nested_element, \
+    nested_any, IndexedValue, ZippedValue, flatten_nested
+
+ParamInit = namedtuple('Param', ('init_param',))
+
+
+def Param(get_shape, init):
+    return ParamInit(init_param=lambda rng, *example_inputs: init(rng, get_shape(*example_inputs)))
+
+
+NoParams = namedtuple('NoParams', ())
+no_params = NoParams()
+
+
+def _is_parametrized(param): return isinstance(param, ParamInit) or isinstance(param,
+                                                                               InputDependent)
+
+
+def _is_parametrized_collection(x):
+    return nested_any(map_nested(_is_parametrized, x, element_types=(ParamInit,)))
+
+
+class InputDependent:
+    def __init__(self, parametrized_from_inputs, name=None):
+        self._fun_from_inputs = parametrized_from_inputs
+        self._name = name if name else parametrized_from_inputs.__name__
+        self._apply = self._get_apply()
+        self.init_params = partial(self._init_params, reuse_only=False)
+
+    def __call__(self, param_values, *inputs):
+        return self._apply(param_values, *inputs)
+
+    def _get_fun(self, *inputs):
+        p = self._fun_from_inputs(*inputs)
+
+        if not isinstance(p, parametrized):
+            raise ValueError("fun_from_inputs has to return a @parameterized function.")
+
+        return p._fun
+
+    def _get_parameters(self, *inputs):
+        fun = self._get_fun(*inputs)
+        parameters = {k: v.default for k, v in signature(fun).parameters.items()
+                      if _is_parametrized_collection(v.default)}
+
+        for name in parameters.keys():
+            if name in dir(self):
+                raise ValueError(f'Submodules cannot be named "{name}", please rename.')
+
+        return parameters
+
+    def _get_Parameters(self, *inputs):
+        return namedtuple(self._name, self._get_parameters(*inputs).keys())
+
+    def _get_param_value_pairs(self, param_values, *inputs):
+        param_values = param_values._asdict() if isinstance(param_values,
+                                                            tuple) else param_values
+        return zip_nested(self._get_parameters(*inputs), param_values, element_types=(ParamInit,))
+
+    def _get_apply(self, sublayer_wrapper=lambda index_path, apply: apply):
+        def apply(param_values, *inputs):
+            def resolve_parameters(index_path, param, param_values):
+                if isinstance(param, ParamInit):
+                    return param_values
+
+                if isinstance(param, InputDependent):
+                    apply = sublayer_wrapper(index_path, param)
+                    return partial(apply, param_values)
+
+                return param
+
+            pairs = self._get_param_value_pairs(param_values, *inputs)
+            indexed_pairs = enumerate_nested(pairs, element_types=(ZippedValue, ParamInit))
+            resolved_params = map_nested(lambda pair: resolve_parameters(pair[0], *pair[1]),
+                                         indexed_pairs, element_types=(IndexedValue, ParamInit))
+            fun = self._get_fun(*inputs)
+            return fun(*inputs, **resolved_params)
+
+        return apply
+
+    def _init_params(self, rng, *example_inputs, reuse=None, reuse_only=False):
+        if isinstance(self, InputDependent):
+            if reuse and self in reuse:
+                return reuse[self]
+
+        def init_param(param, *sub_example_inputs, none_for_submodules=False):
+            if isinstance(param, InputDependent):
+                if none_for_submodules:
+                    return None
+
+            if _is_parametrized(param):
+                if reuse_only:
+                    if isinstance(param, ParamInit):
+                        # TODO: include index path to param in message
+                        raise ValueError(f'No param value specified for {param}.')
+
+                    return param.params_from(values_by_param=reuse)
+
+                nonlocal rng
+                rng, rng_param = random.split(rng)
+                if isinstance(param, ParamInit):
+                    return param.init_param(rng_param, *example_inputs)
+
+                return param.init_params(rng_param, *sub_example_inputs, reuse=reuse)
+
+            if callable(param) and not isinstance(param, InputDependent):
+                return no_params
+
+            assert isinstance(param, np.ndarray)
+            return param
+
+        # TODO refactor: replaced later by tracer, needed for shape information:
+        all_param_values = map_nested(
+            lambda param: init_param(param, none_for_submodules=not reuse_only),
+            self._get_parameters(*example_inputs), tuples_to_lists=True, element_types=(ParamInit,))
+
+        if not reuse_only:
+            def traced_submodule_wrapper(index_path, submodule):
+                def traced_apply(wrong_param_values, *inputs):
+                    param_values = init_param(submodule, *inputs)
+                    set_nested_element(all_param_values, index_path=index_path, value=param_values)
+                    return submodule(param_values, *inputs)
+
+                return traced_apply
+
+            apply = self._get_apply(sublayer_wrapper=traced_submodule_wrapper)
+            apply(all_param_values, *example_inputs)
+
+        return self._get_Parameters(*example_inputs)(**all_param_values)
+
+    def __str__(self):
+        return f'{self._name}({id(self)})'
+
+
+class parametrized(InputDependent):
+    def __init__(self, fun):
+        super().__init__(parametrized_from_inputs=None, name=fun.__name__)
+        self._fun = fun
+        self._parameters = super()._get_parameters()
+        self._Parameters = super()._get_Parameters()
+
+    def _get_fun(self, *inputs):
+        return self._fun
+
+    def _get_parameters(self, *inputs):
+        return self._parameters
+
+    def _get_Parameters(self, *inputs):
+        return self._Parameters
+
+    def __getattr__(self, item):
+        if item in self._parameters.keys():
+            return self._parameters[item]
+
+        return getattr(super(), item)
+
+    def _expand_reuse_dict(self, reuse):
+        r = dict()
+
+        for param, value in reuse.items():
+            r.update({param: value})
+
+            if isinstance(param, parametrized):
+                pairs = param._get_param_value_pairs(value)
+                pairs = flatten_nested(pairs, (ZippedValue,))
+                values_by_submodule = {p: v for (p, v) in pairs}
+
+                r.update(param._expand_reuse_dict(values_by_submodule))
+
+        return r
+
+    def params_from(self, values_by_param):
+        # TODO: optimization wrong, duplicate values, needs param adapter
+        values_by_param = self._expand_reuse_dict(values_by_param)
+        return self._init_params(None, reuse=values_by_param, reuse_only=True)
+
+    def apply_from(self, reuse, *inputs, jit=False):
+        params = self.params_from(values_by_param=reuse)
+        return (jax.jit(self._apply) if jit else self._apply)(params, *inputs)
+
+    def __str__(self):
+        return f'{self._name}({id(self)}):{self._parameters}'
+
+
+def relu(x):
+    return np.maximum(x, 0.)
+
+
+def softplus(x):
+    return np.logaddexp(x, 0.)
+
+
+sigmoid = expit
+
+
+def elu(x):
+    return np.where(x > 0, x, np.exp(x) - 1)
+
+
+def leaky_relu(x):
+    return np.where(x >= 0, x, 0.01 * x)
+
+
+def logsoftmax(x, axis=-1):
+    """Apply log softmax to an array of logits, log-normalizing along an axis."""
+    return x - logsumexp(x, axis, keepdims=True)
+
+
+def softmax(x, axis=-1):
+    """Apply softmax to an array of logits, exponentiating and normalizing along an axis."""
+    unnormalized = np.exp(x - x.max(axis, keepdims=True))
+    return unnormalized / unnormalized.sum(axis, keepdims=True)
+
+
+def flatten(x):
+    return np.reshape(x, (x.shape[0], -1))
+
+
+def fastvar(x, axis, keepdims):
+    """A fast but less numerically-stable variance calculation than np.var."""
+    return np.mean(x ** 2, axis, keepdims=keepdims) - np.mean(x, axis, keepdims=keepdims) ** 2
+
+
+# Initializers
+
+def randn(stddev=1e-2):
+    """An initializer function for random normal coefficients."""
+
+    def init(rng, shape):
+        std = lax.convert_element_type(stddev, np.float32)
+        return std * random.normal(rng, shape, dtype=np.float32)
+
+    return init
+
+
+def glorot(out_axis=0, in_axis=1, scale=onp.sqrt(2)):
+    """An initializer function for random Glorot-scaled coefficients."""
+
+    def init(rng, shape):
+        fan_in, fan_out = shape[in_axis], shape[out_axis]
+        size = onp.prod(onp.delete(shape, [in_axis, out_axis]))
+        std = scale / np.sqrt((fan_in + fan_out) / 2. * size)
+        std = lax.convert_element_type(std, np.float32)
+        return std * random.normal(rng, shape, dtype=np.float32)
+
+    return init
+
+
+zeros = lambda rng, shape: np.zeros(shape, dtype='float32')
+ones = lambda rng, shape: np.ones(shape, dtype='float32')
+
+
+def Dense(out_dim, kernel_init=glorot(), bias_init=randn()):
+    """Layer constructor function for a dense (fully-connected) layer."""
+
+    @parametrized
+    def dense(inputs,
+              kernel=Param(lambda inputs: (inputs.shape[-1], out_dim), kernel_init),
+              bias=Param(lambda _: (out_dim,), bias_init)):
+        return np.dot(inputs, kernel) + bias
+
+    return dense
+
+
+def Sequential(*layers):
+    """Combinator for composing layers in sequence.
+
+    Args:
+      *layers: a sequence of layers, each a function or parametrized function.
+
+    Returns:
+        A new parametrized function.
+    """
+
+    if len(layers) > 0 and hasattr(layers[0], '__iter__'):
+        raise ValueError('Call like Sequential(Dense(10), relu), without "[" and "]". '
+                         '(Or pass iterables with Sequential(*layers).)')
+
+    @parametrized
+    def sequential(inputs, layers=layers):
+        for layer in layers:
+            inputs = layer(inputs)
+        return inputs
+
+    return sequential
+
+
+def GeneralConv(dimension_numbers, out_chan, filter_shape,
+                strides=None, padding='VALID', kernel_init=None, bias_init=randn(1e-6),
+                dilation=None):
+    """Layer construction function for a general convolution layer."""
+    lhs_spec, rhs_spec, out_spec = dimension_numbers
+    one = (1,) * len(filter_shape)
+    strides = strides or one
+    dilation = dilation or one
+    kernel_init = kernel_init or glorot(rhs_spec.index('O'), rhs_spec.index('I'))
+
+    def kernel_shape(inputs):
+        filter_shape_iter = iter(filter_shape)
+
+        return [out_chan if c == 'O' else
+                inputs.shape[lhs_spec.index('C')] if c == 'I' else
+                next(filter_shape_iter) for c in rhs_spec]
+
+    bias_shape = tuple(
+        itertools.dropwhile(lambda x: x == 1, [out_chan if c == 'C' else 1 for c in out_spec]))
+
+    @parametrized
+    def general_conv(inputs,
+                     kernel=Param(kernel_shape, kernel_init),
+                     bias=Param(lambda _: bias_shape, bias_init)):
+        return lax.conv_general_dilated(inputs, kernel, strides, padding, one, dilation,
+                                        dimension_numbers) + bias
+
+    return general_conv
+
+
+Conv = functools.partial(GeneralConv, ('NHWC', 'HWIO', 'NHWC'))
+Conv1D = functools.partial(GeneralConv, ('NTC', 'TIO', 'NTC'))
+
+
+def GeneralConvTranspose(dimension_numbers, out_chan, filter_shape,
+                         strides=None, padding='VALID', kernel_init=None,
+                         bias_init=randn(1e-6)):
+    """Layer construction function for a general transposed-convolution layer."""
+
+    lhs_spec, rhs_spec, out_spec = dimension_numbers
+    one = (1,) * len(filter_shape)
+    strides = strides or one
+    kernel_init = kernel_init or glorot(rhs_spec.index('O'), rhs_spec.index('I'))
+
+    def kernel_shape(inputs):
+        filter_shape_iter = iter(filter_shape)
+
+        return [out_chan if c == 'O' else
+                inputs.shape[lhs_spec.index('C')] if c == 'I' else
+                next(filter_shape_iter) for c in rhs_spec]
+
+    bias_shape = tuple(
+        itertools.dropwhile(lambda x: x == 1, [out_chan if c == 'C' else 1 for c in out_spec]))
+
+    @parametrized
+    def conv_transpose(inputs,
+                       kernel=Param(kernel_shape, kernel_init),
+                       bias=Param(lambda _: bias_shape, bias_init)):
+        return lax.conv_transpose(inputs, kernel, strides, padding,
+                                  dimension_numbers) + bias
+
+    return conv_transpose
+
+
+ConvTranspose = functools.partial(GeneralConvTranspose, ('NHWC', 'HWIO', 'NHWC'))
+Conv1DTranspose = functools.partial(GeneralConvTranspose, ('NTC', 'TIO', 'NTC'))
+
+
+def _pool(reducer, init_val, rescaler=None):
+    def Pool(window_shape, strides=None, padding='VALID'):
+        """Layer construction function for a pooling layer."""
+        strides = strides or (1,) * len(window_shape)
+        rescale = rescaler(window_shape, strides, padding) if rescaler else None
+        dims = (1,) + window_shape + (1,)  # NHWC
+        strides = (1,) + strides + (1,)
+
+        def pool(inputs):
+            out = lax.reduce_window(inputs, init_val, reducer, dims, strides, padding)
+            return rescale(out, inputs) if rescale else out
+
+        return pool
+
+    return Pool
+
+
+MaxPool = _pool(lax.max, -np.inf)
+SumPool = _pool(lax.add, 0.)
+
+
+def _normalize_by_window_size(dims, strides, padding):
+    def rescale(outputs, inputs):
+        one = np.ones(inputs.shape[1:-1], dtype=inputs.dtype)
+        window_sizes = lax.reduce_window(one, 0., lax.add, dims, strides, padding)
+        return outputs / window_sizes[..., np.newaxis]
+
+    return rescale
+
+
+AvgPool = _pool(lax.add, 0., _normalize_by_window_size)
+
+
+def GRUCell(carry_size, param_init):
+    def param(): return Param(lambda carry, x: (x.shape[1] + carry_size, carry_size), param_init)
+
+    @parametrized
+    def gru_cell(carry, x,
+                 update_params=param(),
+                 reset_params=param(),
+                 compute_params=param()):
+        both = np.concatenate((x, carry), axis=1)
+        update = sigmoid(np.dot(both, update_params))
+        reset = sigmoid(np.dot(both, reset_params))
+        both_reset_carry = np.concatenate((x, reset * carry), axis=1)
+        compute = np.tanh(np.dot(both_reset_carry, compute_params))
+        out = update * compute + (1 - update) * carry
+        return out, out
+
+    def carry_init(batch_size):
+        return np.zeros((batch_size, carry_size))
+
+    return gru_cell, carry_init
+
+
+def Rnn(cell, carry_init):
+    """Layer construction function for recurrent neural nets.
+    Expecting input shape (batch, sequence, channels).
+    TODO allow returning last carry."""
+
+    @parametrized
+    def rnn(xs, cell=cell):
+        xs = np.swapaxes(xs, 0, 1)
+        _, ys = scan(cell, carry_init(xs.shape[1]), xs)
+        return np.swapaxes(ys, 0, 1)
+
+    return rnn
+
+
+def Dropout(rate, mode='train'):
+    """Constructor for a dropout function with given rate."""
+
+    def dropout(inputs, *args, **kwargs):
+        if len(args) == 1:
+            rng = args[0]
+        else:
+            rng = kwargs.get('rng', None)
+            if rng is None:
+                msg = ("dropout requires to be called with a PRNG key argument. "
+                       "That is, instead of `dropout(params, inputs)`, "
+                       "call it like `dropout(inputs, key)` "
+                       "where `key` is a jax.random.PRNGKey value.")
+                raise ValueError(msg)
+        if mode == 'train':
+            keep = random.bernoulli(rng, rate, inputs.shape)
+            return np.where(keep, inputs / rate, 0)
+        else:
+            return inputs
+
+    return dropout
+
+
+def BatchNorm(axis=(0, 1, 2), epsilon=1e-5, center=True, scale=True,
+              beta_init=zeros, gamma_init=ones):
+    """Layer construction function for a batch normalization layer."""
+
+    get_shape = lambda input: tuple(d for i, d in enumerate(input.shape) if i not in axis)
+    axis = (axis,) if np.isscalar(axis) else axis
+
+    @parametrized
+    def batch_norm(x,
+                   beta=Param(get_shape, beta_init) if center else None,
+                   gamma=Param(get_shape, gamma_init) if scale else None):
+        ed = tuple(None if i in axis else slice(None) for i in range(np.ndim(x)))
+        mean, var = np.mean(x, axis, keepdims=True), fastvar(x, axis, keepdims=True)
+        z = (x - mean) / np.sqrt(var + epsilon)
+        if center and scale: return gamma[ed] * z + beta[ed]
+        if center: return z + beta[ed]
+        if scale: return gamma[ed] * z
+        return z
+
+    return batch_norm
+
+
+def save_params(params, path: Path):
+    with path.open('wb') as file:
+        dill.dump(params, file)
+
+
+def load_params(path: Path):
+    with path.open('rb') as file:
+        return dill.load(file)
