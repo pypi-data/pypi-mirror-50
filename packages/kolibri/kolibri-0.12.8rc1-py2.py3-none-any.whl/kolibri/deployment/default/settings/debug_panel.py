2972cb51558387ceb1213f58510b2859cb78cd5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:06 +0000

    Add package kolibri/0.12.8rc1/kolibri-0.12.8rc1-py2.py3-none-any.whl

---
+++ packages/kolibri/kolibri-0.12.8rc1-py2.py3-none-any.whl/kolibri/deployment/default/settings/debug_panel.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from .dev import *  # noqa
+
+INTERNAL_IPS = ["127.0.0.1"]
+
+DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": lambda x: True}
+
+MIDDLEWARE.append("debug_panel.middleware.DebugPanelMiddleware")  # noqa
+
+INSTALLED_APPS += ["debug_toolbar", "debug_panel"]  # noqa
+
+DEBUG_PANEL_ACTIVE = True
