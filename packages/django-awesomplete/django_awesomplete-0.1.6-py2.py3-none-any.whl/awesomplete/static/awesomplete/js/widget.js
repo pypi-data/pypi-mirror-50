e7c05da172af1616d6ee8c13ce6d48fbe72a606f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:03 +0000

    Add package django-awesomplete/0.1.6/django_awesomplete-0.1.6-py2.py3-none-any.whl

---
+++ packages/django-awesomplete/django_awesomplete-0.1.6-py2.py3-none-any.whl/awesomplete/static/awesomplete/js/widget.js
@@ -0,0 +1,32 @@
+(function($) {
+
+    function initWidgets($root) {
+        $root.find('.admin-awesomplete').each(function() {
+            var $input = $(this);
+            var $formRow = $input.closest('.form-row');
+
+            if ($input.closest('.empty-form').length) {
+                return
+            }
+
+            // Django's "overflow:hidden" fix
+            if ($formRow.length) {
+                $formRow.css('overflow', 'visible');
+            }
+
+            var object = new Awesomplete(this, {
+                sort: function() {}
+            });
+
+            // fix horizontal position (because of "float:left" on label)
+            object.ul.style.marginLeft = object.input.offsetLeft + 'px';
+        });
+    }
+
+    $(document).ready(function() {
+        initWidgets($(document.body));
+    }).on('formset:added', function(event, $row, formsetName) {
+        initWidgets($row);
+    });
+
+})(django.jQuery);
