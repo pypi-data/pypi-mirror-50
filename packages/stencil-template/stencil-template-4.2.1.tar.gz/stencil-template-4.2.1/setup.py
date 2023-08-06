3cad8dd902da71bca57353b76bb0ecb8eb4235f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:55 +0000

    Add package stencil-template/4.2.1/stencil-template-4.2.1.tar.gz

---
+++ packages/stencil-template/stencil-template-4.2.1.tar.gz/stencil-template-4.2.1/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='stencil-template',
+    version='4.2.1',
+    description='A template engine light enough to embed in your project.',
+    url='https://github.com/funkybob/stencil/',
+    author='Curtis Maloney',
+    license='CC BY-SA 3.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='template',
+    py_modules=['stencil'],
+)
