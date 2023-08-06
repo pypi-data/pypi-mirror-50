99bd5d27af7b1af21ae4d1f1cbf3fb7e005bcfcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:48 +0000

    Add package Pyiiko/0.2.7/Pyiiko-0.2.7.tar.gz

---
+++ packages/Pyiiko/Pyiiko-0.2.7.tar.gz/Pyiiko-0.2.7/Pyiiko/__init__.py
@@ -0,0 +1,6 @@
+from Pyiiko.server import *
+from Pyiiko.biz import *
+from Pyiiko.card5 import *
+from Pyiiko.frontWeb import *
+
+
---
+++ packages/Pyiiko/Pyiiko-0.2.7.tar.gz/Pyiiko-0.2.7/test/settings.py
@@ -0,0 +1,2 @@
+import Pyiiko
+i = Pyiiko.IikoServer(ip='metropol.iiko.it:8080', login='admin', password='resto#test')
