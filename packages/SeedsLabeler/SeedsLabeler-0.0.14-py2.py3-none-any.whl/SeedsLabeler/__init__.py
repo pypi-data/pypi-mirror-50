72e5f367a7c9d893e6410daae7f5d5390d1c0237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:46 +0000

    Add package SeedsLabeler/0.0.14/SeedsLabeler-0.0.14-py2.py3-none-any.whl

---
+++ packages/SeedsLabeler/SeedsLabeler-0.0.14-py2.py3-none-any.whl/SeedsLabeler/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.0.14"
+__authors__ = "Merey Ramazanova, Silvio Giancola"
+__author_email__ = "merey.ramazanova@kaust.edu.sa"
+__github__ = 'https://github.com/meryusha/seeds_labeler/'
