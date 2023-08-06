644b7611c93a022eb5a5b11b2c0572cb73439b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:44 +0000

    Add package simple-latex/0.0.1/simple-latex-0.0.1.tar.gz

---
+++ packages/simple-latex/simple-latex-0.0.1.tar.gz/simple-latex-0.0.1/simple_latex/__init__.py
@@ -0,0 +1,14 @@
+from .latex_preamble import Documentclass, Definition, Preamble, Package, RenewCommand, NewCommand
+from .latex_document import Document, BeginClass, EndClass, MathClass
+from .latex_universal import Command
+all = ['Documentclass',
+       'Command',
+       'Definition',
+       'Preamble',
+       'Package',
+       'RenewCommand',
+       'NewCommand',
+       'Document',
+       'BeginClass',
+       'EndClass',
+       'MathClass']
