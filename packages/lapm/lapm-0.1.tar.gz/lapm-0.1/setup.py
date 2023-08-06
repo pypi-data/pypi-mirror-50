0181c6c831cf2c7cbe4e58f74a4f1ec0a9df263d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:45 +0000

    Add package lapm/0.1/lapm-0.1.tar.gz

---
+++ packages/lapm/lapm-0.1.tar.gz/lapm-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='lapm',
+      version='0.1',
+      description='',
+      url="https://github.com/ramtingh/lapm",
+      author='Ramtin Gharleghi',
+      author_email='gharleghi@gmail.com',
+      license='',
+      install_requires=['tqdm'
+      ],
+      packages=find_packages(),
+      zip_safe=False)
