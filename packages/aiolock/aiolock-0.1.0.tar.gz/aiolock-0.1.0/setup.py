2bd324bcdca7a2248c8902625eb975962377187b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:25 +0000

    Add package aiolock/0.1.0/aiolock-0.1.0.tar.gz

---
+++ packages/aiolock/aiolock-0.1.0.tar.gz/aiolock-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='aiolock',
+      version='0.1.0',
+      packages=find_packages(),
+      description='some aiolock use in python',
+      author='bigpangl',
+      author_email='bigpangl@163.com',
+      url="https://github.com/bigpangl"
+)
