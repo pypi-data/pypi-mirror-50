24399cabbd2ece02af030c97d1e2befe3d8df13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:53 +0000

    Add package flaskerize/0.4.0/flaskerize-0.4.0.tar.gz

---
+++ packages/flaskerize/flaskerize-0.4.0.tar.gz/flaskerize-0.4.0/app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+
+
+def create_app():
+    from _fz_bp import site
+    app = Flask(__name__, static_folder=None)
+    app.register_blueprint(site, url_prefix='/')
+
+    @app.route("/health")
+    def serve():
+        return "app online!"
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run()
---
+++ packages/flaskerize/flaskerize-0.4.0.tar.gz/flaskerize-0.4.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="flaskerize",
+    version="0.4.0",
+    description="Flask CLI build/dev tool for bundling static sites into Flask apps and templated code generation",
+    author="AJ Pryor",
+    author_email="apryor6@gmail.com",
+    url="http://alanpryorjr.com/",
+    packages=find_packages(),
+    install_requires=["Flask>=1.1.1", "termcolor>=1.1.0"],
+    include_package_data=True,
+    scripts=["bin/fz"],
+)
