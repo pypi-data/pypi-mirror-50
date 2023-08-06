0c0ac37d40bf739fbbd8a51c2cfdbba3456c3a64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:17 +0000

    Add package binilla/1.1.1/binilla-1.1.1-py3.5.egg

---
+++ packages/binilla/binilla-1.1.1-py3.5.egg/binilla/__init__.py
@@ -0,0 +1,15 @@
+# ##############
+#   metadata   #
+# ##############
+__author__ = "Devin Bobadilla"
+#           YYYY.MM.DD
+__date__ = "2019.08.04"
+__version__ = (1, 1, 1)
+__all__ = (
+    'defs', 'widgets', 'windows',
+    'app_window', 'constants', 'edit_manager', 'editor_constants', 'handler',
+    )
+
+from binilla import constants
+
+constants.inject()
