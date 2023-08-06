6537d5a4c0368984f3c604e12094dad6e5d866af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:46 +0000

    Add package thundra/2.3.6/thundra-2.3.6-py3-none-any.whl

---
+++ packages/thundra/thundra-2.3.6-py3-none-any.whl/thundra/plugins/invocation/invocation_support.py
@@ -0,0 +1,48 @@
+from thundra import constants
+
+_invocation_tags = {}
+_user_invocation_tags = {}
+function_name = ''
+
+def set_agent_tag(key, value):
+    _invocation_tags[key] = value
+
+def set_many_agent(tags):
+    _invocation_tags.update(tags)
+
+def get_agent_tag(key):
+    if key in _invocation_tags:
+        return _invocation_tags[key]
+    return None
+
+def get_agent_tags():
+    return _invocation_tags.copy()
+
+def remove_agent_tag(key):
+    return _invocation_tags.pop(key, None)
+
+def set_tag(key, value):
+    _user_invocation_tags[key] = value
+
+def set_many(tags):
+    _user_invocation_tags.update(tags)
+
+def get_tag(key):
+    if key in _user_invocation_tags:
+        return _user_invocation_tags[key]
+    return None
+
+def get_tags():
+    return _user_invocation_tags.copy()
+
+def remove_tag(key):
+    return _user_invocation_tags.pop(key, None)
+
+
+def clear():
+    _invocation_tags.clear()
+    _user_invocation_tags.clear()
+
+def parse_invocation_info(context):
+    global function_name
+    function_name = getattr(context, constants.CONTEXT_FUNCTION_NAME, '')
