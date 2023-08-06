2567816c1cdcda2ffcfbc8c61157b29e9ef365b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:39 +0000

    Add package matialvarezs_node_configurations/0.0.9/matialvarezs_node_configurations-0.0.9.tar.gz

---
+++ packages/matialvarezs_node_configurations/matialvarezs_node_configurations-0.0.9.tar.gz/matialvarezs_node_configurations-0.0.9/matialvarezs_node_configurations/api/v1/urls.py
@@ -0,0 +1,15 @@
+from django import VERSION as DJANGO_VERSION
+
+if DJANGO_VERSION >= (2, 0):
+	from django.urls import include, path, re_path as url
+else:
+	from django.conf.urls import include, url
+
+from . import views
+
+app_name = "matialvarezs_node_configurations_api_v1"
+
+urlpatterns = [
+    url('create_identity/', views.create_identity, name='create_identity'),
+
+]
