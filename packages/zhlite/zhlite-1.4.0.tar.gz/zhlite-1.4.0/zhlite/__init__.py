df66faac33e0efee76880a8ef15ad9c3c64422cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:09 +0000

    Add package zhlite/1.4.0/zhlite-1.4.0.tar.gz

---
+++ packages/zhlite/zhlite-1.4.0.tar.gz/zhlite-1.4.0/zhlite/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from zhlite.zhlite import *
+
+__author__ = "zhangbo"
+__email__ = "deplives@deplives.com"
+
+__license__ = "MIT"
+
+__version__ = "1.4.0"
+__date__ = "2019-08-18"
+__release__ = "新增了 Question.topics 为问题标签"
+
+
+class Version(object):
+
+    def __init__(self):
+        self.info = {
+            "version": __version__,
+            "date": __date__,
+            "release": __release__
+        }
+
+    def __str__(self):
+        return " ".join([f"{k}: {v}" for k, v in self.info.items()])
+
+
+version = Version()
+
+__all__ = ["Authorization", "User", "Question", "Answer"]
