78af4ede5a857a9f5e210a6b772fcb1066b84033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:48 +0000

    Add package dlo/0.0.4/dlo-0.0.4.tar.gz

---
+++ packages/dlo/dlo-0.0.4.tar.gz/dlo-0.0.4/src/dlo/utils.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+import requests
+
+HEADERS = {'user-agent': 'Safari/537.36'}
+
+def GET(url, params=None, HEADERS=HEADERS, retries=5):
+
+	response = None
+	while retries:
+		try: response = requests.get(url, params=params, headers=HEADERS, timeout=10)
+		except requests.exceptions.ReadTimeout: 
+			pass
+		if response!=None:
+			return response
+
+		retries -= 1
+		print('Retries left: %d' % (retries))
+	
+	raise requests.exceptions.ReadTimeout
