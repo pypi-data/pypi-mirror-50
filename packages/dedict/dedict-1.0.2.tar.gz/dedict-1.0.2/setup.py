af99b9983c2a9028a13f6f184c2e268b9d44eed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:30 +0000

    Add package dedict/1.0.2/dedict-1.0.2.tar.gz

---
+++ packages/dedict/dedict-1.0.2.tar.gz/dedict-1.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='dedict',
+    version='1.0.2',
+    description='simple library to create objects from dictionaries',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Olivier Verville',
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6'
+    ],
+    packages=['dedict']
+)
