397e9e5ef1197249d898456a6c524b4d61b3a6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:03 +0000

    Add package dhruvil-distributions/0.1/dhruvil_distributions-0.1.tar.gz

---
+++ packages/dhruvil-distributions/dhruvil_distributions-0.1.tar.gz/dhruvil_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dhruvil_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dhruvil_distributions'],
+      author = 'dhruvil karani',
+      author_email = 'karanidhruvil@gmail.com',
+      zip_safe=False)
