e4fe8c9c6c402fa37693bc4059d32f73adf44c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:50 +0000

    Add package dictlib/1.1.5/dictlib-1.1.5.tar.gz

---
+++ packages/dictlib/dictlib-1.1.5.tar.gz/dictlib-1.1.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name = 'dictlib',
+  packages = ['dictlib'],
+  version = '1.1.5',
+  description = 'Dictionary Library including good deep merge and dictionary as objects',
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
+  author = 'Brandon Gillespie',
+  author_email = 'bjg-pypi@cold.org',
+  url = 'https://github.com/srevenant/dictlib', 
+  keywords = ['dict', 'union', 'object', 'dig', 'dug'],
+  classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+  ],
+)
+
