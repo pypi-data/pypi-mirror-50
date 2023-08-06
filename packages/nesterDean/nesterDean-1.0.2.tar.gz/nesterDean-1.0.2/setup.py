e7a3ad63350177ef7a200ad2657ace4c8b57e6ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:04 +0000

    Add package nesterDean/1.0.2/nesterDean-1.0.2.tar.gz

---
+++ packages/nesterDean/nesterDean-1.0.2.tar.gz/nesterDean-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'nesterDean', 
+        version      = '1.0.2', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['nesterDean'],
+        author       = 'dean',
+        author_email = 'dean@headfirstlabs.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+     )
