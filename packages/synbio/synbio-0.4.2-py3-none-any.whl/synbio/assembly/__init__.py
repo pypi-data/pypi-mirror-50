64839f7c3473925bcef514811758dc36e58b3b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:43 +0000

    Add package synbio/0.4.2/synbio-0.4.2-py3-none-any.whl

---
+++ packages/synbio/synbio-0.4.2-py3-none-any.whl/synbio/assembly/__init__.py
@@ -0,0 +1,8 @@
+"""Assembly methods for piecing together SeqRecords."""
+
+from .clone import goldengate
+from .clone import clone
+from .clone import clone_many
+from .gibson import gibson
+from .gibson import gibson_many
+
