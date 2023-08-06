01516c839e8d0142143d90e8a6d476da3d506cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:37 +0000

    Add package django-calvo-comments/0.0.15/django-calvo-comments-0.0.15.tar.gz

---
+++ packages/django-calvo-comments/django-calvo-comments-0.0.15.tar.gz/django-calvo-comments-0.0.15/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-calvo-comments',
+    version='0.0.15',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',  # example license
+    description='A simple Django app to handle comments with javascript.',
+    long_description=README,
+    url='https://github.com/NYARAS/django-calvo-comments',
+    author='Otieno Calvine',
+    author_email='hello@calvine.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
