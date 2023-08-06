2631d9752db8d9eaf0552db4b1a6fbb0da15218b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:58 +0000

    Add package FCMADRL/0.1.dev0/FCMADRL-0.1.dev0.tar.gz

---
+++ packages/FCMADRL/FCMADRL-0.1.dev0.tar.gz/FCMADRL-0.1.dev0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FCMADRL',
+    version='0.1dev',
+    packages=['', 'dqn', 'dqn.scores', 'ddpg', 'ddpg.batch_normalization'],
+    url='https://github.com/Nikunj-Gupta/FCMADRL',
+    license='MIT License',
+    author='Nikunj Gupta',
+    author_email='Nikunj.Gupta@iiitb.org',
+    description='Fully Cooperative Multi-Agent Deep Reinforcement Learning'
+)
