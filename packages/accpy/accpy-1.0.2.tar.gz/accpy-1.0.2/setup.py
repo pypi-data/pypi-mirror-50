2a294efc359a1c59781690858d95b8a732533904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:18 +0000

    Add package accpy/1.0.2/accpy-1.0.2.tar.gz

---
+++ packages/accpy/accpy-1.0.2.tar.gz/accpy-1.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("readme.md",encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="accpy",
+    version="1.0.2",
+    author="台灣.屏東大學.周國華老師",
+    author_email="ckhmike@gmail.com",
+    description="愛嗑派，會計派，會計人一起來吃派吧！",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://www.ais.nptu.edu.tw/PythonCode/PythonCode.html",
+    packages=['accpy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['numpy'],
+)
