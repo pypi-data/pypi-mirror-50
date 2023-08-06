2db3b7ee4e2756d924758277f5e1789f3e2ef61d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:11 +0000

    Add package pharmpy/0.0.2/pharmpy-0.0.2-py3-none-any.whl

---
+++ packages/pharmpy/pharmpy-0.0.2-py3-none-any.whl/pharmpy/epc.py
@@ -0,0 +1,49 @@
+import pharmpy.utils as utils
+
+class EPCEngine:
+
+
+    def __init__(self, digits=11):
+        self.digits = digits
+        self.ndc2epc = utils.read_package(digits)
+
+    def get_epc(self, ndc_lst):
+        """
+        Returns EPC or a list of EPC for the given NDC(s).
+
+        Parameters
+        __________
+        ndc_lst: list of str, or str
+                 A list of NDC codes.
+                 NDC codes can be either 10 digit standard or
+                 11 digit standard based on the constructor's setting.
+        """
+
+        output_type = "list"
+        if not isinstance(ndc_lst, list):
+            output_type = "value"
+            ndc_lst = [ndc_lst]
+
+        epc_lst = []
+        out_default = {"ndc": "na",
+                        "name_proprietary": "na",
+                        "name_generic": "na",
+                        "substance_lst": [],
+                        "pc_lst": []}
+        for ndc in ndc_lst:
+            if ndc in self.ndc2epc:
+                epc_lst.append(self.ndc2epc[ndc])
+            else:
+                epc_lst.append(out_default)
+
+        out = epc_lst
+        if output_type == "value":
+            out = epc_lst[0]
+
+        return out
+
+
+
+
+
+
