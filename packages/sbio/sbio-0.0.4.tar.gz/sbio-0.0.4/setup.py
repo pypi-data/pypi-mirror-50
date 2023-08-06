e68dd272f67161acd56bf59fe62e7de6af05a16a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:58 +0000

    Add package sbio/0.0.4/sbio-0.0.4.tar.gz

---
+++ packages/sbio/sbio-0.0.4.tar.gz/sbio-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sbio",
+    version="0.0.4",
+    author="Tod Stuber",
+    author_email="tod.p.stuber@usda.gov",
+    description="Collection of simple bioinformatic tools.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/USDA-VS/sbio",
+    packages=setuptools.find_packages(),
+    install_requires=['python>=3.6', 're', 'pandas', 'numpy', 'concurrent', 'collections', 'gzip', 'argparse', 'textwrap', 'humanize'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
