6ce55304f5475a0a4c5fe74dc73554018f41965e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:46 +0000

    Add package smolstore/0.2.1a0/smolstore-0.2.1a0.tar.gz

---
+++ packages/smolstore/smolstore-0.2.1a0.tar.gz/smolstore-0.2.1a0/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['smolstore']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'smolstore',
+    'version': '0.2.1a0',
+    'description': 'A simple in-memory Python document store',
+    'long_description': None,
+    'author': 'Raymond Botha',
+    'author_email': 'botha@riseup.net',
+    'url': 'https://github.com/raybotha/smolstore',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
