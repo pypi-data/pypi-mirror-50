5a3236ac65fad3b7e652120b5b627dfa8019f1f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:40 +0000

    Add package eciespy/0.2.0/eciespy-0.2.0-py3-none-any.whl

---
+++ packages/eciespy/eciespy-0.2.0-py3-none-any.whl/ecies/tests/test_crypt.py
@@ -0,0 +1,41 @@
+import os
+import unittest
+
+from ecies import encrypt, decrypt
+from ecies.utils import sha256, generate_eth_key, generate_key, aes_encrypt, aes_decrypt
+
+
+class TestCrypt(unittest.TestCase):
+    def setUp(self):
+        self.test_string = b"this is a test"
+        self.big_data = b"0" * 1024 * 1024 * 100  # 100 MB
+
+    def test_hash(self):
+        self.assertEqual(sha256(b"0" * 16).hex()[:8], "fcdb4b42")
+
+    def test_elliptic(self):
+        data = self.test_string
+        k = generate_eth_key()
+        prvhex = k.to_hex()
+        pubhex = k.public_key.to_hex()
+        self.assertEqual(data, decrypt(prvhex, encrypt(pubhex, data)))
+
+        k = generate_key()
+        prvhex = k.to_hex()
+        pubhex = k.public_key.format(False).hex()
+        self.assertEqual(data, decrypt(prvhex, encrypt(pubhex, data)))
+        self.assertEqual(data, decrypt(bytes.fromhex(prvhex), encrypt(bytes.fromhex(pubhex), data)))
+
+        k = generate_key()
+        prvhex = k.to_hex()
+        pubhex = k.public_key.format(True).hex()
+        self.assertEqual(data, decrypt(prvhex, encrypt(pubhex, data)))
+        self.assertEqual(data, decrypt(bytes.fromhex(prvhex), encrypt(bytes.fromhex(pubhex), data)))
+
+        self.assertRaises(TypeError, encrypt, 1, data)
+        self.assertRaises(TypeError, decrypt, 1, encrypt(bytes.fromhex(pubhex), data))
+
+    def test_aes(self):
+        data = self.big_data
+        key = os.urandom(16)
+        self.assertEqual(data, aes_decrypt(key, aes_encrypt(key, data)))
