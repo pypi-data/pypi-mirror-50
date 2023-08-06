031af7efb94e2932511771fac83241e787d44066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:54 +0000

    Add package causalml/0.2.0/causalml-0.2.0-cp36-cp36m-macosx_10_7_x86_64.whl

---
+++ packages/causalml/causalml-0.2.0-cp36-cp36m-macosx_10_7_x86_64.whl/causalml/dataset/__init__.py
@@ -0,0 +1,6 @@
+from .regression import synthetic_data
+from .regression import simulate_nuisance_and_easy_treatment
+from .regression import simulate_randomized_trial
+from .regression import simulate_easy_propensity_difficult_baseline
+from .regression import simulate_unrelated_treatment_control
+from .classification import make_uplift_classification
