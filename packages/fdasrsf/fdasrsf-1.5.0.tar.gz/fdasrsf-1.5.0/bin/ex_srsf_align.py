823b9d4b727b7e72a8a4038eeea6e3654b6fc0f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:41 +0000

    Add package fdasrsf/1.5.0/fdasrsf-1.5.0.tar.gz

---
+++ packages/fdasrsf/fdasrsf-1.5.0.tar.gz/fdasrsf-1.5.0/bin/ex_srsf_align.py
@@ -0,0 +1,9 @@
+#%%
+import fdasrsf as fs
+import numpy as np
+data = np.load('bin/simu_data.npz')
+time = data['arr_1']
+f = data['arr_0']
+out = fs.srsf_align(f,time,omethod="RBFGS",showplot=False)
+
+#%%
