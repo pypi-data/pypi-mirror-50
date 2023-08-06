56df90c49998413435591e4b72c1ed1d3b19f094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:58 +0000

    Add package ocrd/1.0.0b12/ocrd-1.0.0b12-py3-none-any.whl

---
+++ packages/ocrd/ocrd-1.0.0b12-py3-none-any.whl/ocrd/constants.py
@@ -0,0 +1,11 @@
+"""
+Constants for ocrd.
+"""
+from pkg_resources import resource_filename
+
+TMP_PREFIX = 'pyocrd-'
+DEFAULT_UPLOAD_FOLDER = '/tmp/uploads-pyocrd'
+DOWNLOAD_DIR = '/tmp/ocrd-core-downloads'
+DEFAULT_REPOSITORY_URL = 'http://localhost:5000/'
+BASHLIB_FILENAME = resource_filename(__name__, 'lib.bash')
+BACKUP_DIR = '.backup'
