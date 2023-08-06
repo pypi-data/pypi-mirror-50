904598e9c2d016b1d569f25ae261d8b01a69e548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:19 +0000

    Add package pflacco/0.1/pflacco-0.1.tar.gz

---
+++ packages/pflacco/pflacco-0.1.tar.gz/pflacco-0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='pflacco',  
+     version='0.1',
+     author="Raphael Patrick Prager",
+     author_email="raphael.prager@uni-muenster.de",
+     description="An python interface to the R package flacco for computing ELA features.",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     license = 'MIT',
+     url="https://github.com/Reiyan/pflacco",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
