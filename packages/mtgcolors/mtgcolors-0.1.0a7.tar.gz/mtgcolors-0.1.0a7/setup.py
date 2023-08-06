80ac29f9c6f3d9309f61773442ab372a27254222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:44 +0000

    Add package mtgcolors/0.1.0a7/mtgcolors-0.1.0a7.tar.gz

---
+++ packages/mtgcolors/mtgcolors-0.1.0a7.tar.gz/mtgcolors-0.1.0a7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+version = "0.1.0a7"
+if os.environ.get("CI_COMMIT_TAG"):
+    version = os.environ["CI_COMMIT_TAG"]
+elif os.environ.get("CI_JOB_ID"):
+    version = os.environ["CI_JOB_ID"]
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="mtgcolors",
+    version=version,
+    author="aloisdg",
+    author_email="aloisdg@protonmail.com",
+    description="MtgColors is module to detail mtg colors",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/aloisdegouvello/mtgcolors",
+    packages=find_packages(exclude=("tests", "docs")),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
