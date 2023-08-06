fb9dc0b46107e4451517afbf7ac6c92c3b093afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:31 +0000

    Add package mwoauth/0.2.9/mwoauth-0.2.9-py3.5.egg

---
+++ packages/mwoauth/mwoauth-0.2.9-py3.5.egg/mwoauth/__init__.py
@@ -0,0 +1,21 @@
+"""Provides a collection of utilities for easily working with MediaWiki's
+OAuth1.0a implementation."""
+from .handshaker import Handshaker
+from .tokens import AccessToken, ConsumerToken, RequestToken
+from .errors import OAuthException
+from .functions import initiate, complete, identify
+from .about import (__name__, __version__, __author__, __author_email__,
+                    __description__, __license__, __url__)
+
+__all__ = [
+    AccessToken,
+    complete,
+    ConsumerToken,
+    Handshaker,
+    identify,
+    initiate,
+    OAuthException,
+    RequestToken,
+    __name__, __version__, __author__, __author_email__,
+    __description__, __license__, __url__
+]
