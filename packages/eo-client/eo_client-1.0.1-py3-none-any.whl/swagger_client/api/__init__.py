ea62039349ce7a699369e548d557c1366123b775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:25 +0000

    Add package eo-client/1.0.1/eo_client-1.0.1-py3-none-any.whl

---
+++ packages/eo-client/eo_client-1.0.1-py3-none-any.whl/swagger_client/api/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.atomic_project_api import AtomicProjectApi
+from swagger_client.api.complex_projects_api import ComplexProjectsApi
+from swagger_client.api.organization_teams_api import OrganizationTeamsApi
+from swagger_client.api.organizational_roles_api import OrganizationalRolesApi
+from swagger_client.api.organizations_api import OrganizationsApi
+from swagger_client.api.people_api import PeopleApi
+from swagger_client.api.project_api import ProjectApi
+from swagger_client.api.project_teams_api import ProjectTeamsApi
+from swagger_client.api.team_members_api import TeamMembersApi
+from swagger_client.api.team_purposes_api import TeamPurposesApi
+from swagger_client.api.teams_api import TeamsApi
