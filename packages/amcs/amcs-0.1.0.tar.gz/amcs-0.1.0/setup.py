67f97b296b3269114b908901ef0f24f4e2abd239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:00 +0000

    Add package amcs/0.1.0/amcs-0.1.0.tar.gz

---
+++ packages/amcs/amcs-0.1.0.tar.gz/amcs-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="amcs",
+    version='0.1.0',
+    description="Generates company rankings for peer charts for valuations",
+    author="Salman Ahmed",
+    author_email="salman.ahmed@alvarezandmarsal.com",
+    url="https://github.com/sahmed1997",
+)
