3081af5f36257c40e501115a6bf1497da222139d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:13 +0000

    Add package bareasgi-auth-common/1.3.0/bareASGI-auth-common-1.3.0.tar.gz

---
+++ packages/bareasgi-auth-common/bareASGI-auth-common-1.3.0.tar.gz/bareASGI-auth-common-1.3.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['bareasgi_auth_common',
+ 'bareasgi_auth_common.utils',
+ 'bareasgi_auth_common.utils.yaml_types']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyJWT>=1.7,<2.0', 'PyYaml>=5.1,<6.0', 'bareclient>=3.0,<4.0']
+
+setup_kwargs = {
+    'name': 'bareasgi-auth-common',
+    'version': '1.3.0',
+    'description': '',
+    'long_description': '# bareASGI-auth-common\n\nCommon code for authentication with bareASGI.',
+    'author': 'Rob Blackbourn',
+    'author_email': 'rob.blackbourn@gmail.com',
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
