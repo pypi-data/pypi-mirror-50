dcf0f2448085879cb326fab90ffb9ec8454d913d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:33 +0000

    Add package gsn-bnm-distributions/0.1/gsn_bnm_distributions-0.1.tar.gz

---
+++ packages/gsn-bnm-distributions/gsn_bnm_distributions-0.1.tar.gz/gsn_bnm_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gsn_bnm_distributions',
+      version='0.1',
+      description='gaussian and binomial probability distributions',
+      packages=['gsn_bnm_distributions'],
+      zip_safe=False)
