eb8014dc6fe4d854341d0821248f0e35e938b468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:11 +0000

    Add package nengo-bones/0.4.2/nengo_bones-0.4.2-py3-none-any.whl

---
+++ packages/nengo-bones/nengo_bones-0.4.2-py3-none-any.whl/nengo_bones/__init__.py
@@ -0,0 +1,8 @@
+# pylint: disable=missing-docstring
+
+__copyright__ = "2018-2019, Applied Brain Research"
+__license__ = "Free for non-commercial use; see LICENSE.rst"
+
+from .version import version as __version__
+
+from . import config, templates
