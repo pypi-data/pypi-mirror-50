836134e914d65d5f0e399c1331b0499e34d28d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:41 +0000

    Add package act-atmos/0.0.2/act_atmos-0.0.2.tar.gz

---
+++ packages/act-atmos/act_atmos-0.0.2.tar.gz/act_atmos-0.0.2/act/qc/__init__.py
@@ -0,0 +1,18 @@
+"""
+===============
+act.qc (act.qc)
+===============
+
+.. currentmodule:: act.qc
+
+This module contains procedures for setting up ancillary quality control variable
+and returning a mask from the quality control variable.
+
+.. autosummary::
+    :toctree: generated/
+
+    qcfilter
+    qctests
+"""
+
+from . import qcfilter
---
+++ packages/act-atmos/act_atmos-0.0.2.tar.gz/act_atmos-0.0.2/act/retrievals/__init__.py
@@ -0,0 +1,16 @@
+"""
+===============================
+act.retrievals (act.retrievals)
+===============================
+
+.. currentmodule:: act.retrievals
+
+This module contains various retrievals for ARM datsets.
+
+.. autosummary::
+    :toctree: generated/
+
+    calculate_stability_indicies
+"""
+
+from .stability_indices import calculate_stability_indicies
