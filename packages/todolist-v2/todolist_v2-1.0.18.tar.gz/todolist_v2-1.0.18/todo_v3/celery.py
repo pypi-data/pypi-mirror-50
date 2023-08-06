23009da1d1c8325e1540c7e7392627584e296fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:06 +0000

    Add package todolist-v2/1.0.18/todolist_v2-1.0.18.tar.gz

---
+++ packages/todolist-v2/todolist_v2-1.0.18.tar.gz/todolist_v2-1.0.18/todo_v3/celery.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+import os
+
+from celery import Celery
+from django.conf import settings
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_v3.settings')
+app = Celery('todo_v3')
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
