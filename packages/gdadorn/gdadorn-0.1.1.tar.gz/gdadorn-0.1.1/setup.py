fb4b661c969bd9c1159abfb627b04d2507626dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:26 +0000

    Add package gdadorn/0.1.1/gdadorn-0.1.1.tar.gz

---
+++ packages/gdadorn/gdadorn-0.1.1.tar.gz/gdadorn-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from glob import glob
+import setuptools
+
+setuptools.setup(
+    name="gdadorn",
+    version='0.1.1',
+    url="https://github.com/similarface/gdadorn.git",
+    author="similarface",
+    author_email='similarface@gmail.com',
+    description="jupyer的一些扩展修改，内存显示，下载限制等等",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'psutil',
+        'notebook',
+    ],
+    data_files=[
+        ('share/jupyter/nbextensions/gdadorn', glob('gdadorn/static/*')),
+        ('etc/jupyter/jupyter_notebook_config.d', ['gdadorn/etc/serverextension.json']),
+        ('etc/jupyter/nbconfig/notebook.d', ['gdadorn/etc/nbextension.json'])
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    license ='MIT Licence',
+)
