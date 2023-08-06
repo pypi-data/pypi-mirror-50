329f1adea2ca8ec422966d7f33ede0c4afa76e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:09 +0000

    Add package TermIO/0.1.2/TermIO-0.1.2.tar.gz

---
+++ packages/TermIO/TermIO-0.1.2.tar.gz/TermIO-0.1.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='TermIO',
+    version='0.1.2',
+    scripts=[],
+    author="jakobst1n",
+    author_email="jakob.stendahl@outlook.com",
+    description="Simple terminal manipulator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jakobst1n/TermIO-python-package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
