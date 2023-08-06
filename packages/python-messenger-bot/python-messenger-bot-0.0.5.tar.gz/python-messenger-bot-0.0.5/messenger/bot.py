5ca4261f5f2572d8d6f0e871aad4b35a33a85656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:23 +0000

    Add package python-messenger-bot/0.0.5/python-messenger-bot-0.0.5.tar.gz

---
+++ packages/python-messenger-bot/python-messenger-bot-0.0.5.tar.gz/python-messenger-bot-0.0.5/messenger/bot.py
@@ -0,0 +1,9 @@
+from constant import *
+
+class Bot:
+    def __init__(self, page_access_token, verify_token, **kwargs):
+        self.page_access_token = page_access_token
+        self.verify_token = verify_token
+
+    def print_access():
+        return self.page_token
