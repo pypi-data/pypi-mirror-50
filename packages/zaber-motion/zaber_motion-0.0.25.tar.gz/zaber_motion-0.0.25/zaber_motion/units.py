1475c98c9374963d2c73f23e5a52525589a13497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:19 +0000

    Add package zaber-motion/0.0.25/zaber_motion-0.0.25.tar.gz

---
+++ packages/zaber-motion/zaber_motion-0.0.25.tar.gz/zaber_motion-0.0.25/zaber_motion/units.py
@@ -0,0 +1,78 @@
+#  This file is generated from the Zaber device database. Do not manually edit this file.
+
+from enum import Enum
+
+
+class Units(Enum):
+    Native = ''
+
+    Length_metres = 'Length:metres'
+    Length_centimetres = 'Length:centimetres'
+    Length_millimetres = 'Length:millimetres'
+    Length_micrometres = 'Length:micrometres'
+    Length_nanometres = 'Length:nanometres'
+    Length_inches = 'Length:inches'
+
+    Velocity_metres_per_second = 'Velocity:metres per second'
+    Velocity_centimetres_per_second = 'Velocity:centimetres per second'
+    Velocity_millimetres_per_second = 'Velocity:millimetres per second'
+    Velocity_micrometres_per_second = 'Velocity:micrometres per second'
+    Velocity_nanometres_per_second = 'Velocity:nanometres per second'
+    Velocity_inches_per_second = 'Velocity:inches per second'
+
+    Acceleration_metres_per_second_squared = 'Acceleration:metres per second squared'
+    Acceleration_centimetres_per_second_squared = 'Acceleration:centimetres per second squared'
+    Acceleration_millimetres_per_second_squared = 'Acceleration:millimetres per second squared'
+    Acceleration_micrometres_per_second_squared = 'Acceleration:micrometres per second squared'
+    Acceleration_nanometres_per_second_squared = 'Acceleration:nanometres per second squared'
+    Acceleration_inches_per_second_squared = 'Acceleration:inches per second squared'
+
+    Angle_degrees = 'Angle:degrees'
+    Angle_radians = 'Angle:radians'
+
+    AngularVelocity_degrees_per_second = 'Angular Velocity:degrees per second'
+    AngularVelocity_radians_per_second = 'Angular Velocity:radians per second'
+
+    AngularAcceleration_degrees_per_second_squared = 'Angular Acceleration:degrees per second squared'
+    AngularAcceleration_radians_per_second_squared = 'Angular Acceleration:radians per second squared'
+
+    ACElectricCurrent_amperes_peak = 'AC Electric Current:amperes peak'
+    ACElectricCurrent_amperes_rms = 'AC Electric Current:amperes RMS'
+
+    Percent_percent = 'Percent:percent'
+
+    DCElectricCurrent_amperes = 'DC Electric Current:amperes'
+
+    Force_newtons = 'Force:newtons'
+    Force_millinewtons = 'Force:millinewtons'
+    Force_pounds_force = 'Force:pounds-force'
+    Force_kilonewtons = 'Force:kilonewtons'
+
+    Time_seconds = 'Time:seconds'
+    Time_milliseconds = 'Time:milliseconds'
+    Time_microseconds = 'Time:microseconds'
+
+    Torque_newton_metres = 'Torque:newton metres'
+    Torque_newton_centimetres = 'Torque:newton centimetres'
+    Torque_pound_force_feet = 'Torque:pound-force-feet'
+    Torque_ounce_force_inches = 'Torque:ounce-force-inches'
+
+    Inertia_grams = 'Inertia:grams'
+    Inertia_kilograms = 'Inertia:kilograms'
+    Inertia_milligrams = 'Inertia:milligrams'
+    Inertia_pounds = 'Inertia:pounds'
+    Inertia_ounces = 'Inertia:ounces'
+
+    RotationalInertia_gram_square_metre = 'Rotational Inertia:gram-square metre'
+    RotationalInertia_kilogram_square_metre = 'Rotational Inertia:kilogram-square metre'
+    RotationalInertia_pound_square_feet = 'Rotational Inertia:pound-square-feet'
+
+    ForceConstant_newtons_per_amp = 'Force Constant:newtons per amp'
+    ForceConstant_millinewtons_per_amp = 'Force Constant:millinewtons per amp'
+    ForceConstant_kilonewtons_per_amp = 'Force Constant:kilonewtons per amp'
+    ForceConstant_pounds_force_per_amp = 'Force Constant:pounds-force per amp'
+
+    TorqueConstant_newton_metres_per_amp = 'Torque Constant:newton metres per amp'
+    TorqueConstant_millinewton_metres_per_amp = 'Torque Constant:millinewton metres per amp'
+    TorqueConstant_kilonewton_metres_per_amp = 'Torque Constant:kilonewton metres per amp'
+    TorqueConstant_pound_force_feet_per_amp = 'Torque Constant:pound-force-feet per amp'
