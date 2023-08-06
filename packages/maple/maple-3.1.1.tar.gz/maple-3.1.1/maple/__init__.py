9f7bd55aa5b2261355b9927c11691e224ab4097c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:54 +0000

    Add package maple/3.1.1/maple-3.1.1.tar.gz

---
+++ packages/maple/maple-3.1.1.tar.gz/maple-3.1.1/maple/__init__.py
@@ -0,0 +1,9 @@
+
+__version__ = '3.1.1'
+
+from .log import logger
+from .utils import safe_call, safe_func
+from .worker.worker import Worker
+from .worker.blueprint import Blueprint
+
+from .trigger.trigger import Trigger
