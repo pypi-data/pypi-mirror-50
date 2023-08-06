41c7870c18834c358d4b95e4a4896c400b7b86de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:51 +0000

    Add package libqutrub/1.2.3/libqutrub-1.2.3.tar.gz

---
+++ packages/libqutrub/libqutrub-1.2.3.tar.gz/libqutrub-1.2.3/setup_lib.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python
+from setuptools  import setup
+
+# to install type:
+# python setup.py install --root=/
+
+setup (name='libqutrub', version='1.2.3',
+      description='libqutrub Arabic verb conjuagtion library',
+      author='Taha Zerrouki',
+      author_email='taha.zerrouki@gmail.com',
+      url='http://libqutrub.sourceforge.net/',
+      license='GPL',
+      platform="OS independent",
+      package_dir={'libqutrub': 'libqutrub',},
+      packages=['libqutrub'],
+      install_requires=[ 'pyarabic>=0.6.2',
+      ],         
+      include_package_data=True,
+      package_data = {
+        'libqutrub': ['doc/*.*','doc/html/*'],
+        },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: End Users/Desktop',
+          'Programming Language :: Python',
+          ],
+    );
