4b345eb20b4887f4e2533a934ccd62aade1f98e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:15 +0000

    Add package dicom2fem/1.1.4/dicom2fem-1.1.4.tar.gz

---
+++ packages/dicom2fem/dicom2fem-1.1.4.tar.gz/dicom2fem-1.1.4/dicom2fem/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ['base', 'dicom2fem', 'genfem_base', 'ioutils', 'marching_cubes',
+           'mesh', 'meshio', 'seg2fem', 'viewer', 'vtk2stl']
+
+__version__ = "1.1.4"
+
+from . import base, dicom2fem, genfem_base, ioutils, marching_cubes
+from . import mesh, meshio, seg2fem, viewer, vtk2stl
