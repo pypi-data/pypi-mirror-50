dfc20f47ff5e331ea05268b9ac3853f732ed86ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:30 +0000

    Add package faastapi/0.1.8/faastapi-0.1.8-py3-none-any.whl

---
+++ packages/faastapi/faastapi-0.1.8-py3-none-any.whl/faastapi/template_model.py
@@ -0,0 +1,42 @@
+from typing import Optional, List, Dict, Union
+from pydantic import BaseModel, constr, Schema, Any
+
+
+class MetaConfig(BaseModel):
+    name: str
+    version: str
+    image: str
+    label: Optional[constr(max_length=50)] = None
+    tags: Optional[List[str]] = None
+    description: Optional[str] = None
+
+
+class CopyConfig(BaseModel):
+    src: str
+    dest: Optional[str] = "."
+
+
+class BuildConfig(BaseModel):
+    dependencies: Optional[List[str]] = None
+    copy_files: Optional[List[CopyConfig]] = Schema(default=None, alias="copy")
+    extras: Optional[List[str]] = None
+    plugins: Optional[List[str]] = None
+
+
+class RunConfig(BaseModel):
+    script: str
+    function: str
+    method: str
+    input: Optional[List[Dict[str, Any]]] = None
+    output: Optional[Union[Dict[str, str], str]] = None
+    environment: Optional[Dict[str, str]] = None
+
+    # class Config:
+    #     arbitrary_types_allowed = True
+
+
+class TemplateConfig(BaseModel):
+    apiversion: Optional[str] = "v1"
+    meta: MetaConfig
+    build: BuildConfig
+    run: RunConfig
