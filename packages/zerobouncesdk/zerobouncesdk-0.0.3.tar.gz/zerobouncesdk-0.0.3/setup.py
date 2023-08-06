983870f80348b98925209803de1db1659451a84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:24 +0000

    Add package zerobouncesdk/0.0.3/zerobouncesdk-0.0.3.tar.gz

---
+++ packages/zerobouncesdk/zerobouncesdk-0.0.3.tar.gz/zerobouncesdk-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='zerobouncesdk',
+    version='0.0.3',
+    description='The ZeroBounce SDK for Python programming language',
+    license='MIT',
+    packages=['zerobouncesdk'],
+    author='ZeroBounce',
+    author_email='integrations@zerobounce.net',
+    keywords=['zero', 'bounce', 'sdk'],
+    url='https://github.com/zerobounce-llc/zero-bounce-python-sdk', install_requires=['requests', 'requests_toolbelt']
+)
