512d78284ae3f62521b5c80fbd9d54f34a235f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:50 +0000

    Add package sparqlprog/0.0.2/sparqlprog-0.0.2.tar.gz

---
+++ packages/sparqlprog/sparqlprog-0.0.2.tar.gz/sparqlprog-0.0.2/setup.py
@@ -0,0 +1,28 @@
+#from distutils.core import setup
+from setuptools import setup
+
+import sparqlprog
+
+pt_classifiers = [
+    "Development Status :: 4 - Beta",
+    "Programming Language :: Python :: 3",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Utilities",
+]
+
+setup(
+    name = "sparqlprog",
+    py_modules=["sparqlprog"],
+    version = sparqlprog.__version__,
+    description = "Execute logic program queries against a remote SPARQL endpoint",
+##    long_description=open("README.rst").read(),
+    author = "Chris Mungall",
+    author_email = "cmungall+github@gmail.com",
+    url = "https://github.com/cmungall/sparqlprog-python",
+    license = "BSD3",
+    #tests_require = ["pytest"],
+    keywords = ["prolog", "swipl", "swi-prolog", "logic programming", "pengines", "sparql", "semantic web", "owl"],
+    classifiers = pt_classifiers,
+)
