efb3c05bfca12391c3dfb45b68529e4edaece5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:51 +0000

    Add package pygameoflife-dadeerh/1.1.0/pygameoflife_dadeerh-1.1.0.tar.gz

---
+++ packages/pygameoflife-dadeerh/pygameoflife_dadeerh-1.1.0.tar.gz/pygameoflife_dadeerh-1.1.0/pygameoflife_dadeerh/__init__.py
@@ -0,0 +1,1 @@
+from .pygameoflife_dadeerh import LifeGame
---
+++ packages/pygameoflife-dadeerh/pygameoflife_dadeerh-1.1.0.tar.gz/pygameoflife_dadeerh-1.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme_file_contents():
+    with open("Readme.md") as fd:
+        return fd.read()
+
+setup(
+    name="pygameoflife_dadeerh",
+    version="1.1.0",
+    description="Pygame implementation of Conway's Game of Life",
+    long_description= readme_file_contents(),
+    url="",
+    author="dadeerh",
+    author_email="dadeerh91@gmail.com",
+    license="GPL-3.0",
+    packages=[
+        "pygameoflife_dadeerh"
+    ],
+    scripts=[
+        "bin/pygameoflife_dadeerh",
+        "bin/pygameoflife_dadeerh.bat"
+    ],
+    zip_safe=False,
+    install_requires=[
+        "pygame"
+    ]
+)
