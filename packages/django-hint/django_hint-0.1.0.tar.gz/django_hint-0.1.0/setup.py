a36ba351eb5240a418763f069750d85fe64278b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:34 +0000

    Add package django-hint/0.1.0/django_hint-0.1.0.tar.gz

---
+++ packages/django-hint/django_hint-0.1.0.tar.gz/django_hint-0.1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django_hint",
+    version="0.1.0",
+    author="Vieolo OÜ",
+    author_email="info@vieolo.com",
+    description="Typehinting package for django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Vieolo/django-hint",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
