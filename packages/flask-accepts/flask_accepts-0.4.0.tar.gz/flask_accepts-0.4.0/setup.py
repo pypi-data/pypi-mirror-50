26f4ff72e1aae4e0f7f00481f650510b2a9802f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:10 +0000

    Add package flask-accepts/0.4.0/flask_accepts-0.4.0.tar.gz

---
+++ packages/flask-accepts/flask_accepts-0.4.0.tar.gz/flask_accepts-0.4.0/setup.py
@@ -0,0 +1,18 @@
+# Copyright Alan (AJ) Pryor, Jr. 2018
+
+from setuptools import setup, find_packages
+
+setup(
+    name="flask_accepts",
+    author='Alan "AJ" Pryor, Jr.',
+    author_email="apryor6@gmail.com",
+    version="0.4.0",
+    description="Easy Flask input validation",
+    ext_modules=[],
+    packages=find_packages(),
+    install_requires=[
+        "Flask-RESTful>=0.3.7",
+        "marshmallow>=2.19.5",
+        "flask-restplus>=0.12.1",
+    ],
+)
