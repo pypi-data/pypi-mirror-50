dbb4bd827aae6618b07d8622bdcfdf7f19fbb242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:40 +0000

    Add package k-util/0.2.0/k_util-0.2.0-py3-none-any.whl

---
+++ packages/k-util/k_util-0.2.0-py3-none-any.whl/k_util/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+<ENTER DESCRIPTION HERE>
+"""
+
+__author__ = "Jakrin Juangbhanich"
+__email__ = "juangbhanich.k@gmail.com"
+__version__ = "0.2.0"
+
+from .region import Region
