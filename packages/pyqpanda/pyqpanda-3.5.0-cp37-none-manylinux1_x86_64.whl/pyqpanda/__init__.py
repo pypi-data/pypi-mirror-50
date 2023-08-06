96e32493296c939354aa665faf5a911910cedb73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:27 +0000

    Add package pyqpanda/3.5.0/pyqpanda-3.5.0-cp37-none-manylinux1_x86_64.whl

---
+++ packages/pyqpanda/pyqpanda-3.5.0-cp37-none-manylinux1_x86_64.whl/pyqpanda/__init__.py
@@ -0,0 +1,12 @@
+'''
+QPanda Python\n
+Copyright (C) Origin Quantum 2017-2018\n
+Licensed Under Apache Licence 2.0
+'''
+from .utils import *
+from .pyQPanda import *
+from .Hamiltonian.PauliOperator.pyQPandaPauliOperator import PauliOperator
+from .Variational import back
+
+One=True
+Zero=False
