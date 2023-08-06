04315ff7e695ff44770d85bb00ee1bba06bdd731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:03 +0000

    Add package scraping-class/0.2/scraping_class-0.2.tar.gz

---
+++ packages/scraping-class/scraping_class-0.2.tar.gz/scraping_class-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scraping_class',
+      version='0.2',
+      description='Webscraper for SDS2019',
+      url='https://github.com/elben10/ScrapingClass',
+      author='Snorre Ralund, Jakob Jul Elben',
+      author_email='elbenjakobjul@gmail.com',
+      license='MIT',
+      packages=['scraping_class'],
+      zip_safe=False)
