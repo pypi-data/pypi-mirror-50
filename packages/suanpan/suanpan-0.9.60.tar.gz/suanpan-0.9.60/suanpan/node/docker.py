4f2146472630a9a2efb9a9d0134cf89b1d0cabdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:00 +0000

    Add package suanpan/0.9.60/suanpan-0.9.60.tar.gz

---
+++ packages/suanpan/suanpan-0.9.60.tar.gz/suanpan-0.9.60/suanpan/node/docker.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from lostc import collection as lcc
+from suanpan.node.base import BaseNode, Param, Port
+
+
+class DockerNode(BaseNode):
+    def formatNodeInfo(self, nodeInfo):
+        ports = {p["uuid"]: p for p in nodeInfo.get("ports", [])}
+        node = lcc.classify(
+            nodeInfo.get("params", []),
+            inputs=lambda p: p.get("defInPortUuid", "").startswith("in"),
+            outputs=lambda p: p.get("defInPortUuid", "").startswith("out"),
+            params=lambda p: "defInPortUuid" not in p,
+        )
+        inputs = {
+            i["name"]: Port(
+                uuid=i["defOutPortUuid"],
+                name=i["name"],
+                type=ports[i["defOutPortUuid"]]["type"],
+                subtype=ports[i["defOutPortUuid"]]["subtype"],
+            )
+            for i in node["inputs"]
+        }
+        outputs = {
+            o["name"]: Port(
+                uuid=o["defOutPortUuid"],
+                name=o["name"],
+                type=ports[o["defOutPortUuid"]]["type"],
+                subtype=ports[o["defOutPortUuid"]]["subtype"],
+            )
+            for o in node["outputs"]
+        }
+        params = {
+            p["name"]: Param(uuid=p["controlInstUuid"], name=p["name"], type=p["type"])
+            for p in node["params"]
+        }
+        return {"inputs": inputs, "outputs": outputs, "params": params}
