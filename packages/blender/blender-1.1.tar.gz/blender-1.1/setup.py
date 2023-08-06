28d846be90498bf6fa6344f3a709c8a34379de97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:20 +0000

    Add package blender/1.1/blender-1.1.tar.gz

---
+++ packages/blender/blender-1.1.tar.gz/blender-1.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+# The text of the README file
+README = (HERE/"README.md").read_text()
+
+setup(name='blender',
+      version='1.1',
+      description='Pre-built Blender 2.79 as a python3.5 module bpy for linux.',
+      author='Uzair Akbar',
+      author_email='uzairakbar@outlook.com',
+      url='https://github.com/uzairakbar/blender/archive/v1.1.tar.gz',
+      packages=['blender'],
+      long_description=README,
+      long_description_content_type="text/markdown",
+      include_package_data=True
+      )
