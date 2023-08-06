062398c285e2e48a3777f946d152ecf2cb8b9cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:39 +0000

    Add package matialvarezs_charge_controller/0.0.111/matialvarezs_charge_controller-0.0.111.tar.gz

---
+++ packages/matialvarezs_charge_controller/matialvarezs_charge_controller-0.0.111.tar.gz/matialvarezs_charge_controller-0.0.111/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'matialvarezs_charge_controller',
+  packages = ['matialvarezs_charge_controller'], # this must be the same as the name above
+  version = '0.0.111',
+  install_requires = [
+    'matialvarezs-handlers-easy==0.1.26',
+    'matialvarezs_node_configurations==0.0.7'
+    'python-crontab==2.3.6'
+  ],
+  include_package_data = True,
+  description = 'Charge controller data',
+  author = 'Matias Alvarez Sabate',
+  author_email = 'matialvarezs@gmail.com',  
+  classifiers = [
+    'Programming Language :: Python :: 3.5',
+  ],
+)
