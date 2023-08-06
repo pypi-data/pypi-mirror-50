becfa11c67f5094736d285aa20a91e9d09eb26b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:17 +0000

    Add package Atria/0.0.9/Atria-0.0.9-py3-none-any.whl

---
+++ packages/Atria/Atria-0.0.9-py3-none-any.whl/Atria/atria.py
@@ -0,0 +1,9 @@
+import sys, os
+from Atria import brain
+from Atria import database
+from Atria.brain import Brain
+from Atria.database import DataBase
+import Main
+
+main = Main()
+main.main(sys.argv)
