1ac99203e7c560ea4c4f5bab3021db483f244a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:54 +0000

    Add package creepy-crawly/0.1.0/creepy_crawly-0.1.0-py3-none-any.whl

---
+++ packages/creepy-crawly/creepy_crawly-0.1.0-py3-none-any.whl/creepy_crawly/__init__.py
@@ -0,0 +1,32 @@
+__author__ = "Hayk Khachatryan"
+__version__ = '0.1.0'
+__license__ = "MIT"
+
+#########################
+#                       #
+#                       #
+#         main          #
+#                       #
+#                       #
+#########################
+
+def main():
+    ''' main entry point
+
+    Arguments:
+        none
+    '''
+    print("Hello creepy-crawly!")
+
+
+#########################
+#                       #
+#                       #
+#      run baby         #
+#                       #
+#                       #
+#########################
+
+if __name__ == '__main__':
+
+    main()
