237891c9a2f9d79e24dd587e5f716ccab0884dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:39 +0000

    Add package KLplus/0.1.1/KLplus-0.1.1-py3-none-any.whl

---
+++ packages/KLplus/KLplus-0.1.1-py3-none-any.whl/KLplus/__main__.py
@@ -0,0 +1,5 @@
+from KLplus import klplus
+
+# Invoca o keylogger quando o modulo KLplus for executado como um script, ex.:
+# "python -m KLplus"
+klplus.main()
---
+++ packages/KLplus/KLplus-0.1.1-py3-none-any.whl/KLplus/_version.py
@@ -0,0 +1,13 @@
+# Arquivo com metadados para ser usado em setup.py
+
+__name__ = 'KLplus'
+__summary__ = 'Um Keylogger CLI para OS Linux baseado no principio KISS.'
+__uri__ = 'https://github.com/danbros/KLplus'
+
+__version__ = "0.1.1"
+
+__author__ = 'Dan Barros'
+__email__ = 'dpb4fun@gmail.com'
+
+__license__ = "GNU GPLv2"
+__copyright__ = f"Copyright 2019, {__author__}"
