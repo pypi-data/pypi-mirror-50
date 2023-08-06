1f31645761772be44d14de15b8600b4ade692321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:24 +0000

    Add package zhlite/1.2.3/zhlite-1.2.3-py3-none-any.whl

---
+++ packages/zhlite/zhlite-1.2.3-py3-none-any.whl/zhlite/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from zhlite.zhlite import *
+
+__author__ = "zhangbo"
+__email__ = "deplives@deplives.com"
+
+__license__ = "MIT"
+
+__version__ = "1.2.3"
+__date__ = "2019-08-12"
+__release__ = "重命名 Oauth.mine 为 Oauth.profile "
+
+
+class Version(object):
+
+    def __init__(self):
+        self.info = {
+            "version": __version__,
+            "date": __date__,
+            "release": __release__
+        }
+
+    def __str__(self):
+        return " ".join([f"{k}: {v}" for k, v in self.info.items()])
+
+
+version = Version()
+
+__all__ = ["Oauth", "User", "Question", "Answer"]
