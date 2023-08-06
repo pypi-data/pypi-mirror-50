06981d5c604028a38db6571d460e5eec66ef8a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:39 +0000

    Add package number-guessing-game/0.5/number_guessing_game-0.5.tar.gz

---
+++ packages/number-guessing-game/number_guessing_game-0.5.tar.gz/number_guessing_game-0.5/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="number_guessing_game",
+    version="0.5",
+    author="Jannis Müller",
+    author_email="jannis.mueller@hotmail.de",
+    description="object-oriented package to play a number guessing game",
+    zip_safe=False)
+    
+    
+    
+
+
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
