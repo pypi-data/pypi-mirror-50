f3828957118f4d4e388514c0100ae465dfd68248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:31 +0000

    Add package PDF-Layout-Scanner/1.3/PDF%20Layout%20Scanner-1.3.tar.gz

---
+++ packages/PDF-Layout-Scanner/PDF%20Layout%20Scanner-1.3.tar.gz/PDF Layout Scanner-1.3/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='PDF Layout Scanner',
+    version='1.3',
+    packages=['pdf_layout_scanner'],
+    license='MIT',
+    author='Yoshihiko Ueno',
+    author_email='windows7home@hotmail.co.jp',
+    long_description=open('./README.md').read(),
+    long_description_content_type="text/markdown",
+)
