7a90b1b0eca81d86ee13025a1e165428d01a78bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:02 +0000

    Add package frccontrol/2019.35/frccontrol-2019.35-py3-none-any.whl

---
+++ packages/frccontrol/frccontrol-2019.35-py3-none-any.whl/frccontrol/lqr.py
@@ -0,0 +1,39 @@
+import control as ct
+import numpy as np
+import scipy as sp
+
+
+def lqr(sys, Q, R):
+    """Solves for the optimal linear-quadratic regulator (LQR).
+
+    For a continuous system:
+        xdot = A * x + B * u
+        J = int(0, inf, x.T * Q * x + u.T * R * u)
+    For a discrete system:
+        x(n+1) = A * x(n) + B * u(n)
+        J = sum(0, inf, x.T * Q * x + u.T * R * u)
+
+    Keyword arguments:
+    A -- numpy.array(states x states), The A matrix.
+    B -- numpy.array(inputs x states), The B matrix.
+    Q -- numpy.array(states x states), The state cost matrix.
+    R -- numpy.array(inputs x inputs), The control effort cost matrix.
+
+    Returns:
+    numpy.array(states x inputs), K
+    """
+    m = sys.A.shape[0]
+
+    controllability_rank = np.linalg.matrix_rank(ct.ctrb(sys.A, sys.B))
+    if controllability_rank != m:
+        print(
+            "Warning: Controllability of %d != %d, uncontrollable state"
+            % (controllability_rank, m)
+        )
+
+    if sys.dt == 0.0:
+        P = sp.linalg.solve_continuous_are(a=sys.A, b=sys.B, q=Q, r=R)
+        return np.linalg.inv(R) @ sys.B.T @ P
+    else:
+        P = sp.linalg.solve_discrete_are(a=sys.A, b=sys.B, q=Q, r=R)
+        return np.linalg.inv(R + sys.B.T @ P @ sys.B) @ sys.B.T @ P @ sys.A
