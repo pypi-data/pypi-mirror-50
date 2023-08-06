fe1c1d7b27b6aa3284542a3b7406bcffff606617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:05 +0000

    Add package becke/0.0.3/becke-0.0.3.tar.gz

---
+++ packages/becke/becke-0.0.3.tar.gz/becke-0.0.3/src/becke/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2019, Derrick Yang
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from becke.becke import compute_select_becke, compute_becke_weights
