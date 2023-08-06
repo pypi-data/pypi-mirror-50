47cb41076501c6a4d8d146c40ef7f315ff805844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:55 +0000

    Add package lektor-chameleon/0.5.1/lektor-chameleon-0.5.1.tar.gz

---
+++ packages/lektor-chameleon/lektor-chameleon-0.5.1.tar.gz/lektor-chameleon-0.5.1/README.rst
@@ -0,0 +1,59 @@
+|pypi| |license|
+
+.. |pypi| image:: https://img.shields.io/pypi/v/lektor-chameleon.svg?style=flat-square
+    :target: https://pypi.org/project/lektor-chameleon/
+    :alt: PyPI version.
+
+.. |license| image:: https://img.shields.io/pypi/l/lektor-chameleon.svg?style=flat-square
+    :target: https://github.com/uyar/lektor-chameleon/blob/master/LICENSE.txt
+    :alt: Project license.
+
+lektor-chameleon is a plugin for the `Lektor <https://www.getlektor.com>`_
+static site generator that makes is possible to write the templates using
+the `Chameleon <https://chameleon.readthedocs.io/>`_ template engine.
+
+To use the plugin, add it to your project::
+
+  lektor plugin add lektor-chameleon
+
+Since the plugin modifies the default environment, it requires
+that it will be explicitly enabled.
+To enable the plugin, create the file ``configs/chameleon.ini``
+and put the following lines into it::
+
+  [chameleon]
+  enabled = yes
+
+Usage examples:
+
+.. code-block:: html
+
+   <html lang="${this.alt}">
+
+   <h1 tal:content="this.title">Page title</h1>
+
+   <span tal:replace="bag('translate', this.alt, 'message')">message</span>
+
+Many Lektor and Jinja filters are available using the ``>>`` operator:
+
+.. code-block:: html
+
+   <a href="${'/' >> url}">Home page</a>
+
+   <link rel="stylesheet" href="${'/static/custom.css' >> asseturl}"/>
+
+Filters also accept parameters:
+
+.. code-block:: html
+
+   <a href="${'.' >> url(alt=this.alt)}">link text</a>
+
+But filter parameters must be given as keyword parameters:
+
+.. code-block:: html
+
+   <!-- incorrect -->
+   <div tal:replace="this.body >> indent(4)">Page body</div>
+
+   <!-- correct -->
+   <div tal:replace="this.body >> indent(width=4)">Page body</div>
---
+++ packages/lektor-chameleon/lektor-chameleon-0.5.1.tar.gz/lektor-chameleon-0.5.1/lektor_chameleon.py
@@ -0,0 +1,98 @@
+# Copyright (C) 2018-2019 H. Turgut Uyar <uyar@tekir.org>
+#
+# lektor-chameleon is released under the BSD license. Read the included
+# LICENSE.txt file for details.
+
+import warnings
+from functools import partial
+
+from chameleon import PageTemplateLoader
+from chameleon.loader import TemplateLoader
+from lektor.context import get_ctx
+from lektor.pluginsystem import Plugin, get_plugin
+from markupsafe import Markup
+
+
+_CONTEXT_FILTERS = {"url", "asseturl", "markdown"}
+_STR_FILTERS = {
+    "capitalize",
+    "center",
+    "indent",
+    "length",
+    "lower",
+    "replace",
+    "title",
+    "trim",
+    "truncate",
+    "upper",
+    "wordcount",
+    "wordwrap",
+}
+_JINJA_ENV_FILTERS = {"attr", "replace", "truncate", "wordwrap"}
+
+
+class Filter:
+    def __init__(self, name, func):
+        self.name = name
+        self.func = func
+        self.ctx = self.name in _CONTEXT_FILTERS
+        self.str = self.name in _STR_FILTERS
+
+    def __call__(self, *args, **kwargs):
+        return Filter(self.name, partial(self.func, *args, **kwargs))
+
+    def __rrshift__(self, x):
+        markup = self.str and hasattr(x, "html")
+        p = x.html if markup else x
+        t = self.func(p)
+        y = t.unescape() if hasattr(t, "unescape") else t
+        return Markup(y) if markup else y
+
+
+chameleon_load = TemplateLoader.load
+
+
+def load_template(self, filename, *args, **kwargs):
+    ctx = get_ctx()
+    template = chameleon_load(self, filename, *args, **kwargs)
+    ctx.record_dependency(template.filename)
+    return template
+
+
+def render_template(self, name, pad=None, this=None, values=None, alt=None):
+    if isinstance(name, list):
+        name = name[0]
+    ctx = self.make_default_tmpl_values(pad, this, values, alt, template=name)
+    ctx.update(self.jinja_env.globals)
+    for f_name, f_filter in self.chameleon_filters.items():
+        ctx[f_name] = f_filter(ctx) if f_filter.ctx else f_filter
+    template = self.chameleon_loader.load(name)
+    return template(**ctx)
+
+
+class ChameleonPlugin(Plugin):
+    name = "chameleon"
+    description = "Chameleon support for templating"
+
+    def __init__(self, *args, **kwargs):
+        Plugin.__init__(self, *args, **kwargs)
+
+        config = self.get_config()
+        self.enabled = config.get_bool("chameleon.enabled", False)
+
+        if not self.enabled:
+            warnings.warn("Chameleon plugin is installed but not enabled.")
+        else:
+            TemplateLoader.load = load_template
+
+    def on_setup_env(self, **extra):
+        template_paths = self.env.jinja_env.loader.searchpath
+        self.env.chameleon_loader = PageTemplateLoader(template_paths, auto_reload=True)
+
+        filters = {n: Filter(n, f) for n, f in self.env.jinja_env.filters.items()}
+        for f_name in _JINJA_ENV_FILTERS:
+            filters[f_name] = filters[f_name](self.env.jinja_env)
+        self.env.chameleon_filters = filters
+
+        if self.enabled:
+            self.env.__class__.render_template = render_template
