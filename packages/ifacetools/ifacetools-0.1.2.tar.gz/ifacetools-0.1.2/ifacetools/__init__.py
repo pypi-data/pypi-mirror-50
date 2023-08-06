6d5df967a60aabb05e74d00e831517bb13c5a2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:19 +0000

    Add package ifacetools/0.1.2/ifacetools-0.1.2.tar.gz

---
+++ packages/ifacetools/ifacetools-0.1.2.tar.gz/ifacetools-0.1.2/ifacetools/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/8 19:33
+# @Author  : Vincent
+# @Email   : Vincent@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+from .httputil import HTTPUtil
+from .jsonutil import JSONUtil
+from .redisutil import RedisUtil
+from .time import TimeUtil
+from .randomutil import myrandom
+from .randomutil import identity
+
+__all__=["HTTPUtil","JSONUtil","RedisUtil","TimeUtil","myrandom","identity"]
