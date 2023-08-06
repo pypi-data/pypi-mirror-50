f42ac285ce643b4bd84393a4a7c33d492a3a11b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:37 +0000

    Add package algorthm/0.3/algorthm-0.3.tar.gz

---
+++ packages/algorthm/algorthm-0.3.tar.gz/algorthm-0.3/algorthm/search/__init__.py
@@ -0,0 +1,18 @@
+
+import sys
+
+
+sys.path.append('../algorthm')
+
+from algorthm.search.linearSearch import linearSearch
+
+
+# arr = os.listdir()
+
+# for item in arr :
+#    if item.endswith(".py") and item != "__init__.py":
+#       code = "from algorthm.sort."+str(item[:-3])+" import "+str(item[:-3])
+#      print (code)
+#     exec(code)
+
+        
---
+++ packages/algorthm/algorthm-0.3.tar.gz/algorthm-0.3/algorthm/sort/__init__.py
@@ -0,0 +1,20 @@
+
+import sys
+
+
+sys.path.append('../algorthm')
+
+from algorthm.sort.bubbleSort import bubbleSort
+from algorthm.sort.insertionSort import insertionSort
+from algorthm.sort.mergeSort import mergeSort
+        
+
+# arr = os.listdir()
+
+# for item in arr :
+#    if item.endswith(".py") and item != "__init__.py":
+#       code = "from algorthm.sort."+str(item[:-3])+" import "+str(item[:-3])
+#      print (code)
+#     exec(code)
+
+        
