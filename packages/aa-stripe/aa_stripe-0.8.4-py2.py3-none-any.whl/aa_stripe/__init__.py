555d3caac5ac62c4f742eef3b04b250b32a7d3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:31 +0000

    Add package aa-stripe/0.8.4/aa_stripe-0.8.4-py2.py3-none-any.whl

---
+++ packages/aa-stripe/aa_stripe-0.8.4-py2.py3-none-any.whl/aa_stripe/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__title__ = "Arabella Stripe"
+__version__ = "0.8.4"
+__author__ = "Jacek Ostanski"
+__license__ = "MIT"
+__copyright__ = "Copyright 2019 Arabella"
+
+# Version synonym
+VERSION = __version__
