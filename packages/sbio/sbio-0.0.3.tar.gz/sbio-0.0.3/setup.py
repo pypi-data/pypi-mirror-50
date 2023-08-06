01c5f17c8077a35744afc24bdf6e555422dd4a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:52 +0000

    Add package sbio/0.0.3/sbio-0.0.3.tar.gz

---
+++ packages/sbio/sbio-0.0.3.tar.gz/sbio-0.0.3/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sbio",
+    version="0.0.3",
+    author="Tod Stuber",
+    author_email="tod.p.stuber@usda.gov",
+    description="Collection of simple bioinformatic tools.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/USDA-VS/sbio",
+    packages=setuptools.find_packages(),
+    install_requires=['python>=3.6', 're', 'pandas', 'numpy', 'concurrent', 'collections', 'gzip', 'argparse', 'textwrap', 'humanize'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+import os
+import re
+import pandas as pd
+import numpy as np
+pd.set_option("display.max_colwidth", 800) #prevents truncated reads
+from concurrent import futures
+from collections import namedtuple 
+import gzip
+import argparse
+import textwrap
+import humanize
