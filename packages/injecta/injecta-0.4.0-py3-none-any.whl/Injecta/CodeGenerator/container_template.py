086cbfbbbb878cfa6010acf5fc8ee9ea89c1b633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:49 +0000

    Add package injecta/0.4.0/injecta-0.4.0-py3-none-any.whl

---
+++ packages/injecta/injecta-0.4.0-py3-none-any.whl/Injecta/CodeGenerator/container_template.py
@@ -0,0 +1,40 @@
+from box import Box
+import os
+from Injecta.ContainerInterface import ContainerInterface
+from Injecta.CodeGenerator.ServiceMethodNameTranslator import ServiceMethodNameTranslator
+from Injecta.Service.DIService import DIService
+
+class Container(ContainerInterface):
+
+    def __init__(self, parameters: Box):
+        self.__parameters = parameters
+        self.services = {}
+        self.__serviceMethodNameTranslator = ServiceMethodNameTranslator()
+        self.__tags2Services = self.__generateTags2Services()
+
+    def getConfig(self) -> Box:
+        return self.__parameters
+
+    def get(self, name):
+        if hasattr(name, '__module__'):
+            moduleName = name.__module__
+            className = name.__name__
+            classNameFromModule = moduleName[moduleName.rfind('.'):]
+
+            if classNameFromModule == className:
+                name = moduleName + '.' + className
+            else:
+                name = name.__module__
+
+        methodName = self.__serviceMethodNameTranslator.translate(name)
+        method = getattr(self, '_Container{}'.format(methodName))
+
+        return method()
+
+    def getByTag(self, tag: str):
+        if tag not in self.__tags2Services:
+            raise Exception('No service with tag {}'.format(tag))
+
+        serviceNames = self.__tags2Services[tag]
+
+        return list(map(lambda serviceName: self.get(serviceName), serviceNames))
