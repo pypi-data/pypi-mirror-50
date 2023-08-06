fb1f545c2888f1ccdbc54533d67a20ca4651d0c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:46 +0000

    Add package suanpan/0.9.56/suanpan-0.9.56.tar.gz

---
+++ packages/suanpan/suanpan-0.9.56.tar.gz/suanpan-0.9.56/suanpan/arguments/auto.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan.dw import dw
+from suanpan.node import node
+from suanpan.proxy import Proxy
+from suanpan.storage import storage
+
+
+class AutoArg(Proxy):
+    TYPE_KEY = "argtype"
+    DEFAULT_TYPE = "params"
+
+    def _setBackend(self, BackendClass, *args, **kwargs):
+        kwargs.pop(self.TYPE_KEY)
+        return super(AutoArg, self)._setBackend(BackendClass, *args, **kwargs)
+
+
+class DataFrame(AutoArg):
+    MAPPING = {"data": storage.Csv, "table": dw.Table}
+
+    def __init__(self, key, table, partition, *args, **kwargs):
+        port = node.get(table) or node.get(key)
+        if not port:
+            raise Exception(
+                "No such {}: {}".format(self.name, ",".join([key, table, partition]))
+            )
+        kwargs.setdefault(self.TYPE_KEY, port.type)
+        kwargs.update(key=key, table=table, partition=partition)
+        super(DataFrame, self).__init__(*args, **kwargs)
