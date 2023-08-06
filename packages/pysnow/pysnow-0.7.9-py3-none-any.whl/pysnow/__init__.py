fa65e31c2907a7a91a01460c5b11d8e48dd082e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:51 +0000

    Add package pysnow/0.7.9/pysnow-0.7.9-py3-none-any.whl

---
+++ packages/pysnow/pysnow-0.7.9-py3-none-any.whl/pysnow/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from .client import Client
+from .oauth_client import OAuthClient
+from .query_builder import QueryBuilder
+from .resource import Resource
+from .params_builder import ParamsBuilder
+
+__author__ = "Robert Wikman <rbw@vault13.org>"
+__version__ = "0.7.9"
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:  # pragma: no cover
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
