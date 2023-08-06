458a3d6e46fe0a0a0a06c180eb1ff45ed508a9ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:08 +0000

    Add package LibraryApp/2.0.2/LibraryApp-2.0.2.tar.gz

---
+++ packages/LibraryApp/LibraryApp-2.0.2.tar.gz/LibraryApp-2.0.2/catalog/apps.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+
+# include any application configuration for the app.
+# Using this, you can configure
+# some of the attributes of the application.
+
+# Application configuration objects store metadata for an application.
+# Some attributes
+# can be configured in AppConfig subclasses.
+
+
+class CatalogConfig(AppConfig):
+    name = 'catalog'
