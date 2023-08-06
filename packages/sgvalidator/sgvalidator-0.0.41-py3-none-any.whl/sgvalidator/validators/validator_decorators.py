9053dba8aae7f03e63c57fbee1c964a9004aa6e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:17 +0000

    Add package sgvalidator/0.0.41/sgvalidator-0.0.41-py3-none-any.whl

---
+++ packages/sgvalidator/sgvalidator-0.0.41-py3-none-any.whl/sgvalidator/validators/validator_decorators.py
@@ -0,0 +1,15 @@
+from .validator_utils import ValidatorUtils
+from ..color_printer import ColorPrinter
+from .validator_status import ValidatorStatus
+
+
+def ignorable(decorated):
+    def wrapper(self):
+        if self.classesToIgnore and self.className() in self.classesToIgnore:
+            ColorPrinter.printRedBanner("!!!!!!! Ignoring {} !!!!!!!".format(self.className()))
+            return
+        status = decorated(self)
+        if status == ValidatorStatus.FAIL:
+            ValidatorUtils.printIgnoreMessage(self.className())
+        return status
+    return wrapper
