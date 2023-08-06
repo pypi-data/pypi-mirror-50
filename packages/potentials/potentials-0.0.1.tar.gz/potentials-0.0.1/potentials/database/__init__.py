e8be75aa82c8a648b808d298eedd2a8c75ae4599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:46 +0000

    Add package potentials/0.0.1/potentials-0.0.1.tar.gz

---
+++ packages/potentials/potentials-0.0.1.tar.gz/potentials-0.0.1/potentials/database/__init__.py
@@ -0,0 +1,8 @@
+from .Citation import Citation
+from .Artifact import Artifact
+from .Potential import Potential
+from .WebLink import WebLink
+from .Potential import Potential
+from .Implementation import Implementation
+
+from .Database import Database
---
+++ packages/potentials/potentials-0.0.1.tar.gz/potentials-0.0.1/potentials/tools/__init__.py
@@ -0,0 +1,3 @@
+from .aslist import iaslist, aslist
+from .parse_authors import parse_authors
+__all__ = ['aslist', 'iaslist', 'parse_authors']
