0946e0cd6fa21f73e3e2be90800912c316278a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:34 +0000

    Add package DiscoveryDV/0.1/DiscoveryDV-0.1.tar.gz

---
+++ packages/DiscoveryDV/DiscoveryDV-0.1.tar.gz/DiscoveryDV-0.1/DiscoveryDV/__init__.py
@@ -0,0 +1,10 @@
+# --------------------------------------------------------------------------
+# Company:      DecisionVis LLC
+# Description:  DiscoveryDV/__init__.py
+# Author:       Joshua Kollat
+# Created:      8/9/2019
+# Copyright:    (c) 2012-2019
+# License:      See DiscoveryDV Licensing
+# --------------------------------------------------------------------------
+
+name = "DiscoveryDV"
