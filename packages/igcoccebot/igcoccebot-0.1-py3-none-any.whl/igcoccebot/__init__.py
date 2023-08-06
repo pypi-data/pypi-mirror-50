129ca692e03f59ef3c3ac44ea005ba3ed724e6f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:05 +0000

    Add package igcoccebot/0.1/igcoccebot-0.1-py3-none-any.whl

---
+++ packages/igcoccebot/igcoccebot-0.1-py3-none-any.whl/igcoccebot/__init__.py
@@ -0,0 +1,7 @@
+from igcoccebot.IGCocceBot import IGCocceBot
+from igcoccebot.IGNavigation import IGNavigation
+from igcoccebot.IGData import IGData
+from igcoccebot.IGAction import IGAction
+from igcoccebot.IGAlgo import IGAlgo
+from igcoccebot.tools import *
+__version__ = '0.1'
