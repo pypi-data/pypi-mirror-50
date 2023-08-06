ea254c65ceef8a2a830eb335f108c045a7905c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:15 +0000

    Add package Akuanduba/0.1.4/Akuanduba-0.1.4.tar.gz

---
+++ packages/Akuanduba/Akuanduba-0.1.4.tar.gz/Akuanduba-0.1.4/Akuanduba/core/constants.py
@@ -0,0 +1,13 @@
+# Time constants
+Second = 1.
+Minute = 60 * Second
+Hour = 60 * Minute
+Day = 24 * Hour
+Week = 7 * Day
+Year = 365.25 * Day
+
+# CoreClasses constants
+MAX_FIFO = 100000
+
+# StoreGateSvc constants
+relative_save_dir = 'saves/'
