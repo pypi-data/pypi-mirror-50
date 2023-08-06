a477023ea6fd41009fffde2664526a02ec132b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:57 +0000

    Add package bond-home/0.0.2/bond-home-0.0.2.tar.gz

---
+++ packages/bond-home/bond-home-0.0.2.tar.gz/bond-home-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='bond-home',
+      version='0.0.2',
+      description='Python library for controlling BOND Home Hub',
+      url='http://github.com/nguyer/bond-home',
+      author='Nicko Guyer',
+      author_email='nguyer@gmail.com',
+      license='MIT',
+      packages=['bond'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
