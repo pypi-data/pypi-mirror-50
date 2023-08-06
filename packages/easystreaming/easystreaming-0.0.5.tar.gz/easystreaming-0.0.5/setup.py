60c3b7438a4cd63476a79659e7e25af55f57e0f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:28 +0000

    Add package easystreaming/0.0.5/easystreaming-0.0.5.tar.gz

---
+++ packages/easystreaming/easystreaming-0.0.5.tar.gz/easystreaming-0.0.5/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easystreaming",
+    version="0.0.5",
+    author="Jakob Kirsch",
+    author_email="jakob.kirsch@teckids.org",
+    description="A small Server and Client creator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://edugit.org/jakobkir/simplestreaming",
+    packages=["easystreaming"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
