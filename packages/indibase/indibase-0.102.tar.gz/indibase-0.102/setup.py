3c08b5727af15881c23064a135f6da66a5813964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:58 +0000

    Add package indibase/0.102/indibase-0.102.tar.gz

---
+++ packages/indibase/indibase-0.102.tar.gz/indibase-0.102/setup.py
@@ -0,0 +1,22 @@
+# from distutils.core import setup
+from setuptools import setup
+from indibase.indiBase import Client
+
+setup(
+    name='indibase',
+    version=Client.version,
+    packages=[
+        'indibase',
+    ],
+    python_requires='>=3.7.2',
+    install_requires=[
+        'PyQt5>=5.13',
+        'numpy==1.17',
+    ],
+    url='https://github.com/mworion/indibase',
+    license='APL 2.0',
+    author='mw',
+    author_email='michael@wuertenberger.org',
+    description='indi base client in python based on qt',
+    zip_safe=True,
+)
