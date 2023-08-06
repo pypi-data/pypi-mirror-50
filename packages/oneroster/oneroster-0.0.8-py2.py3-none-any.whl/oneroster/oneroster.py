a5b7618eeaf9f2ff071cc818ba51b1501aa6beae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:16 +0000

    Add package oneroster/0.0.8/oneroster-0.0.8-py2.py3-none-any.whl

---
+++ packages/oneroster/oneroster-0.0.8-py2.py3-none-any.whl/oneroster/oneroster.py
@@ -0,0 +1,16 @@
+
+
+from .classlink import ClasslinkConnector
+from .clever import CleverConnector
+
+
+def get_connector(options):
+    platform = options['platform']
+    if platform == 'classlink':
+        return ClasslinkConnector(options)
+    elif platform == 'clever':
+        return CleverConnector(options)
+
+    raise NotImplementedError("No module for " + platform +
+                              " was found. Supported are: [classlink, clever]")
+
