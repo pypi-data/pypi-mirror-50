e27a8c6c05a5c4de17249012141e56fa61da69c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:41 +0000

    Add package lib-bgp-data/0.2.2/lib_bgp_data-0.2.2-py3-none-any.whl

---
+++ packages/lib-bgp-data/lib_bgp_data-0.2.2-py3-none-any.whl/lib_bgp_data/rovpp/rovpp_statistics.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""This module contains class ROVPP_Statistics"""
+
+# Include something about the assumption that if an ann 
+# has a hijack its hijacked and larger proof that if ann has
+# a hijack then its hijacked unless there is a data plane mechanism
+# That doesnt exist in the control plane
+
+__author__ = "Justin Furuness"
+__credits__ = ["Justin Furuness", "Reynaldo"]
+__Lisence__ = "MIT"
+__maintainer__ = "Justin Furuness"
+__email__ = "jfuruness@gmail.com"
+__status__ = "Development"
+
+from pprint import pprint
+from .enums import Policies, Non_BGP_Policies, AS_Type, Planes, Conditions
+from ..utils import error_catcher, utils
+from .rovpp_data_plane_statistics import ROVPP_Data_Plane_Statistics
+from .rovpp_control_plane_statistics import ROVPP_Control_Plane_Statistics
+
+class ROVPP_Statistics_Calculator:
+    """This class simulates ROVPP.
+
+    In depth explanation at the top of the file
+    """
+
+    __slots__ = ['path', 'csv_dir', 'logger', 'start_time', 'stats',
+                 'subprefix_hijack']
+
+    @error_catcher()
+    def __init__(self, args={}):
+        """Initializes logger and path variables."""
+
+        # Sets path vars, logger, config, etc
+        utils.set_common_init_args(self, args)
+
+        # I know this could be in a dict comp but ARE YOU NUTS???
+        self.stats = dict()
+        for non_bgp_policy in Non_BGP_Policies.__members__.values():
+            self.stats[non_bgp_policy.value] = dict()
+            for percent in args.get("percents"):
+                self.stats[non_bgp_policy.value][percent] = dict()
+                sim = self.stats[non_bgp_policy.value][percent]
+                # For each policy
+                for policy in Policies.__members__.values():
+                    # Set that policy to have a dict which will contain data planes
+                    sim[policy.value] = dict()
+                    # For storing the total number of ases
+                    # For each kind of data plane
+                    for plane in Planes.__members__.values():
+                        sim[policy.value][plane.value] = dict()
+                        # For each kind of condition
+                        for cond in Conditions.__members__.values():
+                            # Sets rates to be a list for multiple attacks
+                            sim[policy.value][plane.value][cond.value] = []
+
+
+########################
+### Helper Functions ###
+########################
+
+    @error_catcher()
+    def calculate_stats(self, as_table, subprefix_hijack, percentage, policy):
+        """Calculates success rates"""
+
+        # Appends a zero onto all stats
+        self._initialize_attack_stats(percentage, policy)
+        ases_dict = self._get_ases(as_table,
+                                   subprefix_hijack["more_specific_prefix"])
+
+        # Needed for shorter lines
+        # It's a reference so it's cool alright? Geez chill
+        sim = self.stats[policy][percentage]
+        hijacked_ases = set(ases_dict[AS_Type.RECIEVED_HIJACK.value].keys())
+
+        self._update_ases_recieved_hijack_stats(sim, hijacked_ases, ases_dict)
+
+        blackholed_asns = self._get_blackhole_asns(as_table)
+
+        ROVPP_Control_Plane_Statistics().calculate_not_received_hijack_stats(
+            as_table, sim, ases_dict, blackholed_asns)
+
+        ROVPP_Data_Plane_Statistics().calculate_not_recieved_hijack_stats(
+            ases_dict, subprefix_hijack["victim"], hijacked_ases, sim, blackholed_asns)
+        self.logger.debug(sim)
+
+    def _update_ases_recieved_hijack_stats(self, sim, hijacked, ases_dict):
+        """Updates stats for all ASes that recieved the hijack"""
+
+        # THIS SHOULD BE OPTIMIZED - THIS SHOULD BE A COUNT QUERY TO POSTGRES
+        # NOT A FOR LOOP IN PYTHON!!!
+
+        # If the AS has recieved the hijack:
+        for asn in hijacked:
+            # Increase control plane and data plane hijacked
+            for plane in Planes.__members__.values():
+                # Add one to the statistic
+                as_info = ases_dict[AS_Type.RECIEVED_HIJACK.value][asn]
+                as_type = as_info["as_type"]
+                sim[as_type][plane.value][Conditions.HIJACKED.value][-1] += 1
+                #if plane.value == Planes.DATA_PLANE.value:
+                as_info["data_plane_conditions"].add(Conditions.HIJACKED.value)
+
+    @error_catcher()
+    def _initialize_attack_stats(self, percentage, policy):
+        """Defaults the next statistic to be 0 for everything"""
+
+        sim = self.stats[policy][percentage]
+        #I know this could be in a dict comp but ARE YOU NUTS???
+        for policy in Policies.__members__.values():
+            for plane in Planes.__members__.values():
+                for cond in Conditions.__members__.values():
+                    sim[policy.value][plane.value][cond.value].append(0)
+
+    @error_catcher()
+    def _get_ases(self, as_table, more_specific_prefix):
+
+        # Gets all asns that where hijacked
+        sql = """SELECT exr.asn, exr.prefix, exr.origin, exr.received_from_asn,
+                 ases.as_type FROM rovpp_extrapolation_results exr
+                 INNER JOIN rovpp_ases ases
+                 ON ases.asn = exr.asn
+                 AND exr.prefix = %s;"""
+        data = [more_specific_prefix]
+        ases = {AS_Type.RECIEVED_HIJACK.value: {x["asn"]: x for x in
+                                         as_table.execute(sql, data)}}
+
+
+        # Get's all asns that do not have a corresponding entry for a hijack
+        sql = """SELECT exr.asn, exr.prefix, exr.origin, exr.received_from_asn,
+                 ases.as_type FROM rovpp_extrapolation_results exr
+                 LEFT JOIN (SELECT * FROM rovpp_extrapolation_results exr_og
+                            WHERE exr_og.prefix = %s) exr_hijacked
+                           ON exr.asn = exr_hijacked.asn
+                 INNER JOIN rovpp_ases ases ON ases.asn = exr.asn
+                 WHERE exr_hijacked.asn IS NULL"""
+
+        ases[AS_Type.NOT_RECIEVED_HIJACK.value] = {x["asn"]: x for x in 
+                                             as_table.execute(sql, data)}
+
+        # Inits data plane onditions to be none
+        for recieved_or_not in ases.keys():
+            for asn in ases[recieved_or_not].keys():
+                ases[recieved_or_not][asn]["data_plane_conditions"] = set()
+
+        return ases
+
+    def _get_blackhole_asns(self, as_table):
+        """Gets all blackhole asns"""
+
+        return set([x["asn"]
+                for x in as_table.execute("SELECT * FROM rovpp_blackholes;")])
---
+++ packages/lib-bgp-data/lib_bgp_data-0.2.2-py3-none-any.whl/lib_bgp_data/rovpp/tables.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""This module contains ROVPP_ASes_Table and Subprefix_Hijack_Temp_Table
+
+These two classes inherits from the Database class. The Database class
+does allow for the conection to a database upon initialization. Also
+upon initialization the _create_tables function is called to initialize
+any tables if they do not yet exist. Beyond that the class can clear the
+table, create an index, and has the name and columns properties that are
+used in utils function to insert CSVs into the database. This class does
+not contain an index creation function, because it would only ever be
+used when combining with the roas table, which does a parallel seq_scan,
+thus any indexes are not used since they are not efficient. Each table
+follows the table name followed by a _Table since it inherits from the
+database class.
+
+Possible future improvements:
+    -Add test cases
+"""
+
+from random import sample
+from ..utils import Database, error_catcher
+
+__author__ = "Justin Furuness"
+__credits__ = ["Justin Furuness"]
+__Lisence__ = "MIT"
+__maintainer__ = "Justin Furuness"
+__email__ = "jfuruness@gmail.com"
+__status__ = "Development"
+
+
+class ROVPP_ASes_Table(Database):
+    """Class with database functionality.
+
+    In depth explanation at the top of the file."""
+
+    __slots__ = []
+
+    @error_catcher()
+    def _create_tables(self):
+        """Creates tables if they do not exist.
+
+        Called during initialization of the database class.
+        """
+
+        sql = """CREATE UNLOGGED TABLE IF NOT EXISTS rovpp_ases (
+                 asn bigint,
+                 as_type text
+                 );"""
+        self.cursor.execute(sql)
+
+    @error_catcher()
+    def clear_table(self):
+        """Clears the rovpp_ases table.
+
+        Should be called at the start of every run.
+        """
+
+        self.logger.info("Dropping ROVPP_ASes")
+        self.cursor.execute("DROP TABLE IF EXISTS rovpp_ases")
+        self.logger.info("ROVPP_ASes Table dropped")
+
+    @error_catcher()
+    def fill_table(self):
+        self.clear_table()
+        sql = """CREATE UNLOGGED TABLE IF NOT EXISTS rovpp_ases AS (
+                 SELECT customer_as AS asn, 'bgp' AS as_type FROM (
+                     SELECT DISTINCT customer_as FROM rovpp_customer_providers
+                     UNION SELECT provider_as FROM rovpp_customer_providers
+                     UNION SELECT peer_as_1 FROM rovpp_peers
+                     UNION SELECT peer_as_2 FROM rovpp_peers) union_temp
+                 );"""
+        self.cursor.execute(sql)
+
+
+    @error_catcher()
+    def change_routing_policies(self, asns, policy):
+        """Changes routing policies to policy for a list of asns"""
+
+        self._create_tables()
+        sql = "UPDATE rovpp_ases SET as_type = %s WHERE asn = %s"
+        # Should this be a bulk update? Yes. Does it matter? Do it later.
+        for asn in asns:
+            self.cursor.execute(sql, [policy, asn])
+
+    @error_catcher()
+    def get_all(self):
+        """Gets everything, for convenience only"""
+
+        return self.execute("SELECT * FROM rovpp_ases;")
+
+class ROVPP_MRT_Announcements_Table(Database):
+    """Class with database functionality.
+
+    In depth explanation at the top of the file."""
+
+    __slots__ = ['attacker_asn', 'attacker_prefix', 'victim_asn',
+                 'victim_prefix']
+
+    @error_catcher()
+    def _create_tables(self):
+        """Creates tables if they do not exist.
+
+        Called during initialization of the database class.
+        """
+
+        self.clear_table()
+        sql = """CREATE UNLOGGED TABLE IF NOT EXISTS
+                 rovpp_mrt_announcements (
+                 origin bigint,
+                 as_path bigint ARRAY,
+                 prefix CIDR
+                 );"""
+        self.cursor.execute(sql)
+
+    @error_catcher()
+    def clear_table(self):
+        """Clears the rovpp_ases table.
+
+        Should be called at the start of every run.
+        """
+
+        self.logger.info("Dropping ROVPP_MRT_Announcements")
+        self.cursor.execute("DELETE FROM rovpp_mrt_announcements")
+        self.logger.info("ROVPP_MRT_Announcements Table dropped")
+
+    @error_catcher()
+    def populate_mrt_announcements(self, subprefix_hijack):
+        """Populates the mrt announcements table"""
+
+        sql = """INSERT INTO rovpp_mrt_announcements(
+              origin, as_path, prefix) VALUES
+              (%s, %s, %s)"""
+        attacker_data = [subprefix_hijack["attacker"],
+                         [subprefix_hijack["attacker"]],
+                         subprefix_hijack["more_specific_prefix"]]
+        victim_data = [subprefix_hijack["victim"],
+                       [subprefix_hijack["victim"]],
+                       subprefix_hijack["expected_prefix"]]
+        for data in [attacker_data, victim_data]:
+            self.cursor.execute(sql, data)
+
+class Subprefix_Hijack_Temp_Table(Database):
+    """Class with database functionality.
+
+    THIS SHOULD BE DELETED LATER AND ADDED INTO BGPSTREAM.COM CLASS!!!
+    THIS IS JUST FOR FAKE DATA!!!!
+
+    In depth explanation at the top of the file."""
+
+    __slots__ = []
+
+    @error_catcher()
+    def _create_tables(self):
+        """Creates tables if they do not exist.
+
+        Called during initialization of the database class.
+        """
+
+        self.clear_table()
+        sql = """CREATE UNLOGGED TABLE subprefix_hijack_temp(
+                 more_specific_prefix CIDR,
+                 attacker bigint,
+                 url varchar(200),
+                 expected_prefix CIDR,
+                 victim bigint
+                 );"""
+        self.cursor.execute(sql)
+
+    @error_catcher()
+    def clear_table(self):
+        """Clears the rovpp_ases table.
+
+        Should be called at the start of every run.
+        """
+
+        self.logger.info("Dropping Subprefix Hijacks")
+        self.cursor.execute("DROP TABLE IF EXISTS subprefix_hijack_temp;")
+        self.logger.info("Subprefix Hijacks Table dropped")
+
+    @error_catcher()
+    def populate(self, ases):
+        """Populates table with fake data"""
+
+        # Gets two random ases without duplicates
+        attacker, victim = sample(ases, k=2)
+        sql = """INSERT INTO subprefix_hijack_temp(
+              more_specific_prefix, attacker, expected_prefix, victim) VALUES
+              (%s, %s, %s, %s)"""
+        data = ['1.2.3.0/24',  # more_specfic_prefix
+                attacker,  # Random attacker
+                '1.2.0.0/16',  # expected_prefix
+                victim]
+        self.cursor.execute(sql, data)
