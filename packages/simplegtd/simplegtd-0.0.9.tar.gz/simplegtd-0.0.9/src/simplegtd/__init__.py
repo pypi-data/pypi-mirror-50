fc1e370ab770c2392171063b003403f3d4eac71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:21 +0000

    Add package simplegtd/0.0.9/simplegtd-0.0.9.tar.gz

---
+++ packages/simplegtd/simplegtd-0.0.9.tar.gz/simplegtd-0.0.9/src/simplegtd/__init__.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python3
+
+__version__ = "0.0.9"
+
+
+import collections
+import hashlib
+import os
+
+import gi
+gi.require_version('Gdk', '3.0')
+gi.require_version('Gtk', '3.0')
+from gi.repository import GObject, GLib, Gdk, Gtk, Gio
+
+import simplegtd.todotxt
+import simplegtd.mainwindow
+import simplegtd.resource
+
+
+def hash_path(filename):
+    h = hashlib.md5()
+    h.update(filename.encode("utf-8", "ignore"))
+    h = h.hexdigest()
+    return h
+
+
+class _SimpleGTDAppState(object):
+
+    todo_txts = None
+    __app_state_file = None
+
+    def __init__(self, filename):
+        self.__app_state_file = filename
+        f = GLib.KeyFile.new()
+        try:
+            f.load_from_file(filename, GLib.KeyFileFlags.NONE)
+            self.todo_txts = f.get_string_list("general", "todo_txts")
+        except GLib.Error:
+            pass
+        return self
+
+    def persist_app_state(self):
+        f = GLib.KeyFile.new()
+        try:
+            f.load_from_file(self.__app_state_file, GLib.KeyFileFlags.NONE)
+        except GLib.Error:
+            pass
+        if self.todo_txts is not None:
+            f.set_string_list("general", "todo_txts", self.todo_txts)
+        f.save_to_file(self.__app_state_file)
+
+
+class SimpleGTD(Gtk.Application, _SimpleGTDAppState):
+
+    def __init__(self):
+        Gtk.Application.__init__(self)
+        self.config_dir = simplegtd.resource.config_dir()
+        self.data_dir = simplegtd.resource.data_home()
+        for d in self.config_dir, self.data_dir:
+            if not os.path.isdir(d):
+                os.makedirs(d)
+
+        settings_file = os.path.join(self.config_dir, "app-settings")
+        _SimpleGTDAppState.__init__(self, settings_file)
+        self.default_todo_txt = os.path.join(self.data_dir, "todo.txt")
+        if not self.todo_txts:
+            self.todo_txts = [self.default_todo_txt]
+
+        self.connect("activate", lambda _: [self.add_new_todo_window(x) for x in self.todo_txts] and None)
+        self.connect("window-removed", self.todo_window_removed)
+        self.models_to_windows = {}
+
+    def delegate_open_file_activated(self, requestor):
+        choosefile_dialog = Gtk.FileChooserDialog(
+            title="Select an existing TODO.TXT file or create a new one",
+            parent=requestor,
+            action=Gtk.FileChooserAction.SAVE,
+            buttons=(
+                Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                Gtk.STOCK_OPEN, Gtk.ResponseType.OK,
+                "Use default", Gtk.ResponseType.NONE,
+            )
+        )
+        response = choosefile_dialog.run()
+        choosefile_dialog.hide()
+        if response == Gtk.ResponseType.OK:
+            self.add_new_todo_window(choosefile_dialog.get_filename())
+        elif response == Gtk.ResponseType.NONE:
+            self.add_new_todo_window(self.default_todo_txt)
+        choosefile_dialog.destroy()
+
+    def delegate_new_window_activated(self, requestor):
+        for data_file, (unused_model, windows) in self.models_to_windows.items():
+            if requestor in windows:
+                self.add_new_todo_window(data_file)
+
+    def delegate_exit_activated(self, unused_requestor):
+        self.quit()
+
+    def add_new_todo_window(self, data_file):
+        try:
+            if data_file in self.models_to_windows:
+                model = self.models_to_windows[data_file][0]
+            else:
+                model = simplegtd.todotxt.TodoTxt.from_file(data_file)
+            window = simplegtd.mainwindow.SimpleGTDMainWindow(model, os.path.join(self.config_dir, "window-state-" + hash_path(data_file)))
+            self.add_window(window)
+            if data_file not in self.models_to_windows:
+                self.models_to_windows[data_file] = (model, [])
+            self.models_to_windows[data_file][1].append(window)
+            window.connect("open-file-activated", self.delegate_open_file_activated)
+            window.connect("new-window-activated", self.delegate_new_window_activated)
+            window.connect("exit-activated", self.delegate_exit_activated)
+            window.connect("destroy", self.remove_window)
+            window.show_all()
+        except BaseException:
+            Gtk.main_quit()
+            raise
+
+    def todo_window_removed(self, unused_ref, window):
+        for data_file, (model, windows) in list(self.models_to_windows.items()):
+            if window in windows:
+                windows.remove(window)
+            if not windows:
+                model.close()
+                del self.models_to_windows[data_file]
+        if not self.models_to_windows:
+            self.quit()
+        else:
+            self.todo_txts = list(self.models_to_windows.keys())
+            self.persist_app_state()
+
+    def quit(self):
+        if self.models_to_windows:
+            # First, we disconnect our own todo_window_removed handler,
+            # since while quitting we do not want that handler to handle
+            # whittling down the window list.
+            self.disconnect_by_func(self.todo_window_removed)
+            # Now we persist everything.
+            self.todo_txts = list(self.models_to_windows.keys())
+            self.persist_app_state()
+            for model, windows in list(self.models_to_windows.values()):
+                for window in windows:
+                    window.destroy()
+            for model, windows in list(self.models_to_windows.values()):
+                model.close()
+        Gtk.main_quit()
+
+
+def main():
+    app = SimpleGTD()
+    GLib.idle_add(app.run)
+    Gtk.main()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/simplegtd/simplegtd-0.0.9.tar.gz/simplegtd-0.0.9/src/simplegtd/mainwindow.py
@@ -0,0 +1,135 @@
+import os
+
+import gi
+gi.require_version('Gdk', '3.0')
+gi.require_version('Gtk', '3.0')
+from gi.repository import GObject, GLib, Gdk, Gtk
+
+import simplegtd.rememberingwindow
+import simplegtd.filterlist
+import simplegtd.resource
+import simplegtd.views
+
+
+class SimpleGTDMainWindow(Gtk.ApplicationWindow, simplegtd.rememberingwindow.RememberingWindow):
+
+    __gsignals__ = {
+        'open-file-activated': (GObject.SIGNAL_RUN_LAST | GObject.SIGNAL_ACTION, None, ()),
+        'new-window-activated': (GObject.SIGNAL_RUN_LAST | GObject.SIGNAL_ACTION, None, ()),
+        'close-window-activated': (GObject.SIGNAL_RUN_LAST | GObject.SIGNAL_ACTION, None, ()),
+        'exit-activated': (GObject.SIGNAL_RUN_LAST | GObject.SIGNAL_ACTION, None, ()),
+        'help-activated': (GObject.SIGNAL_RUN_LAST | GObject.SIGNAL_ACTION, None, ()),
+        'text-filter-focus-request': (GObject.SIGNAL_RUN_LAST | GObject.SIGNAL_ACTION, None, ()),
+    }
+
+    def __init__(self, todotxt, window_state_file):
+        self.selection_filters = []
+        self.search_filters = []
+
+        Gtk.ApplicationWindow.__init__(self)
+        self.set_default_size(800, 600)
+        simplegtd.rememberingwindow.RememberingWindow.__init__(self, window_state_file)
+
+        accel_group = Gtk.AccelGroup()
+        self.add_accel_group(accel_group)
+
+        self.add_accelerator("close-window-activated", accel_group, ord('w'), Gdk.ModifierType.CONTROL_MASK, 0)
+        self.connect("close-window-activated", lambda _: self.destroy())
+
+        header_bar = Gtk.HeaderBar()
+        header_bar.set_property('expand', False)
+        if todotxt.name():
+            n = todotxt.name()
+            bn = os.path.basename(n)
+            dn = simplegtd.resource.strip_data_home(os.path.dirname(n))
+            if dn:
+                dn = simplegtd.resource.shorten_path(dn)
+                title = "%s (%s)" % (bn, dn)
+            else:
+                title = "%s" % bn
+            header_bar.set_title(title)
+            self.set_title(title)
+        else:
+            header_bar.set_title('Simple GTD (in memory)')
+            self.set_title('Simple GTD (in memory)')
+        # FIXME: use subtitle to list total and outstanding tasks, filtered and non-filtered
+        header_bar.set_subtitle(simplegtd.resource.shorten_path(todotxt.name() or "(no file)"))
+        header_bar.set_show_close_button(True)
+        self.set_titlebar(header_bar)
+
+        text_filter_entry = Gtk.SearchEntry()
+        text_filter_entry.set_placeholder_text("Search tasks...")
+        text_filter_entry.connect("search-changed", lambda entry: self.filter_text_changed(entry.get_text()))
+        text_filter_entry.connect("stop-search", lambda entry: entry.set_text("") or self.task_view.grab_focus())
+        text_filter_entry.connect("activate", lambda unused_entry: self.task_view.grab_focus())
+        self.add_accelerator("text-filter-focus-request", accel_group, ord('f'), Gdk.ModifierType.CONTROL_MASK, 0)
+        self.connect("text-filter-focus-request", lambda _: text_filter_entry.grab_focus())
+        header_bar.pack_start(text_filter_entry)
+
+        exit_button = Gtk.Button.new_from_icon_name("application-exit", Gtk.IconSize.LARGE_TOOLBAR)
+        exit_button.connect("clicked", lambda _: self.emit("exit-activated"))
+        self.add_accelerator("exit-activated", accel_group, ord('q'), Gdk.ModifierType.CONTROL_MASK, 0)
+        header_bar.pack_end(exit_button)
+
+        new_view_button = Gtk.Button.new_from_icon_name("window-new", Gtk.IconSize.LARGE_TOOLBAR)
+        new_view_button.connect("clicked", lambda _: self.emit("new-window-activated"))
+        self.add_accelerator("new-window-activated", accel_group, ord('n'), Gdk.ModifierType.CONTROL_MASK, 0)
+        header_bar.pack_end(new_view_button)
+
+        choosefile_button = Gtk.Button.new_from_icon_name("document-open", Gtk.IconSize.LARGE_TOOLBAR)
+        choosefile_button.connect("clicked", lambda _: self.emit("open-file-activated"))
+        self.add_accelerator("open-file-activated", accel_group, ord('o'), Gdk.ModifierType.CONTROL_MASK, 0)
+        header_bar.pack_end(choosefile_button)
+
+        help_button = Gtk.Button.new_from_icon_name("system-help", Gtk.IconSize.LARGE_TOOLBAR)
+        help_button.connect("clicked", lambda _: self.show_shortcuts_window())
+        self.add_accelerator("help-activated", accel_group, Gdk.KEY_F1, 0, 0)
+        self.connect("help-activated", lambda _: self.show_shortcuts_window())
+        header_bar.pack_end(help_button)
+
+        self.task_view = simplegtd.views.TaskView()
+        task_view_scroller = Gtk.ScrolledWindow()
+        task_view_scroller.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+        task_view_scroller.add(self.task_view)
+
+        self.filter_view = simplegtd.views.FilterView()
+        self.filter_view.get_selection().connect("changed", self.filter_selection_changed)
+        filter_view_scroller = Gtk.ScrolledWindow()
+        filter_view_scroller.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        filter_view_scroller.add(self.filter_view)
+
+        filters = simplegtd.filterlist.FilterList(todotxt)
+        self.task_view.set_model(todotxt)
+        self.filter_view.set_model(filters)
+
+        GLib.idle_add(self.task_view.focus_first)
+        GLib.idle_add(self.filter_view.select_first)
+
+        paned = Gtk.Paned()
+        paned.set_wide_handle(True)
+        paned.pack1(filter_view_scroller, False, False)
+        paned.add2(task_view_scroller)
+
+        self.add(paned)
+        self.task_view.grab_focus()
+
+    def filter_selection_changed(self, tree_selection):
+        filter_strings = self.filter_view.get_filters_from_selection(tree_selection)
+        self.selection_filters = filter_strings
+        self.task_view.set_filters(self.selection_filters + self.search_filters)
+
+    def filter_text_changed(self, new_text):
+        filter_strings = []
+        if new_text.strip():
+            filter_strings.append(new_text.strip())
+        self.search_filters = filter_strings
+        self.task_view.set_filters(self.selection_filters + self.search_filters)
+
+    def show_shortcuts_window(self):
+        builder = Gtk.Builder()
+        builder.add_from_file(simplegtd.resource.find_data_file("shortcuts-window.ui"))
+        shortcuts_window = builder.get_object("shortcuts-simplegtd")
+        shortcuts_window.set_transient_for(self)
+        shortcuts_window.show_all()
+        shortcuts_window.set_property("view-name", "")
+        shortcuts_window.set_property("section-name", "shortcuts")
