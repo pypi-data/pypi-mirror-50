0a108e65612a083a1e78670c36fae9e91b755bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:24 +0000

    Add package TSimplePy/0.0.2/TSimplePy-0.0.2.tar.gz

---
+++ packages/TSimplePy/TSimplePy-0.0.2.tar.gz/TSimplePy-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='TSimplePy',
+    version='0.0.2',
+    url='https://github.com/mstuttgart/codigo-avulso-test-tutorial',
+    license='MIT License',
+    author='Thiago Piassi Bonfogo',
+    author_email='michellstut@gmail.com',
+    keywords='Projeto de biblioteca Python',
+    description=u'Esse Projeto é dedicado facilitar ainda mais as atividades de programadores python através do TSimplePy, uma biblioteca contendo funções prontas para realizar automaticamente as rotinas mais usadas por programadores, buscando a facilidade e eficiência em cada detalhe, assim permitindo que os desenvolvedores alcancem resultados mais rapidamente e que cometam menos erros.',
+    packages=['src'],
+    install_requires=[],
+)
