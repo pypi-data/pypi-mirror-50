8569fc640ed1fa3f46c74b6af9ddc2a2af23c4c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:20 +0000

    Add package pyykov/1.1/pyykov-1.1.tar.gz

---
+++ packages/pyykov/pyykov-1.1.tar.gz/pyykov-1.1/README.md
@@ -0,0 +1,2 @@
+# pyykov
+Markov text generator for python.  Optimized for creating multiple sentences using the same source text.
---
+++ packages/pyykov/pyykov-1.1.tar.gz/pyykov-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(name='pyykov',
+    version='1.1',
+    description='Markov generator for python.',
+    url='https://github.com/maymike321/pyykov',
+    author='Michael May',
+    author_email='yamekim@comcast.net',
+    packages=find_packages(exclude=("test")),
+    license='MIT',
+    test_suite='pyykov.pyykov-test.py',
+    zip_safe=False)
