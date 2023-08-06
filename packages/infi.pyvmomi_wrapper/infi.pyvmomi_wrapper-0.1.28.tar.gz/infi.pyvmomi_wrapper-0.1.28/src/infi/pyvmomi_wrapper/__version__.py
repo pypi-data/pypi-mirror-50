73113fb98c35ec02b738d8d4b8996591a6b23950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:14 +0000

    Add package infi.pyvmomi_wrapper/0.1.28/infi.pyvmomi_wrapper-0.1.28.tar.gz

---
+++ packages/infi.pyvmomi_wrapper/infi.pyvmomi_wrapper-0.1.28.tar.gz/infi.pyvmomi_wrapper-0.1.28/src/infi/pyvmomi_wrapper/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.28"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'develop'
+__git_remote_tracking_branch__ = u'origin/develop'
+__git_remote_url__ = u'git@git.infinidat.com:host-opensource/infi.pyvmomi_wrapper.git'
+__git_head_hash__ = '0198ab982433f7552036a45ca5e751e792f427a0'
+__git_head_subject__ = u'Finished Release v0.1.28'
+__git_head_message__ = u''
+__git_dirty_diff__ = u'diff --git a/buildout.cfg b/buildout.cfg\nindex d3d5191..7f85e5b 100644\n--- a/buildout.cfg\n+++ b/buildout.cfg\n@@ -30,7 +30,7 @@ pre_uninstall_script_name = None\n \n [isolated-python]\n recipe = infi.recipe.python\n-version = v2.7.8.8\n+version = v2.7.8.13\n \n [setup.py]\n recipe = infi.recipe.template.version\n'
+__git_commit_date__ = '2019-08-05 18:04:56'
