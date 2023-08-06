51026f9606d04b581035402ffbcfa122acc1a8ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:50 +0000

    Add package pptop/0.2.1/pptop-0.2.1.tar.gz

---
+++ packages/pptop/pptop-0.2.1.tar.gz/pptop-0.2.1/pptop/logger.py
@@ -0,0 +1,35 @@
+'''
+pptop don't use logging because two processes must write to the same file
+'''
+
+import fcntl
+import traceback
+
+from datetime import datetime
+from types import SimpleNamespace
+
+config = SimpleNamespace(fname=None, name='')
+
+
+def log(*args):
+    if not config.fname:
+        return
+    s = '{} {}  {}'.format(
+        datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'),
+        config.name, ' '.join([str(x) for x in args]))
+    with open(config.fname, 'a') as fh:
+        fcntl.flock(fh, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        fh.seek(0, 2)
+        fh.write(s + '\n')
+        fcntl.flock(fh, fcntl.LOCK_UN)
+
+
+def log_traceback(msg=None):
+    if not config.fname:
+        return
+    args = ()
+    if msg is not None:
+        args += (msg,)
+
+    args += (traceback.format_exc(),)
+    log(*args)
---
+++ packages/pptop/pptop-0.2.1.tar.gz/pptop-0.2.1/setup.py
@@ -0,0 +1,29 @@
+__version__ = "0.2.1"
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pptop',
+    version=__version__,
+    author='Altertech Group',
+    author_email='div@altertech.com',
+    description='Open, extensible Python injector/profiler/analyzer',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/alttch/pptop',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    scripts=['bin/pptop'],
+    include_package_data=True,
+    install_requires=[
+        'psutil', 'tabulate', 'atasker', 'pyyaml', 'yappi', 'termcolor',
+        'pygments'
+    ],
+    classifiers=('Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License',
+                 'Topic :: Software Development :: Debuggers',
+                 'Topic :: Software Development :: Testing'),
+)
