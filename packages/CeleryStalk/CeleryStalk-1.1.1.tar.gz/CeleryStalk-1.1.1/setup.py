df2264f26bfa95a3173bb46c02a1aeed7af82b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:09 +0000

    Add package CeleryStalk/1.1.1/CeleryStalk-1.1.1.tar.gz

---
+++ packages/CeleryStalk/CeleryStalk-1.1.1.tar.gz/CeleryStalk-1.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+
+setup(
+    name='CeleryStalk',
+    version='1.1.1',
+    author='Rudy Venguswamy',
+    author_email='rvenguswamy@vmware.com',
+    packages=['CeleryStalk'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/CeleryStalk/',
+    license='LICENSE.txt',
+    description='Change Large CSVs Faster with Easy Parallel Processing',
+    long_description="README.txt",
+    install_requires=[
+        "celery >= 4.2.0",
+        "redis >= 2.10.5",
+    ],
+)
