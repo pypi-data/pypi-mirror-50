6e3de6409e2e8cac4958bd5a43c24dd338db1302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:15 +0000

    Add package pynearmap/0.1/pynearmap-0.1.tar.gz

---
+++ packages/pynearmap/pynearmap-0.1.tar.gz/pynearmap-0.1/pynearmap/BaseRequest.py
@@ -0,0 +1,33 @@
+from ..Config.Auth import auth
+import abc
+
+import requests
+
+class BaseRequest(abc.ABC):
+    '''
+    Request class responsible for handling API calls to nearmap
+    '''
+    api_key = auth.get("API_KEY")
+    base_url = auth.get("BASE_URL")
+    base_uri = None  # eg tiles/v3/surveys
+
+    def __init__(self):
+        self.base_request_url = self.base_url + self.base_uri
+        self.queries = {}
+
+    def set_params(self, **kwargs):
+        self.params = kwargs
+
+    def set_uri(self, uri: str):
+        self.base_request_url = self.base_url + self.base_uri + uri
+        return self
+
+    def call(self):
+        self.queries.update({
+            "apiKey": self.api_key
+        })
+
+        # remove empty key
+        params = {key: value for key, value in self.queries.items() if value is not None}
+
+        return requests.get(self.base_request_url, params=params)
---
+++ packages/pynearmap/pynearmap-0.1.tar.gz/pynearmap-0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'pynearmap',
+  packages = ['pynearmap'],
+  version = '0.1',
+  license='MIT',
+  description = 'Lightweight Nearmap API client implemented in pure Python. Read the API documentation at https://docs.nearmap.com/',
+  author = 'Sam Ngu',
+  author_email = 'sam.ngu@yandex.com',
+  url = 'https://github.com/sam-ngu/pynearmap',
+  download_url = 'https://github.com/sam-ngu/pynearmap/archive/v0.1.tar.gz',
+  keywords = ['nearmap', 'api-client'],
+  install_requires=[
+          'Pillow',
+          'python-dotenv',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',      # Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
