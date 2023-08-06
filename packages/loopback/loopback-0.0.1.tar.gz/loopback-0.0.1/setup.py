443101d873d494b0394b28a74de908485d8b9742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:44 +0000

    Add package loopback/0.0.1/loopback-0.0.1.tar.gz

---
+++ packages/loopback/loopback-0.0.1.tar.gz/loopback-0.0.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['loopback']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'loopback',
+    'version': '0.0.1',
+    'description': 'Thread manager and decorator for asynchronous background function execution.',
+    'long_description': '# loopback\nThread manager and decorator for asynchronous background function execution\n\n## Simple usage in code:\n```\nfrom loopback import manager, loop\nfrom time import sleep\n\n@loop(manager, interval = 1)\ndef threaded_function(arg):\n    print("running ")\n    sleep(arg)\n    return \'OK\'\n\n\nwhile True:\n    print(threaded_function(4))\n    sleep(1)\n```\n\n## Source Code:\n* [https://github.com/vpuhoff/loopback](https://github.com/vpuhoff/loopback)\n\n## Travis CI Deploys:\n* [https://travis-ci.com/vpuhoff/loopback](https://travis-ci.com/vpuhoff/loopback) [![Build Status](https://travis-ci.com/vpuhoff/loopback.svg?branch=master)](https://travis-ci.com/vpuhoff/loopback)\n',
+    'author': 'vpuhoff',
+    'author_email': 'vpuhoff@live.ru',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+}
+
+
+setup(**setup_kwargs)
