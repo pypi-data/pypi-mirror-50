19f05bb5ec4c0d267ce832773e676a477810c346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:56 +0000

    Add package listnest/1.2.0/listnest-1.2.0.tar.gz

---
+++ packages/listnest/listnest-1.2.0.tar.gz/listnest-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name = 'listnest',
+        version = '1.2.0',
+        py_modules = ['listnest'],
+        author = 'khojarohan',
+        author_email = 'khojarohan@ymail.com',
+        description = 'A simple printer of nested list'
+      )
