26194585e82537a1f0301112dc47d8cd1b9e02a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:50 +0000

    Add package synbio/0.2.0/synbio-0.2.0.tar.gz

---
+++ packages/synbio/synbio-0.2.0.tar.gz/synbio-0.2.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="synbio",
+    version="0.2.0",
+    author="JJTimmons",
+    author_email="jtimmons@latticeautomation.com",
+    description="Synbio design and build library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
