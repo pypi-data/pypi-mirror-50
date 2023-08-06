9fcca98a944a194f7495754890b8b94551b52e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:46 +0000

    Add package django-app-helper/2.0.0a1/django-app-helper-2.0.0a1.tar.gz

---
+++ packages/django-app-helper/django-app-helper-2.0.0a1.tar.gz/django-app-helper-2.0.0a1/djangocms_helper/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+This is a compatibility package to allow legacy import to work without any changes
+"""
+from __future__ import absolute_import, print_function, unicode_literals
+
+
+# clone app_helper in djangocms_helper module
+import app_helper  # NOQA
+
+sys.modules[__name__] = sys.modules['app_helper']
