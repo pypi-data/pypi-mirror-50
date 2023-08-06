7bad99b08da371fafc51b28df8a24d004aaac972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:23 +0000

    Add package kdbase/3.1.1/kdbase-3.1.1.tar.gz

---
+++ packages/kdbase/kdbase-3.1.1.tar.gz/kdbase-3.1.1/setup.py
@@ -0,0 +1,20 @@
+#!/user/bin/env python
+#!-*- coding:utf-8 -*-
+'''
+Created on 20190506
+@auther: dq
+'''
+from setuptools import setup,find_packages
+setup(name = 'kdbase',
+      version= '3.1.1',
+      keywords = ('pip', 'kdbase', 'featureextraction'),
+      description = 'Custom script',
+      license = 'MIT Licence',
+      url = 'http://songyanjing@git.kuandeng.com/scm/tin/bigdata_support.git',
+      author = 'dq',
+      author_email = 'duquan@kuandeng.com',
+      packages = find_packages(),
+      include_package_data = True,
+      platforms = "any"
+      #packages = ['kd_infra'],
+     )
