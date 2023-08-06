296e1e5e00946505e4d8c4574e9b0e7be48ccaee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:01 +0000

    Add package chemml/0.5.5/chemml-0.5.5-py3-none-any.whl

---
+++ packages/chemml/chemml-0.5.5-py3-none-any.whl/chemml/__init__.py
@@ -0,0 +1,7 @@
+# __name__ = "chemml"
+__version__ = "0.5.5"
+__author__ = ["Mojtaba Haghighatlari (mojtabah@buffalo.edu)", "Johannes Hachmann (hachmann@buffalo.edu)"]
+
+
+# import sys
+# sys.dont_write_bytecode = True
