d563baffd4d9c35c7d89a776ed4d6ed41d69e627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:15 +0000

    Add package synbio/0.4.1/synbio-0.4.1-py3-none-any.whl

---
+++ packages/synbio/synbio-0.4.1-py3-none-any.whl/synbio/__init__.py
@@ -0,0 +1,10 @@
+"""Library for synthetic biology design and build."""
+
+from .design import Combinatorial
+from .design import CombinatorialBins
+from .design import Plasmid
+from .primers import Primers
+from .protocol import Protocol
+from .mix import Mix
+from .reagents import Reagent
+from .species import Species
---
+++ packages/synbio/synbio-0.4.1-py3-none-any.whl/synbio/protocols/__init__.py
@@ -0,0 +1,4 @@
+"""Pre-configured assembly protocols."""
+
+from .gibson import Gibson
+from .goldengate import GoldenGate
