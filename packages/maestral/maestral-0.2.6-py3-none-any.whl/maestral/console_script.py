7808542ae883b0e310cd906d5ce2590227616127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:42 +0000

    Add package maestral/0.2.6/maestral-0.2.6-py3-none-any.whl

---
+++ packages/maestral/maestral-0.2.6-py3-none-any.whl/maestral/console_script.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 30 13:51:32 2018
+
+@author: samschott
+"""
+
+import click
+
+
+@click.group()
+def main():
+    """Maestral Dropbox Client for Linux and macOS."""
+    pass
+
+
+@main.command()
+def sync():
+    """Runs Maestral from the command line."""
+    from maestral.main import Maestral
+    m = Maestral()
+    m.monitor.connection_thread.join()  # join until quit by user
+    return m
+
+
+@main.command()
+def gui():
+    """Runs Maestral with a GUI."""
+    from maestral.gui.main import run
+    run()
+
+
+@main.command()
+def configure():
+    """Runs the command line configuration wizard."""
+    from maestral.main import Maestral
+    m = Maestral(run=False)
+    m.set_dropbox_directory()
+    m.select_excluded_folders()
+
+
+@main.command()
+def unlink():
+    """Unlinks your Dropbox account."""
+    from maestral.main import Maestral
+    m = Maestral(run=False)
+    m.unlink()
+
+
+@main.command()
+@click.argument('dropbox_path', type=click.Path())
+@click.argument('local_path', type=click.Path())
+def download(dropbox_path: str, local_path: str):
+    """Downloads a file from Dropbox."""
+    from maestral.client import MaestralApiClient
+    c = MaestralApiClient()
+    c.download(dropbox_path, local_path)
+
+
+@main.command()
+@click.argument('local_path', type=click.Path())
+@click.argument('dropbox_path', type=click.Path())
+def upload(local_path: str, dropbox_path: str):
+    """Uploads a file to Dropbox."""
+    from maestral.client import MaestralApiClient
+    c = MaestralApiClient()
+    c.upload(local_path, dropbox_path)
+
+
+@main.command()
+@click.argument('old_path', type=click.Path())
+@click.argument('new_path', type=click.Path())
+def move(old_path: str, new_path: str):
+    """Moves or renames a file or folder on Dropbox."""
+    from maestral.client import MaestralApiClient
+    c = MaestralApiClient()
+    c.move(old_path, new_path)
+
+
+@main.command()
+@click.argument('dropbox_path', type=click.Path())
+def ls(dropbox_path: str):
+    """Lists contents of a folder on Dropbox."""
+    from maestral.client import MaestralApiClient
+    from dropbox.files import FolderMetadata
+    c = MaestralApiClient()
+    res = c.list_folder(dropbox_path, recursive=False)
+    entry_types = ("Folder" if isinstance(md, FolderMetadata) else "File" for md in
+                   res.entries)
+    entry_names = (md.name for md in res.entries)
+    for t, n in zip(entry_types, entry_names):
+        click.echo("{0}:\t{1}".format(t, n))
+
+
+@main.command()
+@click.argument('dropbox_path', type=click.Path())
+def mkdir(dropbox_path: str):
+    """Creates a new directory on Dropbox."""
+    from maestral.client import MaestralApiClient
+    c = MaestralApiClient()
+    c.make_dir(dropbox_path)
+
+
+@main.command()
+def account_info():
+    """Prints Dropbox account info."""
+    from maestral.client import MaestralApiClient
+    c = MaestralApiClient()
+    res = c.get_account_info()
+    print("{0}, {1}".format(res.email, res.account_type))
+
+
+@main.command()
+@click.option('--yes/--no', '-Y/-N', default=True)
+def autostart(yes: bool):
+    """Starts Maestral on login. May not work on some Linux distributions."""
+    from maestral.utils.autostart import AutoStart
+    ast = AutoStart()
+    if yes:
+        ast.enable()
+    else:
+        ast.disable()
---
+++ packages/maestral/maestral-0.2.6-py3-none-any.whl/maestral/gui/resources/__init__.py
@@ -0,0 +1,94 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 16:23:13 2018
+
+@author: samschott
+"""
+import sys
+import os
+import re
+import platform
+from PyQt5 import QtWidgets, QtCore, QtGui
+
+_root = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+
+_icon_provider = QtWidgets.QFileIconProvider()
+
+APP_ICON_PATH = _root + "/Maestral.png"
+TRAY_ICON_PATH = _root + "/menubar_icon_{0}_{1}.svg"
+
+FOLDERS_DIALOG_PATH = _root + "/folders_dialog.ui"
+SETUP_DIALOG_PATH = _root + "/setup_dialog.ui"
+SETTINGS_WINDOW_PATH = _root + "/settings_window.ui"
+UNLINK_DIALOG_PATH = _root + "/unlink_dialog.ui"
+REBUILD_INDEX_DIALOG_PATH = _root + "/rebuild_index_dialog.ui"
+SYNC_ISSUES_WINDOW_PATH = _root + "/sync_issues_window.ui"
+SYNC_ISSUE_WIDGET_PATH = _root + "/sync_issue_widget.ui"
+
+
+def get_native_item_icon(item_path):
+
+    if not os.path.exists(item_path):
+        # fall back to default file icon
+        return get_native_file_icon()
+    else:
+        # get system icon for file type
+        return _icon_provider.icon(QtCore.QFileInfo(item_path))
+
+
+def get_native_folder_icon():
+    # use a real folder here because Qt may return the wrong folder icon
+    # in macOS with dark mode activated
+    return _icon_provider.icon(QtCore.QFileInfo("/usr"))
+
+
+def get_native_file_icon():
+    return _icon_provider.icon(_icon_provider.File)
+
+
+def get_system_tray_icon(status, icon_color="dark"):
+    assert status in ("idle", "syncing", "paused", "disconnected", "error")
+
+    gnome_version = __get_gnome_version(return_str=False)
+    has_gnome3 = gnome_version is not None and gnome_version[0] >= 3
+
+    if platform.system() == "Linux" and has_gnome3:
+        QtGui.QIcon.setThemeSearchPaths([os.path.join(_root, "icon-theme-gnome")])
+        icon = QtGui.QIcon.fromTheme("menubar_icon_{0}-symbolic".format(status))
+    elif platform.system() == "Darwin":
+        icon = QtGui.QIcon(TRAY_ICON_PATH.format(status, "dark"))
+        icon.setIsMask(True)
+    else:
+        icon = QtGui.QIcon(TRAY_ICON_PATH.format(status, icon_color))
+        icon.setIsMask(True)
+
+    return icon
+
+
+def __get_gnome_version(return_str=False):
+    gnome3_config_path = "/usr/share/gnome/gnome-version.xml"
+    gnome2_config_path = "/usr/share/gnome-about/gnome-version.xml"
+
+    xml = None
+
+    for path in (gnome2_config_path, gnome3_config_path):
+        if os.path.isfile(path):
+            try:
+                with open(path, "r") as f:
+                    xml = f.read()
+            except OSError:
+                pass
+
+    if xml:
+        p = re.compile(r"<platform>(?P<maj>\d+)</platform>\s+<minor>"
+                       r"(?P<min>\d+)</minor>\s+<micro>(?P<mic>\d+)</micro>")
+        m = p.search(xml)
+        version = "{0}.{1}.{2}".format(m.group("maj"), m.group("min"), m.group("mic"))
+
+        if return_str:
+            return version
+        else:
+            return tuple(int(v) for v in version.split("."))
+    else:
+        return None
