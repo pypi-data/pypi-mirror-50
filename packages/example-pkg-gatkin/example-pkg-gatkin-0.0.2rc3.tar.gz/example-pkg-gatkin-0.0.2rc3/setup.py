8345d287f16ae7638605098364b28329e8a376a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:35 +0000

    Add package example-pkg-gatkin/0.0.2rc3/example-pkg-gatkin-0.0.2rc3.tar.gz

---
+++ packages/example-pkg-gatkin/example-pkg-gatkin-0.0.2rc3.tar.gz/example-pkg-gatkin-0.0.2rc3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-gatkin",
+    version="0.0.2rc3",
+    author="Greg Atkin",
+    author_email="greg.scott.atkin@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gatkin/automated-pypi-deploy-example",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
