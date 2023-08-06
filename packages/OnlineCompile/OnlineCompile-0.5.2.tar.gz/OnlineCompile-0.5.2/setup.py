b139b0c850b9d413c6ec99afee0465395f3bfac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:05 +0000

    Add package OnlineCompile/0.5.2/OnlineCompile-0.5.2.tar.gz

---
+++ packages/OnlineCompile/OnlineCompile-0.5.2.tar.gz/OnlineCompile-0.5.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="OnlineCompile",
+    version="0.5.2",
+    author="Will F",
+    author_email="forsbergw82@gmail.com",
+    description="A Python package for running python scripts from the internet",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['Online'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
