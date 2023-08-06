1c9fd1d03ab842b6d4dc2b034397acaf7fd3270d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:51 +0000

    Add package micropython-uploader/0.1.0/micropython-uploader-0.1.0.tar.gz

---
+++ packages/micropython-uploader/micropython-uploader-0.1.0.tar.gz/micropython-uploader-0.1.0/uploader/configPattern.py
@@ -0,0 +1,19 @@
+
+def getCom(port):
+    comPort = "AMPY_PORT=" + port
+    return comPort
+
+
+def getSpeed(preferSpeed=2):
+    speed = [9600, 57600, 115200, 230400, 460800, 921600]
+    boardSpeed = "AMPY_BAUD=" + str(speed[preferSpeed])
+    return boardSpeed  # я так захотел
+
+
+def createAmpyConfig(port):
+    comPort = getCom(port)
+    with open(".ampy", "w") as f:
+        f.write(comPort)
+        f.write(getSpeed())
+
+    print("Config file .ampy was created")
