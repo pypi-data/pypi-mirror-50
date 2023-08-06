25bbe6aa639f66ed763bb3051f314b072edfc214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:59 +0000

    Add package keyup/1.1.16/keyup-1.1.16.tar.gz

---
+++ packages/keyup/keyup-1.1.16.tar.gz/keyup-1.1.16/keyup/__init__.py
@@ -0,0 +1,18 @@
+import pyaws
+from keyup._version import __version__ as version
+
+
+__author__ = 'Blake Huber'
+__version__ = version
+__email__ = "blakeca00@gmail.com"
+
+
+# ---  ancillary modules below line -------------------------------------------
+
+
+
+from keyup import logd
+logger = logd.getLogger(__version__)
+
+# set pyaws log function to keyup global logger
+pyaws.logger = logger
