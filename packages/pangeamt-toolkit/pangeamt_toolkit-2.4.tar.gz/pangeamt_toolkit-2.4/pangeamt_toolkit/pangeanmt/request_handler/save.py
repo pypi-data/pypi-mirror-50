a41e25ac91fe6ac2cb8db5925e0b42e5ba615226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:47 +0000

    Add package pangeamt-toolkit/2.4/pangeamt_toolkit-2.4.tar.gz

---
+++ packages/pangeamt-toolkit/pangeamt_toolkit-2.4.tar.gz/pangeamt_toolkit-2.4/pangeamt_toolkit/pangeanmt/request_handler/save.py
@@ -0,0 +1,26 @@
+from aiohttp import web
+
+async def save(req):
+    nmt = req.app['nmt']
+    lock = req.app['lock']
+    ol = req.app['ol']
+    extended_path = req.app['extended_model_path']
+
+    try:
+        if not ol:
+            raise Exception('Online Learning is not active.')
+        async with lock:
+            req = await req.json()
+            list_path = extended_path.split('/')
+            main_path = ('/').join(list_path[:-2])
+            extend = '/extended_model'
+            path = main_path + f"/{list_path[-2]}_{req['name']}{extend}"
+            nmt.save_model(path)
+            resp = {
+                'rc': 0
+            }
+            return web.json_response(resp, status=200)
+
+    except Exception as e:
+        response_obj = {'status': 'failed', 'reason': str(e)}
+        return web.json_response(response_obj, status=500)
