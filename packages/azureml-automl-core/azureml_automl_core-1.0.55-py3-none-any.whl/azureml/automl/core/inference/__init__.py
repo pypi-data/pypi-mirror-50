7d71ea536e2a0cc1b7b84f3edc76fe370027b51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:53 +0000

    Add package azureml-automl-core/1.0.55/azureml_automl_core-1.0.55-py3-none-any.whl

---
+++ packages/azureml-automl-core/azureml_automl_core-1.0.55-py3-none-any.whl/azureml/automl/core/inference/__init__.py
@@ -0,0 +1,8 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+"""Init for inference module."""
+
+from .inference import _get_scoring_file, _create_conda_env_file, _get_child_run_number, _get_model_id, \
+    NumpyParameterType, PandasParameterType, AutoMLCondaPackagesList, AutoMLPipPackagesList, AMLArtifactIDHeader, \
+    AutoMLInferenceArtifactIDs
