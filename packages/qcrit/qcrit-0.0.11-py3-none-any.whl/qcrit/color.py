b3901f97e461487489c0c40706290192471343d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:11 +0000

    Add package qcrit/0.0.11/qcrit-0.0.11-py3-none-any.whl

---
+++ packages/qcrit/qcrit-0.0.11-py3-none-any.whl/qcrit/color.py
@@ -0,0 +1,11 @@
+'''
+ANSI Colors
+'''
+def red(s):
+	return '\033[91m' + s + '\033[0m'
+def green(s):
+	return '\033[92m' + s + '\033[0m'
+def yellow(s):
+	return '\033[93m' + s + '\033[0m'
+def purple(s):
+	return '\033[95m' + s + '\033[0m'
