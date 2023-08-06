87ff0b09d75acae2007529c30e69423c53f92039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:25 +0000

    Add package netNMFsc/2.0/netNMFsc-2.0.tar.gz

---
+++ packages/netNMFsc/netNMFsc-2.0.tar.gz/netNMFsc-2.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="netNMFsc",
+    version="2.0",
+    author="Rebecca Elyanow",
+    decription="netNMF-sc",
+    url="https://github.com/raphael-group/netNMF-sc",
+    packages=setuptools.find_packages(),
+classifiers=[
+        "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+],
+)
