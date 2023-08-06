4f5187b264e43803b4bdee3741dbd2f2fd853a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:07 +0000

    Add package oceandb-elasticsearch-driver/0.2.0/oceandb-elasticsearch-driver-0.2.0.tar.gz

---
+++ packages/oceandb-elasticsearch-driver/oceandb-elasticsearch-driver-0.2.0.tar.gz/oceandb-elasticsearch-driver-0.2.0/oceandb_elasticsearch_driver/indexes.py
@@ -0,0 +1,15 @@
+#  Copyright 2018 Ocean Protocol Foundation
+#  SPDX-License-Identifier: Apache-2.0
+
+price = "service.attributes.main.price"
+license = "service.attributes.main.license"
+sample = "service.attributes.additionalInformation.links.type"
+categories = "service.attributes.additionalInformation.categories"
+tags = "service.attributes.additionalInformation.tags"
+created = "created"
+datePublished = "service.attributes.main.datePublished"
+dateCreated = "service.attributes.main.dateCreated"
+updated_frequency = "service.attributes.additionalInformation.updateFrequency"
+service_type = "service.attributes.main.type"
+
+list_indexes = [price, license, sample, categories, created, updated_frequency, service_type]
