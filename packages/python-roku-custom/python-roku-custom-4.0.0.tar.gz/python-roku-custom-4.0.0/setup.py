961499ed615a53e8977e115c54c9432f50d1e0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:35 +0000

    Add package python-roku-custom/4.0.0/python-roku-custom-4.0.0.tar.gz

---
+++ packages/python-roku-custom/python-roku-custom-4.0.0.tar.gz/python-roku-custom-4.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
+
+setup(
+    name='python-roku-custom',
+    version='4.0.0',
+    description='Client for the Roku media player',
+    long_description=readme,
+    author='Jeremy Carbaugh',
+    author_email='jcarbaugh@gmail.com',
+    url='https://github.com/jcarbaugh/python-roku',
+    packages=find_packages(),
+    install_requires=[
+        'lxml>=3.6,<3.7',
+        'requests>=2.10,<2.11',
+        'six'
+    ],
+    license='BSD License',
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
