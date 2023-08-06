b03ed43243e28760ae54b7d241a89c12fa8acec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:15 +0000

    Add package iambic/0.5.0/iambic-0.5.0.tar.gz

---
+++ packages/iambic/iambic-0.5.0.tar.gz/iambic-0.5.0/tests/static/table.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+TABLE = {
+    "Dramatis Personae": ["Foo", "A Bar", "Bar's Foo", "Bar"],
+    "First Appearance": [2, 4, 6, 12],
+    "Lines": [5, 1, 1, 3],
+    "P": ["X", "X", "X", ""],
+    "I.P": ["", "", "", "X"],
+    "INT": ["", "", "", ""],
+    "I.i": ["X", "", "", "X"],
+    "I.E": ["X", "", "", "O"],
+}
+
+MATRIX = [
+    ("Dramatis Personae", "First Appearance", "Lines", "P", "I.P", "INT", "I.i", "I.E"),
+    ("Foo", 2, 5, "X", "", "", "X", "X"),
+    ("A Bar", 4, 1, "X", "", "", "", ""),
+    ("Bar's Foo", 6, 1, "X", "", "", "", ""),
+    ("Bar", 12, 3, "", "X", "", "X", "O"),
+]
