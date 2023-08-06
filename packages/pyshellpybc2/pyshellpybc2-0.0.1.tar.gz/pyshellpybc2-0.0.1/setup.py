cf8e64d570dfb91cfde2ccfdcb6eeb1528505d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:08 +0000

    Add package pyshellpybc2/0.0.1/pyshellpybc2-0.0.1.tar.gz

---
+++ packages/pyshellpybc2/pyshellpybc2-0.0.1.tar.gz/pyshellpybc2-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyshellpybc2",
+    version="0.0.1",
+    author="PYBC_2",
+    author_email="mmothuku@adobe.com",
+    description="A small example package",
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
