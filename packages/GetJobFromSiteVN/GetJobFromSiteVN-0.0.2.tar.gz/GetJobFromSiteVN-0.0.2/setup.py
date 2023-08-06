806b3428e260d229fae8e9415ddde07e51670358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:36 +0000

    Add package GetJobFromSiteVN/0.0.2/GetJobFromSiteVN-0.0.2.tar.gz

---
+++ packages/GetJobFromSiteVN/GetJobFromSiteVN-0.0.2.tar.gz/GetJobFromSiteVN-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GetJobFromSiteVN",
+    version="0.0.2",
+    author="duonghao314",
+    author_email="nhatquang.nv14@gmail.com",
+    description="Get Job From Site VN",
+    long_description="Full description",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+
+    ],
+    install_requires=['beautifulsoup4', 'requests', 'selenium'],
+)
