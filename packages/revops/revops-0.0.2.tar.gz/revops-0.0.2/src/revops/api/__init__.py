6b0e1a806cc20888be8f271b83251924c2fe3942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:59 +0000

    Add package revops/0.0.2/revops-0.0.2.tar.gz

---
+++ packages/revops/revops-0.0.2.tar.gz/revops-0.0.2/src/revops/api/__init__.py
@@ -0,0 +1,49 @@
+import os
+import sys
+
+import requests
+import logging
+
+__LOGGING_DEFAULTS__ = {'level': logging.DEBUG}
+__DEFAULT_ENDPOINT__ = 'https://api.revops.io'
+
+logging.basicConfig(**__LOGGING_DEFAULTS__)
+
+class RevOpsAPI(object):
+    headers = {}
+    """
+    This is the RevOps API Client
+
+    Attributes:
+        api_key (str): API Key used to access RevOps API Access.
+        endpoint (str): API Endpoint to access your RevOps instance.
+        If not defined, defaults to 'https://api.revops.io'.
+    """
+    def __init__(self, api_key = None, endpoint = __DEFAULT_ENDPOINT__):
+        self.api_key = os.environ.get('REVOPS_API_KEY', api_key)
+        if self.api_key == None or self.api_key == '':
+            raise Exception("REVOPS_API_KEY environment variable is not set.")
+        self.api_endpoint = os.environ.get('REVOPS_API_ENDPOINT', endpoint)
+        self.headers = {
+            'X-RevOps-API-Key': self.api_key,
+            'Content-Type': 'application/json',
+        }
+
+    def __getattr__(self, name):
+        resource = __import__(
+            "revops.resources.{}".format(name),
+            fromlist=["revops.resources"]
+        )
+        resource.init(self)
+        return resource
+
+    def request(self, data, api_resource = None, http_method = "GET"):
+        url = "{}/{}".format(self.api_endpoint, api_resource)
+
+        response = requests.request(
+            http_method,
+            url,
+            data=data,
+            headers=self.headers,
+        )
+        return response
