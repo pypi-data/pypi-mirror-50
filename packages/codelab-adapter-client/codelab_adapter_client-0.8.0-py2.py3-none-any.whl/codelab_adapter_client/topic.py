0c4d9d14b67ad87f99401219c3a31a16d33d44c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:16 +0000

    Add package codelab-adapter-client/0.8.0/codelab_adapter_client-0.8.0-py2.py3-none-any.whl

---
+++ packages/codelab-adapter-client/codelab_adapter_client-0.8.0-py2.py3-none-any.whl/codelab_adapter_client/topic.py
@@ -0,0 +1,25 @@
+# adapter data
+ADAPTER_TOPIC = "adapter/extensions/data"  # 来自插件的消息，topic
+
+# scratch command
+SCRATCH_TOPIC = "scratch/extensions/command"
+# JUPYTER_TOPIC = "from_jupyter/extensions"
+
+# core
+EXTENSIONS_OPERATE_TOPIC = "core/extensions/operate"
+EXTENSIONS_STATUS_TOPIC = "core/extensions/status"
+NOTIFICATION_TOPIC = "core/notification"
+
+# ble
+ADAPTER_BLE_TOPIC = "adapter/ble"
+SCRATCH_BLE_TOPIC = "scratch/ble"
+
+# mqtt
+TO_MQTT_TOPIC = "to_mqtt"
+FROM_MQTT_TOPIC = "from_mqtt"
+
+# jupyter
+
+# Home Assistant
+FROM_HA_TOPIC = "from_HA"
+TO_HA_TOPIC = "to_HA"
