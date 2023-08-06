6065e66641aa33af18b1784ac5326a190e911596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:13 +0000

    Add package test-pkg-abcd/1/test_pkg_abcd-1.tar.gz

---
+++ packages/test-pkg-abcd/test_pkg_abcd-1.tar.gz/test_pkg_abcd-1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="test_pkg_abcd",
+    version="1",
+    author="Shivam Arora",
+    author_email="shivam.arora2@delhivery.com",
+    description="A small example package",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
