2194824af20a20bf7189f27607a1f98af2213a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:28 +0000

    Add package codelab-adapter-mqtt-client/0.1.0/codelab_adapter_mqtt_client-0.1.0.tar.gz

---
+++ packages/codelab-adapter-mqtt-client/codelab_adapter_mqtt_client-0.1.0.tar.gz/codelab_adapter_mqtt_client-0.1.0/codelab_adapter_mqtt_client/topic.py
@@ -0,0 +1,21 @@
+# adapter data
+ADAPTER_TOPIC = "adapter/extensions/data"  # 来自插件的消息，topic
+
+# scratch command
+SCRATCH_TOPIC = "scratch/extensions/command"
+# JUPYTER_TOPIC = "from_jupyter/extensions"
+
+# core
+EXTENSIONS_OPERATE_TOPIC = "core/extensions/operate"
+EXTENSIONS_STATUS_TOPIC = "core/extensions/status" # 主动广播
+NOTIFICATION_TOPIC = "core/notification"
+
+# ble
+ADAPTER_BLE_TOPIC = "adapter/ble"
+SCRATCH_BLE_TOPIC = "scratch/ble"
+
+# mqtt 使用adapter视角
+TO_MQTT_TOPIC = "to_mqtt"
+FROM_MQTT_TOPIC = "from_mqtt"
+
+# jupyter
