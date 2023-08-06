e8633455c98f68a0246a67a00677c772c104d681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:38 +0000

    Add package invana-bot/0.1.36/invana-bot-0.1.36.tar.gz

---
+++ packages/invana-bot/invana-bot-0.1.36.tar.gz/invana-bot-0.1.36/invana_bot/__init__.py
@@ -0,0 +1,3 @@
+"""
+Webspider module will crawl through the configured sites and gather the rss feeds and blogs/data.
+"""
---
+++ packages/invana-bot/invana-bot-0.1.36.tar.gz/invana-bot-0.1.36/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages, __version__ as setuptools_version
+
+
+setup(name='invana-bot',
+      version='0.1.36',
+      description='A web spider framework that can transform websites into datasets'
+                  ' with Crawl, Transform and Index workflow.',
+      author='Ravi Raja Merugu',
+      author_email='ravi@invanalabs.ai',
+      url='https://github.com/invanalabs/invana-bot',
+      packages=find_packages(
+          exclude=("bin", "dist", "docs", "example", "tests", "dist")
+      ),
+      install_requires=[
+          'elasticsearch==6.1.1',
+          'feedparser==5.2.1',
+          'lxml==4.1.1',
+          'pymongo==3.7.2',
+          'requests==2.21.0',
+          'Scrapy==1.6.0',
+          'pyyaml==5.1',
+          'python-slugify '
+      ],
+      entry_points={
+          'console_scripts': ['invana-bot = invana_bot.cmd.run:invana_bot_run']
+      },
+      )
