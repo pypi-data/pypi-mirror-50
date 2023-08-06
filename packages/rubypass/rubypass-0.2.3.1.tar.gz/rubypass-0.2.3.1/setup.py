6792e227fc0fb1d63cc255a235dea9cb1099c536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:04 +0000

    Add package rubypass/0.2.3.1/rubypass-0.2.3.1.tar.gz

---
+++ packages/rubypass/rubypass-0.2.3.1.tar.gz/rubypass-0.2.3.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md') as f:
+	ld = f.read()
+
+setuptools.setup(
+	name="rubypass",
+	version="0.2.3.1",
+	description="A package made to extract video urls from 2 russian websites",
+	long_description=ld,
+	long_description_content_type="text/markdown",
+	author="okawo",
+	author_email="okawo.198@gmail.com",
+	url="https://github.com/okawo80085/rubypass",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.6",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+
+	py_modules=['rubypass'],
+)
