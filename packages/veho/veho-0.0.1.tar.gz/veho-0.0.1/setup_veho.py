9d37dfff40ddec1e8e53eabd21aadb0b76dfe529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:58 +0000

    Add package veho/0.0.1/veho-0.0.1.tar.gz

---
+++ packages/veho/veho-0.0.1.tar.gz/veho-0.0.1/setup_veho.py
@@ -0,0 +1,37 @@
+import setuptools
+
+__name__ = 'veho'
+__version__ = '0.0.1'
+__root__ = 'src'
+__keywords__ = ["array", "vector", "matrix"]
+__description__ = "A helper for working with 1d & 2d array."
+
+
+def including(kw):
+    return f"*.{kw}", f"*.{kw}.*", f"{kw}.*", f"{kw}"
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name=__name__,
+    version=__version__,
+    author="Hoyeung Wong",
+    author_email="hoyeungw@outlook.com",
+    description=__description__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hoyeungw/",
+    package_dir={'': __root__},
+    packages=setuptools.find_packages(where=__root__,
+                                      include=including(__name__)),
+    keywords=__keywords__,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
