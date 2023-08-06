9dd6c8a7c001334a902152e2d79552ba2a8d73e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:55 +0000

    Add package czy-ld/1.2/czy_ld-1.2.tar.gz

---
+++ packages/czy-ld/czy_ld-1.2.tar.gz/czy_ld-1.2/setup.py
@@ -0,0 +1,20 @@
+'''
+@File       : setup.py
+@Copyright  : Rainbol
+@Date       : 2019/8/15
+@Desc       :
+'''
+from setuptools import setup
+
+setup(
+    name='czy_ld',
+    version='1.2',
+    description='Recursively delete the remaining folders',
+    author='Rainbol',
+    author_email='2497643794@qq.com',
+    url='https://www.cnblogs.com/RainBol/',
+    py_modules=['czy_ld.czy_ld'])
+
+
+
+import czy_ld.czy_ld
