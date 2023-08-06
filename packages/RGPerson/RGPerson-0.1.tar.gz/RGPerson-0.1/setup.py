5b7ae30f6c00a5b15829c040d6bf7ad67c5dcc08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:07 +0000

    Add package RGPerson/0.1/RGPerson-0.1.tar.gz

---
+++ packages/RGPerson/RGPerson-0.1.tar.gz/RGPerson-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "RGPerson",
+    version = "0.1",
+    keywords = ("pip", "rgperson","featureextraction"),
+    description = "RGPerson",
+    long_description = "RANDOM GENERATE A FAKE PERSON",
+    license = "MIT Licence",
+
+    url = "https://github.com/gh0stkey/RGPerson",
+    author = "Vulkey_Chen",
+    author_email = "admin@gh0st.cn",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+)
