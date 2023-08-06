9920a4233786d7b6abc06f8c4f5bec2bc3a52c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:31 +0000

    Add package apexxapi/1.0.3/apexxapi-1.0.3.tar.gz

---
+++ packages/apexxapi/apexxapi-1.0.3.tar.gz/apexxapi-1.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="apexxapi",
+    version="1.0.3",
+    author="Hugo Wickham",
+    author_email="hugo.wickham@apexx.global",
+    description="Package containing functions for accessing Apexx API 2.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HugoWickham/apexxpython",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
