df65053642686d04710d221f4b00d266d050d39b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:27 +0000

    Add package pltshow/0.1.2/pltshow-0.1.2-py3-none-any.whl

---
+++ packages/pltshow/pltshow-0.1.2-py3-none-any.whl/pltshow.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import requests
+import pandas
+
+class Client:
+    def __init__(self, username = '', password = ''):
+        self.session = requests.Session()
+        r = self.session.post('http://login.pltshow.com/api/login', json={'username': username, 'password': password})
+        if r.status_code < 300:
+            r = s.get('http://pltshow.com')
+        if r.status_code > 300:
+            raise Exception('login failed')
+
+    def show(self, chart, df):
+        result = self.session.patch('%s/api/charts?name=%s' % ('http://pltshow.com', chart), json={'chart_data': df.to_csv()})
+        if result.status_code > 300:
+            raise Exception(result.text)
