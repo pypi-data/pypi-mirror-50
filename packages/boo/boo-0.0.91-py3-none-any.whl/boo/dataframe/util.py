53b485d98aa8b8bf6d2f8bc6a1fdaf5b6ac73e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:40 +0000

    Add package boo/0.0.91/boo-0.0.91-py3-none-any.whl

---
+++ packages/boo/boo-0.0.91-py3-none-any.whl/boo/dataframe/util.py
@@ -0,0 +1,32 @@
+def n_largest(df, col: str, n: int):
+    return sort(df, col).head(n)
+
+
+def contains(df, text):
+    ix = df.title.str.lower().str.contains(text.lower()).fillna(False)
+    return df[ix]
+
+
+def sort(df, col):
+    return df.sort_values(col, ascending=False)
+
+
+def region(df, x: int):
+    return df[df.region == x]
+
+
+def inn(df, x: str):
+    return df.loc[str(x), :]
+
+
+def inns(df, xs):
+    xs = list(map(str, xs))
+    ix = df.index.isin(xs)
+    return df.loc[ix, :]
+
+
+def industry(df, ok1, ok2=None):
+    if ok2 is None:
+        return df[df.ok1 == ok1]     
+    else:
+        return df[(df.ok1 == ok1) & (df.ok2 == ok2)]        
