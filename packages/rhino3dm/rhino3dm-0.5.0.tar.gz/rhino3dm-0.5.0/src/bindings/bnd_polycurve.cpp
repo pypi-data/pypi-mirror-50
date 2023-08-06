db1b4441b73b9fd205038f37eb87b7e797662d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:35 +0000

    Add package rhino3dm/0.5.0/rhino3dm-0.5.0.tar.gz

---
+++ packages/rhino3dm/rhino3dm-0.5.0.tar.gz/rhino3dm-0.5.0/src/bindings/bnd_polycurve.cpp
@@ -0,0 +1,36 @@
+#include "bindings.h"
+
+BND_PolyCurve::BND_PolyCurve()
+{
+  SetTrackedPointer(new ON_PolyCurve(), nullptr);
+}
+
+BND_PolyCurve::BND_PolyCurve(ON_PolyCurve* polycurve, const ON_ModelComponentReference* compref)
+{
+  SetTrackedPointer(polycurve, compref);
+}
+
+void BND_PolyCurve::SetTrackedPointer(ON_PolyCurve* polycurve, const ON_ModelComponentReference* compref)
+{
+  m_polycurve = polycurve;
+  BND_Curve::SetTrackedPointer(polycurve, compref);
+}
+
+/////////////////////////////////////////////////////////////////
+
+#if defined(ON_PYTHON_COMPILE)
+namespace py = pybind11;
+void initPolyCurveBindings(pybind11::module& m)
+{
+  py::class_<BND_PolyCurve, BND_Curve>(m, "Polycurve");
+}
+#endif
+
+#if defined(ON_WASM_COMPILE)
+using namespace emscripten;
+
+void initPolyCurveBindings(void*)
+{
+  class_<BND_PolyCurve, base<BND_Curve>>("Polycurve");
+}
+#endif
