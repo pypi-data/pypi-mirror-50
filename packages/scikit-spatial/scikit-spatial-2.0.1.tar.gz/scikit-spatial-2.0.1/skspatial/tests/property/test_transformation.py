e9f54cee685ac0e61ea2f8e356bd2cff07fbc93b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:50 +0000

    Add package scikit-spatial/2.0.1/scikit-spatial-2.0.1.tar.gz

---
+++ packages/scikit-spatial/scikit-spatial-2.0.1.tar.gz/scikit-spatial-2.0.1/skspatial/tests/property/test_transformation.py
@@ -0,0 +1,46 @@
+import hypothesis.strategies as st
+import numpy as np
+from hypothesis import given
+from numpy.testing import assert_array_almost_equal
+
+from skspatial.objects import Points
+from skspatial.tests.property.strategies import arrays_fixed, lines, multi_points
+
+
+@given(st.data())
+def test_mean_center(data):
+
+    dim = data.draw(st.integers(min_value=2, max_value=4))
+    points = data.draw(multi_points(dim))
+
+    points_centered, _ = points.mean_center()
+
+    # The centroid of the centered points should be the origin.
+    centroid_new = points_centered.centroid()
+    origin = np.zeros(points.dimension)
+
+    assert_array_almost_equal(centroid_new, origin)
+
+
+@given(st.data())
+def test_transform_points_line(data):
+
+    n_points = data.draw(st.integers(min_value=2, max_value=5))
+    dim = data.draw(st.integers(min_value=2, max_value=4))
+
+    points = Points([data.draw(arrays_fixed(dim)) for _ in range(n_points)])
+    line = data.draw(lines(dim))
+
+    # Transform the points into 1D coordinates.
+    coordinates = line.transform_points(points)
+
+    # Project the points onto the line.
+    points_projected = np.apply_along_axis(line.project_point, 1, points)
+
+    # Find the signed distances from the line point to the projected points.
+    vectors_to_projections = points_projected - line.point
+    direction_unit = line.direction.unit()
+    distances_signed = np.apply_along_axis(direction_unit.dot, 1, vectors_to_projections)
+
+    # The coordinates from the transformation should be equal to the signed distances.
+    assert_array_almost_equal(distances_signed, coordinates)
