99dbb297a6711fb0cfccc26858576fef9475b383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:48 +0000

    Add package agreementr/1.1/agreementr-1.1.tar.gz

---
+++ packages/agreementr/agreementr-1.1.tar.gz/agreementr-1.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+setuptools.setup(
+    name="agreementr",
+    version="1.1",
+    author="Junjie Wu",
+    author_email="wujj38@mail2.sysu.edu.cn",
+    description="agreementr",
+    python_requires=">=3.6",
+    include_package_data=True,
+    packages=setuptools.find_packages()
+    #package_data = {'':['data']}
+)
