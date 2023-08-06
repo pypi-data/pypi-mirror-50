ec013642c3ccb54a26f34c0c4e8962d2f1f28466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:17 +0000

    Add package print-helpers/0.2/print_helpers-0.2.tar.gz

---
+++ packages/print-helpers/print_helpers-0.2.tar.gz/print_helpers-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+        name='print_helpers',
+        version='0.2',
+        packages=find_packages(),
+        long_description = long_description
+    )
+
