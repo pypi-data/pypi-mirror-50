20672dbd04462fa552cb6713f5859a4b23079923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:22 +0000

    Add package lkv/0.0.4/lkv-0.0.4-py3-none-any.whl

---
+++ packages/lkv/lkv-0.0.4-py3-none-any.whl/lkvpy/lkv.py
@@ -0,0 +1,9 @@
+import random
+
+def output():
+    print('this is a demo. if you are interested, contact 2427080101@qq.com')
+
+def get_food():
+	A = ["zijing", "taoli", "yushu", "qingfen", "zhilan", "tingtao", "guanchou", "heyuan"]
+	randint = random.randint(0,1000)%8
+	print(A[randint])
