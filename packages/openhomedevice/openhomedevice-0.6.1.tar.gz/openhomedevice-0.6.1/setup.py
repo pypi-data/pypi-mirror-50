8f7493c205f5246efa319d330bbea3d29529aa5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:56 +0000

    Add package openhomedevice/0.6.1/openhomedevice-0.6.1.tar.gz

---
+++ packages/openhomedevice/openhomedevice-0.6.1.tar.gz/openhomedevice-0.6.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'openhomedevice',
+  packages = ['openhomedevice'],
+  version = '0.6.1',
+  description = 'Provides an API for requesting information from an Openhome device',
+  author = 'Barry John Williams',
+  author_email = 'barry@bjw.me.uk',
+  url = 'https://github.com/bazwilliams/openhomedevice',
+  download_url = 'https://github.com/bazwilliams/openhomedevice/tarball/0.4.3',
+  keywords = ['upnp', 'dlna', 'openhome', 'linn', 'ds', 'music', 'render'],
+  install_requires = ['requests', 'lxml'],
+  classifiers = [],
+)
