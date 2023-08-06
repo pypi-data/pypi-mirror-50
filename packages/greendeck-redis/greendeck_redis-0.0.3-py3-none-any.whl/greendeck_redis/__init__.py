6796f16e511388c57ec03b46c782caa74b9d52a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:09 +0000

    Add package greendeck-redis/0.0.3/greendeck_redis-0.0.3-py3-none-any.whl

---
+++ packages/greendeck-redis/greendeck_redis-0.0.3-py3-none-any.whl/greendeck_redis/__init__.py
@@ -0,0 +1,10 @@
+# your package information
+name = "greendeck-redis"
+__version__ = "0.0.3"
+author = "Yashvardhan Srivastava"
+author_email = "yash@greendeck.co"
+url = ""
+
+# import sub packages
+from .src.redis.redis import RedisSet
+from .src.redis.redis import RedisQueue
