9549c61c54583ada25af4cf936466218e1cf33b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:36 +0000

    Add package pyawslog/0.1.3/pyawslog-0.1.3.tar.gz

---
+++ packages/pyawslog/pyawslog-0.1.3.tar.gz/pyawslog-0.1.3/setup.py
@@ -0,0 +1,26 @@
+import os.path
+
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyawslog",
+    version="0.1.3",
+    author="Maurya Allimuthu",
+    author_email="catchmaurya@gmail.com",
+    description="Python AWS Custom Log",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/catchmaurya/pyawslog",
+    packages=find_packages(),
+    scripts=['pyawslog.py'],
+    install_requires=['boto3>=1.9'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
