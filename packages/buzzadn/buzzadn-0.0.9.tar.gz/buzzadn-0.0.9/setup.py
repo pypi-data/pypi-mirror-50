ff95073bdcef9576897974cc529965e2180aafea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:11 +0000

    Add package buzzadn/0.0.9/buzzadn-0.0.9.tar.gz

---
+++ packages/buzzadn/buzzadn-0.0.9.tar.gz/buzzadn-0.0.9/setup.py
@@ -0,0 +1,33 @@
+import os
+import setuptools
+
+
+class CleanCommand(setuptools.Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('rm -vrf ./dist ./*.egg-info')
+
+
+setuptools.setup(
+    name='buzzadn',
+    version='0.0.9',
+    author='Buzzvil',
+    author_email='ernie.kusdavletov@buzzvil.com',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    cmdclass={
+        'clean': CleanCommand,
+    }
+)
