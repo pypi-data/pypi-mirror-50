a97f2f5e76fdc8ed512b2cfed9c777732f3c6cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:03 +0000

    Add package pgc-interface/1.1.9/pgc_interface-1.1.9-py2-none-any.whl

---
+++ packages/pgc-interface/pgc_interface-1.1.9-py2-none-any.whl/pgc_interface/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '1.1.9'
+__description__ = 'PGC Interface'
+__url__ = 'https://github.com/UniversalDevicesInc/pgc-python-interface'
+__author__ = 'James Milne'
+__authoremail__ = 'milne.james@gmail.com'
+__license__ = 'MIT'
+
+from .pgc_interface import Interface, Node, Controller, LOGGER
+
+LOGGER.info('{} {} Starting...'.format(__description__, __version__))
