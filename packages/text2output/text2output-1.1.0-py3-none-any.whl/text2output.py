c65245968e331a027c63b178a8dfeda96c470645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:00 +0000

    Add package text2output/1.1.0/text2output-1.1.0-py3-none-any.whl

---
+++ packages/text2output/text2output-1.1.0-py3-none-any.whl/text2output.py
@@ -0,0 +1,38 @@
+def text2output(file_name, lang='Python', line_prefix='', line_suffix='', file_prefix='', file_suffix=''):
+    lines = []
+    lang_prefix = ''
+    lang_suffix = ''
+    lang = lang.lower()
+    if 'python' in lang:
+        if lang == 'python3' or 'python3' in lang.replace(' ', ''):
+            lang_prefix = "print(r'"
+            lang_suffix = " ')"
+        elif lang == 'python2' or 'python2' in lang.replace(' ', ''):
+            lang_prefix = "print r'"
+            lang_suffix = " '"
+
+    elif lang == 'c' or lang == 'c++' or lang == 'cpp':
+        lang_prefix = 'printf("%s","'
+        lang_suffix = r'\n");'
+
+    elif lang == 'c#' or lang == 'csharp' or lang == 'cs':
+        lang_prefix = 'Console.WriteLine("'
+        lang_suffix = r'");'
+
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+
+    lines_better = []
+    for i, line in enumerate(lines):
+        if i == 0:
+            lines_better.append(file_prefix)
+
+        line = line.replace('\n', '')
+        if lang_prefix == 'printf("%s","' or lang_prefix == 'Console.WriteLine("':
+            line = line.replace('\\', '\\\\')
+        lines_better.append(line_prefix + lang_prefix + line + lang_suffix + line_suffix + '\n')
+
+        if i == len(lines)-1:
+            lines_better.append(file_suffix)
+    with open(file_name.replace('.txt', '') + '_new.txt', 'w') as f_new:
+        f_new.writelines(lines_better)
