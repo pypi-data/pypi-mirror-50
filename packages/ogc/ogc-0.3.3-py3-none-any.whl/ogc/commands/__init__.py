2345da3aae0a3a23320ff5df744a7d9b4f8ecc9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:20 +0000

    Add package ogc/0.3.3/ogc-0.3.3-py3-none-any.whl

---
+++ packages/ogc/ogc-0.3.3-py3-none-any.whl/ogc/commands/__init__.py
@@ -0,0 +1,5 @@
+from .charm import *
+from .snap import *
+from .report import *
+from .execute import *
+from .dep import *
---
+++ packages/ogc/ogc-0.3.3-py3-none-any.whl/ogc/commands/execute.py
@@ -0,0 +1,87 @@
+""" Command to execute a spec
+"""
+import click
+import sys
+from .base import cli
+from ..state import app
+from ..spec import SpecProcessException, SpecConfigException
+from ..enums import SPEC_PHASES
+
+
+@click.command()
+@click.option(
+    "--phase",
+    metavar="<phase>",
+    required=False,
+    help="Run a certain phase of the spec",
+)
+@click.option(
+    "-t",
+    "--tag",
+    metavar="<tag>",
+    required=False,
+    multiple=True,
+    help="Only run specific plugin(s) which matches a tag",
+)
+def execute(phase, tag):
+    """ Execute OGC Specification
+    """
+
+    plugins = []
+    if phase:
+        plugins_for_phase = app.phases.get(phase, None)
+        if not plugins_for_phase:
+            app.log.error(f'Cannot run {phase} phase, does not exist in this spec.')
+            sys.exit(1)
+
+        plugins = [
+            plugin
+            for plugin in plugins_for_phase
+        ]
+
+    if tag:
+        if plugins:
+            _plugins = plugins
+        else:
+            _plugins = [
+                app.phases[phase]
+                for phase in app.phases.keys()
+                if app.phases.get(phase, None)
+            ]
+
+        plugins = [
+            plugin
+            for plugin in _plugins
+            if "tags" in plugin.spec
+            and set(plugin.get_plugin_option("tags")).intersection(tag)
+        ]
+
+    app.log.info(f"Setup environment")
+    for plugin in plugins:
+        # Setup environment
+        try:
+            plugin.env()
+        except SpecProcessException as error:
+            app.log.error(error)
+            sys.exit(1)
+
+    app.log.info(f"Checking for conflicts")
+    for plugin in plugins:
+        # Check for any option conflicts
+        try:
+            plugin.conflicts()
+        except (SpecProcessException, SpecConfigException) as error:
+            app.log.error(error)
+            sys.exit(1)
+
+    app.log.info(f"Processing tasks")
+    for plugin in plugins:
+        # Execute the spec
+        try:
+            plugin.process()
+        except SpecProcessException as error:
+            app.log.error(error)
+            sys.exit(1)
+
+
+cli.add_command(execute)
