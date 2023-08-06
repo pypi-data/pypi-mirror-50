6c3a5e22939df9c0403b1c293250870d5f6d2426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:45 +0000

    Add package pypact/1.3.2/pypact-1.3.2.tar.gz

---
+++ packages/pypact/pypact-1.3.2.tar.gz/pypact-1.3.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+
+setup(name='pypact',
+      version='1.3.2',
+      description='The package for reading and manipulating the fispact output text file.',
+      url='https://github.com/fispact/pypact',
+      author='UKAEA',
+      author_email='thomas.stainer@ukaea.uk',
+      license='Apache License 2.0',
+      packages=[
+            'pypact',
+            'pypact.analysis',
+            'pypact.library',
+            'pypact.input',
+            'pypact.output',
+            'pypact.printlib',
+            'pypact.tools',
+            'pypact.util',
+      ],
+      install_requires=[],
+      python_requires='>=3',
+      scripts=[
+           'pypact/tools/fispactconverter.py'
+      ],
+      setup_requires=[
+            'pytest-runner',
+      ],
+      test_suite='tests.testsuite',
+      tests_require=[
+            'pytest',
+            'pytest-cov>=2.3.1',
+            'mock',
+            'jsonschema',
+            'numpy',
+      ],
+      zip_safe=False,
+      entry_points={
+            'console_scripts': [
+                  'fispactconverter = pypact.tools.fispactconverter:main',
+            ]
+      },
+      )
