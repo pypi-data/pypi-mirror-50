ed0e5dcfd77e610e4c0ab2a832d8d7041da7b329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:57 +0000

    Add package eepro/0.1.0/eepro-0.1.0.tar.gz

---
+++ packages/eepro/eepro-0.1.0.tar.gz/eepro-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from os import path
+import re
+
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
+
+setup(
+    name='eepro',
+    version='0.1.0',
+    author='Daniel Grießhaber',
+    author_email='dangrie158@gmail.com',
+    url='https://github.com/dangrie158/EEPROgraMmer',
+    packages=['eepro'],
+    include_package_data=True,
+    license='MIT',
+    description='Arduino based EEPROM Programmer for parallel EEPROMS',
+    long_description=read('../README.rst'),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+    ],
+    install_requires=[
+        'pyserial'
+    ],
+    entry_points = {
+        'console_scripts': ['eepro=eepro.eepro:main'],
+    }
+)
