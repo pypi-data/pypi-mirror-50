54706929d51c3d73ba5b2a8b20edd2c4ac8e40aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:50 +0000

    Add package hp-report-portal/0.0.4/hp-report-portal-0.0.4.tar.gz

---
+++ packages/hp-report-portal/hp-report-portal-0.0.4.tar.gz/hp-report-portal-0.0.4/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['hp_report_portal']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['hook-plug>=0.0.1,<0.0.2', 'reportportal-client>=3.2,<4.0']
+
+setup_kwargs = {
+    'name': 'hp-report-portal',
+    'version': '0.0.4',
+    'description': 'behave report portal hook plugin',
+    'long_description': None,
+    'author': 'dunossauro',
+    'author_email': 'mendesxeduardo@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
