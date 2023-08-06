ce4c44cb37e810317d97ed95b57504a1bf2e10ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:01 +0000

    Add package zlhawq/2.0.1/zlhawq-2.0.1.zip

---
+++ packages/zlhawq/zlhawq-2.0.1.zip/zlhawq-2.0.1/zlhawq/dst/bid/api.py
@@ -0,0 +1,18 @@
+from lmf.dbv2 import db_command ,db_query
+from lmf.bigdata import pg2csv
+import sys 
+import os 
+from fabric import Connection
+from zlhawq.bid.core import add_quyu_tmp_pc,restart_quyu_tmp_pc,add_quyu_zlsys
+import traceback
+
+
+def add_quyu(quyu,tag='all'):
+    if quyu.startswith('anhui'):
+        add_quyu_tmp_pc(quyu,tag)
+    elif quyu.startswith('zlsys'):
+        add_quyu_zlsys(quyu,tag)
+    else:
+        print("暂不支持 区域%s"%quyu)
+
+
