e1dc7e9e9ed3f73d93a705089f2d28a4cba04321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:51 +0000

    Add package tf-reader/0.1/tf_reader-0.1.tar.gz

---
+++ packages/tf-reader/tf_reader-0.1.tar.gz/tf_reader-0.1/setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup
+
+def get_requirements():
+    with open("requirements.txt") as fp:
+        return fp.read()
+
+setup(
+    name="tf_reader",
+    py_modules=['tf_reader'],
+    version=0.01,
+    description="functions to read tfrecord file as python values.",
+    author="Ilya Kamenshchikov",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+    license="MIT",
+    install_requires=get_requirements(),
+    python_requires=">=3.6",
+)
