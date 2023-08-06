cabebfea7173ec7dc6311d5dcfa4b4e8d59d3a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:12 +0000

    Add package retrievepankey/0.1.10/retrievepankey-0.1.10.tar.gz

---
+++ packages/retrievepankey/retrievepankey-0.1.10.tar.gz/retrievepankey-0.1.10/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="retrievepankey",
+    version="0.1.10",
+    author="Brian Trost",
+    author_email="trostb@gmail.com",
+    description="Shortcut functions for coding and working with PAN devices.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    install_requires=['pyyaml'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
