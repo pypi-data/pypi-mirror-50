eeaf9a902ddaf21f5aaf06e4167f73fa456a167e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:41 +0000

    Add package hmtnote/0.7.1/hmtnote-0.7.1-py2.py3-none-any.whl

---
+++ packages/hmtnote/hmtnote-0.7.1-py2.py3-none-any.whl/hmtnote/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Created by Roberto Preste
+from hmtnote.hmtnote import annotate, dump
+
+__author__ = """Roberto Preste"""
+__email__ = 'robertopreste@gmail.com'
+__version__ = '0.7.1'
