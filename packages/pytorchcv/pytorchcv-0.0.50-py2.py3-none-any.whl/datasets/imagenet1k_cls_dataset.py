86ec2ae45a266f82c26cb693eec68116a6c38856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:48 +0000

    Add package pytorchcv/0.0.50/pytorchcv-0.0.50-py2.py3-none-any.whl

---
+++ packages/pytorchcv/pytorchcv-0.0.50-py2.py3-none-any.whl/datasets/imagenet1k_cls_dataset.py
@@ -0,0 +1,189 @@
+"""
+    ImageNet-1K classification dataset.
+"""
+
+import os
+import math
+import cv2
+import numpy as np
+from PIL import Image
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as transforms
+from .dataset_metainfo import DatasetMetaInfo
+
+
+class ImageNet1K(ImageFolder):
+    """
+    ImageNet-1K classification dataset.
+
+    Parameters
+    ----------
+    root : str, default '~/.torch/datasets/imagenet'
+        Path to the folder stored the dataset.
+    mode : str, default 'train'
+        'train', 'val', or 'test'.
+    transform : function, default None
+        A function that takes data and label and transforms them.
+    """
+    def __init__(self,
+                 root=os.path.join("~", ".torch", "datasets", "imagenet"),
+                 mode="train",
+                 transform=None):
+        split = "train" if mode == "train" else "val"
+        root = os.path.join(root, split)
+        super(ImageNet1K, self).__init__(root=root, transform=transform)
+
+
+class ImageNet1KMetaInfo(DatasetMetaInfo):
+    def __init__(self):
+        super(ImageNet1KMetaInfo, self).__init__()
+        self.label = "ImageNet1K"
+        self.short_label = "imagenet"
+        self.root_dir_name = "imagenet"
+        self.dataset_class = ImageNet1K
+        self.num_training_samples = None
+        self.in_channels = 3
+        self.num_classes = 1000
+        self.input_image_size = (224, 224)
+        self.resize_inv_factor = 0.875
+        self.train_metric_capts = ["Train.Top1"]
+        self.train_metric_names = ["Top1Error"]
+        self.train_metric_extra_kwargs = [{"name": "err-top1"}]
+        self.val_metric_capts = ["Val.Top1", "Val.Top5"]
+        self.val_metric_names = ["Top1Error", "TopKError"]
+        self.val_metric_extra_kwargs = [{"name": "err-top1"}, {"name": "err-top5", "top_k": 5}]
+        self.saver_acc_ind = 1
+        self.train_transform = imagenet_train_transform
+        self.val_transform = imagenet_val_transform
+        self.test_transform = imagenet_val_transform
+        self.ml_type = "imgcls"
+        self.use_cv_resize = False
+
+    def add_dataset_parser_arguments(self,
+                                     parser,
+                                     work_dir_path):
+        super(ImageNet1KMetaInfo, self).add_dataset_parser_arguments(parser, work_dir_path)
+        parser.add_argument(
+            "--input-size",
+            type=int,
+            default=self.input_image_size[0],
+            help="size of the input for model")
+        parser.add_argument(
+            "--resize-inv-factor",
+            type=float,
+            default=self.resize_inv_factor,
+            help="inverted ratio for input image crop")
+        parser.add_argument(
+            '--use-cv-resize',
+            action='store_true',
+            help='use OpenCV resize preprocessing')
+
+    def update(self,
+               args):
+        super(ImageNet1KMetaInfo, self).update(args)
+        self.input_image_size = (args.input_size, args.input_size)
+        self.use_cv_resize = args.use_cv_resize
+
+
+def imagenet_train_transform(ds_metainfo,
+                             mean_rgb=(0.485, 0.456, 0.406),
+                             std_rgb=(0.229, 0.224, 0.225),
+                             jitter_param=0.4):
+    input_image_size = ds_metainfo.input_image_size
+    return transforms.Compose([
+        transforms.RandomResizedCrop(input_image_size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(
+            brightness=jitter_param,
+            contrast=jitter_param,
+            saturation=jitter_param),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=mean_rgb,
+            std=std_rgb)
+    ])
+
+
+def imagenet_val_transform(ds_metainfo,
+                           mean_rgb=(0.485, 0.456, 0.406),
+                           std_rgb=(0.229, 0.224, 0.225)):
+    input_image_size = ds_metainfo.input_image_size
+    resize_value = calc_val_resize_value(
+        input_image_size=ds_metainfo.input_image_size,
+        resize_inv_factor=ds_metainfo.resize_inv_factor)
+    return transforms.Compose([
+        CvResize(resize_value) if ds_metainfo.use_cv_resize else transforms.Resize(resize_value),
+        transforms.CenterCrop(size=input_image_size),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=mean_rgb,
+            std=std_rgb)
+    ])
+
+
+class CvResize(object):
+    """
+    Resize the input PIL Image to the given size via OpenCV.
+
+    Parameters
+    ----------
+    size : int or tuple of (W, H)
+        Size of output image.
+    interpolation : int, default PIL.Image.BILINEAR
+        Interpolation method for resizing. By default uses bilinear
+        interpolation.
+    """
+    def __init__(self,
+                 size,
+                 interpolation=Image.BILINEAR):
+        self.size = size
+        self.interpolation = interpolation
+
+    def __call__(self, img):
+        """
+        Resize image.
+
+        Parameters
+        ----------
+        img : PIL.Image
+            input image.
+
+        Returns
+        -------
+        PIL.Image
+            Resulted image.
+        """
+        if self.interpolation == Image.NEAREST:
+            cv_interpolation = cv2.INTER_NEAREST
+        elif self.interpolation == Image.BILINEAR:
+            cv_interpolation = cv2.INTER_LINEAR
+        elif self.interpolation == Image.BICUBIC:
+            cv_interpolation = cv2.INTER_CUBIC
+        elif self.interpolation == Image.LANCZOS:
+            cv_interpolation = cv2.INTER_LANCZOS4
+        else:
+            raise ValueError()
+
+        cv_img = np.array(img)
+
+        if isinstance(self.size, int):
+            w, h = img.size
+            if (w <= h and w == self.size) or (h <= w and h == self.size):
+                return img
+            if w < h:
+                out_size = (self.size, int(self.size * h / w))
+            else:
+                out_size = (int(self.size * w / h), self.size)
+            cv_img = cv2.resize(cv_img, dsize=out_size, interpolation=cv_interpolation)
+            return Image.fromarray(cv_img)
+        else:
+            cv_img = cv2.resize(cv_img, dsize=self.size, interpolation=cv_interpolation)
+            return Image.fromarray(cv_img)
+
+
+def calc_val_resize_value(input_image_size=(224, 224),
+                          resize_inv_factor=0.875):
+    if isinstance(input_image_size, int):
+        input_image_size = (input_image_size, input_image_size)
+    resize_value = int(math.ceil(float(input_image_size[0]) / resize_inv_factor))
+    return resize_value
---
+++ packages/pytorchcv/pytorchcv-0.0.50-py2.py3-none-any.whl/pytorchcv/models/preresnet_cifar10.py
@@ -0,0 +1,212 @@
+"""
+    PreResNet for CIFAR-10, implemented in PyTorch.
+    Original papers: 'Identity Mappings in Deep Residual Networks,' https://arxiv.org/abs/1603.05027.
+"""
+
+__all__ = ['CIFAR10PreResNet', 'preresnet20_cifar10', 'preresnet56_cifar10', 'preresnet110_cifar10']
+
+import os
+import torch.nn as nn
+import torch.nn.init as init
+from .common import conv3x3
+from .preresnet import PreResUnit, PreResActivation
+
+
+class CIFAR10PreResNet(nn.Module):
+    """
+    PreResNet model for CIFAR-10 from 'Identity Mappings in Deep Residual Networks,' https://arxiv.org/abs/1603.05027.
+
+    Parameters:
+    ----------
+    channels : list of list of int
+        Number of output channels for each unit.
+    init_block_channels : int
+        Number of output channels for the initial unit.
+    in_channels : int, default 3
+        Number of input channels.
+    in_size : tuple of two ints, default (32, 32)
+        Spatial size of the expected input image.
+    num_classes : int, default 10
+        Number of classification classes.
+    """
+    def __init__(self,
+                 channels,
+                 init_block_channels,
+                 in_channels=3,
+                 in_size=(32, 32),
+                 num_classes=10):
+        super(CIFAR10PreResNet, self).__init__()
+        self.in_size = in_size
+        self.num_classes = num_classes
+
+        self.features = nn.Sequential()
+        self.features.add_module("init_block", conv3x3(
+            in_channels=in_channels,
+            out_channels=init_block_channels))
+        in_channels = init_block_channels
+        for i, channels_per_stage in enumerate(channels):
+            stage = nn.Sequential()
+            for j, out_channels in enumerate(channels_per_stage):
+                stride = 2 if (j == 0) and (i != 0) else 1
+                stage.add_module("unit{}".format(j + 1), PreResUnit(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    stride=stride,
+                    bottleneck=False,
+                    conv1_stride=False))
+                in_channels = out_channels
+            self.features.add_module("stage{}".format(i + 1), stage)
+        self.features.add_module("post_activ", PreResActivation(in_channels=in_channels))
+        self.features.add_module("final_pool", nn.AvgPool2d(
+            kernel_size=8,
+            stride=1))
+
+        self.output = nn.Linear(
+            in_features=in_channels,
+            out_features=num_classes)
+
+        self._init_params()
+
+    def _init_params(self):
+        for name, module in self.named_modules():
+            if isinstance(module, nn.Conv2d):
+                init.kaiming_uniform_(module.weight)
+                if module.bias is not None:
+                    init.constant_(module.bias, 0)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.output(x)
+        return x
+
+
+def get_preresnet_cifar10(blocks,
+                          model_name=None,
+                          pretrained=False,
+                          root=os.path.join('~', '.torch', 'models'),
+                          **kwargs):
+    """
+    Create PreResNet model for CIFAR-10 with specific parameters.
+
+    Parameters:
+    ----------
+    blocks : int
+        Number of blocks.
+    model_name : str or None, default None
+        Model name for loading pretrained model.
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.torch/models'
+        Location for keeping the model parameters.
+    """
+
+    assert ((blocks - 2) % 6 == 0)
+    layers = [(blocks - 2) // 6] * 3
+    channels_per_layers = [16, 32, 64]
+    init_block_channels = 16
+
+    channels = [[ci] * li for (ci, li) in zip(channels_per_layers, layers)]
+
+    net = CIFAR10PreResNet(
+        channels=channels,
+        init_block_channels=init_block_channels,
+        **kwargs)
+
+    if pretrained:
+        if (model_name is None) or (not model_name):
+            raise ValueError("Parameter `model_name` should be properly initialized for loading pretrained model.")
+        from .model_store import download_model
+        download_model(
+            net=net,
+            model_name=model_name,
+            local_model_store_dir_path=root)
+
+    return net
+
+
+def preresnet20_cifar10(**kwargs):
+    """
+    PreResNet-20 model for CIFAR-10 from 'Identity Mappings in Deep Residual Networks,'
+    https://arxiv.org/abs/1603.05027.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.torch/models'
+        Location for keeping the model parameters.
+    """
+    return get_preresnet_cifar10(blocks=20, model_name="preresnet20_cifar10", **kwargs)
+
+
+def preresnet56_cifar10(**kwargs):
+    """
+    PreResNet-56 model for CIFAR-10 from 'Identity Mappings in Deep Residual Networks,'
+    https://arxiv.org/abs/1603.05027.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.torch/models'
+        Location for keeping the model parameters.
+    """
+    return get_preresnet_cifar10(blocks=56, model_name="preresnet56_cifar10", **kwargs)
+
+
+def preresnet110_cifar10(**kwargs):
+    """
+    PreResNet-110 model for CIFAR-10 from 'Identity Mappings in Deep Residual Networks,'
+    https://arxiv.org/abs/1603.05027.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.torch/models'
+        Location for keeping the model parameters.
+    """
+    return get_preresnet_cifar10(blocks=110, model_name="preresnet110_cifar10", **kwargs)
+
+
+def _calc_width(net):
+    import numpy as np
+    net_params = filter(lambda p: p.requires_grad, net.parameters())
+    weight_count = 0
+    for param in net_params:
+        weight_count += np.prod(param.size())
+    return weight_count
+
+
+def _test():
+    import torch
+    from torch.autograd import Variable
+
+    pretrained = False
+
+    models = [
+        preresnet20_cifar10,
+        preresnet56_cifar10,
+        preresnet110_cifar10,
+    ]
+
+    for model in models:
+
+        net = model(pretrained=pretrained)
+
+        # net.train()
+        net.eval()
+        weight_count = _calc_width(net)
+        print("m={}, {}".format(model.__name__, weight_count))
+        assert (model != preresnet20_cifar10 or weight_count == 272282)
+        assert (model != preresnet56_cifar10 or weight_count == 855578)
+        assert (model != preresnet110_cifar10 or weight_count == 1730522)
+
+        x = Variable(torch.randn(1, 3, 32, 32))
+        y = net(x)
+        assert (tuple(y.size()) == (1, 10))
+
+
+if __name__ == "__main__":
+    _test()
