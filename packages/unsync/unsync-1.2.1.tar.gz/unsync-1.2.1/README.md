13a731c4cd7966f803ad2fad8f47baf20d62b2a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:34 +0000

    Add package unsync/1.2.1/unsync-1.2.1.tar.gz

---
+++ packages/unsync/unsync-1.2.1.tar.gz/unsync-1.2.1/README.md
@@ -0,0 +1,124 @@
+# unsync
+Unsynchronize `asyncio` by using an ambient event loop in a separate thread.
+
+# Rules for unsync
+1. Mark all async functions with `@unsync`. May also mark regular functions to execute in a separate thread.
+    * All `@unsync` functions, async or not, return an `Unfuture`
+2. All `Futures` must be `Unfutures` which includes the result of an `@unsync` function call,
+    or wrapping `Unfuture(asyncio.Future)` or `Unfuture(concurrent.Future)`.
+    `Unfuture` combines the behavior of `asyncio.Future` and `concurrent.Future`:
+    * `Unfuture.set_value` is threadsafe unlike `asyncio.Future`
+    * `Unfuture` instances can be awaited, even if made from `concurrent.Future`
+    * `Unfuture.result()` is a blocking operation *except* in `unsync.loop`/`unsync.thread` where
+    it behaves like `asyncio.Future.result` and will throw an exception if the future is not done
+3. Functions will execute in different contexts:
+    * `@unsync` async functions will execute in an event loop in `unsync.thread`
+    * `@unsync` regular functions will execute in `unsync.thread_executor`, a `ThreadPoolExecutor`
+    * `@unsync(cpu_bound=True)` regular functions will execute in `unsync.process_executor`, a `ProcessPoolExecutor`
+
+
+# Examples
+## Simple Sleep
+A simple sleeping example with `asyncio`:
+```python
+async def sync_async():
+    await asyncio.sleep(0.1)
+    return 'I hate event loops'
+
+annoying_event_loop = asyncio.get_event_loop()
+future = asyncio.ensure_future(sync_async(), loop=annoying_event_loop)
+annoying_event_loop.run_until_complete(future)
+print(future.result())
+```
+
+Same example with `unsync`:
+```python
+@unsync
+async def unsync_async():
+    await asyncio.sleep(0.1)
+    return 'I like decorators'
+
+print(unsync_async().result())
+```
+
+## Threading a synchronous function
+Synchronous functions can be made to run asynchronously by executing them in a `concurrent.ThreadPoolExecutor`.
+This can be easily accomplished by marking the regular function `@unsync`.
+```python
+@unsync
+def non_async_function(seconds):
+    time.sleep(seconds)
+    return 'Run in parallel!'
+
+start = time.time()
+tasks = [non_async_function(0.1) for _ in range(10)]
+print([task.result() for task in tasks])
+print('Executed in {} seconds'.format(time.time() - start))
+```
+Which prints:
+
+    ['Run in parallel!', 'Run in parallel!', ...]
+    Executed in 0.10807514190673828 seconds
+
+## Continuations
+Using Unfuture.then chains asynchronous calls and returns an Unfuture that wraps both the source, and continuation.
+The continuation is invoked with the source Unfuture as the first argument.
+Continuations can be regular functions (which will execute synchronously), or `@unsync` functions.
+```python
+@unsync
+async def initiate(request):
+    await asyncio.sleep(0.1)
+    return request + 1
+
+@unsync
+async def process(task):
+    await asyncio.sleep(0.1)
+    return task.result() * 2
+
+start = time.time()
+print(initiate(3).then(process).result())
+print('Executed in {} seconds'.format(time.time() - start))
+```
+Which prints:
+
+    8
+    Executed in 0.20314741134643555 seconds
+
+## Mixing methods
+
+We'll start by converting a regular synchronous function into a threaded `Unfuture` which will begin our request.
+```python
+@unsync
+def non_async_function(num):
+    time.sleep(0.1)
+    return num, num + 1
+```
+We may want to refine the result in another function, so we define the following continuation.
+```python
+@unsync
+async def result_continuation(task):
+    await asyncio.sleep(0.1)
+    num, res = task.result()
+    return num, res * 2
+```
+We then aggregate all the results into a single dictionary in an async function.
+```python
+@unsync
+async def result_processor(tasks):
+    output = {}
+    for task in tasks:
+        num, res = await task
+        output[num] = res
+    return output
+```
+Executing the full chain of `non_async_function`&rightarrow;`result_continuation`&rightarrow;`result_processor` would look like:
+```python
+start = time.time()
+print(result_processor([non_async_function(i).then(result_continuation) for i in range(10)]).result())
+print('Executed in {} seconds'.format(time.time() - start))
+```
+
+Which prints:
+
+    {0: 2, 1: 4, 2: 6, 3: 8, 4: 10, 5: 12, 6: 14, 7: 16, 8: 18, 9: 20}
+    Executed in 0.22115683555603027 seconds
---
+++ packages/unsync/unsync-1.2.1.tar.gz/unsync-1.2.1/unsync/unsync.py
@@ -0,0 +1,135 @@
+import asyncio
+import concurrent
+import functools
+import inspect
+import threading
+import os
+from threading import Thread
+from typing import Generic, TypeVar
+
+
+class unsync(object):
+    thread_executor = concurrent.futures.ThreadPoolExecutor()
+    process_executor = concurrent.futures.ProcessPoolExecutor()
+    loop = asyncio.new_event_loop()
+    thread = None
+    unsync_functions = {}
+
+    @staticmethod
+    def thread_target(loop):
+        asyncio.set_event_loop(loop)
+        loop.run_forever()
+
+    def __init__(self, *args, **kwargs):
+        self.args = []
+        self.kwargs = {}
+        if len(args) == 1 and inspect.isfunction(args[0]):
+            self._set_func(args[0])
+        else:
+            self.args = args
+            self.kwargs = kwargs
+            self.func = None
+
+    @property
+    def cpu_bound(self):
+        return 'cpu_bound' in self.kwargs and self.kwargs['cpu_bound']
+
+    def _set_func(self, func):
+        assert inspect.isfunction(func)
+        self.func = func
+        functools.update_wrapper(self, func)
+        unsync.unsync_functions[(func.__module__, func.__name__)] = func
+
+    def __call__(self, *args, **kwargs):
+        if self.func is None:
+            self._set_func(args[0])
+            return self
+        if inspect.iscoroutinefunction(self.func):
+            if self.cpu_bound:
+                raise TypeError('The CPU bound unsync function %s may not be async or a coroutine' % self.func.__name__)
+            future = self.func(*args, **kwargs)
+        else:
+            if self.cpu_bound:
+                future = unsync.process_executor.submit(
+                    _multiprocess_target, (self.func.__module__, self.func.__name__), *args, **kwargs)
+            else:
+                future = unsync.thread_executor.submit(self.func, *args, **kwargs)
+        return Unfuture(future)
+
+    def __get__(self, instance, owner):
+        def _call(*args, **kwargs):
+            return self(instance, *args, **kwargs)
+
+        functools.update_wrapper(_call, self.func)
+        return _call
+
+
+def _multiprocess_target(func_name, *args, **kwargs):
+    # On Windows MP turns the main module into __mp_main__ in multiprocess targets
+    if os.name == 'nt' and func_name[0] == '__main__':
+        func_name = ('__mp_main__', func_name[1])
+    __import__(func_name[0])
+    return unsync.unsync_functions[func_name](*args, **kwargs)
+
+
+T = TypeVar('T')
+
+class Unfuture(Generic[T]):
+    @staticmethod
+    def from_value(value):
+        future = Unfuture()
+        future.set_result(value)
+        return future
+
+    def __init__(self, future=None):
+        def callback(source, target):
+            try:
+                asyncio.futures._chain_future(source, target)
+            except Exception as exc:
+                if self.concurrent_future.set_running_or_notify_cancel():
+                    self.concurrent_future.set_exception(exc)
+                raise
+
+        if asyncio.iscoroutine(future):
+            future = asyncio.ensure_future(future, loop=unsync.loop)
+        if isinstance(future, concurrent.futures.Future):
+            self.concurrent_future = future
+            self.future = asyncio.Future(loop=unsync.loop)
+            self.future._loop.call_soon_threadsafe(callback, self.concurrent_future, self.future)
+        else:
+            self.future = future or asyncio.Future(loop=unsync.loop)
+            self.concurrent_future = concurrent.futures.Future()
+            self.future._loop.call_soon_threadsafe(callback, self.future, self.concurrent_future)
+
+    def __iter__(self):
+        return self.future.__iter__()
+
+    __await__ = __iter__
+
+    def result(self, *args, **kwargs) -> T:
+        # The asyncio Future may have completed before the concurrent one
+        if self.future.done():
+            return self.future.result()
+        # Don't allow waiting in the unsync.thread loop since it will deadlock
+        if threading.current_thread() == unsync.thread and not self.concurrent_future.done():
+            raise asyncio.InvalidStateError
+        # Wait on the concurrent Future outside unsync.thread
+        return self.concurrent_future.result(*args, **kwargs)
+
+    def done(self):
+        return self.future.done() or self.concurrent_future.done()
+
+    def set_result(self, value):
+        return self.future._loop.call_soon_threadsafe(lambda: self.future.set_result(value))
+
+    @unsync
+    async def then(self, continuation):
+        await self
+        result = continuation(self)
+        if hasattr(result, '__await__'):
+            return await result
+        return result
+
+
+unsync.thread = Thread(target=unsync.thread_target, args=(unsync.loop,), daemon=True)
+unsync.thread.start()
