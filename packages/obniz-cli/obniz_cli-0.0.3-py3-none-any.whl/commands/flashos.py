e3c9c9062c92feda7e46f9b665edb321670039e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:18 +0000

    Add package obniz-cli/0.0.3/obniz_cli-0.0.3-py3-none-any.whl

---
+++ packages/obniz-cli/obniz_cli-0.0.3-py3-none-any.whl/commands/flashos.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import tempfile
+import subprocess
+
+from . import util
+
+def command(args):
+    if args.port:
+        selected_port = args.port
+    else:
+        selected_port = util.select_port()
+
+    # obnizOSのクローンおよび更新
+    print()
+    ## 一時ファイルにobnizOSをクローンしてくる
+    with tempfile.TemporaryDirectory() as dirname:
+        proc = subprocess.run(
+            "git clone git@github.com:obniz/obnizos-esp32w.git",
+            shell=True,
+            cwd=dirname
+        )
+        # obnizOSの書き込み
+        print()
+        proc = subprocess.run(
+            "esptool.py --port {} -b {} --after hard_reset write_flash 0x1000 bootloader.bin 0x10000 obniz.bin 0x8000 partitions.bin".format(selected_port, args.bps),
+            shell=True,
+            cwd=os.path.join(dirname, "obnizos-esp32w")
+        )
