c7e3485c2a7c96b615c7630fb48571d5ce86755a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:52 +0000

    Add package hearthstone-card-generator/1.1/hearthstone_card_generator-1.1-py3-none-any.whl

---
+++ packages/hearthstone-card-generator/hearthstone_card_generator-1.1-py3-none-any.whl/hearthstone_card_generator/card_generator.py
@@ -0,0 +1,84 @@
+from .hearthstone_card import HearthstoneCard
+from pyykov import Pyykov
+import random
+
+class CardGenerator:
+    def __init__(self, cardInformationDatabase, descriptionClarity = 2, phraseLength = 2):
+        self.__descriptionClarity = min(max(1, descriptionClarity), 5)
+        self.__phraseLength = max(1, phraseLength)
+        self.__cardInformationDatabase = cardInformationDatabase
+        self.__load_card_information()
+
+    def __load_card_information(self):
+        self.__cards = self.__cardInformationDatabase.get_cards()
+        self.__load_names()
+        self.__load_descriptions()
+        self.__load_player_classes()
+        self.__load_card_types()
+        self.__load_rarities()
+        self.__load_races()
+        self.__load_images()
+        self.__load_gold_images()
+
+    def __load_names(self):
+        self.__names = unique(map(lambda card: card.name, self.__cards))
+    def __load_descriptions(self):
+        self.__descriptions = list(filter(lambda description: description != '', map(lambda card: card.description, self.__cards)))
+        self.__markovGenerator = Pyykov(self.__descriptionClarity)
+        self.__markovGenerator.set_source(self.__descriptions)
+    def __load_player_classes(self):
+        playerClasses = unique(map(lambda card: card.playerClass, self.__cards))
+        self.__playerClasses = playerClasses
+        self.__playerClassWeights = list(map(lambda playerClass: sum(1 for card in self.__cards if card.playerClass == playerClass), playerClasses))
+    def __load_card_types(self):
+        cardTypes = unique(map(lambda card: card.cardType, self.__cards))
+        self.__cardTypes = cardTypes
+        self.__cardTypesWeights = list(map(lambda cardType: sum(1 for card in self.__cards if card.cardType == cardType), cardTypes))
+    def __load_rarities(self):
+        rarities = unique(map(lambda card : card.rarity, self.__cards))
+        self.__rarities = rarities
+        self.__raritiesWeights = list(map(lambda rarity: sum(1 for card in self.__cards if card.rarity == rarity), rarities))
+    def __load_races(self):
+        races = unique(map(lambda card: card.race, self.__cards))
+        self.__races = races
+        self.__racesWeights = list(map(lambda race: sum(1 for card in self.__cards if card.race == race), races))
+    def __load_images(self):
+        self.__images = unique(map(lambda card: card.image, self.__cards))
+    def __load_gold_images(self):
+        self.__goldImages = unique(map(lambda card: card.goldImage, self.__cards))
+
+    def generate_random_card(self, name=None, description=None, manaCost=None, attack=None, health=None, durability=None, playerClass=None, cardType=None, rarity=None, race=None, image=None, goldImage=None):
+        cardType = cardType or random.choices(self.__cardTypes, weights=self.__cardTypesWeights)[0]
+        attack = attack or (random.choice(range(13)) if cardType != 'Spell' else 0)
+        health = health or (random.choice(range(1, 13)) if cardType == 'Minion' else 0)
+        durability = durability or (random.choice(range(1, 13)) if cardType == 'Weapon' else 0)
+        race = race or (random.choices(self.__races, weights=self.__racesWeights)[0] if cardType == 'Minion' else '')
+        return HearthstoneCard(
+            name = name or random.choice(self.__names),
+            description = description or self.__generate_description(),
+            manaCost = manaCost or random.choice(range(11)),
+            attack = attack,
+            health = health,
+            durability = durability,
+            playerClass = playerClass or random.choices(self.__playerClasses, weights=self.__playerClassWeights)[0],
+            cardType = cardType,
+            rarity = rarity or random.choices(self.__rarities, weights=self.__raritiesWeights)[0],
+            race = race,
+            image = image or random.choice(self.__images),
+            goldImage = goldImage or random.choice(self.__goldImages))
+
+    def __generate_description(self):
+        description = ''
+        for i in range(self.__phraseLength):
+            description += self.__markovGenerator.generate_phrase()
+            if i != self.__phraseLength - 1:
+                if description[-1] != '.':
+                    description += '.'
+                description += '  '
+        return description
+
+    def get_card_information(self, name):
+        return next((card for card in self.__cards if card.name.lower() == name.lower()), None)
+
+def unique(collection):
+    return list(set(collection))
---
+++ packages/hearthstone-card-generator/hearthstone_card_generator-1.1-py3-none-any.whl/hearthstone_card_generator/card_generator_test.py
@@ -0,0 +1,67 @@
+import unittest
+from .card_generator import CardGenerator
+from .hearthstone_card import HearthstoneCard
+
+class CardGeneratorTests(unittest.TestCase):
+    def setUp(self):
+        self.name = 'name'
+        self.description = 'This is a description.'
+        self.manaCost = 1
+        self.attack = 2
+        self.health = 3
+        self.durability = 4
+        self.playerClass = 'playerClass'
+        self.cardType = 'cardType'
+        self.rarity = 'rarity'
+        self.race = 'race'
+        self.image = 'image'
+        self.goldImage = 'goldImage'
+        self.hearthstoneCard = HearthstoneCard(self.name, self.description, self.manaCost, self.attack, self.health, self.durability, self.playerClass, self.cardType, self.rarity, self.race, self.image, self.goldImage)
+    
+    def test_generate_random_card_with_set_values_uses_those_values(self):
+        hearthstoneCard2 = HearthstoneCard('asdf', 'fdsa', 99, 102, 1000, 1020, 'madeup', 'nonsense', 'words', 'here', 'for', 'testing')
+        database = MockCardDatabase([self.hearthstoneCard, hearthstoneCard2])
+        self.cardGenerator = CardGenerator(database)
+        self.assertEqual(self.cardGenerator.generate_random_card(name=self.name).name, self.name)
+        self.assertEqual(self.cardGenerator.generate_random_card(description=self.description).description, self.description)
+        self.assertEqual(self.cardGenerator.generate_random_card(manaCost=self.manaCost).manaCost, self.manaCost)
+        self.assertEqual(self.cardGenerator.generate_random_card(attack=self.attack).attack, self.attack)
+        self.assertEqual(self.cardGenerator.generate_random_card(health=self.health).health, self.health)
+        self.assertEqual(self.cardGenerator.generate_random_card(playerClass=self.playerClass).playerClass, self.playerClass)
+        self.assertEqual(self.cardGenerator.generate_random_card(cardType=self.cardType).cardType, self.cardType)
+        self.assertEqual(self.cardGenerator.generate_random_card(rarity=self.rarity).rarity, self.rarity)
+        self.assertEqual(self.cardGenerator.generate_random_card(race=self.race).race, self.race)
+        self.assertEqual(self.cardGenerator.generate_random_card(image=self.image).image, self.image)
+        self.assertEqual(self.cardGenerator.generate_random_card(goldImage=self.goldImage).goldImage, self.goldImage)
+
+    def test_generate_random_card_properly_handles_card_type_spell(self):
+        database = MockCardDatabase([self.hearthstoneCard])
+        self.cardGenerator = CardGenerator(database)
+        spell = self.cardGenerator.generate_random_card(cardType='Spell')
+        self.assertEqual(spell.attack, 0)
+        self.assertEqual(spell.health, 0)
+        self.assertEqual(spell.durability, 0)
+
+    def test_generate_random_card_properly_handles_card_type_weapon(self):
+        database = MockCardDatabase([self.hearthstoneCard])
+        self.cardGenerator = CardGenerator(database)
+        weapon = self.cardGenerator.generate_random_card(cardType='Weapon')
+        self.assertEqual(weapon.health, 0)
+    
+    def test_generate_random_card_properly_handles_card_type_minion(self):
+        database = MockCardDatabase([self.hearthstoneCard])
+        self.cardGenerator = CardGenerator(database)
+        minion = self.cardGenerator.generate_random_card(cardType='Minion')
+        self.assertEqual(minion.durability, 0)
+
+    def test_get_card_information_gets_proper_card_info(self):
+        database = MockCardDatabase([self.hearthstoneCard])
+        self.cardGenerator = CardGenerator(database)
+        self.assertEqual(self.cardGenerator.get_card_information(self.name), self.hearthstoneCard)
+
+
+class MockCardDatabase:
+    def __init__(self, cards):
+        self.__cards = cards
+    def get_cards(self):
+        return self.__cards
