eeb4e6804b0cb2c7557d63e92d56139399a93cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:27 +0000

    Add package md-test-package/0.1/md_test_package-0.1.tar.gz

---
+++ packages/md-test-package/md_test_package-0.1.tar.gz/md_test_package-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='md_test_package',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['md_test_package'],
+      zip_safe=False)
