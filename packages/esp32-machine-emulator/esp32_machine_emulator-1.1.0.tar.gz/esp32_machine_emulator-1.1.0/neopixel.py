1ee68ee8e3b3e66f9c14617548320459a016ce12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:43 +0000

    Add package esp32-machine-emulator/1.1.0/esp32_machine_emulator-1.1.0.tar.gz

---
+++ packages/esp32-machine-emulator/esp32_machine_emulator-1.1.0.tar.gz/esp32_machine_emulator-1.1.0/neopixel.py
@@ -0,0 +1,55 @@
+import time, copy
+
+class NeoPixel:
+
+    def __init__(self, pin, n, bpp=3, timing=1):
+        self.pin = pin
+        self.n = n
+        self.bpp = bpp
+        self.timing = timing
+        self.writesForTesting = []
+        self.firstWriteTimeForTesting = None
+        self.currentPixelColors = StrandHistoryForTesting(n, bpp)
+
+    def fill(self, color):
+        for i in range(self.n):
+            self[i] = color
+
+    def __setitem__(self, index, val):
+        self.currentPixelColors[index] = val
+
+    def __getitem__(self, index):
+        return self.currentPixelColors[index]
+
+    def write(self):
+        if self.firstWriteTimeForTesting is None:
+            self.firstWriteTimeForTesting = self._perfTimeMs()
+            self.currentPixelColors.timeFromFirstWrite = 0
+        else:
+            self.currentPixelColors.timeFromFirstWrite = self._perfTimeMs() - self.firstWriteTimeForTesting
+
+        self.writesForTesting.append(copy.deepcopy(self.currentPixelColors))
+
+    def _perfTimeMs(self):
+        return round(time.perf_counter() * 1000)
+
+class StrandHistoryForTesting:
+
+    timeFromFirstWrite = 0
+
+    def __init__(self, numPixels, bytesPerPixel):
+        self.n = numPixels
+        self.bytesPerPixel = bytesPerPixel
+        if bytesPerPixel == 3:
+            self.defaultColor = (0,0,0)
+        elif bytesPerPixel == 4:
+            self.defaultColor = (0, 0, 0, 0)
+        else:
+            raise OSError
+        self.colorList = [self.defaultColor] * numPixels
+
+    def __setitem__(self, index, val):
+        self.colorList[index] = val
+
+    def __getitem__(self, index):
+        return self.colorList[index]
