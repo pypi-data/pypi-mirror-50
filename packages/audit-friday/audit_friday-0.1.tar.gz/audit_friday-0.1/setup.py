c4f8383458c97b8a95ab87ef3384a76adae4ffb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:42 +0000

    Add package audit-friday/0.1/audit_friday-0.1.tar.gz

---
+++ packages/audit-friday/audit_friday-0.1.tar.gz/audit_friday-0.1/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+     name='audit_friday',    # This is the name of your PyPI-package.
+     version='0.1',                          # Update the version number for new releases
+     scripts=['app.py']                  # The name of your scipt, and also the command you'll be using for calling it
+ )
