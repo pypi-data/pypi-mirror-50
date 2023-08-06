b6a8a799dc46c3b8970f6cdde270974521e1c0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:01 +0000

    Add package opossum/0.0.8/opossum-0.0.8.tar.gz

---
+++ packages/opossum/opossum-0.0.8.tar.gz/opossum-0.0.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup_args = dict(
+    name='opossum',
+    version='0.0.8',
+    description='Simulated Data Generating Process',
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    license='MIT',
+    url="https://humboldt-wi.github.io/blog/research/",
+    author='Tobias Krebs, Julian Winkel',
+    author_email='julian.winkel@hu-berlin.de'
+)
+
+install_requires = [
+    'numpy',
+    'statsmodels',
+    'seaborn',
+    'matplotlib',
+    'time',
+    'scipy',
+    'os'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
+
+
