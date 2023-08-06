6933a6c8854b184c6abe7b907569badd8aba2eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:52 +0000

    Add package nhentai-downloader/0.5.5.2/nhentai-downloader-0.5.5.2.tar.gz

---
+++ packages/nhentai-downloader/nhentai-downloader-0.5.5.2.tar.gz/nhentai-downloader-0.5.5.2/nhentai_downloader/constant.py
@@ -0,0 +1,32 @@
+import os
+
+
+urls = {
+    'LOGIN_URL' : 'https://nhentai.net/login/',
+    'FAV_URL' : 'https://nhentai.net/favorites/?q=',
+    'API_URL' : 'https://nhentai.net/api/gallery/',
+    'MEDIA_URL' : 'https://i.nhentai.net/galleries/',
+    'SEARCH' : 'https://nhentai.net/search/?q=',
+    'GALLERY_URL' : 'https://nhentai.net/g/'
+}
+
+INVALID_CHARACTERS = ['/', '\\', ':', '*', '?', '\"', '<', '>', '|']
+CONTROL_CHARACTERS = ['\\t','\\n']
+ok_code = 200
+
+VERBOSE_LEVEL = 15
+
+
+FILE_EXTENSION_LENGHT = 5
+WINDOWS_MAX_PATH_LENGHT = 260 - FILE_EXTENSION_LENGHT
+LINUX_MAX_FILENAME_LENGHT = 255 - FILE_EXTENSION_LENGHT
+
+
+def bytes_to_si(value,target_unit):
+    units = {
+        'K' : 1024,
+        'M' : 1024*1024,
+        'G' : 1024*1024*1024
+    }
+    
+    return value/units[target_unit]
---
+++ packages/nhentai-downloader/nhentai-downloader-0.5.5.2.tar.gz/nhentai-downloader-0.5.5.2/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+from nhentai_downloader import __version__,__author__,__email__
+
+
+with open("requirements.txt") as file:
+    requirements = file.read().splitlines()
+    
+with open("README.md", encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='nhentai-downloader',
+    install_requires=requirements,
+    data_files = [("", [".commit_date","requirements.txt"])],
+    version = __version__,
+    packages = find_packages(),
+    author=__author__,
+    author_email=__email__,
+    keywords='nhentai, doujinshi',
+    description="Nhentai doujinshi fetcher and downloader",
+    include_package_data=True,
+    zip_safe=False,
+    license = 'Apache',
+    url = 'https://gitlab.com/DukeValentine/nhentai-downloader',
+    download_url = 'https://gitlab.com/DukeValentine/nhentai-downloader/-/archive/master/nhentai-downloader-master.tar.gz',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    
+    entry_points={
+        'console_scripts': [
+            'nhentai-downloader = nhentai_downloader.nhentai:main',
+    ]},
+        
+    
+        
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.7',
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        
+    ],
+    
+)
+
