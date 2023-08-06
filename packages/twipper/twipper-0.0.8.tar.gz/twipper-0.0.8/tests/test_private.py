dcdedfe8e9d9bc78a3e0404d04145a7d77a83691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:32 +0000

    Add package twipper/0.0.8/twipper-0.0.8.tar.gz

---
+++ packages/twipper/twipper-0.0.8.tar.gz/twipper-0.0.8/tests/test_private.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Alvaro Bartolome @ alvarob96 on GitHub'
+__version__ = '0.0.8'
+
+from twipper.streaming import stream_country_tweets
+from twipper.credentials import Twipper
+
+
+credentials = Twipper(consumer_key='4mfeYj9SH1PXsff5HgZ5bTOzW',
+                      consumer_secret='aiEfkE5tYCKcKEg42OcHbNgVvA2uikwwpRuBYwl5VmGSAm7N7e',
+                      access_token='254490342-xIGPeyGYLDyD1uFIVgybDsd9ImUPaz467kowIoem',
+                      access_token_secret='JrEuYnm1s44JR46AQDdoJagee3Uw9Ip0jxlp7z3XFcPDg')
+
+api = credentials.get_api()
+auth = credentials.get_oauth()
+
+try:
+    tweets = stream_country_tweets(api=api,
+                                   auth=auth,
+                                   country='spain',
+                                   language='es',
+                                   filter_retweets=False,
+                                   tweet_limit=10,
+                                   retry='no_limit')  # after cleaning maybe some are empty or invalid
+except:
+    credentials.invalidate_oauth_token()
+
+credentials.invalidate_oauth_token()
+
+for tweet in tweets:
+    print(tweet)
