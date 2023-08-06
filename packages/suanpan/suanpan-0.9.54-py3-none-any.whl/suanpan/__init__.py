4aa0310d13fde9b9f051d4f4abf6f9c2aa9090ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:02 +0000

    Add package suanpan/0.9.54/suanpan-0.9.54-py3-none-any.whl

---
+++ packages/suanpan/suanpan-0.9.54-py3-none-any.whl/suanpan/__init__.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan.objects import Global
+from suanpan.run import cli, env, run
+
+__version__ = "0.9.54"
+
+g = Global()
