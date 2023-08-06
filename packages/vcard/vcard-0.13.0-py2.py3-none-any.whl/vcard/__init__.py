5b3a994dd3d46d394f604133cfca5856c233c63b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:36 +0000

    Add package vcard/0.13.0/vcard-0.13.0-py2.py3-none-any.whl

---
+++ packages/vcard/vcard-0.13.0-py2.py3-none-any.whl/vcard/__init__.py
@@ -0,0 +1,10 @@
+"""vcard package"""
+__package__ = "vcard"
+__author__ = "Victor Engmark"
+__copyright__ = "Copyright (C) 2010-2019 Victor Engmark"
+__credits__ = ["Victor Engmark", "Zearin"]
+__maintainer__ = "Victor Engmark"
+__email__ = "victor.engmark@gmail.com"
+__license__ = "AGPLv3+"
+__url__ = "https://gitlab.com/victor-engmark/vcard"
+__version__ = "0.13.0"
