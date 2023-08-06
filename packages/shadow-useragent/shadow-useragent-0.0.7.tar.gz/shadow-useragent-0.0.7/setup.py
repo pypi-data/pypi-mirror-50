cbe929fba8b54691a748355d2919187fc8808658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:49 +0000

    Add package shadow-useragent/0.0.7/shadow-useragent-0.0.7.tar.gz

---
+++ packages/shadow-useragent/shadow-useragent-0.0.7.tar.gz/shadow-useragent-0.0.7/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="shadow-useragent",
+    version="0.0.7",
+    author="Simon Rochwerg",
+    author_email="simon.rochwerg@lobstr.io",
+    description="Always get best user-agent",
+    long_description="Always keep your user-agent updated with the most recent and most common user-agents",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Lobstrio/shadow-useragent",
+    packages=setuptools.find_packages(),
+    include_package_data = True,
+    install_requires=[
+        'requests',
+        'pytz',
+        'coloredlogs'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
