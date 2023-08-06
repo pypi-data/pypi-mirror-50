f58200e2aefa98822a3e008dd276884cce13b9a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:48 +0000

    Add package cenv-tool/1.0.2/cenv_tool-1.0.2.tar.gz

---
+++ packages/cenv-tool/cenv_tool-1.0.2.tar.gz/cenv_tool-1.0.2/cenv_tool/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Conda environment creation and update from meta.yaml."""
+from pkg_resources import get_distribution
+
+try:
+    __version__ = get_distribution('cenv_tool').version
+except AttributeError:
+    __version__ = ''
