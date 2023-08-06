ce22ac88ca4e0fbf603e5f8002b3175d0f185e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:39 +0000

    Add package infi.rpc/0.2.9/infi.rpc-0.2.9.tar.gz

---
+++ packages/infi.rpc/infi.rpc-0.2.9.tar.gz/infi.rpc-0.2.9/src/infi/rpc/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.9"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.2.9)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '8a5c35806718d463d3661ca978740fc6415261b7'
+__git_head_subject__ = u'Finished Release v0.2.9'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2019-08-05 18:04:00'
