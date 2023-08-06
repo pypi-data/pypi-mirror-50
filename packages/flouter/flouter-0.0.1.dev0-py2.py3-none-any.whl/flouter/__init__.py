397ce708dcfbc15efa3527988a37de9bacbf2ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:00 +0000

    Add package flouter/0.0.1.dev0/flouter-0.0.1.dev0-py2.py3-none-any.whl

---
+++ packages/flouter/flouter-0.0.1.dev0-py2.py3-none-any.whl/flouter/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    flouter
+    ~~~~~
+
+    Convenient routing for a flask application
+
+    :copyright: 2019 Chris Zimmerman
+    :license: BSD-3-Clause
+"""
+
+__version__ = "0.0.1.dev"
