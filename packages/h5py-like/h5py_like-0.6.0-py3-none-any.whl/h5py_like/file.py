39eca0446a139c26a45988f5709bd1b489511e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:00 +0000

    Add package h5py-like/0.6.0/h5py_like-0.6.0-py3-none-any.whl

---
+++ packages/h5py-like/h5py_like-0.6.0-py3-none-any.whl/h5py_like/file.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+from .common import Mode, classname
+
+
+class FileMixin(ABC):
+    _is_file = True
+
+    @abstractmethod
+    def __init__(self, name, mode=Mode.default()):
+        self.filename: Path = Path(name).resolve()
+        self.basename = "/"
+        self._mode: Mode = Mode.from_str(mode)
+        self.parent = self
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.flush()
+        self.close()
+
+    def flush(self):
+        pass
+
+    def close(self):
+        pass
+
+    @property
+    def mode(self):
+        return self._mode
+
+    @property
+    def name(self):
+        return self.basename
+
+    def __str__(self):
+        return f"<{classname(self)}(filename='{self.filename}', mode='{self.mode}')>"
+
+    def __eq__(self, other):
+        try:
+            return all(
+                (
+                    isinstance(other, FileMixin),
+                    other._is_file,
+                    self.filename == other.filename,
+                    self.mode == other.mode,
+                )
+            )
+        except AttributeError:
+            return True
