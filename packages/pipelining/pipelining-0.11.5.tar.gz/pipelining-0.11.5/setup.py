a53fd0d9238e560019086fa8e1fccbd93c75691a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:46 +0000

    Add package pipelining/0.11.5/pipelining-0.11.5.tar.gz

---
+++ packages/pipelining/pipelining-0.11.5.tar.gz/pipelining-0.11.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pipelining',
+      version='0.11.5',
+      python_requires='>=3',
+      description='Declarative pipelines on top of python multiprocessing artifacts',
+      packages=['pipelining'],
+      install_requires=[],
+      zip_safe=False,
+    )
