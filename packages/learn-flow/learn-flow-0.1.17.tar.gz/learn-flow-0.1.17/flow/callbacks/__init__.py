1cfcc4d03e36eabf3a8535dd04166aeed96c2256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:16 +0000

    Add package learn-flow/0.1.17/learn-flow-0.1.17.tar.gz

---
+++ packages/learn-flow/learn-flow-0.1.17.tar.gz/learn-flow-0.1.17/flow/callbacks/__init__.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+from enum import Enum
+from blinker import signal
+
+class ModeEnum(Enum):
+    """Enumerator describing the optimization training strategy."""
+    MIN = 0
+    MAX = 1
+
+on_epoch_begin = signal("on_epoch_begin")
+on_epoch_end = signal("on_epoch_end")
+on_batch_begin = signal("on_batch_begin")
+on_batch_end = signal("on_batch_end")
+on_train_begin = signal("on_train_begin")
+on_train_end = signal("on_train_end")
+on_validate_begin = signal("on_validate_begin")
+on_validate_end = signal("on_validate_end")
+before_session_initialization = signal("before_session_initialization")
