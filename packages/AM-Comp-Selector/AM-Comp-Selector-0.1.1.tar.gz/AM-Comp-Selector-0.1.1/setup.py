e82e47bdd425c0b1dc08f4e03c56f903035f043c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:51 +0000

    Add package AM-Comp-Selector/0.1.1/AM-Comp-Selector-0.1.1.tar.gz

---
+++ packages/AM-Comp-Selector/AM-Comp-Selector-0.1.1.tar.gz/AM-Comp-Selector-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="AM-Comp-Selector",
+    version='0.1.1',
+    description="Generates company rankings for peer charts for valuations",
+    author="Salman Ahmed",
+    author_email="salman.ahmed@alvarezandmarsal.com",
+    url="https://github.com/sahmed1997",
+)
