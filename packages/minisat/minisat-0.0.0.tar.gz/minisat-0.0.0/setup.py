a1bbaf43e31d065055c98526c5b5b659077ad061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:05 +0000

    Add package minisat/0.0.0/minisat-0.0.0.tar.gz

---
+++ packages/minisat/minisat-0.0.0.tar.gz/minisat-0.0.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="minisat",
+    version="0.0.0",
+    packages=["minisat"],
+    author="pgdr",
+    author_email="pgdr@equinor.com",
+    url="https://github.com/pgdr",
+)
