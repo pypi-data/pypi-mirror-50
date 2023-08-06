ab921e154cafadc156672e36b85d5c968f651011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:05 +0000

    Add package cli-tool-test/0.0.2/cli_tool_test-0.0.2.tar.gz

---
+++ packages/cli-tool-test/cli_tool_test-0.0.2.tar.gz/cli_tool_test-0.0.2/cli_tool_test/__main__.py
@@ -0,0 +1,4 @@
+# from .prog import main
+
+
+# main()
---
+++ packages/cli-tool-test/cli_tool_test-0.0.2.tar.gz/cli_tool_test-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(name='cli_tool_test',
+      version='0.0.2',
+      description='Sample CLI tool in python',
+      packages=find_packages(),
+        # py_modules=['prog'],
+      install_requires=[
+          'Click',
+      ],
+        entry_points='''
+            [console_scripts]
+            prog=cli_tool_test.prog:greet
+            generate_ascii_art=cli_tool_test.prog:generate_ascii_art
+        ''',
+      zip_safe=True,
+      include_package_data=True
+      )
