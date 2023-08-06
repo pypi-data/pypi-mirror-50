98db62b56369c64dab15b7a0f84286d34ad83c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:39 +0000

    Add package flask-slack-template/0.0.6/flask_slack_template-0.0.6.tar.gz

---
+++ packages/flask-slack-template/flask_slack_template-0.0.6.tar.gz/flask_slack_template-0.0.6/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flask_slack_template",
+    version="0.0.6",
+    author="Jamie Wu",
+    author_email="jamiewu2@illinois.com",
+    description="A light decorator around flask and slack to remove the boilerplate for handling Slack slash commands",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jamiewu2/flask-slack-template",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'Flask',
+        'slackclient',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
