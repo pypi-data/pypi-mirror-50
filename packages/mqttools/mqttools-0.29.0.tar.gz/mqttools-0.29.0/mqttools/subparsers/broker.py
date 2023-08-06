7c47c27541fae96ea5ea263eb38f22c7187d6bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:18 +0000

    Add package mqttools/0.29.0/mqttools-0.29.0.tar.gz

---
+++ packages/mqttools/mqttools-0.29.0.tar.gz/mqttools-0.29.0/mqttools/subparsers/broker.py
@@ -0,0 +1,53 @@
+import asyncio
+import ssl
+
+from ..broker import Broker
+
+
+def _do_broker(args):
+    print(f"Starting a broker at '{args.host}:{args.port}'.")
+
+    if all([args.cafile, args.certfile, args.keyfile]):
+        print(f"Certfile: '{args.certfile}'")
+        print(f"Keyfile:  '{args.keyfile}'")
+        print(f"CA File:  '{args.cafile}'")
+        print(f"Check hostname: {not args.no_check_hostname}")
+
+        ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH,
+                                                 cafile=args.cafile)
+        ssl_context.check_hostname = not args.no_check_hostname
+        ssl_context.load_cert_chain(certfile=args.certfile, keyfile=args.keyfile)
+    else:
+        ssl_context = None
+
+    broker = Broker(args.host, args.port, ssl=ssl_context)
+    asyncio.run(broker.serve_forever())
+
+
+def add_subparser(subparsers):
+    subparser = subparsers.add_parser('broker',
+                                      description='A simple broker.')
+    subparser.add_argument('--host',
+                           default='localhost',
+                           help="Broker host (default: %(default)s).")
+    subparser.add_argument('--port',
+                           type=int,
+                           default=1883,
+                           help='Broker port (default: %(default)s).')
+    subparser.add_argument(
+        '--cafile',
+        default='',
+        help='MQTT broker CA file.')
+    subparser.add_argument(
+        '--certfile',
+        default='',
+        help='MQTT broker certificate file.')
+    subparser.add_argument(
+        '--keyfile',
+        default='',
+        help='MQTT broker key file.')
+    subparser.add_argument(
+        '--no-check-hostname',
+        action='store_true',
+        help='Do not check certificate hostname.')
+    subparser.set_defaults(func=_do_broker)
