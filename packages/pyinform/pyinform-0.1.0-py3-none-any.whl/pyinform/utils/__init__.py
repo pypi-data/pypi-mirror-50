da2dd5956bcdf6bf3206297f9f3e60d9eb77400e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:46 +0000

    Add package pyinform/0.1.0/pyinform-0.1.0-py3-none-any.whl

---
+++ packages/pyinform/pyinform-0.1.0-py3-none-any.whl/pyinform/utils/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2016-2019 Douglas G. Moore. All rights reserved.
+# Use of this source code is governed by a MIT
+# license that can be found in the LICENSE file.
+from pyinform.utils.binning import series_range, bin_series  # noqa: F401
+from pyinform.utils.coalesce import coalesce_series          # noqa: F401
+from pyinform.utils.encoding import encode, decode           # noqa: F401
