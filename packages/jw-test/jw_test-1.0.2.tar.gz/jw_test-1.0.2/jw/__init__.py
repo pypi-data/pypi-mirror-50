30b3f387ca4112dee9831a9905f6df1781207134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:08 +0000

    Add package jw-test/1.0.2/jw_test-1.0.2.tar.gz

---
+++ packages/jw-test/jw_test-1.0.2.tar.gz/jw_test-1.0.2/jw/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ['test']
+
+# from Test.test import a
+
+# a()
+
+if __name__ == '__main__':
+    print('作为主程序运行')
+else:
+    print('package_modules 初始化')
---
+++ packages/jw-test/jw_test-1.0.2.tar.gz/jw_test-1.0.2/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="jw_test",
+      version="1.0.2",
+      description="jw module",
+      author="jw",
+      author_email="jw397@126.com",
+      py_modules=["jw.__init__", "jw.test"])
