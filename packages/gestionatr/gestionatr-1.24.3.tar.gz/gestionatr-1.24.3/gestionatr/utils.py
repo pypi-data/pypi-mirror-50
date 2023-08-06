25e6f1b7a6e6cc986c6858a8b0cd28666667aa36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:24 +0000

    Add package gestionatr/1.24.3/gestionatr-1.24.3.tar.gz

---
+++ packages/gestionatr/gestionatr-1.24.3.tar.gz/gestionatr-1.24.3/gestionatr/utils.py
@@ -0,0 +1,76 @@
+from gestionatr import defs
+from gestionatr import defs_gas
+import os
+from collections import namedtuple
+
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_data(path):
+    filename = isinstance(path, (list, tuple)) and path[0] or path
+    return os.path.join(_ROOT, 'data', filename)
+
+
+def get_rec_attr(obj, attr, default=None):
+    try:
+        res = reduce(getattr, attr.split('.'), obj)
+    except:
+        if default is not None:
+            res = default
+        else:
+            raise
+    return res
+
+
+def get_description(code, table_name):
+    table = getattr(defs, table_name, None)
+    if not table:
+        raise ValueError(
+            "The table with the name '{}' doesn't exist".format(table_name)
+        )
+    res = dict(table).get(code, None)
+    if not res:
+        raise ValueError(
+            "The key '{}' in the table '{}' doesn't exist".format(
+                code, table_name
+            )
+        )
+    return res
+
+
+def get_description_gas(code, table_name):
+    table = getattr(defs_gas, table_name, None)
+    if not table:
+        raise ValueError(
+            "The table with the name '{}' doesn't exist".format(table_name)
+        )
+    res = dict(table).get(code, None)
+    if not res:
+        raise ValueError(
+            "The key '{}' in the table '{}' doesn't exist".format(
+                code, table_name
+            )
+        )
+    return res
+
+
+ValidationResult = namedtuple('ValidationResult', ['valid', 'error'])
+
+
+def validate_xml(data, is_gas=False):
+    try:
+        from gestionatr.input.messages import Message
+        m = Message(data)
+        m.parse_xml()
+        return ValidationResult(True, None)
+    except Exception as e:
+        try:
+            from gestionatr.input.messages import MessageGas
+            m = MessageGas(data)
+            m.parse_xml()
+            return ValidationResult(True, None)
+        except Exception as e2:
+            if is_gas:
+                return ValidationResult(False, u'Invalid File: {0}'.format(str(e2.value)))
+            else:
+                return ValidationResult(False, u'Invalid File: {0}'.format(str(e.value)))
