e64400b2fa54e8e2251b350c050e400434c56b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:50 +0000

    Add package otzovik-com/0.1.6/otzovik_com-0.1.6-py2.py3-none-any.whl

---
+++ packages/otzovik-com/otzovik_com-0.1.6-py2.py3-none-any.whl/otzovik_com/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Otzovik com."""
+from .otzovik_com import OtzovikCom
+
+__author__ = """NMelis"""
+__email__ = 'melis.zhoroev@gmail.com'
+__version__ = '0.1.6'
+__name__ = 'Отзовик'
+__slug_img_link__ = 'https://i.ibb.co/jfyL3hK/image.png'
+__how_get_slug__ = """
+Slug это https://otzovik.com/reviews/ТО_ЧТО_ЗДЕСЬ/ (без слешей "/")
+<img src="{}" alt="image" border="0">
+""".format(__slug_img_link__)
+
+
+provider = OtzovikCom
