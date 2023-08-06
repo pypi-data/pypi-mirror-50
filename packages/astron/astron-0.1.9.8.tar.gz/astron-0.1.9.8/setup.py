a54578a01b2663c16eec113846d6c56b15382475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:02 +0000

    Add package astron/0.1.9.8/astron-0.1.9.8.tar.gz

---
+++ packages/astron/astron-0.1.9.8.tar.gz/astron-0.1.9.8/setup.py
@@ -0,0 +1,14 @@
+import os,sys
+from setuptools import setup
+
+setup(name='astron',
+      version='0.1.9.8',
+      description='...',
+      url='https://www.python.org',
+      author='Addy Bhatia',
+      author_email='jude.addy999@gmail.com',
+      license='MIT',
+      install_requires=['autopep8', 'pygame', 'numpy', 'pywin32'],
+      packages=['astron'], 
+      include_package_data=True,
+      zip_safe=True)
