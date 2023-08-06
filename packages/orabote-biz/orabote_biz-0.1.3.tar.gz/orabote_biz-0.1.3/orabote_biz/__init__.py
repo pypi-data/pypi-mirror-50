b71321c55604e539abbf0365be515df4871793ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:46 +0000

    Add package orabote-biz/0.1.3/orabote_biz-0.1.3.tar.gz

---
+++ packages/orabote-biz/orabote_biz-0.1.3.tar.gz/orabote_biz-0.1.3/orabote_biz/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for orabote.biz."""
+from .orabote_biz import OraboteBiz
+
+__author__ = """NMelis"""
+__email__ = 'melis.zhoroev+scrubbers@gmail.com'
+__version__ = '0.1.3'
+__title__ = 'OraboteBiz'
+__slug_img_link__ = 'https://i.ibb.co/v44RKcD/image.png'
+__how_get_slug__ = """
+Slug это цифры в конце url'а конкретной компании
+<img src="{}" alt="image" border="0">
+""".format(__slug_img_link__)
+
+provider = OraboteBiz
