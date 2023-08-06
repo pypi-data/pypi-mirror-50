45be648e31ed81846801f18ff0d2c490daa3b243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:23 +0000

    Add package pyspedas/0.8.12/pyspedas-0.8.12.tar.gz

---
+++ packages/pyspedas/pyspedas-0.8.12.tar.gz/pyspedas-0.8.12/setup.py
@@ -0,0 +1,37 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+
+setup(
+    name='pyspedas',
+    version='0.8.12',
+    description='Python Space Physics Environment Data Analysis \
+                    Software (SPEDAS)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/spedas/pyspedas',
+    author='Nick Hatzigeorgiu, Eric Grimes',
+    author_email='nikos@berkeley.edu',
+    license='MIT',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Science/Research',
+                 'Topic :: Scientific/Engineering',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 3',
+                 ],
+    keywords='spedas data tools',
+    project_urls={'Information': 'http://spedas.org/wiki/',
+                  },
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['pyqtwebengine', 'requests', 'scipy',
+                      'pytplot>=1.4', 'cdflib', 'msgpack', 'bokeh', 'nodejs',
+                      'pyqtgraph', 'numpy', 'pydivide', 'pandas>=0.24'],
+    python_requires='>=3.5',
+)
