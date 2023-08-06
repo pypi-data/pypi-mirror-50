e55a180d19a26f80366821c9e5a0c46ee376d39b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:00 +0000

    Add package text2output/1.1.0/text2output-1.1.0.tar.gz

---
+++ packages/text2output/text2output-1.1.0.tar.gz/text2output-1.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='text2output',
+    version='1.1.0',
+    description='Convert your text to print function in any language',
+    py_modules=["text2output"],
+    package_dir={'': 'src'},
+
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/orishamir/",
+    author="Ori Shamir",
+
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Artistic Software",
+        "Operating System :: OS Independent"
+    ]
+)
