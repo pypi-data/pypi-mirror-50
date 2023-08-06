091d6bace18b41f67baf2d2275c5429f7682965c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:00 +0000

    Add package suanpan/0.9.60/suanpan-0.9.60-py3-none-any.whl

---
+++ packages/suanpan/suanpan-0.9.60-py3-none-any.whl/suanpan/version.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+
+VERSION = "0.9.59"
+VERSION_TEMPLATE = "Suanpan SDK (ver: {})"
+VERSION_STRING = VERSION_TEMPLATE.format(VERSION)
+
+
+if __name__ == "__main__":
+    print(VERSION_STRING)
