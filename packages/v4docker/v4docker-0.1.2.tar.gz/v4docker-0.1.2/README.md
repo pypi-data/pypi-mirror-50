b4eec4fda834bac879f5c172cdf0b75e8ccdbe1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:24 +0000

    Add package v4docker/0.1.2/v4docker-0.1.2.tar.gz

---
+++ packages/v4docker/v4docker-0.1.2.tar.gz/v4docker-0.1.2/README.md
@@ -0,0 +1,26 @@
+# v4docker
+
+Simple Terminal UI tool to manage docker images.
+
+## install
+
+python/pip 3.6+ required
+
+```bash
+$ pip3 install --user v4docker
+```
+
+
+## usage
+
+key `up` and `down` to move cursor.
+
+key `d` to remove image.
+
+key `tab` switch to quit button, press `enter` to quit.
+
+
+## features TODO
+
+- [] sort images by repository
+- [] show image detail/history
---
+++ packages/v4docker/v4docker-0.1.2.tar.gz/v4docker-0.1.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="v4docker",
+    version="0.1.2",
+    author="lethe3000",
+    author_email="lethe30003000@gmail.com",
+    description="Simple TUI to manage docker images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lethe3000/v4docker",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['docker==4.0.2', 'picotui==1.0.1'],
+    python_requires='>=3.6',
+    scripts=['v4docker/bin/v4docker'],
+)
