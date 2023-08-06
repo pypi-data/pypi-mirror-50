0e92d97806da8e08f09de8775b4263539c9e3d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:15 +0000

    Add package demojd/0.44/demojd-0.44.tar.gz

---
+++ packages/demojd/demojd-0.44.tar.gz/demojd-0.44/demojd/scripts/hello.py
@@ -0,0 +1,11 @@
+from demojd.src import uncle, other
+from demojd.src.parents import mom, dad
+from demojd.src.parents.kids import boy, girl
+
+def hello():
+    mom.hello()
+    dad.hello()
+    boy.hello()
+    girl.hello()
+    other.hello()
+    uncle.hello()
