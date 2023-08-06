9260b5894d67ed567a99966ce940ec15e98b00e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:26 +0000

    Add package resampy/0.2.2/resampy-0.2.2.tar.gz

---
+++ packages/resampy/resampy-0.2.2.tar.gz/resampy-0.2.2/setup.py
@@ -0,0 +1,49 @@
+'''resampy install script'''
+import imp
+
+from setuptools import setup
+
+
+VERSION = imp.load_source('resampy.version', 'resampy/version.py')
+
+
+setup(
+    author="Brian McFee",
+    author_email="brian.mcfee@nyu.edu",
+    name='resampy',
+    version=VERSION.version,
+    url='https://github.com/bmcfee/resampy',
+    download_url='https://github.com/bmcfee/resampy/releases',
+    description='Efficient signal resampling',
+    license='ISC',
+    packages=['resampy'],
+    package_data={'resampy': ['data/*']},
+    install_requires=[
+        'numpy>=1.10',
+        'scipy>=0.13',
+        'numba>=0.32',
+        'six>=1.3'],
+    extras_require={
+        'docs': [
+            'sphinx!=1.3.1',  # autodoc was broken in 1.3.1
+            'numpydoc',
+        ],
+        'tests': [
+            'pytest < 4',
+            'pytest-cov',
+        ],
+    },
+    classifiers=[
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
