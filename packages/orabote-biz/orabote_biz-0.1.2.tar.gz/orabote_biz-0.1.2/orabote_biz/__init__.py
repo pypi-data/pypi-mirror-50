b7ff16bb50f3925ced8a327d0585f53ba3a78fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:15 +0000

    Add package orabote-biz/0.1.2/orabote_biz-0.1.2.tar.gz

---
+++ packages/orabote-biz/orabote_biz-0.1.2.tar.gz/orabote_biz-0.1.2/orabote_biz/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for orabote.biz."""
+from .orabote_biz import OraboteBiz
+
+__author__ = """NMelis"""
+__email__ = 'melis.zhoroev@gmail.com'
+__version__ = '0.1.0'
+__title__ = 'OraboteBiz'
+
+provider = OraboteBiz
