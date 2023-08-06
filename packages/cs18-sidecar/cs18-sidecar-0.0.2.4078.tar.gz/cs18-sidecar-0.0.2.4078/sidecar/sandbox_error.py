2613aeaaf928fc9be9ca726047fc018786524912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:20 +0000

    Add package cs18-sidecar/0.0.2.4078/cs18-sidecar-0.0.2.4078.tar.gz

---
+++ packages/cs18-sidecar/cs18-sidecar-0.0.2.4078.tar.gz/cs18-sidecar-0.0.2.4078/sidecar/sandbox_error.py
@@ -0,0 +1,9 @@
+class SandboxError:
+    def __init__(self, time: str, code: str, message: str):
+        self.message = message
+        self.code = code
+        self.time = time
+
+
+class ErrorCodes:
+    ARTIFACTS_DOWNLOAD_FAILED = "ARTIFACTS_DOWNLOAD_FAILED"
