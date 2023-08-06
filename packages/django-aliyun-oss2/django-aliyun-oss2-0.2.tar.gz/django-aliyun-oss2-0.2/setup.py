4e5a5e26a86b497480cddcd90805149d56157852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:49 +0000

    Add package django-aliyun-oss2/0.2/django-aliyun-oss2-0.2.tar.gz

---
+++ packages/django-aliyun-oss2/django-aliyun-oss2-0.2.tar.gz/django-aliyun-oss2-0.2/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-aliyun-oss2',
+    version='0.2',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',  # example license
+    description='A simple Django storage backend for aliyun oss2.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/sai628/django-aliyun-oss2',
+    author='Sai628',
+    author_email='sai100628@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.6',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=['oss2', 'django>=1.6.0'],
+)
