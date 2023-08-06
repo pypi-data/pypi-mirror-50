c7729ccdfa80fd2fc94ceb5a826ae7c80768b880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:19 +0000

    Add package thonny/3.2.0/thonny-3.2.0-py3-none-any.whl

---
+++ packages/thonny/thonny-3.2.0-py3-none-any.whl/thonny/languages.py
@@ -0,0 +1,22 @@
+BASE_LANGUAGE_CODE = "en_US"
+BASE_LANGUAGE_NAME = "English"
+
+LANGUAGES_DICT = {
+    "de_DE": "Deutsch [BETA]",
+    "nl_NL": "Nederlands [BETA]",
+    "et_EE": "Eesti",
+    BASE_LANGUAGE_CODE: BASE_LANGUAGE_NAME,
+    "es_ES": "Español [BETA]",
+    "fr_FR": "Français [BETA]",
+    "pt_BR": "Português [BETA]",
+    "ru_RU": "Русский [BETA]",
+    "zh_TW": "繁體中文-TW [BETA]",
+    "ja_JP": "日本語  [BETA]",
+    "zh_CN": "简体中文  [BETA]",
+}
+
+
+def get_language_code_by_name(name):
+    for code in LANGUAGES_DICT:
+        if LANGUAGES_DICT[code] == name:
+            return code
