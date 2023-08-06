236d7fcddf3cf3f9a01b93207bd453f2a7aace92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:04 +0000

    Add package raiden-py/0.1.2/raiden_py-0.1.2.tar.gz

---
+++ packages/raiden-py/raiden_py-0.1.2.tar.gz/raiden_py-0.1.2/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup
+
+PARENT_DIR = pathlib.Path(__file__).parent
+README = (PARENT_DIR / "README.md").read_text()
+
+setup(
+    name='raiden_py',
+    version='0.1.2',
+    packages=['raiden_py'],
+    include_package_data=True,
+    url='https://github.com/viraja1/raiden_py/',
+    license='MIT',
+    author='viraj',
+    author_email='',
+    description='Raiden Python Client Library',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    install_requires=[
+       'requests>=2.20.0'
+    ],
+    keywords="raiden",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    python_requires=">=3.7"
+)
