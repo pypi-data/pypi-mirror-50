05fb4a4502fecaafe040bd25194b42263c8a12ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:28 +0000

    Add package locuspeerexplorer/0.1.post3/LocusPeerExplorer-0.1.post3.tar.gz

---
+++ packages/locuspeerexplorer/LocusPeerExplorer-0.1.post3.tar.gz/LocusPeerExplorer-0.1.post3/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'LocusPeerExplorer',         # How you named your package folder (MyLib)
+  packages = ['LocusPeerExplorer'],   # Chose the same as "name"
+  version = '0.1.post3',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'A package that lets you find similar community peers based off of functional or outcomee data',   # Give a short description about your library
+  author = 'Locus Analytics',                   # Type in your name
+  author_email = 'alee@locus.co',      # Type in your E-Mail
+  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  download_url =
+  'https://github.com/LocusAnalytics/peer-explorer/archive/v0.1.post2.tar.gz',
+  keywords = ['Economics', 'Peer Finder'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'pandas',
+          'scipy',
+          'fastdtw',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
