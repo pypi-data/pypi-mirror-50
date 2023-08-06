7bd61d4805aa4622327fc6bfeacf1b9486d39e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:36 +0000

    Add package filebutler/0.26.0/filebutler-0.26.0.tar.gz

---
+++ packages/filebutler/filebutler-0.26.0.tar.gz/filebutler-0.26.0/filebutler/Sorter.py
@@ -0,0 +1,36 @@
+# Copyright 2017-2018 Simon Guest
+#
+# This file is part of filebutler.
+#
+# Filebutler is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Filebutler is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with filebutler.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import (
+    bytes, dict, int, list, object, range, str,
+    ascii, chr, hex, input, next, oct, open,
+    pow, round, super,
+    filter, map, zip)
+
+class Sorter(object):
+
+    def __init__(self, bySize=False):
+        self.bySize = bySize
+
+    def key(self):
+        if self.bySize:
+            # negate size, so we sort largest first
+            return lambda fs: -fs.size
+        else:
+            return lambda fs: 0
