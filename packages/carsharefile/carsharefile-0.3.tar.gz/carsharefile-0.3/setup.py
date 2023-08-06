7bef7fb830a3c9cf983e29f5512c04c4045a9499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:55 +0000

    Add package carsharefile/0.3/carsharefile-0.3.tar.gz

---
+++ packages/carsharefile/carsharefile-0.3.tar.gz/carsharefile-0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import pathlib
+
+a = pathlib.Path(__file__).parent
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup(
+    name='carsharefile',
+    version='0.3',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    author='Sam Rubanowitz',
+    packages=find_packages(),
+    author_email='therealsamurban@gmail.com',
+    description='Python Package that provides easy interaction with the ShareFile API.',
+    install_requires=["http", "mimetypes", "urllib"]
+)
