1b72ccf85f75841537241e9c1fc6bd2fa00f316a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:15 +0000

    Add package trustar/0.3.20/trustar-0.3.20.tar.gz

---
+++ packages/trustar/trustar-0.3.20.tar.gz/trustar-0.3.20/setup.py
@@ -0,0 +1,37 @@
+# git tag 0.3.20 -m "Bump version" && git push --tags origin master
+# python setup.py sdist
+# twine upload --skip-existing dist/*
+
+from setuptools import setup, find_packages
+from glob import glob
+
+# read version
+version_globals = {}
+with open("trustar/version.py") as fp:
+    exec(fp.read(), version_globals)
+version = version_globals['__version__']
+
+setup(
+    name='trustar',
+    packages=find_packages(),
+    version=version,
+    author='TruSTAR Technology, Inc.',
+    author_email='support@trustar.co',
+    url='https://github.com/trustar/trustar-python',
+    download_url='https://github.com/trustar/trustar-python/tarball/%s' % version,
+    description='Python SDK for the TruSTAR REST API',
+    license='MIT',
+    install_requires=['future',
+                      'python-dateutil',
+                      'pytz',
+                      'requests',
+                      'configparser',
+                      'unicodecsv',
+                      'tzlocal',
+                      'PyYAML',
+                      'six'
+                      ],
+    include_package_data=True,
+    scripts=glob('trustar/examples/**/*.py') + glob('trustar/examples/*.py'),
+    use_2to3=True
+)
---
+++ packages/trustar/trustar-0.3.20.tar.gz/trustar-0.3.20/trustar/models/__init__.py
@@ -0,0 +1,9 @@
+from .enclave import Enclave, EnclavePermissions
+from .intelligence_source import IntelligenceSource
+from .indicator import Indicator
+from .indicator_summary import *
+from .page import Page
+from .report import Report
+from .tag import Tag
+from .request_quota import RequestQuota
+from .enum import *
