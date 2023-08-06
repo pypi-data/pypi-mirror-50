226a3972e9090b09fbe091ef237e9841ac05535f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:21 +0000

    Add package Fmagic/0.2/Fmagic-0.2.tar.gz

---
+++ packages/Fmagic/Fmagic-0.2.tar.gz/Fmagic-0.2/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+     name='Fmagic',  
+     version='0.2',
+     scripts=['Fmagic'] ,
+     author="Abhishek Rana",
+     author_email="abhishek.rana@mantralabsglobal.com",
+     description="FlowMagic SDK",
+
+ )
