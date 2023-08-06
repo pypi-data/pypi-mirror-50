44709f71e739b0fea072d7c6c63630e9a3151c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:48 +0000

    Add package ray/0.7.3/ray-0.7.3-cp37-cp37m-macosx_10_6_intel.whl

---
+++ packages/ray/ray-0.7.3-cp37-cp37m-macosx_10_6_intel.whl/ray-0.7.3.data/purelib/ray/tune/examples/mnist_pytorch.py
@@ -0,0 +1,135 @@
+# Original Code here:
+# https://github.com/pytorch/examples/blob/master/mnist/main.py
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+import ray
+from ray import tune
+from ray.tune import track
+from ray.tune.schedulers import AsyncHyperBandScheduler
+
+# Change these values if you want the training to run quicker or slower.
+EPOCH_SIZE = 512
+TEST_SIZE = 256
+
+
+class Net(nn.Module):
+    def __init__(self, config):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 3, kernel_size=3)
+        self.fc = nn.Linear(192, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 3))
+        x = x.view(-1, 192)
+        x = self.fc(x)
+        return F.log_softmax(x, dim=1)
+
+
+def train(model, optimizer, train_loader, device):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if batch_idx * len(data) > EPOCH_SIZE:
+            return
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+
+
+def test(model, data_loader, device):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(data_loader):
+            if batch_idx * len(data) > TEST_SIZE:
+                break
+            data, target = data.to(device), target.to(device)
+            outputs = model(data)
+            _, predicted = torch.max(outputs.data, 1)
+            total += target.size(0)
+            correct += (predicted == target).sum().item()
+
+    return correct / total
+
+
+def get_data_loaders():
+    mnist_transforms = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.1307, ), (0.3081, ))])
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(
+            "~/data", train=True, download=True, transform=mnist_transforms),
+        batch_size=64,
+        shuffle=True)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST("~/data", train=False, transform=mnist_transforms),
+        batch_size=64,
+        shuffle=True)
+    return train_loader, test_loader
+
+
+def train_mnist(config):
+    use_cuda = config.get("use_gpu") and torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    train_loader, test_loader = get_data_loaders()
+    model = Net(config).to(device)
+
+    optimizer = optim.SGD(
+        model.parameters(), lr=config["lr"], momentum=config["momentum"])
+
+    while True:
+        train(model, optimizer, train_loader, device)
+        acc = test(model, test_loader, device)
+        track.log(mean_accuracy=acc)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="PyTorch MNIST Example")
+    parser.add_argument(
+        "--cuda",
+        action="store_true",
+        default=False,
+        help="Enables GPU training")
+    parser.add_argument(
+        "--smoke-test", action="store_true", help="Finish quickly for testing")
+    parser.add_argument(
+        "--ray-redis-address",
+        help="Address of Ray cluster for seamless distributed execution.")
+    args = parser.parse_args()
+    if args.ray_redis_address:
+        ray.init(redis_address=args.ray_redis_address)
+    datasets.MNIST("~/data", train=True, download=True)
+    sched = AsyncHyperBandScheduler(
+        time_attr="training_iteration", metric="mean_accuracy")
+    tune.run(
+        train_mnist,
+        name="exp",
+        scheduler=sched,
+        stop={
+            "mean_accuracy": 0.98,
+            "training_iteration": 5 if args.smoke_test else 20
+        },
+        resources_per_trial={
+            "cpu": 2,
+            "gpu": int(args.cuda)
+        },
+        num_samples=1 if args.smoke_test else 10,
+        config={
+            "lr": tune.sample_from(lambda spec: 10**(-10 * np.random.rand())),
+            "momentum": tune.uniform(0.1, 0.9),
+            "use_gpu": int(args.cuda)
+        })
