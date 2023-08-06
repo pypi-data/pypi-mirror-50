bcfb3da15e0aead7ed2a55286ac3ad162b7d3dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:36 +0000

    Add package caisson/0.0.4/caisson-0.0.4-py3-none-any.whl

---
+++ packages/caisson/caisson-0.0.4-py3-none-any.whl/caisson/__main__.py
@@ -0,0 +1,34 @@
+import logging
+
+from caisson.decompressors import get_decompressors
+from caisson.parser import build_arg_parser
+from caisson.io import decompress
+from caisson import log
+
+
+def caisson(sources, destination, configuration):
+    decompressors = get_decompressors(configuration)
+    decompress(sources, destination, decompressors)
+    while decompress([destination], destination, decompressors,
+                     subdir=False, remove_source=True):
+        log.info("some files were decompressed, doing another pass")
+
+
+def main():
+    parser = build_arg_parser()
+    args = parser.parse_args()
+
+    if args.quiet:
+        logging.basicConfig(level=logging.CRITICAL, format="%(message)s")
+    elif args.verbose:
+        logging.basicConfig(level=logging.INFO, format="%(message)s")
+    elif args.debug:
+        logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+
+    caisson(sources=args.source,
+            destination=args.destination[0],
+            configuration=args)
+
+
+if __name__ == "__main__":
+    main()
