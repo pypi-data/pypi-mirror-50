c13ff43ae097d796ce506a70c26f4186f3632c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:16 +0000

    Add package jovian-pro/0.1.62/jovian-pro-0.1.62.tar.gz

---
+++ packages/jovian-pro/jovian-pro-0.1.62.tar.gz/jovian-pro-0.1.62/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+import re
+
+VERSIONFILE = "./jovian/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+with open("README.md", "rb") as fh:
+    long_description = fh.read().decode('utf-8', errors='ignore')
+
+setuptools.setup(
+    name="jovian-pro",
+    version=verstr,
+    author="SwiftAce",
+    author_email="opensource@swiftace.ai",
+    entry_points={
+        'console_scripts': ['jovian=jovian.cli:main'],
+    },
+    description="Jovian Python SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://jovian.ai/",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=['requests', 'uuid', 'pyyaml']
+)
