163c0c14294ee7f702d77c54e615c85dcf298e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:17 +0000

    Add package Atria/0.0.9/Atria-0.0.9.tar.gz

---
+++ packages/Atria/Atria-0.0.9.tar.gz/Atria-0.0.9/README.md
@@ -0,0 +1,13 @@
+# Atria: Message Bot
+
+Atria is a simple message bot that can be used by other developers.
+
+# How to run
+
+from Atria import Main
+#Create an instance of Main class
+main = Main()
+#Create an argument list
+args = ["Hello"]
+#Pass the arguments to the class
+main.main(args)
---
+++ packages/Atria/Atria-0.0.9.tar.gz/Atria-0.0.9/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name="Atria",
+    version="0.0.9",
+    author="RedDog",
+    author_email="bremo.lincolin101@gmail.com",
+    description="A simple messaging bot for python",
+    long_description=description,
+    url="https://github.com/RedDogCode/Atria",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+        ]
+)
