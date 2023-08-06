94b7bf3e6f1d6c581e8cdacfc482b9c3f2b59d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:05 +0000

    Add package sangfor-trans/0.1.5/sangfor_trans-0.1.5.tar.gz

---
+++ packages/sangfor-trans/sangfor_trans-0.1.5.tar.gz/sangfor_trans-0.1.5/setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os.path
+import re
+import sys
+
+from setuptools import setup, find_packages
+
+
+def get_file(*paths):
+    path = os.path.join(*paths)
+    try:
+        with open(path, 'rb') as f:
+            return f.read().decode('utf8')
+    except IOError:
+        pass
+
+
+def get_version():
+    init_py = get_file(os.path.dirname(__file__), 'sangfor_trans', '__init__.py')
+    pattern = r"{0}\W*=\W*'([^']+)'".format('__version__')
+    version, = re.findall(pattern, init_py)
+    return version
+
+
+def get_description():
+    init_py = get_file(os.path.dirname(__file__), 'sangfor_trans', '__init__.py')
+    pattern = r'"""(.*?)"""'
+    description, = re.findall(pattern, init_py, re.DOTALL)
+    return description
+
+
+def get_readme():
+    # return "This is  Sangfor translation third-party library."
+    return get_file(os.path.dirname(__file__), 'README.md')
+
+
+def install():
+    setup(
+        name='sangfor_trans',
+        version=get_version(),
+        description=get_description(),
+        long_description=get_readme(),
+        long_description_content_type="text/markdown",
+        license='MIT',
+        author='scoefield',
+        author_email='scoefield' '@' '163.com',
+        url='https://github.com/Scoefield/SangforTrans',
+        classifiers=['Development Status :: 5 - Production/Stable',
+                     'Intended Audience :: Education',
+                     'Intended Audience :: End Users/Desktop',
+                     'License :: Freeware',
+                     'Operating System :: POSIX',
+                     'Operating System :: Microsoft :: Windows',
+                     'Operating System :: MacOS :: MacOS X',
+                     'Topic :: Education',
+                     'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3.4',
+                     'Programming Language :: Python :: 3.5',
+                     'Programming Language :: Python :: 3.6'],
+        packages=find_packages(exclude=['docs']),
+        keywords='sangfor translate translator',
+    )
+
+
+if __name__ == "__main__":
+    install()
