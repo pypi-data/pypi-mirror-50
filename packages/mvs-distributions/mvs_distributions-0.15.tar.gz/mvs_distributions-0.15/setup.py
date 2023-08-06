8498bb393829128f84adf3fbb7eae988ce8b697d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:56 +0000

    Add package mvs-distributions/0.15/mvs_distributions-0.15.tar.gz

---
+++ packages/mvs-distributions/mvs_distributions-0.15.tar.gz/mvs_distributions-0.15/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mvs_distributions',
+      version='0.15',
+      description='Gaussian distributions',
+      packages=['mvs_distributions'],
+      zip_safe=False)
