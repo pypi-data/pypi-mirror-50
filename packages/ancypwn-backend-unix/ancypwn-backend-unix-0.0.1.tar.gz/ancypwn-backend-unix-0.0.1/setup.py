5dde21ec37dde0cb0f88bb0fcdb2a4472858294f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:48 +0000

    Add package ancypwn-backend-unix/0.0.1/ancypwn-backend-unix-0.0.1.tar.gz

---
+++ packages/ancypwn-backend-unix/ancypwn-backend-unix-0.0.1.tar.gz/ancypwn-backend-unix-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    'docker',
+    'appdirs',
+]
+
+setup(
+    name='ancypwn-backend-unix',
+    version='0.0.1',
+    description='ancypwn unix universal backend',
+    url='https://github.com/Escapingbug/ancypwn-backend-unix',
+    author='Anciety',
+    author_email='anciety@pku.edu.cn',
+    packages=['ancypwn_backend_unix'],
+    package_dir={'ancypwn_backend_unix': 'src'},
+    install_requires=install_requires
+)
