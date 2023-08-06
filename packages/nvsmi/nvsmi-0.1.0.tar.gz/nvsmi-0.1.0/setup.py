51e109dc589506efb4984b157383ac2c2c7fa497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:02 +0000

    Add package nvsmi/0.1.0/nvsmi-0.1.0.tar.gz

---
+++ packages/nvsmi/nvsmi-0.1.0.tar.gz/nvsmi-0.1.0/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+modules = \
+['nvsmi']
+entry_points = \
+{'console_scripts': ['nvsmi = nvsmi:main']}
+
+setup_kwargs = {
+    'name': 'nvsmi',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Panos Mavrogiorgos',
+    'author_email': 'pmav99@gmail.com',
+    'url': None,
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+}
+
+
+setup(**setup_kwargs)
