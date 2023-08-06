e2c19edca18416ac4420aaa928cec7a39ceebc0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:10 +0000

    Add package hearthstone-card-generator/1.2/hearthstone_card_generator-1.2.tar.gz

---
+++ packages/hearthstone-card-generator/hearthstone_card_generator-1.2.tar.gz/hearthstone_card_generator-1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(name='hearthstone_card_generator',
+    version='1.2',
+    description='Generates completely random hearthstone cards (random mana cost, attack, health, name, and a description generated using markov chains from existing descriptions)',
+    url='https://github.com/maymike321/hearthstone_card_generator',
+    author='Michael May',
+    author_email='yamekim@comcast.net',
+    packages=find_packages(exclude=("test")),
+    license='MIT',
+    install_requires=['pyykov'],
+    test_suite='hearthstone_card_generator.hearthstone-card-generator-test.py',
+    zip_safe=False)
