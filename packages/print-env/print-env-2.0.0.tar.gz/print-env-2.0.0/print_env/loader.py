8be0e5cc0101a2ad1dba3bf2e5258e01ba5c5c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:02 +0000

    Add package print-env/2.0.0/print-env-2.0.0.tar.gz

---
+++ packages/print-env/print-env-2.0.0.tar.gz/print-env-2.0.0/print_env/loader.py
@@ -0,0 +1,88 @@
+import os
+import codecs
+
+# dotenv loader
+from dotenv import dotenv_values
+# yaml loader
+import yaml
+try:
+    from yaml import CLoader as YLoader
+except ImportError:
+    from yaml import Loader as YLoader
+# json loader
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+from .exts import (
+    get_defaults,
+    is_yaml,
+    is_json,
+)
+from .utils import secho
+
+
+def load_default(verbose=False):
+    cwd = os.getcwd()
+    defaults = get_defaults()
+
+    for fname in defaults:
+        env_file = os.path.join(cwd, fname)
+
+        if os.path.isfile(env_file):
+            return load_file(env_file, verbose)
+    else:
+        if verbose:
+            secho(
+                msg='No default ({ds}) found at {cwd}/'.format(
+                    ds=', '.join(defaults),
+                    cwd=cwd
+                ).strip(),
+                lvl='warning'
+            )
+
+        return {}
+
+
+def load_file(fname, verbose=False):
+    env_vars = {}
+
+    try:
+        _, ext = os.path.splitext(fname)
+
+        if not is_yaml(ext) and not is_json(ext):
+            env_vars = dotenv_values(fname)
+        else:
+            with codecs.open(fname) as f:
+                if is_yaml(ext):
+                    env_vars = yaml.load(f, Loader=YLoader)
+                elif is_json(ext):
+                    env_vars = json.load(f)
+
+        if verbose:
+            if not env_vars:
+                secho(
+                    msg='Sourced from invalid file {}'.format(fname),
+                    lvl='warning'
+                )
+            else:
+                secho(msg='Sourced from file {}'.format(fname), lvl='debug')
+    except Exception as e:
+        if verbose:
+            secho(
+                msg='Failed to load file {f}\n{e}'.format(
+                    f=fname,
+                    e=e
+                ),
+                lvl='error'
+            )
+
+    return env_vars
+
+
+def load_system(verbose=False):
+    if verbose:
+        secho(msg='Sourced system environment variables')
+
+    return dict(os.environ)
