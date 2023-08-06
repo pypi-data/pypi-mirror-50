8a606f18eb970813bfeadfc4307f259879915848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:25 +0000

    Add package WechatRobots/1.0.1/WechatRobots-1.0.1.tar.gz

---
+++ packages/WechatRobots/WechatRobots-1.0.1.tar.gz/WechatRobots-1.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="WechatRobots",
+    version="1.0.1",
+    author="ClodLingxi",
+    author_email="ClodLingxi@qq.com",
+    url="https://github.com/ClodLingxi",
+    packages=find_packages())
