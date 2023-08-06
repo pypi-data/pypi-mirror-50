b6de3791a035f1d838e8457f01b56b591c2235a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:04 +0000

    Add package raleigh/1.0.4/raleigh-1.0.4-py3-none-any.whl

---
+++ packages/raleigh/raleigh-1.0.4-py3-none-any.whl/raleigh/algebra/cublas_wrap.py
@@ -0,0 +1,122 @@
+# Copyright 2019 United Kingdom Research and Innovation 
+# Author: Evgueni Ovtchinnikov (evgueni.ovtchinnikov@stfc.ac.uk)
+
+"""CUBLAS loader/wrapper.
+"""
+
+import ctypes
+import glob
+import numpy
+from sys import platform
+
+from . import verbosity
+
+
+POINTER = ctypes.POINTER
+
+
+try:
+    if platform == 'win32':
+        from .cuda_wrap import cuda_path
+        cublas_dll = glob.glob(cuda_path + '/cublas64*')[0]
+        cublas = ctypes.CDLL(cublas_dll, mode=ctypes.RTLD_GLOBAL)
+    else:
+        cublas = ctypes.CDLL('libcublas.so', mode=ctypes.RTLD_GLOBAL)
+
+    create = cublas.cublasCreate_v2
+    create.argtypes = [POINTER(POINTER(ctypes.c_ubyte))]
+    create.restype = ctypes.c_int
+
+    destroy = cublas.cublasDestroy_v2
+    destroy.restype = ctypes.c_int
+
+    cublasVersion = cublas.cublasGetVersion_v2
+    cublasVersion.restype = ctypes.c_int
+
+    cublas_handle = POINTER(ctypes.c_ubyte)()
+    err = create(ctypes.byref(cublas_handle))
+    v = ctypes.c_int()
+    err = cublasVersion(cublas_handle, ctypes.byref(v))
+    version = v.value
+    if verbosity.level > 0:
+        print('CUBLAS version: %d' % version)
+    destroy(cublas_handle)
+except:
+    if verbosity.level > 0:
+        print('CUBLAS not found, switching to cpu...')
+    raise RuntimeError('CUBLAS not found')
+
+
+class Cublas:
+    '''CUBLAS wrapper.
+    '''
+
+    NoTrans = 0
+    Trans = 1
+    ConjTrans = 2
+
+    def __init__(self, dt):
+        self.handle = POINTER(ctypes.c_ubyte)()
+        err = create(ctypes.byref(self.handle))
+        if err != 0:
+            raise RuntimeError('cublasCreate failure')
+        if dt == numpy.float32:
+            self.dsize = 4
+            self.copy = cublas.cublasScopy_v2
+            self.copy.restype = ctypes.c_int
+            self.axpy = cublas.cublasSaxpy_v2
+            self.axpy.restype = ctypes.c_int
+            self.scal = cublas.cublasSscal_v2
+            self.scal.restype = ctypes.c_int
+            self.norm = cublas.cublasSnrm2_v2
+            self.norm.restype = ctypes.c_int
+            self.dot = cublas.cublasSdot_v2
+            self.dot.restype = ctypes.c_int
+            self.gemm = cublas.cublasSgemm_v2
+            self.gemm.restype = ctypes.c_int
+        elif dt == numpy.float64:
+            self.dsize = 8
+            self.copy = cublas.cublasDcopy_v2
+            self.copy.restype = ctypes.c_int
+            self.axpy = cublas.cublasDaxpy_v2
+            self.axpy.restype = ctypes.c_int
+            self.scal = cublas.cublasDscal_v2
+            self.scal.restype = ctypes.c_int
+            self.norm = cublas.cublasDnrm2_v2
+            self.norm.restype = ctypes.c_int
+            self.dot = cublas.cublasDdot_v2
+            self.dot.restype = ctypes.c_int
+            self.gemm = cublas.cublasDgemm_v2
+            self.gemm.restype = ctypes.c_int
+        elif dt == numpy.complex64:
+            self.dsize = 8
+            self.copy = cublas.cublasCcopy_v2
+            self.copy.restype = ctypes.c_int
+            self.axpy = cublas.cublasCaxpy_v2
+            self.axpy.restype = ctypes.c_int
+            self.scal = cublas.cublasCscal_v2
+            self.scal.restype = ctypes.c_int
+            self.norm = cublas.cublasScnrm2_v2
+            self.norm.restype = ctypes.c_int
+            self.dot = cublas.cublasCdotc_v2
+            self.dot.restype = ctypes.c_int
+            self.gemm = cublas.cublasCgemm_v2
+            self.gemm.restype = ctypes.c_int
+        elif dt == numpy.complex128:
+            self.dsize = 16
+            self.copy = cublas.cublasZcopy_v2
+            self.copy.restype = ctypes.c_int
+            self.axpy = cublas.cublasZaxpy_v2
+            self.axpy.restype = ctypes.c_int
+            self.scal = cublas.cublasZscal_v2
+            self.scal.restype = ctypes.c_int
+            self.norm = cublas.cublasDznrm2_v2
+            self.norm.restype = ctypes.c_int
+            self.dot = cublas.cublasZdotc_v2
+            self.dot.restype = ctypes.c_int
+            self.gemm = cublas.cublasZgemm_v2
+            self.gemm.restype = ctypes.c_int
+        else:
+            raise ValueError('data type %s not supported' % repr(dt))
+    def __del__(self):
+        destroy(self.handle)
---
+++ packages/raleigh/raleigh-1.0.4-py3-none-any.whl/raleigh/examples/eigenimages/show_errors.py
@@ -0,0 +1,120 @@
+# Copyright 2019 United Kingdom Research and Innovation 
+# Author: Evgueni Ovtchinnikov (evgueni.ovtchinnikov@stfc.ac.uk)
+
+'''Compares images with their PCA approximations computed by the script
+   compute_eigenimages.py.
+
+Usage:
+    show_errors [--help | -h] <file>
+
+Arguments:
+    file       images file (must end with .npy)
+'''
+
+try:
+    from docopt import docopt
+    __version__ = '0.1.0'
+    have_docopt = True
+except:
+    have_docopt = False
+
+import numpy
+import numpy.linalg as nla
+import pylab
+import sys
+
+
+def _norm(a, axis):
+    return numpy.apply_along_axis(nla.norm, axis, a)
+
+
+if have_docopt:
+    __version__ = '0.1.0'
+    args = docopt(__doc__, version=__version__)
+    file = args['<file>']
+else:
+    file = sys.argv[1]
+
+u = numpy.load('eigenimages.npy')
+v = numpy.load('coordinates.npy')
+nc, nyu, nxu = u.shape
+m = v.shape[0]
+print('%d eigenimages of size %dx%d loaded' % (nc, nyu, nxu))
+try:
+    mean = numpy.load('mean.npy')
+    print('loaded mean image')
+except:
+    mean = None
+    print('mean image not present')
+
+print('loading images from %s...' % file)
+images = numpy.load(file)
+ni, ny, nx = images.shape
+print('%d images of size %dx%d loaded' % (ni, ny, nx))
+vmax = numpy.amax(images)
+
+if m > ni:
+    m = ni
+if nx != nxu or ny != nyu:
+    raise ValueError('mismatching image sizes (%d, %d) and (%d, %d)' % \
+          (ny, nx, nyu, nxu))
+
+sigma = _norm(v, axis=0)
+
+pylab.figure()
+pylab.plot(numpy.arange(1, nc + 1, 1), sigma)
+pylab.xscale('log')
+pylab.yscale('log')
+pylab.grid()
+pylab.title('singular values')
+pylab.show()
+
+n = nx*ny
+images = numpy.reshape(images, (ni, n))
+u = numpy.reshape(u, (nc, n))
+
+print('measuring PCA errors...')
+norm = _norm(images[:m, :], axis=1)
+if mean is None:
+    pca_images = numpy.dot(v, u)
+else:
+    ones = numpy.ones((m, 1))
+    mean = numpy.reshape(mean, (1, n))
+    pca_images = numpy.dot(v, u) + numpy.dot(ones, mean)
+mean = numpy.reshape(mean, n)
+proj = _norm(v, axis=1)
+err = _norm(images[:m, :] - pca_images, axis=1)/norm
+ind = numpy.argsort(-err)
+pylab.figure()
+pylab.plot(numpy.arange(1, m + 1, 1), err[ind])
+pylab.title('PCA errors')
+pylab.show()
+k = 100
+print('%d images with the largest PCA error:' % k)
+print(ind[:k])
+
+while True:
+    i = int(input('image number (negative to exit): '))
+    if i < 0 or i >= ni:
+        break
+    image = numpy.reshape(images[i,:], (ny, nx))
+    print('PCA error: %.1e' % err[i])
+    if i < m:
+        w = v[i, :]
+    else:
+        w = numpy.dot(u, images[i, :])
+    pylab.figure()
+    pylab.title('image %d' % i)
+    img = images[i, :]
+    image = numpy.reshape(img, (ny, nx))
+    pylab.imshow(image, cmap='gray')
+    img = numpy.dot(w, u)
+    if mean is not None:
+        img += mean
+    pca_image = numpy.reshape(img, (ny, nx))
+    pylab.figure()
+    pylab.title('PCA approximation of the image')
+    pylab.imshow(pca_image, cmap='gray')
+    pylab.show()
+
+print('done')
