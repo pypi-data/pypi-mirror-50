2c09e1bcbb5cba743fad11746ea94a2b7c02ae1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:23 +0000

    Add package pygetdp/0.0.3/pygetdp-0.0.3.tar.gz

---
+++ packages/pygetdp/pygetdp-0.0.3.tar.gz/pygetdp-0.0.3/pygetdp/postoperation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Benjamin Vial
+# License: MIT
+
+
+class PostOperation(object):
+    """Exporting results"""
+
+    def __init__(self):
+        pass
---
+++ packages/pygetdp/pygetdp-0.0.3.tar.gz/pygetdp-0.0.3/pygetdp/rendering/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+#
+
+from .prolexer import CustomLexer
+from .prostyle import ProStyle
+from .render import render
