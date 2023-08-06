dbb29f50c7082f1954578674c1428a8f4d12bb06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:13 +0000

    Add package tapioca-sectigo-api-ssl/0.3/tapioca_sectigo_api_ssl-0.3-py2-none-any.whl

---
+++ packages/tapioca-sectigo-api-ssl/tapioca_sectigo_api_ssl-0.3-py2-none-any.whl/tapioca_sectigo_api_ssl/tapioca_sectigo_api_ssl.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+from tapioca import (
+    TapiocaAdapter, generate_wrapper_from_adapter, JSONAdapterMixin)
+
+
+from .resource_mapping import RESOURCE_MAPPING
+
+
+class Sectigo_api_sslClientAdapter(JSONAdapterMixin, TapiocaAdapter):
+    api_root = 'https://secure.comodo.com'
+    resource_mapping = RESOURCE_MAPPING
+
+    def get_request_kwargs(self, api_params, *args, **kwargs):
+        params = super(Sectigo_api_sslClientAdapter, self).get_request_kwargs(
+            api_params, *args, **kwargs)
+
+        params['headers'].update({
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Accept': 'application/json'})
+
+        return params
+
+    def get_iterator_list(self, response_data):
+        return response_data
+
+    def get_iterator_next_request_kwargs(self, iterator_request_kwargs,
+                                         response_data, response):
+        pass
+
+
+Sectigo_api_ssl = generate_wrapper_from_adapter(Sectigo_api_sslClientAdapter)
