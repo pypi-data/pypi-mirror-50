2eb38f253a0b30f2c5e0fb6338e82842d96e9c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:03 +0000

    Add package django-hint/0.1.2/django_hint-0.1.2.tar.gz

---
+++ packages/django-hint/django_hint-0.1.2.tar.gz/django_hint-0.1.2/django_hint/__init__.py
@@ -0,0 +1,25 @@
+from .typehint import QueryType, RequestType, DRFTokenRequestType, List, Optional, Union, Deque, Dict, DefaultDict, FrozenSet, ChainMap, Counter, Set
+from .typehint import Generic, Callable, Tuple, TypeVar, Type, ClassVar
+__all__ = [
+    "QueryType",
+    "RequestType",
+    "DRFTokenRequestType",
+    "List",
+    "Optional",
+    "Union",
+    "Dict",
+    "DefaultDict",
+    "Set",
+    "FrozenSet",
+    "Counter",
+    "Deque",
+    "ChainMap",
+    'Generic',
+    'Callable',
+    'Tuple',
+    'TypeVar',
+    'Type',
+    'ClassVar'
+]
+
+
