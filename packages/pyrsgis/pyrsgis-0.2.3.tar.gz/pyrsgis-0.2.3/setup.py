5df57b9293b479dce2a5b336537fd691b1143deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:23 +0000

    Add package pyrsgis/0.2.3/pyrsgis-0.2.3.tar.gz

---
+++ packages/pyrsgis/pyrsgis-0.2.3.tar.gz/pyrsgis-0.2.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyrsgis",
+    version="0.2.3",
+    author="Pratyush Tripathy",
+    author_email="pratkrt@gmail.com",
+    description="Processing satellite data directly from TAR files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pratkrt/Python-for-Remote-Sensing-and-GIS",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
