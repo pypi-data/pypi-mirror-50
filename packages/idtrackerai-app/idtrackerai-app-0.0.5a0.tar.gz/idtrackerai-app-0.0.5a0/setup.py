f1a866ba6343e158e653d89c500b6bed15dcee0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:51 +0000

    Add package idtrackerai-app/0.0.5a0/idtrackerai-app-0.0.5a0.tar.gz

---
+++ packages/idtrackerai-app/idtrackerai-app-0.0.5a0.tar.gz/idtrackerai-app-0.0.5a0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import re
+
+version = ''
+with open('idtrackerai_app/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
+
+setup(
+    name='idtrackerai-app',
+    version=version,
+    description="""""",
+    author=['Ricardo Ribeiro', 'Francisco Romero Ferrero'],
+    author_email='idtrackerai@gmail.com, ricardojvr@gmail.com, paco.romero.ferrero@gmail.com',
+    url='https://idtrackerai-app.readthedocs.org',
+    packages=find_packages(),
+    install_requires=[
+        'coloredlogs',
+        'idtrackerai',
+        'pyforms-terminal'
+    ],
+    extras_require={
+        'gui':      ['pyforms-gui', 'python-video-annotator'],
+        'only-gui': ['pyforms-gui'],
+    },
+    entry_points={
+        'console_scripts': [
+            'idtrackerai=idtrackerai_app.__main__:start',
+        ],
+    }
+)
