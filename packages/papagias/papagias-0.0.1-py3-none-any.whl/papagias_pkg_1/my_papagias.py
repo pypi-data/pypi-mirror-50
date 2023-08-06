ea0c1b74eb70d622ba03d7e28a67a294db76b691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:56 +0000

    Add package papagias/0.0.1/papagias-0.0.1-py3-none-any.whl

---
+++ packages/papagias/papagias-0.0.1-py3-none-any.whl/papagias_pkg_1/my_papagias.py
@@ -0,0 +1,6 @@
+def ordering_fries():
+    print("Can I have some fried potatoes please?")
+
+
+def countries_papagias_preferes():
+    print("Albania-Bulgaria-Romania...!")
---
+++ packages/papagias/papagias-0.0.1-py3-none-any.whl/papagias_pkg_2/papagias_biostatistcs.py
@@ -0,0 +1,3 @@
+def print_biostatics_message():
+    print("My name is Papagias23 and I am a Biostatistics Engineer.")
+    print("Come with us please, and we will have some fun ;)")
