f1795f903441c9e16ebfb968c7deac8c132a08db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:32 +0000

    Add package tabout/1.0.1/tabout-1.0.1.tar.gz

---
+++ packages/tabout/tabout-1.0.1.tar.gz/tabout-1.0.1/tabout/tabout.py
@@ -0,0 +1,11 @@
+from .soundit import soundit
+from .slackit import slackit
+from pox.shutils import find
+
+def alertme(sound = True, token = False, channel = False, script = False):
+	sound_file = find('alert1.mp3')[0]
+
+	if sound:
+		soundit(sound_file)
+	if token and channel:
+		slackit(token, channel, script)
