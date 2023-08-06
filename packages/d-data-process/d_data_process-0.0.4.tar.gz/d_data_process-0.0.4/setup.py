9efe1b382214c440ef5932ebdf869b4bafaa355e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:45 +0000

    Add package d-data-process/0.0.4/d_data_process-0.0.4.tar.gz

---
+++ packages/d-data-process/d_data_process-0.0.4.tar.gz/d_data_process-0.0.4/setup.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+"""
+Created on 2018年5月28日
+
+@author: Damon
+"""
+from setuptools import setup, find_packages
+setup(name='d_data_process',
+      version='0.0.4',
+      description='This is a sample package',
+      author='Damon',
+      author_email='5178646@qq.com',
+      license='MIT',
+      packages=find_packages('data_process'))
