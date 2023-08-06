15e796fa7948466e005ba71fddee4843d4c91f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:11 +0000

    Add package pya/0.2.1/pya-0.2.1-py3-none-any.whl

---
+++ packages/pya/pya-0.2.1-py3-none-any.whl/pya/__init__.py
@@ -0,0 +1,16 @@
+"""Collection of classes and functions for processing audio signals
+ in python and jupyter notebooks, for synthesis, effects, analysis and plotting.
+"""
+from .pya import Asig, Aspec, Astft
+from .Aserver import Aserver
+from .Ugen import Ugen
+from .helpers import ampdb, dbamp, cpsmidi, midicps, linlin, clip, record, timeit, audio_from_file, buf_to_float
+from .version import __version__
+
+
+def startup(**kwargs):
+    return Aserver.startup_default_server(**kwargs)
+
+
+def shutdown(**kwargs):
+    Aserver.shutdown_default_server(**kwargs)
