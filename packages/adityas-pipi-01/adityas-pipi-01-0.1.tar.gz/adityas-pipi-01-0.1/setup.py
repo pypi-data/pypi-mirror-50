b47078c5e47222b83f26e3254a178ca1feb7ea50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:16 +0000

    Add package adityas-pipi-01/0.1/adityas-pipi-01-0.1.tar.gz

---
+++ packages/adityas-pipi-01/adityas-pipi-01-0.1.tar.gz/adityas-pipi-01-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='adityas-pipi-01',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['adityas-pipi-01'],
+      zip_safe=False)
