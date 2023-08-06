e666fa7feefa3f79c5af50ea4f9da5bc6e8089f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:31 +0000

    Add package hivfacts/2019.8/hivfacts-2019.8.tar.gz

---
+++ packages/hivfacts/hivfacts-2019.8.tar.gz/hivfacts-2019.8/hivfacts/__init__.py
@@ -0,0 +1,7 @@
+from .hivaapcnt import HIVAAPcnt
+from .hivcodonpcnt import HIVCodonPcnt
+from .hivapobec import HIVAPOBEC
+
+version = '2019.8'
+
+__all__ = ['HIVAAPcnt', 'HIVCodonPcnt', 'HIVAPOBEC']
---
+++ packages/hivfacts/hivfacts-2019.8.tar.gz/hivfacts-2019.8/setup.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from glob import glob
+from shutil import copy2
+
+import setuptools
+from setuptools.command.build_py import build_py
+
+version = '2019.8'
+
+
+class CopyHIVFactsDataCmd(build_py):
+
+    def run(self):
+        for fname in glob('../data/aapcnt/*.json'):
+            copy2(fname, 'hivfacts/data/aapcnt/')
+        for fname in glob('../data/codonpcnt/*.json'):
+            copy2(fname, 'hivfacts/data/codonpcnt/')
+        for fname in glob('../data/apobecs/*.json'):
+            copy2(fname, 'hivfacts/data/apobecs/')
+        super(CopyHIVFactsDataCmd, self).run()
+
+
+setup_params = dict(
+    name='hivfacts',
+    version=version,
+    url='https://github.com/hivdb/hivfacts/tree/master/hivfacts-python',
+    author='Philip Tzou',
+    author_email='philiptz@stanford.edu',
+    description='Amino acid prevalence data of HIV-1 pol',
+    packages=['hivfacts', 'hivfacts/data'],
+    include_package_data=True,
+    cmdclass={
+        'copy_data': CopyHIVFactsDataCmd
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'],
+    zip_safe=True)
+
+if __name__ == '__main__':
+    setuptools.setup(**setup_params)
