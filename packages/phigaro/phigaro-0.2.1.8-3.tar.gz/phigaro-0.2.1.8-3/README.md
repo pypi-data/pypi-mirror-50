670a49587c8cb3832b5f67ad9057014b607e791b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:09 +0000

    Add package phigaro/0.2.1.8-3/phigaro-0.2.1.8-3.tar.gz

---
+++ packages/phigaro/phigaro-0.2.1.8-3.tar.gz/phigaro-0.2.1.8-3/README.md
@@ -0,0 +1,108 @@
+# Phigaro v 0.2.1.8
+Phigaro is a command-line tool for predicting and annotating phages and prophages from nucleid acid sequences (including metagenome assemblies) and is based on identifying phage genes via pVOG profile HMMs and a smoothing window algorithm. https://doi.org/10.1101/598243
+
+## Requirements
+In order to run Phigaro, you need to have Prodigal and HMMER installed.
+To install Prodigal, download it at https://github.com/hyattpd/Prodigal/wiki/installation and follow the instructions.
+To install HMMER, download it at http://hmmer.org/.
+You also need `locate`. It is pre-installed in the latest Ubuntu distributions, but in case you don't have it, please run `sudo apt-get install locate`.
+
+## Installation
+
+```
+sudo -H pip3 install phigaro
+```
+then create a config file with:
+```
+phigaro-setup
+```
+It may take some time, since you are downloading the databases.
+
+## Permissions
+By default, root permissions are required for the installation. But you can disable it by adding a flag to '''phigaro-setup''':
+```
+phigaro-setup --no-updatedb
+```
+Moreover, you may want to change a path of a config installation file r reconfigurate ypur Phigaro - these can be also done by adding special flags:
+```
+phigaro-setup --help
+usage: phigaro-setup [-h] [-c CONFIG] [-p PVOG] [-f] [--no-updatedb]
+
+Phigaro setup helper
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CONFIG, --config CONFIG
+                        Path to a config.yml, default is
+                        /home/polly/.phigaro/config.yml (default:
+                        /home/polly/.phigaro/config.yml)
+  -p PVOG, --pvog PVOG  pvogs directory, default is /home/polly/.phigaro/pvog
+                        (default: /home/polly/.phigaro/pvog)
+  -f, --force           Force configuration and rewrite config.yml if exists
+                        (default: False)
+  --no-updatedb         Do not run sudo updatedb (default: False)
+```
+
+## Usage
+
+```
+phigaro -h                                                                                                                                                                          15:04:32
+usage: phigaro [-h] [-V] -f FASTA_FILE [-c CONFIG] [-p] [-e EXTENSION [EXTENSION ...]] [-o OUTPUT] [--not-open] [-t THREADS]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f FASTA_FILE, --fasta-file FASTA_FILE
+                        Assembly scaffolds\contigs or full genomes
+  -c CONFIG, --config CONFIG
+                        config file
+  -v, --verbose
+  -t THREADS, --threads THREADS
+                        num of threads (default is num of CPUs)
+  -o, --output OUTPUT 
+                        output filename for html and txt outputs. Required by default, but not required for stdout only output
+  -p, --print-vogs
+                        print phage vogs for each region
+  --no-html 
+                        do not generate output html file
+  --not-open 
+                        do not open automatically html file
+  -e, --extension
+                        type of the output: html, txt or stdout. Default is html. You can specify several file formats with a space as a separator. Example: -e txt html stdout
+  --not-open
+                        do not open html file automatically, if html output type is specified
+```
+Running time depends on the size of your input data and the number of CPUs used.
+The running time for a metagenomic assembly file of 150MB is about 20 minutes.
+
+## Output
+The output can be annotated prophage genome maps (html) or tabular format (text or stdout).
+
+## Test data
+Test data is available in `test_data` folder. 
+In order to run Phigaro on test data, enter the following command from your Phigaro folder:
+
+```
+phigaro -f test_data/Bacillus_anthracis_str_ames.fna -o test_data/Bacillus_anthracis_str_ames.phg -p --not-open
+```
+This command generates `Bacillus_anthracis_str_ames.phg` and `Bacillus_anthracis_str_ames.phg.html` files in `test_data` folder.
+If output file is not specified with `-o`, the following output is generated:
+```
+scaffold        begin   end     taxonomy
+NC_003997.3     451613  457261  Siphoviridae
+NC_003997.3     460328  482139  Siphoviridae
+NC_003997.3     3460450 3482979 Siphoviridae
+NC_003997.3     3495703 3505502 Siphoviridae
+NC_003997.3     3749518 3776811 Siphoviridae
+NC_003997.3     3779698 3784171 Siphoviridae
+```
+
+## Methods overview
+Open-reading frames (i.e. proteins) are predicted from the input FASTA file using Prodigal. Phage genes are annotated with prokaryotic viral orthologous groups (pVOGs) profile Hidden Markov Models (HMMs), which can be downloaded stand-alone from http://dmk-brain.ecn.uiowa.edu/pVOGs/. Each contig is represented as a sequence of phage and non-phage genes. A smoothing window algorithm (a triangular window function) determines regions with a high density of phage genes and therefore the prophage regions and boundaries, considering the pVOG annotations and the GC content.
+
+## Known issues
+Phigaro is tested on Linux systems. For MacOS, you may need to add the following softlink `ln -s /usr/libexec/locate.updatedb /usr/local/bin/updated` and run `brew install wget`. If you encounter any issues while running Phigaro on test data, please report them to us at estarikova@rcpcm.org.
+
+## Publication
+Elizaveta V. Starikova, Polina O. Tikhonova, Nikita A. Prianichnikov, Chris M. Rands, Evgeny M. Zdobnov, Vadim M. Govorun (2019), Phigaro: high throughput prophage sequence annotation, bioRxiv 598243; doi: https://doi.org/10.1101/598243
+
+(c) E.Starikova, P. Tikhonova, N.Pryanichnikov, 2019
---
+++ packages/phigaro/phigaro-0.2.1.8-3.tar.gz/phigaro-0.2.1.8-3/phigaro/batch/task/run_phigaro.py
@@ -0,0 +1,120 @@
+import csv
+from builtins import super
+
+from phigaro.data import read_prodigal_output, read_hmmer_output, hmmer_res_to_npn, hmmer_res_to_gc, Gene, define_taxonomy
+from phigaro.finder.v2 import V2Finder
+from phigaro import const
+from .base import AbstractTask
+from .prodigal import ProdigalTask
+from .hmmer import HmmerTask
+import os
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from phigaro.to_html.preprocess import plot_html, form_sequence, if_transposable
+from phigaro.to_html.html_formation import form_html_document
+
+class RunPhigaroTask(AbstractTask):
+    task_name = 'run_phigaro'
+
+    def __init__(self, hmmer_task, prodigal_task):
+        """
+        :type hmmer_task: HmmerTask
+        :type prodigal_task: ProdigalTask
+        """
+        super().__init__()
+        self.hmmer_task = hmmer_task
+        self.prodigal_task = prodigal_task
+
+    def _prepare(self):
+        self.finder = V2Finder(
+            window_len=self.config['phigaro']['window_len'],
+            threshold_min=self.config['phigaro']['threshold_min'],
+            threshold_max=self.config['phigaro']['threshold_max'],
+        )
+        self._print_vogs = self.config['phigaro'].get('print_vogs', False)
+        self._filename = self.config['phigaro'].get('filename', False)
+        self._no_html = self.config['phigaro'].get('no_html', False)
+        self._not_open = self.config['phigaro'].get('not_open', False)
+        self._save_fasta = self.config['phigaro'].get('save_fasta', False)
+        self._output = self.config['phigaro'].get('output', False)
+        self._uuid = self.config['phigaro'].get('uuid', False)
+
+    def output(self):
+        return self.file('{}.tsv'.format(self.sample))
+
+    def run(self):
+        max_evalue = self.config['hmmer']['e_value_threshold']
+        penalty_black = self.config['phigaro']['penalty_black']
+        penalty_white = self.config['phigaro']['penalty_white']
+
+        pvogs_black_list = const.DEFAULT_BLACK_LIST
+        pvogs_white_list = const.DEFAULT_WHITE_LIST
+
+        scaffold_set = read_prodigal_output(self.prodigal_task.output())
+        hmmer_result = read_hmmer_output(self.hmmer_task.output())
+
+        with open(self.output(), 'w') as of:
+            writer = csv.writer(of, delimiter='\t')
+
+            if self._print_vogs:
+                writer.writerow(('scaffold', 'begin', 'end', 'transposable', 'taxonomy', 'vog'))
+            else:
+                writer.writerow(('scaffold', 'begin', 'end', 'transposable', 'taxonomy'))
+
+            plotly_plots = []
+            phage_info = []
+            transposables_status = []
+            phage_num = 0
+            for scaffold in scaffold_set:
+                phage_info.append([scaffold.name, []])
+                genes = list(scaffold)  # type: list[Gene]
+                npn = hmmer_res_to_npn(scaffold, hmmer_result, max_evalue=max_evalue,
+                                       penalty_black = penalty_black, penalty_white = penalty_white,
+                                       pvogs_black_list = pvogs_black_list, pvogs_white_list = pvogs_white_list)
+                gc = hmmer_res_to_gc(scaffold, hmmer_result, max_evalue=max_evalue)
+
+                phages = self.finder.find_phages(npn, gc)
+                for phage in phages:
+                    phage_num += 1
+                    begin = genes[phage.begin].begin
+                    end = genes[phage.end].end
+                    hmmer_records = [
+                        hmmer_result.min_record(hmmer_result.get_records(scaffold.name, gene.name))
+                        for gene in genes[phage.begin: phage.end]
+                    ]
+                    hmmer_pvogs_records = (
+                        record.vog_name
+                        for record in hmmer_records
+                        if record and record.evalue <= max_evalue
+                    )
+                    pvogs_records_str = ', '.join(hmmer_pvogs_records)
+                    taxonomy = define_taxonomy(pvogs_records_str)
+                    hmmer_records = [
+                        record
+                        for record in hmmer_records
+                        if record and record.evalue <= max_evalue
+                    ]
+                    transposable = if_transposable(hmmer_records)
+                    if self._print_vogs:
+                        writer.writerow((scaffold.name, begin, end, transposable, taxonomy,  pvogs_records_str))
+                    else:
+                        writer.writerow((scaffold.name, begin, end, transposable, taxonomy))
+
+                    sequence, record = form_sequence(self._filename, '%s_prophage_%d' % (scaffold.name, phage_num), begin, end, scaffold.name)
+                    transposables_status.append(transposable)
+                    the_phage_info = [begin, end, taxonomy, sequence, pvogs_records_str]
+                    phage_info[-1][1].append(the_phage_info)
+                    if self._save_fasta:
+                        with open(self._output+'.fasta', 'a') as f:
+                            SeqIO.write(SeqRecord(record, id='%s_prophage_%d' % (scaffold.name, phage_num),
+                                                  description='%s_prophage_%d' % (scaffold.name, phage_num)), f, "fasta")
+                    if (not self._no_html) and (self._output):
+                        plotly_plots.append(plot_html(hmmer_records, begin, end))
+                phage_info = phage_info if len(phage_info[-1][1]) > 0 else phage_info[:-1]
+            if (len(phage_info) > 0) and (not self._no_html) and (self._output):
+                html = form_html_document(phage_info, transposables_status, plotly_plots, self._filename, self._uuid)
+                with open(self._output+'.html', 'w') as f:
+                    f.write(html)
+                if not self._not_open:
+                    os.system('xdg-open '+self._output+'.html'+' > /dev/null 2>/dev/null')
