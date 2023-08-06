9a626e3fcb15d52ff9d141238a40ae8e4013b23d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:11 +0000

    Add package img2sh/0.1.8/img2sh-0.1.8.tar.gz

---
+++ packages/img2sh/img2sh-0.1.8.tar.gz/img2sh-0.1.8/img2sh/cli.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from renderer import Renderer
+from pallette import XTERM_PALLETTE, BINARY_PALLETTE
+
+
+def isLink(test_string):
+    return test_string.startswith('http')
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description='Show images directly on terminal.')
+    parser.add_argument("Image", help="the directory of the image which will be opened")
+    parser.add_argument(
+        "-w", "--width",
+        help="image width on the terminal",
+        type=int
+    )
+    parser.add_argument(
+        "-i", "--interactive",
+        default=False, action='store_true',
+        help="open image in interactive mode",
+    )
+
+    args = parser.parse_args()
+
+    r = Renderer(args.Image, XTERM_PALLETTE, wsize=args.width)
+    if r.error==None:
+        r.render()
+        r.show(interactive=args.interactive)
+
+if __name__ == "__main__":
+    main()
---
+++ packages/img2sh/img2sh-0.1.8.tar.gz/img2sh-0.1.8/img2sh/renderer.py
@@ -0,0 +1,114 @@
+
+import math
+import os
+from six.moves import input
+from rect import Rect
+from colored import stylize, bg
+from PIL import Image
+import numpy as np
+
+
+def findNearestColor(color, pallette):
+    if len(color) == 3:
+        (colorr, colorg, colorb) = color
+    elif len(color) == 4:
+        (colorr, colorg, colorb, alpha) = color
+        if alpha == 0:
+            return None  # pallette.index((255,255,255))
+    c = (colorr, colorg, colorb)
+    diff = pallette - c
+    diffSum = np.sum( np.square(diff), axis=1)
+    return np.argmin(diffSum)
+
+
+def getTerminalSize():
+    rows, columns = os.popen('stty size', 'r').read().split()
+    r = int(rows)
+    c = int(columns)
+    return r, c
+
+
+class Renderer(object):
+    FONT_RATIO = 2.0
+    ERROR_IMAGE_READ = 0
+    ERROR_RENDER = 1
+
+    def __init__(self, fileName, colorPallette, wsize=None):
+        self.error = None
+        if wsize == None:
+            _, wsize = getTerminalSize()
+        try:
+            self.img = Image.open(fileName)
+            self.wsize = wsize
+            self.colorPallette = colorPallette
+            self.renderCount = 0
+            self.imgX, self.imgY = self.img.size
+            # no crop at the beginning
+            self.crop = Rect(0, 0, self.imgX, self.imgY)
+        except:
+            print("ERROR: Image read")
+            self.error = self.ERROR_IMAGE_READ
+
+    def render(self, crop=None):
+        self.renderCount += 1
+        imgR = self.img
+        if crop != None:
+            self.crop = crop
+            imgR = imgR.crop((crop.x1, crop.y1, crop.x2, crop.y2))
+        imgRX, imgRY = imgR.size
+        wpercent = (self.wsize/float(imgRX))
+        hsize = int((float(imgRY)*float(wpercent))/self.FONT_RATIO)
+        imgR = imgR.resize((self.wsize, hsize), Image.ANTIALIAS)
+        self._convertString(imgR)
+        return True
+
+    def show(self, interactive=False):
+        os.system("clear")
+        print(self.imageString)
+        print("Crop: ", self.crop)
+        if interactive:
+            self._interactive()
+
+    def _convertString(self, imgR):
+        imgX, imgY = imgR.size
+        imageString = "\n"
+        for j in range(imgY):
+            for i in range(imgX):
+                color = findNearestColor(
+                    imgR.getpixel((i, j)), self.colorPallette)
+                if color == None:
+                    imageString += " "
+                else:
+                    colorValue = bg(color)
+                    imageString += stylize(" ", colorValue)
+            imageString += "\n"
+        self.imageString = imageString
+
+    def _interactive(self):
+        cmd = input("q: quit z: zoom+ x: zoom- c: reset \narrow keys for navigation \ncmd: ")
+        if cmd == "q":
+            return
+        elif cmd == "z":
+            self.render(
+                crop=self.crop.zoomRect()
+            )            
+        elif cmd == "x":
+            self.render(
+                crop=Rect(0,0,self.imgX,self.imgY)
+            )
+        elif cmd == "c":
+            self.render(
+                crop=Rect(0,0,self.imgX,self.imgY)
+            )
+        elif cmd == '\x1b[A': # up
+            self.render(crop=self.crop.upRect())
+        elif cmd == '\x1b[B': # down
+            self.render(crop=self.crop.downRect())
+        elif cmd == '\x1b[C': # right
+            self.render(crop=self.crop.rightRect())
+        elif cmd == '\x1b[D': # left
+            self.render(crop=self.crop.leftRect())
+        else:
+            print("Unkown", cmd)
+        self.show(interactive=True)
+        #print(self.renderCount)
