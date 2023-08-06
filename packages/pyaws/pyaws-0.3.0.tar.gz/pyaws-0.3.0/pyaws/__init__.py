d55cf16d299f5b301526a19f359afc7c498666e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:05 +0000

    Add package pyaws/0.3.0/pyaws-0.3.0.tar.gz

---
+++ packages/pyaws/pyaws-0.3.0.tar.gz/pyaws-0.3.0/pyaws/__init__.py
@@ -0,0 +1,31 @@
+import inspect
+from pyaws._version import __version__ as version
+from pyaws import environment
+from pyaws.statics import local_config
+
+
+__author__ = 'Blake Huber'
+__version__ = version
+__credits__ = []
+__license__ = "GPL-3.0"
+__maintainer__ = "Blake Huber"
+__email__ = "blakeca00@gmail.com"
+__status__ = "Development"
+
+
+## the following imports require __version__  ##
+
+try:
+
+    from libtools import Colors
+    from libtools import logd
+
+    # shared, global logger object
+    logd.local_config = local_config
+    logger = logd.getLogger(__version__)
+
+    from pyaws.core import exit_codes
+
+except Exception as e:
+    fx = inspect.stack()[0][3]
+    logger.exception('{}: Uknown failure during initialization of pyaws library: {e}'.format(fx, e))
