a607af0e588598beb88974f5df9cb403fbaa199d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:31 +0000

    Add package espapp-pkg/0.4/espapp_pkg-0.4.tar.gz

---
+++ packages/espapp-pkg/espapp_pkg-0.4.tar.gz/espapp_pkg-0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='espapp_pkg',
+      version='0.4',
+      description='ESP applicaion',
+      author='Karan',
+      author_email='karan.rathore@viriminfotech.com',
+      license='Virim',
+      url = "https://github.com/emb-karan/espapp",
+      packages=setuptools.find_packages(),
+      install_requires=[
+          'uuid',
+          'python-crontab',
+          'requests',
+      ],
+      entry_points={  # Optional
+        'console_scripts': [
+            'espapp=espapp:main',
+        ],
+       },
+      )
+      
