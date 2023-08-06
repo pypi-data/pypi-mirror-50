63dd1f02e3f9118dd2b032d3a8efd465325a62a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:48 +0000

    Add package pyfujitsu/0.9.0/pyfujitsu-0.9.0.tar.gz

---
+++ packages/pyfujitsu/pyfujitsu-0.9.0.tar.gz/pyfujitsu-0.9.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyfujitsu",
+    version="0.9.0",
+    author="Mehdi Modarressi",
+    author_email="Luckposht@gmail.com",
+    maintainer="@Mmodarre",
+    description='Python library to control Fujitsu General Airconditioners on AylaNetworks IoT platform',
+    #long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mmodarre/pyfujitsu",
+    packages=setuptools.find_packages(),
+    install_requires=['requests','certifi','chardet','idna','urllib3'] ,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
