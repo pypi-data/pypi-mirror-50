b1f5bc27230191501a2dabb62560003411b6605f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:46 +0000

    Add package django-prices/2.1.0a4/django-prices-2.1.0a4.tar.gz

---
+++ packages/django-prices/django-prices-2.1.0a4.tar.gz/django-prices-2.1.0a4/setup.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+from setuptools import setup
+
+CLASSIFIERS = [
+    "Environment :: Web Environment",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Topic :: Internet :: WWW/HTTP",
+    "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    "Topic :: Software Development :: Libraries :: Application Frameworks",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+
+setup(
+    name="django-prices",
+    author="Mirumee Software",
+    author_email="hello@mirumee.com",
+    description="Django fields for the prices module",
+    license="BSD",
+    version="2.1.0a4",
+    url="https://github.com/mirumee/django-prices",
+    packages=["django_prices", "django_prices.templatetags", "django_prices.utils"],
+    include_package_data=True,
+    classifiers=CLASSIFIERS,
+    install_requires=["Babel>=2.2", "Django>=1.11,<3", "django-babel", "prices>=1.0.0"],
+    platforms=["any"],
+    zip_safe=False,
+)
