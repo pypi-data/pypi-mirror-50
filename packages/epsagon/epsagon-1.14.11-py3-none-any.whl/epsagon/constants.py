d92f65400afae96835d0924cd22bd730c0936067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:39 +0000

    Add package epsagon/1.14.11/epsagon-1.14.11-py3-none-any.whl

---
+++ packages/epsagon/epsagon-1.14.11-py3-none-any.whl/epsagon/constants.py
@@ -0,0 +1,24 @@
+"""General constants"""
+
+import os
+
+__version__ = '1.14.11'
+
+DEFAULT_REGION = 'us-east-1'
+REGION = os.getenv('AWS_REGION', DEFAULT_REGION)
+
+TRACE_COLLECTOR_URL = '{protocol}{region}.tc.epsagon.com'
+COLD_START = True
+
+# Customer original handler.
+EPSAGON_HANDLER = 'EPSAGON_HANDLER'
+
+TIMEOUT_GRACE_TIME_MS = 200
+# How long we try to send traces in seconds.
+TIMEOUT_ENV = float(os.getenv('EPSAGON_SEND_TIMEOUT_SEC', '0'))
+SEND_TIMEOUT = TIMEOUT_ENV if TIMEOUT_ENV else TIMEOUT_GRACE_TIME_MS / 1000.0
+
+MAX_LABEL_SIZE = 100 * 1024
+
+# User-defined HTTP minimum status code to be treated as an error.
+HTTP_ERR_CODE = int(os.getenv('EPSAGON_HTTP_ERR_CODE', '500'))
