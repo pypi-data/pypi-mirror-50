1a08cddac3b9a513c60bfcacb8e23c645ebdde00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:15 +0000

    Add package koreanforpygame/0.1/koreanforpygame-0.1.tar.gz

---
+++ packages/koreanforpygame/koreanforpygame-0.1.tar.gz/koreanforpygame-0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'koreanforpygame',
+    version          = '0.1',
+    description      = 'Korean text input for pygame',
+    author           = 'Seonghyun Jin',
+    author_email     = 'jinotter@gmail.com',
+    url              = 'https://github.com/jinotter3/ktifpg',
+    install_requires = ['hgtk'],
+    packages         = ['sample'],
+    keywords         = ['korean', 'pygame','input'],
+    python_requires  = '>=2',
+    zip_safe=False,
+    classifiers      = [
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
