c7a4730bd5888e88e7e76aaf1d5456c91e44abdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:46 +0000

    Add package ahvl/0.4/ahvl-0.4.tar.gz

---
+++ packages/ahvl/ahvl-0.4.tar.gz/ahvl-0.4/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'ahvl',
+  packages = ['ahvl'],
+  version = '0.4',
+  license = 'MIT',                                  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Base libraries for the Netson HashiCorp Vault Lookup Plugin for Ansible',
+  author = 'RH Sonnenberg',
+  author_email = 'r.sonnenberg@netson.nl',
+  url = 'https://github.com/netson/ahvl',           # Provide either the link to your github or to your website
+  download_url = 'https://github.com/netson/ahvl/archive/v0_4.tar.gz',    # I explain this later on
+  keywords = ['ansible', 'hashicorp', 'vault', 'lookup'],   # Keywords that define your package best
+  install_requires=['passlib','hvac'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',              # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',              # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',       # Again, pick a license
+    'Programming Language :: Python :: 3',          # Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
