9cd9a1b2e5a6d43ba63c73949aed44ada740344f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:46 +0000

    Add package DiscountEstimator/1.3.0/DiscountEstimator-1.3.0.zip

---
+++ packages/DiscountEstimator/DiscountEstimator-1.3.0.zip/DiscountEstimator-1.3.0/setup.py
@@ -0,0 +1,44 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='DiscountEstimator',  # Required
+    version='1.3.0',  # Required
+    description='A python project that estimator delay discounting rates',  # Optional
+    url='https://github.com/LoatmanP/DiscountEstimator',  # Optional
+    author='Phillip Loatman',
+    author_email='loatmanp@gmail.com',  # Optional
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+    ],
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Requireds
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+    install_requires=['numpy','pandas','scipy.optimize','scipy'],  # Optional
+
+)
