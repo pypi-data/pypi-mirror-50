296ed6fcdc644e080d6bab63e38eb06a437270ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:13 +0000

    Add package blueCup/0.0.1/blueCup-0.0.1.tar.gz

---
+++ packages/blueCup/blueCup-0.0.1.tar.gz/blueCup-0.0.1/blueCup/conf.py
@@ -0,0 +1,34 @@
+from kombu import Exchange
+from kombu import Queue
+
+# REDIS_URL = 'redis://*******/10'
+CELERY_TIMEZONE = 'Asia/Shanghai'
+# BROKER_URL = REDIS_URL
+# CELERY_RESULT_BACKEND = REDIS_URL
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_RESULT_EXTENDED = True
+CELERY_RESULT_PERSISTENT = True
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_TASK_RESULT_EXPIRES = None
+CELERY_ACCEPT_CONTENT = ['json', 'json']
+CELERY_INCLUDE = [
+    'async_task.task_inner_platform',
+    'async_task.task_notification',
+    'async_task.task_xinyan',
+]
+
+CELERY_ROUTES = {
+    'async_task.task_notification.*': {'queue': 'notification'},
+}
+
+CELERY_QUEUES = (
+    Queue(name='notification', routing_key='task_notification.#',
+          exchange=Exchange(name='Exchange_notification', type='topic')),
+)
+
+CELERY_TASK_ANNOTATIONS = {
+    'send_email': {'rate_limit': '1/s'},
+    'send_email_attach': {'rate_limit': '1/s'},
+    'send_dingding_text': {'rate_limit': '1/s'},
+    'send_dingding_markdown': {'rate_limit': '1/s'},
+}
