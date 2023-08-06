23924816192cc4fc74fb0f2dd7e3a435ffc290e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:02 +0000

    Add package hkube-python-wrapper/0.2.3/hkube_python_wrapper-0.2.3.tar.gz

---
+++ packages/hkube-python-wrapper/hkube_python_wrapper-0.2.3.tar.gz/hkube_python_wrapper-0.2.3/hkube_python_wrapper/wrapper.py
@@ -0,0 +1,149 @@
+
+import os
+import sys
+import importlib
+from .wc import WebsocketClient
+import hkube_python_wrapper.messages as messages
+import hkube_python_wrapper.methods as methods
+from events import Events
+import threading
+
+
+class Algorunner:
+    def __init__(self):
+        self._url = None
+        self._algorithm = dict()
+        self._input = None
+        self._events = Events()
+        self._loadAlgorithmError = None
+
+    def loadAlgorithm(self, options):
+        try:
+            cwd = os.getcwd()
+            package = options["path"]
+            entry = options["entryPoint"]
+            entryPoint = entry.replace("/", ".")
+            entryPoint = os.path.splitext(entryPoint)[0]
+            __import__(package)
+            os.chdir('{cwd}/{package}'.format(cwd=cwd, package=package))
+            print('loading {entry}'.format(entry=entry))
+            mod = importlib.import_module('.{entryPoint}'.format(entryPoint=entryPoint), package=package)
+            print('algorithm code loaded')
+
+            for m in dir(methods):
+                if not m.startswith("__"):
+                    method = getattr(methods, m)
+                    methodName = method["name"]
+                    try:
+                        self._algorithm[methodName] = getattr(mod, methodName)
+                        print('found method {methodName}'.format(methodName=methodName))
+                    except Exception as e:
+                        mandatory = "mandatory" if method["mandatory"] else "optional"
+                        error = 'unable to find {mandatory} method {methodName}'.format(mandatory=mandatory, methodName=methodName)
+                        if (method["mandatory"]):
+                            raise Exception(error)
+                        print(error)
+
+        except Exception as e:
+            self._loadAlgorithmError = e
+            print(e)
+
+    def connectToWorker(self, options):
+        if (options["url"] is not None):
+            self._url = options["url"]
+        else:
+            self._url = '{protocol}://{host}:{port}'.format(**options)
+
+        self._wsc = WebsocketClient()
+        self._registerToWorkerEvents()
+
+        print('connecting to {url}'.format(url=self._url))
+        t = threading.Thread(target=self._wsc.startWS, args=(self._url, ))
+        t.start()
+
+    def _registerToWorkerEvents(self):
+        self._wsc.events.on_connection += self._connection
+        self._wsc.events.on_disconnect += self._disconnect
+        self._wsc.events.on_init += self._init
+        self._wsc.events.on_start += self._start
+        self._wsc.events.on_stop += self._stop
+        self._wsc.events.on_exit += self._exit
+
+    def _connection(self):
+        print('connected to ' + self._url)
+
+    def _disconnect(self):
+        print('disconnected from '+ self._url)
+
+    def _getMethod(self, method):
+        return self._algorithm.get(method["name"])
+
+    def _init(self, options):
+        try:
+            if (self._loadAlgorithmError):
+                self._sendError(self._loadAlgorithmError)
+            else:
+                self._input = options
+                method = self._getMethod(methods.init)
+                if (method is not None):
+                    method(options)
+
+                self._sendCommand(messages.outgoing["initialized"], None)
+
+        except Exception as e:
+            self._sendError(e)
+
+    def _start(self, options):
+        try:
+            self._sendCommand(messages.outgoing["started"], None)
+            method = self._getMethod(methods.start)
+            output = method(self._input)
+            self._sendCommand(messages.outgoing["done"], output)
+
+        except Exception as e:
+            self._sendError(e)
+
+    def _stop(self, options):
+        try:
+            method = self._getMethod(methods.stop)
+            if (method is not None):
+                method(options)
+
+            self._sendCommand(messages.outgoing["stopped"], None)
+            
+        except Exception as e:
+            self._sendError(e)
+
+    def _exit(self, options):
+        try:
+            self._wsc.stopWS()
+            method = self._getMethod(methods.exit)
+            if (method is not None):
+                method(options)
+           
+            option = options if options is not None else dict()
+            code = option.get('exitCode', 0)
+            print('Got exit command. Exiting with code', code)
+            sys.exit(code)
+
+        except Exception as e:
+            self._sendError(e)
+            
+    def _sendCommand(self, command, data):
+        try:
+            self._wsc.send({"command": command, "data": data})
+        except Exception as e:
+            self._sendError(e)
+
+    def _sendError(self, error):
+        try:
+            print(error)
+            self._wsc.send({
+                "command": messages.outgoing["error"],
+                "error": {
+                    "code": "Failed",
+                    "message": str(error)
+                }
+            })
+        except Exception as e:
+            print(e)
