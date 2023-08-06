63f057701d5d510d9bf5703795f23c9083f0c74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:45 +0000

    Add package pirahansiah/0.0.9/pirahansiah-0.0.9.tar.gz

---
+++ packages/pirahansiah/pirahansiah-0.0.9.tar.gz/pirahansiah-0.0.9/README.md
@@ -0,0 +1,5 @@
+### Farshid August 2019
+### 14 august 2019
+# farshid
+Computer Vision Class
+OpenCV workshop
---
+++ packages/pirahansiah/pirahansiah-0.0.9.tar.gz/pirahansiah-0.0.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pirahansiah",
+    version="0.0.9",
+    author="Farshid PirahanSiah",
+    author_email="pirahansiah@gmail.com",
+    description="Farshid PirahanSiah workshop computer vision OpenCV",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/pirahansiah/farshid",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
