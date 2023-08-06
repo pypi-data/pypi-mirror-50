e058a42a201a81c51f042436dcda4302bed5c284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:53 +0000

    Add package invenio-oarepo-multilingual/1.1.0/invenio_oarepo_multilingual-1.1.0.tar.gz

---
+++ packages/invenio-oarepo-multilingual/invenio_oarepo_multilingual-1.1.0.tar.gz/invenio_oarepo_multilingual-1.1.0/invenio_oarepo_multilingual/marshmallow/__init__.py
@@ -0,0 +1,17 @@
+from invenio_records_rest.schemas import StrictKeysMixin
+from invenio_records_rest.schemas.fields import SanitizedUnicode
+from marshmallow.fields import List, Nested
+
+
+class MultilingualStringPartSchemaV1(StrictKeysMixin):
+    """Multilingual string"""
+
+    value = SanitizedUnicode(required=True)
+    lang = SanitizedUnicode(required=True)
+
+
+def MultilingualStringSchemaV1(**kwargs):
+    return Nested(MultilingualStringPartSchemaV1(many=True), many=True, **kwargs)
+
+
+__all__ = ('MultilingualStringSchemaV1',)
