d290bb5d1d8b24792c0b63d4aeef9506590b5d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:56 +0000

    Add package egads-lineage/1.0.1/egads_lineage-1.0.1-py3-none-any.whl

---
+++ packages/egads-lineage/egads_lineage-1.0.1-py3-none-any.whl/egads/algorithms/__init__.py
@@ -0,0 +1,16 @@
+"""
+Module contaning all EGADS algorithms, classified into separate categories.
+"""
+
+__author__ = "ohenry"
+__date__ = "2018-03-05 11:13"
+__version__ = "1.1"
+
+import egads.algorithms.comparisons
+import egads.algorithms.corrections
+import egads.algorithms.mathematics
+import egads.algorithms.microphysics
+import egads.algorithms.thermodynamics
+import egads.algorithms.transforms
+import egads.algorithms.radiation
+import egads.algorithms.user
